d45c99d4cdfa42b23bba666c0f8dae15bf1903e0
Scott Whitty <scott.whitty@mail.utoronto.ca>
2011-08-23 21:16:59 +0000

    Copyright Info Included in vqm2blif.cpp and vqm2blif.h

--- SRC/include/vqm2blif.h
+++ SRC/include/vqm2blif.h
@@ -8,6 +8,27 @@
 * Author:	S. Whitty
 *		May 20, 2011
 * 
+* Copyright (C)  2011 by 	Scott Whitty, Jason Luu, Jonathan Rose and Vaughn Betz
+*					The Edward S. Rogers Sr. Department of Electrical and Computer Engineering
+*					University of Toronto
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
 * 			BACKGROUND
 *
 * VQM is an internal file format used by Altera® inside their development tool,
