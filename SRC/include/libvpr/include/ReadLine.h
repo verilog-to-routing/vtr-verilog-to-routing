54e2ca80564c6ba7a5047fe0006438cb3346a20c
Scott Whitty <scott.whitty@mail.utoronto.ca>
2012-01-31 22:59:34 +0000

    New File Structure with Regression Test and Benchmarks

--- SRC/include/libvpr/include/ReadLine.h
+++
@@ -1,9 +0,0 @@
-#ifndef READLINE_H
-#define READLINE_H
-
-extern char **ReadLineTokens(INOUTP FILE * InFile, INOUTP int *LineNum);
-extern int CountTokens(INP char **Tokens);
-extern void FreeTokens(INOUTP char ***TokensPtr);
-
-#endif
-
