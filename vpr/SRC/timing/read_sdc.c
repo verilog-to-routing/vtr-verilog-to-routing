808031361eee8b6c719d654340559c5001ebeef7
Michael Wainberg <mikewainberg@gmail.com>
2012-06-06 23:57:38 +0000

    [Multi-clock] Finished parsing SDC file, still need to calculate constraints for create_clock commands and test for bugs
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@351 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- vpr/SRC/timing/read_sdc.c
+++ vpr/SRC/timing/read_sdc.c
@@ -17,11 +17,13 @@
 static FILE *sdc;
 t_sdc_clock * sdc_clocks = NULL; /* List of clock periods and offsets, read *
 								  * in directly from SDC file				*/
+int * exclusive_clock_groups = NULL; /* Temporary structure used to interpret   *
+									  * the command set_clock_groups -exclusive */
 
 /***************** Subroutines local to this module *************************/
 
-void get_sdc_tok(char * buffer, int num_lines);
-void check_if_number(char * ptr, int num_lines);
+boolean get_sdc_tok(char * buffer, int num_lines);
+boolean check_if_number(char * ptr);
 void calculate_constraints(void);
 
 /********************* Subroutine definitions *******************************/
@@ -54,7 +56,7 @@ void read_sdc(char * sdc_file, int num_clocks) {
 	
 	/* If an SDC file exists, parse the file line-by-line */
 	while (my_fgets(buf, BUFSIZE, sdc) != NULL) { 
-		get_sdc_tok(buf, num_lines);
+		if(get_sdc_tok(buf, num_lines)) return; /* return only if get_sdc_tok finds a special wildcard case (see below) */
 		num_lines++;
 	}
 	
@@ -62,18 +64,23 @@ void read_sdc(char * sdc_file, int num_clocks) {
 
 	/* Based on the information from sdc_clocks, calculate constraints for all paths not excluded with set_false_path or set_clock_groups -exclusive */
 	calculate_constraints();
+	
+	/* Since all the information we need is stored in timing_constraints, free other data structures used in this routine */
+	free(sdc_clocks);
+	return;
 }
 
-void get_sdc_tok(char * buf, int num_lines) {
+boolean get_sdc_tok(char * buf, int num_lines) {
 	/* Figures out which, if any, token is at the start of this line and *
-	 * takes the appropriate action.                                     */
+	 * takes the appropriate action.  Returns FALSE unless a special wildcard case is found, 
+	 * in which case the calling function immediately returns. */
 
 #define SDC_TOKENS " \t\n{}" 
-	/*We're using so little of the SDC syntax that we can ignore braces */
+	/* We're using so little of the SDC syntax that we can ignore braces */
 
 	char * ptr;
 	float temp_clock_period, temp_rising_edge, temp_falling_edge;
-	int i,j;
+	int i,j, num_exclusive_groups = 0;
 	boolean found;
 
 	/* my_strtok splits the string into tokens - little character arrays separated by the SDC_TOKENS defined above.          *
@@ -81,11 +88,11 @@ void get_sdc_tok(char * buf, int num_lines) {
 	ptr = my_strtok(buf, SDC_TOKENS, sdc, buf);
 
 	if (ptr == NULL) { /* blank line */
-		return;
+		return FALSE;
 	}
 
 	if(ptr[0] == '#') { /* line starts with a comment */
-		return; 
+		return FALSE; 
 	}
 
 	if (strcmp(ptr, "create_clock") == 0) {
@@ -102,24 +109,33 @@ void get_sdc_tok(char * buf, int num_lines) {
 		ptr = my_strtok(NULL, SDC_TOKENS, sdc, buf);
 
 		/* check if the token following -period is actually a number*/
-		check_if_number(ptr, num_lines);
+		if(!check_if_number(ptr)) {
+			fprintf(stderr, "token following '-period' is not a number on line %d of SDC file", num_lines);
+			exit(1);
+		}
 		
 		/* for now, store the clock period in a temp variable */
 		temp_clock_period = (float) strtod(ptr, NULL);
 
 		ptr = my_strtok(NULL, SDC_TOKENS, sdc, buf);
 		if(!ptr) {
-			fprintf(stderr, "missing clock nets at end of line %d", num_lines);
+			fprintf(stderr, "clock net(s) not specified at end of line %d of SDC file", num_lines);
 			exit(1);
 		}
 		
 		if(strcmp(ptr, "-waveform") == 0) {
 			/* Get the first float, which is the rising edge, and the second, which is the falling edge */
 			ptr = my_strtok(NULL, SDC_TOKENS, sdc, buf);
-			check_if_number(ptr, num_lines);
+			if(!check_if_number(ptr)) {
+				fprintf(stderr, "first token following '-waveform' is not a number on line %d of SDC file", num_lines);
+				exit(1);
+			}
 			temp_rising_edge = (float) strtod(ptr, NULL);
 			ptr = my_strtok(NULL, SDC_TOKENS, sdc, buf);
-			check_if_number(ptr, num_lines);
+			if(!check_if_number(ptr)) {
+				fprintf(stderr, "second token following '-waveform' is not a number on line %d of SDC file", num_lines);
+				exit(1);
+			}
 			temp_falling_edge = (float) strtod(ptr, NULL);
 			ptr = my_strtok(NULL, SDC_TOKENS, sdc, buf); /* We need this extra one to advance the ptr to the right spot */
 		} else {
@@ -129,20 +145,20 @@ void get_sdc_tok(char * buf, int num_lines) {
 		}
 
 		/* If the next token is a wildcard, we're done!  Set up the timing constraint matrix so that every entry *
-		 *  has T_crit equal to the specified clock period, then return     */
+		 *  has T_crit equal to the specified clock period, then return TRUE so we can exit read_sdc() as well */
 		if(strcmp(ptr, "*") == 0) {
 			for(i=0;i<num_netlist_clocks;i++) {
 				for(j=0;j<num_netlist_clocks;j++) {
 					timing_constraints[i][j] = temp_clock_period;
 				}
 			}
-			return;
+			return TRUE;
 		} else {
 			/* Parse through to the end of the line.  All that's left are the clock nets to be associated with this clock period */
 			for(;;) {
 				ptr = my_strtok(NULL, SDC_TOKENS, sdc, buf);
-				if(ptr == NULL) { /* blank line */
-					return; 
+				if(ptr == NULL) { /* end of line */
+					return FALSE; 
 				}
 
 				/* ptr contains the name of a single clock net.  Search for its index in the array clock_list */
@@ -176,38 +192,78 @@ void get_sdc_tok(char * buf, int num_lines) {
 			exit(1);
 		}
 		
-		/* now all that should be left on this line are a bunch of -group commands, followed by lists of clocks in that group */
-		/*for(;;) {
+		/* Parse through to the end of the line.  All that should be left on this line are a bunch of 
+		 * -group commands, followed by lists of clocks in that group.  An array exclusive_clock_groups will
+		 * store the group number of each clock at the index location of that clock in clock_list - i.e. if the clock
+		 * at index 5 in clock_list appears after the second -group token, then exclusive_clock_groups[5] = 2. 
+		 * At the end, we set timing_constraints to not analyse between clock domains with different group numbers.  */
+		for(;;) {
 			ptr = my_strtok(NULL, SDC_TOKENS, sdc, buf);
-			if(strcmp(ptr, "-group"
-		}*/
-	} else if (strcmp(ptr, "set_false_paths") == 0) {
+			if(ptr == NULL) { /* end of line */
+				break; /* exit the infinite for loop - but don't return yet - we still have to populate timing_constraints!  */
+			}
+			if(strcmp(ptr, "-group") == 0) {
+			/* add 1 to the group number we're assigning clocks to every time the token -group is hit */
+				num_exclusive_groups++;
+			} else { 
+				if(!exclusive_clock_groups) {
+					exclusive_clock_groups = (int *) my_calloc(num_netlist_clocks, sizeof(int));
+				}
+				/* make sure the token we have is the name of a clock, and find its index in clock_list */
+				found = FALSE;
+				for(i=0;i<num_netlist_clocks;i++) {
+					if(strcmp(ptr, clock_list[i].name) == 0) {
+						found = TRUE;
+						break; /* exits the num_netlist_clocks for loop */
+					}
+				}
+				if(found == FALSE) {
+					fprintf(stderr, "clock name %s does not correspond to a net on line %d of SDC file", ptr, num_lines);
+					exit(1);
+				}
+				exclusive_clock_groups[i] = num_exclusive_groups;
+			}
+		}
 
+		/* Finally, set every element of timing_constraints for which the two indices have different group numbers to DO_NOT_ANALYSE */
+		for(i=0;i<num_netlist_clocks;i++) {
+			for(j=0;j<num_netlist_clocks;j++) {
+				/* if either source or sink domain is part of group 0 (i.e. not part of an exclusive group), don't touch it */
+				if(exclusive_clock_groups[i] != 0 && exclusive_clock_groups[j] != 0) {
+					if(exclusive_clock_groups[i] != exclusive_clock_groups[j]) {
+						timing_constraints[i][j] = DO_NOT_ANALYSE;
+					}
+				}
+			}
+		}
+		free(exclusive_clock_groups);
+		return FALSE;
+
+	} else if (strcmp(ptr, "set_false_paths") == 0) {
+		return FALSE;
 	} else {
 		fprintf(stderr, "Incorrect or unsupported syntax near start of line %d of SDC file", num_lines);
 		exit(1);
 	}
 }
 
-void check_if_number(char * ptr, int num_lines) {
-/* Checks if the character array ptr represents a number.					    *
- * To return without throwing an error, all characters must be digits, although *
- * there can also be no more than one decimal point.                            */
+boolean check_if_number(char * ptr) {
+/* Checks if the character array ptr represents a number.  *
+ * To return TRUE, all characters must be digits, although *
+ * there can also be no more than one decimal point.       */
 	int i, num_decimal_points = 0, len=strlen(ptr);
 	for (i=0;i<len;i++) {
 		if((ptr[i]<'0'||ptr[i]>'9')) {
 			if(ptr[i] != '.') {
-				fprintf(stderr, "'-period' on line %d of SDC file is not followed by a number", num_lines);
-				exit(1);
+				return FALSE;
 			}
 			num_decimal_points++;
 			if(num_decimal_points > 1) {
-				fprintf(stderr, "'-period' on line %d of SDC file is not followed by a number", num_lines);
-				exit(1);
+				return FALSE;
 			}
 		}
 	}
-	return;
+	return TRUE;
 }
 
 void calculate_constraints(void) {
