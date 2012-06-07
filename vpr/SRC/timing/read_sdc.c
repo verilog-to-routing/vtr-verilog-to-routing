8afcc0dd3131e60a5c6156b7c230cda35f03bd74
Michael Wainberg <mikewainberg@gmail.com>
2012-06-07 02:50:04 +0000

    [Multi-clock] Actually finished parsing SDC file, still need to calculate constraints for create_clock commands and test for bugs
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@352 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- vpr/SRC/timing/read_sdc.c
+++ vpr/SRC/timing/read_sdc.c
@@ -22,9 +22,10 @@ int * exclusive_clock_groups = NULL; /* Temporary structure used to interpret   
 
 /***************** Subroutines local to this module *************************/
 
-boolean get_sdc_tok(char * buffer, int num_lines);
+boolean get_sdc_tok(char * buf, int num_lines);
 boolean check_if_number(char * ptr);
-void calculate_constraints(void);
+int find_clock(char * ptr, int num_lines);
+float calculate_constraint(t_sdc_clock source_domain, t_sdc_clock sink_domain);
 
 /********************* Subroutine definitions *******************************/
 
@@ -35,7 +36,8 @@ void read_sdc(char * sdc_file, int num_clocks) {
 * timing_constraints pointing to NULL.                  */
 
 	char buf[BUFSIZE];
-	int num_lines = 1, i, j; /* Line counter for SDC file, used to report errors */
+	int num_lines = 1; /* Line counter for SDC file, used to report errors */
+	int i, j;
 		
 	/* Allocate matrix of timing constraints [0..num_clocks-1][0..num_clocks-1] */
 	timing_constraints = (float **) alloc_matrix(0, num_clocks-1, 0, num_clocks-1, sizeof(float));
@@ -62,9 +64,14 @@ void read_sdc(char * sdc_file, int num_clocks) {
 	
 	fclose(sdc);
 
-	/* Based on the information from sdc_clocks, calculate constraints for all paths not excluded with set_false_path or set_clock_groups -exclusive */
-	calculate_constraints();
-	
+	/* Based on the information from sdc_clocks, calculate constraints for all paths not excluded by set_false_path or set_clock_groups -exclusive */
+		for(i=0;i<num_netlist_clocks;i++) {
+			for(j=0;j<num_netlist_clocks;j++) {
+				if(timing_constraints[i][j] != DO_NOT_ANALYSE)
+					timing_constraints[i][j] = calculate_constraint(sdc_clocks[i], sdc_clocks[j]);
+			}
+		}
+
 	/* Since all the information we need is stored in timing_constraints, free other data structures used in this routine */
 	free(sdc_clocks);
 	return;
@@ -80,8 +87,7 @@ boolean get_sdc_tok(char * buf, int num_lines) {
 
 	char * ptr;
 	float temp_clock_period, temp_rising_edge, temp_falling_edge;
-	int i,j, num_exclusive_groups = 0;
-	boolean found;
+	int i,j, from, to, num_exclusive_groups = 0;
 
 	/* my_strtok splits the string into tokens - little character arrays separated by the SDC_TOKENS defined above.          *
 	 * Throughout this code, ptr refers to the tokens we fetch, one at a time.  The token changes at each call of my_strtok. */
@@ -141,7 +147,7 @@ boolean get_sdc_tok(char * buf, int num_lines) {
 		} else {
 			/* The clock's rising edge is by default at 0, and the falling edge is at the half-period */
 			temp_rising_edge = 0.;
-			temp_falling_edge = temp_clock_period / 2;
+			temp_falling_edge = temp_clock_period / 2.0;
 		}
 
 		/* If the next token is a wildcard, we're done!  Set up the timing constraint matrix so that every entry *
@@ -154,34 +160,28 @@ boolean get_sdc_tok(char * buf, int num_lines) {
 			}
 			return TRUE;
 		} else {
-			/* Parse through to the end of the line.  All that's left are the clock nets to be associated with this clock period */
+			/* Parse through to the end of the line.  All that should be left on this line are a bunch of 
+			 * clock nets to be associated with this clock period.  An array sdc_clocks will
+			 * store the period and offset of each clock at the same index which that clock has in clock_list.
+			 * After everything has been parsed, we take the information from this array to calculate the actual timing constraints
+			 * which these periods and offsets imply, and put them in the matrix timing_constraints. */
+
 			for(;;) {
 				ptr = my_strtok(NULL, SDC_TOKENS, sdc, buf);
 				if(ptr == NULL) { /* end of line */
 					return FALSE; 
 				}
 
-				/* ptr contains the name of a single clock net.  Search for its index in the array clock_list */
-				found = FALSE;
-				for(i=0;i<num_netlist_clocks;i++) {
-					if(strcmp(ptr, clock_list[i].name) == 0) {
-						found = TRUE;
-						break; /* exits the num_netlist_clocks for loop */
-					}
-				}
-				if(found == FALSE) {
-					fprintf(stderr, "clock name %s does not correspond to a net on line %d of SDC file", ptr, num_lines);
-					exit(1);
-				}
-				
 				/* Store the clock's period and offset in the local array sdc_clocks.  If it doesn't exist yet, malloc it. */
 				if(!sdc_clocks) {
 					sdc_clocks = (t_sdc_clock *) my_malloc(num_netlist_clocks * sizeof(t_sdc_clock));
 				}
+
+				i = find_clock(ptr, num_lines);
 				
-				/* The value of i is currently the index of the clock in num_netlist_clocks, which is how we'll be thinking of the clock from now on. */ 
+				/* The value of i is currently the index of the clock in clock_list */ 
 				sdc_clocks[i].period = temp_clock_period;
-				sdc_clocks[i].offset = temp_rising_edge; /* Note: temp_falling_edge is irrelevant unless you care about duty cycles */
+				sdc_clocks[i].offset = temp_rising_edge; /* Note: temp_falling_edge is irrelevant unless you care about duty cycles - could be added in later */
 			}	
 		}
 	} else if (strcmp(ptr, "set_clock_groups") == 0) {
@@ -196,7 +196,7 @@ boolean get_sdc_tok(char * buf, int num_lines) {
 		 * -group commands, followed by lists of clocks in that group.  An array exclusive_clock_groups will
 		 * store the group number of each clock at the index location of that clock in clock_list - i.e. if the clock
 		 * at index 5 in clock_list appears after the second -group token, then exclusive_clock_groups[5] = 2. 
-		 * At the end, we set timing_constraints to not analyse between clock domains with different group numbers.  */
+		 * Finally, we set timing_constraints to not analyse between clock domains with different group numbers.  */
 		for(;;) {
 			ptr = my_strtok(NULL, SDC_TOKENS, sdc, buf);
 			if(ptr == NULL) { /* end of line */
@@ -206,22 +206,12 @@ boolean get_sdc_tok(char * buf, int num_lines) {
 			/* add 1 to the group number we're assigning clocks to every time the token -group is hit */
 				num_exclusive_groups++;
 			} else { 
+				/* Presumptively, we have a clock. Calloc the array exclusive_clock_groups if not done already, then *
+				 * find the index of the clock named by ptr, and set exclusive_clock_groups[that clock's index] equal to the current group number */
 				if(!exclusive_clock_groups) {
 					exclusive_clock_groups = (int *) my_calloc(num_netlist_clocks, sizeof(int));
 				}
-				/* make sure the token we have is the name of a clock, and find its index in clock_list */
-				found = FALSE;
-				for(i=0;i<num_netlist_clocks;i++) {
-					if(strcmp(ptr, clock_list[i].name) == 0) {
-						found = TRUE;
-						break; /* exits the num_netlist_clocks for loop */
-					}
-				}
-				if(found == FALSE) {
-					fprintf(stderr, "clock name %s does not correspond to a net on line %d of SDC file", ptr, num_lines);
-					exit(1);
-				}
-				exclusive_clock_groups[i] = num_exclusive_groups;
+				exclusive_clock_groups[find_clock(ptr, num_lines)] = num_exclusive_groups;
 			}
 		}
 
@@ -240,7 +230,29 @@ boolean get_sdc_tok(char * buf, int num_lines) {
 		return FALSE;
 
 	} else if (strcmp(ptr, "set_false_paths") == 0) {
+
+		ptr = my_strtok(NULL, SDC_TOKENS, sdc, buf);
+		if(strcmp(ptr, "-from") != 0) {
+			fprintf(stderr, "set_false_path must be followed by '-from <clock_name>' on line %d of SDC file", num_lines);
+			exit(1);
+		}
+
+		ptr = my_strtok(NULL, SDC_TOKENS, sdc, buf);
+		from = find_clock(ptr, num_lines);
+
+		ptr = my_strtok(NULL, SDC_TOKENS, sdc, buf);
+		if(strcmp(ptr, "-to") != 0) {
+			fprintf(stderr, "set_false_path must be followed by '-from <clock_name> -to <clock_name> on line %d of SDC file", num_lines);
+			exit(1);
+		}
+		ptr = my_strtok(NULL, SDC_TOKENS, sdc, buf);
+		to = find_clock(ptr, num_lines);
+
+		/* Set the path from 'from' to 'to' in timing_constraints to not be analysed */
+		timing_constraints[from][to] = DO_NOT_ANALYSE;
+		
 		return FALSE;
+
 	} else {
 		fprintf(stderr, "Incorrect or unsupported syntax near start of line %d of SDC file", num_lines);
 		exit(1);
@@ -248,7 +260,7 @@ boolean get_sdc_tok(char * buf, int num_lines) {
 }
 
 boolean check_if_number(char * ptr) {
-/* Checks if the character array ptr represents a number.  *
+/* Checks if the character array ptr represents a valid floating-point number.  *
  * To return TRUE, all characters must be digits, although *
  * there can also be no more than one decimal point.       */
 	int i, num_decimal_points = 0, len=strlen(ptr);
@@ -266,6 +278,21 @@ boolean check_if_number(char * ptr) {
 	return TRUE;
 }
 
-void calculate_constraints(void) {
-	return;
+int find_clock(char * ptr, int num_lines) {
+/* Given a token ptr, find whether it's the name of a clock.  If it's not, print an error and exit; *
+ * if it is, return the clock's index in the array clock_list. */
+	int index;
+	for(index=0;index<num_netlist_clocks;index++) {
+		if(strcmp(ptr, clock_list[index].name) == 0) {
+			return index;
+		}
+	}
+	fprintf(stderr, "clock name %s does not correspond to a net on line %d of SDC file", ptr, num_lines);
+	exit(1);
 }
+
+float calculate_constraint(t_sdc_clock source_domain, t_sdc_clock sink_domain) {
+	/* blank for now */
+	return 0.;
+}
+
