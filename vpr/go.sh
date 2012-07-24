1deab717b6b583ce45288306cb5b3c6690df4ff5
Suya Liu <suya1122@gmail.com>
2012-07-24 03:17:24 +0000

    Didn't mean to commit this... reverting to r630
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@632 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- vpr/go.sh
+++ vpr/go.sh
@@ -2,6 +2,5 @@
 # Example of how to run AAPack and vpr
 
 # Run AAPack then placement and routing to pack a heterogeneous FPGA
-#./vpr sample_arch.xml or1200.latren
+./vpr sample_arch.xml or1200.latren
 
-./vpr ../vtr_flow/arch/timing/k6_N10_memDepth16384_memData64_40nm_timing_molecule.xml ../../vqm_to_blif/REG_TEST/BENCHMARKS/BLIF/Stratix_IV_default/RS_dec_stratixiv.blif
