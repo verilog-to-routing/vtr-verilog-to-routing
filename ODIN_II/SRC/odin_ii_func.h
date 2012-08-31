2342cbede2cb2a309be9c0862f6d6b0bc96eb635
Kons Na <Kons.Na@gmail.com>
2012-08-31 17:13:15 +0000

    Changed Odin structure to create the ability to use Odin II as a library for the visualization.
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@854 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

---
+++ ODIN_II/SRC/odin_ii_func.h
@@ -0,0 +1,31 @@
+/*
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/ 
+
+#ifndef ODIN_II_FUNC_H
+#define ODIN_II_FUNC_H
+void set_default_options();
+void do_high_level_synthesis();
+void do_simulation_of_netlist();
+void do_activation_estimation(int num_types, t_type_descriptor * type_descriptors);
+
+#endif //ODIN_II_FUNC
