a9fe7360ccf4892b64cd7a03cad81d3564513b7a
Jeff Rudolph <jrudolph.at.work@gmail.com>
2012-10-25 14:29:09 +0000

    WIP - 10/25/12
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@1249 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- toro/TIO_InputOutputHandlers/TIO_SkinHandler.h
+++ toro/TIO_InputOutputHandlers/TIO_SkinHandler.h
@@ -13,11 +13,27 @@
 //
 //===========================================================================//
 
-#include <string>
-using namespace std;
+//---------------------------------------------------------------------------//
+// Copyright (C) 2012 Jeff Rudolph, Texas Instruments (jrudolph@ti.com)      //
+//                                                                           //
+// This program is free software; you can redistribute it and/or modify it   //
+// under the terms of the GNU General Public License as published by the     //
+// Free Software Foundation; version 3 of the License, or any later version. //
+//                                                                           //
+// This program is distributed in the hope that it will be useful, but       //
+// WITHOUT ANY WARRANTY; without even an implied warranty of MERCHANTABILITY //
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License   //
+// for more details.                                                         //
+//                                                                           //
+// You should have received a copy of the GNU General Public License along   //
+// with this program; if not, see <http://www.gnu.org/licenses>.             //
+//---------------------------------------------------------------------------//
 
 #ifndef TIO_SKIN_HANDLER_H
 #define TIO_SKIN_HANDLER_H
+
+#include <string>
+using namespace std;
 
 //===========================================================================//
 // Purpose        : Class declaration
