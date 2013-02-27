d3ada6ad176c1c286b56ebdd74688fa305101a76
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-02-27 16:36:41 +0000

    Version 1.227.0
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@1666 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- toro/TOS_OptionsStore/TOS_StringUtils.h
+++ toro/TOS_OptionsStore/TOS_StringUtils.h
@@ -4,7 +4,7 @@
 //===========================================================================//
 
 //---------------------------------------------------------------------------//
-// Copyright (C) 2012 Jeff Rudolph, Texas Instruments (jrudolph@ti.com)      //
+// Copyright (C) 2012-2013 Jeff Rudolph, Texas Instruments (jrudolph@ti.com) //
 //                                                                           //
 // This program is free software; you can redistribute it and/or modify it   //
 // under the terms of the GNU General Public License as published by the     //
@@ -47,8 +47,7 @@ void TOS_ExtractStringRouteAlgorithmMode( TOS_RouteAlgorithmMode_t mode, string*
 void TOS_ExtractStringRouteAbstractMode( TOS_RouteAbstractMode_t mode, string* psrMode );
 void TOS_ExtractStringRouteResourceMode( TOS_RouteResourceMode_t mode, string* psrMode );
 void TOS_ExtractStringRouteCostMode( TOS_RouteCostMode_t mode, string* psrMode );
-
-void TOS_ExtractStringInputDataMode( TOS_InputDataMode_t mode, string* psrMode );
+void TOS_ExtractStringRouteOrderMode( TOS_RouteOrderMode_t mode, string* psrMode );
 
 void TOS_ExtractStringOutputLaffMask( int mask, string* psrMask );
 
