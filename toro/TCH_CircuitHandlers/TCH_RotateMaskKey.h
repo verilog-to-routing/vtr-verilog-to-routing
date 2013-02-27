ea5d07e867f03951505244b72e10e20f7f472ae2
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-02-27 17:19:34 +0000

    Version 1.227.0
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@1706 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

---
+++ toro/TCH_CircuitHandlers/TCH_RotateMaskKey.h
@@ -0,0 +1,119 @@
+//===========================================================================//
+// Purpose : Declaration and inline definitions for the TCH_RotateMaskKey 
+//           class.
+//
+//           Inline methods include:
+//           - SetOriginPoint, SetNodeIndex
+//           - GetOriginPoint, GetNodeIndex
+//           - IsValid
+//
+//===========================================================================//
+
+//---------------------------------------------------------------------------//
+// Copyright (C) 2013 Jeff Rudolph, Texas Instruments (jrudolph@ti.com)      //
+//                                                                           //
+// This program is free software; you can redistribute it and/or modify it   //
+// under the terms of the GNU General Public License as published by the     //
+// Free Software Foundation; version 3 of the License, or any later version. //
+//                                                                           //
+// This program is distributed in the hope that it will be useful, but       //
+// WITHOUT ANY WARRANTY; without even an implied warranty of MERCHANTABILITY //
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License   //
+// for more details.                                                         //
+//                                                                           //
+// You should have received a copy of the GNU General Public License along   //
+// with this program; if not, see <http://www.gnu.org/licenses>.             //
+//---------------------------------------------------------------------------//
+
+#ifndef TCH_ROTATE_MASK_KEY_H
+#define TCH_ROTATE_MASK_KEY_H
+
+#include <cstdio>
+using namespace std;
+
+#include "TGO_Point.h"
+
+//===========================================================================//
+// Purpose        : Class declaration
+// Author         : Jeff Rudolph
+//---------------------------------------------------------------------------//
+// Version history
+// 01/15/13 jeffr : Original
+//===========================================================================//
+class TCH_RotateMaskKey_c
+{
+public:
+
+   TCH_RotateMaskKey_c( void );
+   TCH_RotateMaskKey_c( const TGO_Point_c& originPoint, 
+                        size_t nodeIndex ); 
+   TCH_RotateMaskKey_c( int x, int y, int z,
+                        size_t nodeIndex ); 
+   TCH_RotateMaskKey_c( int x, int y,
+                        size_t nodeIndex ); 
+   TCH_RotateMaskKey_c( const TCH_RotateMaskKey_c& rotateMaskKey );
+   ~TCH_RotateMaskKey_c( void );
+
+   TCH_RotateMaskKey_c& operator=( const TCH_RotateMaskKey_c& rotateMaskKey );
+   bool operator<( const TCH_RotateMaskKey_c& rotateMaskKey ) const;
+   bool operator==( const TCH_RotateMaskKey_c& rotateMaskKey ) const;
+   bool operator!=( const TCH_RotateMaskKey_c& rotateMaskKey ) const;
+
+   void Print( FILE* pfile = stdout, size_t spaceLen = 0 ) const;
+
+   void SetOriginPoint( const TGO_Point_c& originPoint );
+   void SetNodeIndex( size_t nodeIndex );
+
+   const TGO_Point_c& GetOriginPoint( void ) const;
+   size_t GetNodeIndex( void ) const;
+
+   bool IsValid( void ) const;
+
+private:
+
+   TGO_Point_c originPoint_;
+   size_t      nodeIndex_;
+};
+
+//===========================================================================//
+// Purpose        : Class inline definition(s)
+// Author         : Jeff Rudolph
+//---------------------------------------------------------------------------//
+// Version history
+// 01/15/13 jeffr : Original
+//===========================================================================//
+inline void TCH_RotateMaskKey_c::SetOriginPoint( 
+      const TGO_Point_c& originPoint )
+{
+   this->originPoint_ = originPoint;
+} 
+
+//===========================================================================//
+inline void TCH_RotateMaskKey_c::SetNodeIndex( 
+      size_t nodeIndex )
+{
+   this->nodeIndex_ = nodeIndex;
+} 
+
+//===========================================================================//
+inline const TGO_Point_c& TCH_RotateMaskKey_c::GetOriginPoint( 
+      void ) const
+{
+   return( this->originPoint_ );
+}
+
+//===========================================================================//
+inline size_t TCH_RotateMaskKey_c::GetNodeIndex( 
+      void ) const
+{
+   return( this->nodeIndex_ );
+}
+
+//===========================================================================//
+inline bool TCH_RotateMaskKey_c::IsValid( 
+      void ) const
+{
+   return( this->originPoint_.IsValid( ) && this->nodeIndex_ != SIZE_MAX ? true : false );
+}
+
+#endif
