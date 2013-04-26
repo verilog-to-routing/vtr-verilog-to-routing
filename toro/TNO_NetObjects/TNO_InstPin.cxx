8d43980cdeebb8ff3961c338e484b78324a67cee
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-04-26 15:16:54 +0000

    Version 4.426
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@2016 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- toro/TNO_NetObjects/TNO_InstPin.cxx
+++ toro/TNO_NetObjects/TNO_InstPin.cxx
@@ -13,7 +13,7 @@
 //===========================================================================//
 
 //---------------------------------------------------------------------------//
-// Copyright (C) 2012 Jeff Rudolph, Texas Instruments (jrudolph@ti.com)      //
+// Copyright (C) 2012-2013 Jeff Rudolph, Texas Instruments (jrudolph@ti.com) //
 //                                                                           //
 // This program is free software; you can redistribute it and/or modify it   //
 // under the terms of the GNU General Public License as published by the     //
@@ -32,6 +32,8 @@
 
 #include "TIO_PrintHandler.h"
 
+#include "TNO_Typedefs.h"
+#include "TNO_StringUtils.h"
 #include "TNO_InstPin.h"
 
 //===========================================================================//
@@ -44,8 +46,42 @@
 TNO_InstPin_c::TNO_InstPin_c( 
       void )
       :
+      portIndex_( TNO_PORT_INDEX_INVALID ),
+      pinIndex_( TNO_PIN_INDEX_INVALID ),
       type_( TC_TYPE_UNDEFINED )
 {
+} 
+
+//===========================================================================//
+TNO_InstPin_c::TNO_InstPin_c( 
+      const string&       srInstName,
+      const string&       srPortName,
+            size_t        portIndex,
+      const string&       srPinName,
+            size_t        pinIndex,
+            TC_TypeMode_t type )
+      :
+      portIndex_( TNO_PORT_INDEX_INVALID ),
+      pinIndex_( TNO_PIN_INDEX_INVALID ),
+      type_( TC_TYPE_UNDEFINED )
+{
+   this->Set( srInstName, srPortName, portIndex, srPinName, pinIndex, type );
+} 
+
+//===========================================================================//
+TNO_InstPin_c::TNO_InstPin_c( 
+      const char*         pszInstName,
+      const char*         pszPortName,
+            size_t        portIndex,
+      const char*         pszPinName,
+            size_t        pinIndex,
+            TC_TypeMode_t type )
+      :
+      portIndex_( TNO_PORT_INDEX_INVALID ),
+      pinIndex_( TNO_PIN_INDEX_INVALID ),
+      type_( TC_TYPE_UNDEFINED )
+{
+   this->Set( pszInstName, pszPortName, portIndex, pszPinName, pinIndex, type );
 } 
 
 //===========================================================================//
@@ -55,6 +91,8 @@ TNO_InstPin_c::TNO_InstPin_c(
       const string&       srPinName,
             TC_TypeMode_t type )
       :
+      portIndex_( TNO_PORT_INDEX_INVALID ),
+      pinIndex_( TNO_PIN_INDEX_INVALID ),
       type_( TC_TYPE_UNDEFINED )
 {
    this->Set( srInstName, srPortName, srPinName, type );
@@ -67,51 +105,11 @@ TNO_InstPin_c::TNO_InstPin_c(
       const char*         pszPinName,
             TC_TypeMode_t type )
       :
+      portIndex_( TNO_PORT_INDEX_INVALID ),
+      pinIndex_( TNO_PIN_INDEX_INVALID ),
       type_( TC_TYPE_UNDEFINED )
 {
    this->Set( pszInstName, pszPortName, pszPinName, type );
-} 
-
-//===========================================================================//
-TNO_InstPin_c::TNO_InstPin_c( 
-      const string&       srInstName,
-      const string&       srPortName,
-            unsigned int  portIndex,
-      const string&       srPinName,
-            unsigned int  pinIndex,
-            TC_TypeMode_t type )
-      :
-      type_( TC_TYPE_UNDEFINED )
-{
-   this->Set( srInstName, srPortName.data( ), srPinName.data( ), type );
-} 
-
-//===========================================================================//
-TNO_InstPin_c::TNO_InstPin_c( 
-      const char*         pszInstName,
-      const char*         pszPortName,
-            unsigned int  portIndex,
-      const char*         pszPinName,
-            unsigned int  pinIndex,
-            TC_TypeMode_t type )
-      :
-      type_( TC_TYPE_UNDEFINED )
-{
-   char szIndex[TIO_FORMAT_STRING_LEN_VALUE];
-
-   string srPortName( pszPortName );
-   sprintf( szIndex, "%u", portIndex );
-   srPortName += "[";
-   srPortName += szIndex;
-   srPortName += "]";
-
-   string srPinName( pszPinName );
-   sprintf( szIndex, "%u", pinIndex );
-   srPinName += "[";
-   srPinName += szIndex;
-   srPinName += "]";
-
-   this->Set( pszInstName, srPortName, srPinName, type );
 } 
 
 //===========================================================================//
@@ -144,6 +142,8 @@ TNO_InstPin_c::TNO_InstPin_c(
       srInstName_( instPin.srInstName_ ),
       srPortName_( instPin.srPortName_ ),
       srPinName_( instPin.srPinName_ ),
+      portIndex_( instPin.portIndex_ ),
+      pinIndex_( instPin.pinIndex_ ),
       type_( instPin.type_ )
 {
 } 
@@ -176,6 +176,8 @@ TNO_InstPin_c& TNO_InstPin_c::operator=(
       this->srInstName_ = instPin.srInstName_;
       this->srPortName_ = instPin.srPortName_;
       this->srPinName_ = instPin.srPinName_;
+      this->portIndex_ = instPin.portIndex_;
+      this->pinIndex_ = instPin.pinIndex_;
       this->type_ = instPin.type_;
    }
    return( *this );
@@ -209,6 +211,8 @@ bool TNO_InstPin_c::operator==(
           ( this->srInstName_ == instPin.srInstName_ ) &&
           ( this->srPortName_ == instPin.srPortName_ ) &&
           ( this->srPinName_ == instPin.srPinName_ ) &&
+          ( this->portIndex_ == instPin.portIndex_ ) &&
+          ( this->pinIndex_ == instPin.pinIndex_ ) &&
           ( this->type_ == instPin.type_ ) ?
           true : false );
 }
@@ -268,16 +272,24 @@ void TNO_InstPin_c::ExtractString(
          }
          if( this->srPortName_.length( ))
          {
+            string srPortNameIndex;
+            TNO_FormatNameIndex( this->srPortName_, this->portIndex_, 
+                                 &srPortNameIndex );
+
             *psrInstPin += psrInstPin->length( ) ? " " : "";
             *psrInstPin += "port=\"";
-            *psrInstPin += this->srPortName_;
+            *psrInstPin += srPortNameIndex;
             *psrInstPin += "\"";
          }
          if( this->srPinName_.length( ))
          {
+            string srPinNameIndex;
+            TNO_FormatNameIndex( this->srPinName_, this->pinIndex_, 
+                                 &srPinNameIndex );
+
             *psrInstPin += psrInstPin->length( ) ? " " : "";
             *psrInstPin += "pin=\"";
-            *psrInstPin += this->srPinName_;
+            *psrInstPin += srPinNameIndex;
             *psrInstPin += "\"";
          }
 
@@ -310,21 +322,66 @@ void TNO_InstPin_c::ExtractString(
 void TNO_InstPin_c::Set( 
       const string&       srInstName,
       const string&       srPortName,
+            size_t        portIndex,
       const string&       srPinName,
+            size_t        pinIndex,
             TC_TypeMode_t type )
 {
-   this->srInstName_ = srInstName;
-   this->srPortName_ = srPortName;
-   this->srPinName_ = srPinName;
+   const char* pszInstName = ( srInstName.length( ) ? srInstName.data( ) : 0 );
+   const char* pszPortName = ( srPortName.length( ) ? srPortName.data( ) : 0 );
+   const char* pszPinName = ( srPinName.length( ) ? srPinName.data( ) : 0 );
+
+   this->Set( pszInstName, pszPortName, portIndex, pszPinName, pinIndex, type );
+} 
+
+//===========================================================================//
+void TNO_InstPin_c::Set( 
+      const char*         pszInstName,
+      const char*         pszPortName,
+            size_t        portIndex,
+      const char*         pszPinName,
+            size_t        pinIndex,
+            TC_TypeMode_t type )
+{
+   this->srInstName_ = TIO_PSZ_STR( pszInstName );
+   this->srPortName_ = TIO_PSZ_STR( pszPortName );
+   this->srPinName_ = TIO_PSZ_STR( pszPinName );
+   this->portIndex_ = portIndex;
+   this->pinIndex_ = pinIndex;
+   this->type_ = type;
 
    this->srName_ = "";
    this->srName_ += this->srInstName_;
-   this->srName_ += "|";
-   this->srName_ += this->srPortName_;
-   this->srName_ += "|";
-   this->srName_ += this->srPinName_;
+   if( pszPortName ) 
+   {
+      string srPortNameIndex;
+      TNO_FormatNameIndex( this->srPortName_, this->portIndex_, 
+                           &srPortNameIndex );
+      this->srName_ += "|";
+      this->srName_ += srPortNameIndex;
+   }
+   if( pszPinName ) 
+   {
+      string srPinNameIndex;
+      TNO_FormatNameIndex( this->srPinName_, this->pinIndex_, 
+                           &srPinNameIndex );
+      this->srName_ += "|";
+      this->srName_ += srPinNameIndex;
+   }
+} 
 
-   this->type_ = type;
+//===========================================================================//
+void TNO_InstPin_c::Set( 
+      const string&       srInstName,
+      const string&       srPortName,
+      const string&       srPinName,
+            TC_TypeMode_t type )
+{
+   const char* pszInstName = ( srInstName.length( ) ? srInstName.data( ) : 0 );
+   const char* pszPortName = ( srPortName.length( ) ? srPortName.data( ) : 0 );
+   const char* pszPinName = ( srPinName.length( ) ? srPinName.data( ) : 0 );
+
+   this->Set( pszInstName, pszPortName, pszPinName, type );
 } 
 
 //===========================================================================//
@@ -334,18 +391,18 @@ void TNO_InstPin_c::Set(
       const char*         pszPinName,
             TC_TypeMode_t type )
 {
-   this->srInstName_ = TIO_PSZ_STR( pszInstName );
-   this->srPortName_ = TIO_PSZ_STR( pszPortName );
-   this->srPinName_ = TIO_PSZ_STR( pszPinName );
+   string srPortName( "" );
+   string srPinName( "" );
+   size_t portIndex = TNO_PORT_INDEX_INVALID;
+   size_t pinIndex = TNO_PIN_INDEX_INVALID;
 
-   this->srName_ = "";
-   this->srName_ += this->srInstName_;
-   this->srName_ += "|";
-   this->srName_ += this->srPortName_;
-   this->srName_ += "|";
-   this->srName_ += this->srPinName_;
+   TNO_ParseNameIndex( pszPortName, &srPortName, &portIndex );
+   TNO_ParseNameIndex( pszPinName, &srPinName, &pinIndex );
 
-   this->type_ = type;
+   pszPortName = ( srPortName.length( ) ? srPortName.data( ) : 0 );
+   pszPinName = ( srPinName.length( ) ? srPinName.data( ) : 0 );
+
+   this->Set( pszInstName, pszPortName, portIndex, pszPinName, pinIndex, type );
 } 
 
 //===========================================================================//
@@ -354,16 +411,10 @@ void TNO_InstPin_c::Set(
       const string&       srPinName,
             TC_TypeMode_t type )
 {
-   this->srInstName_ = srInstName;
-   this->srPortName_ = "";
-   this->srPinName_ = srPinName;
+   const char* pszInstName = ( srInstName.length( ) ? srInstName.data( ) : 0 );
+   const char* pszPinName = ( srPinName.length( ) ? srPinName.data( ) : 0 );
 
-   this->srName_ = "";
-   this->srName_ += this->srInstName_;
-   this->srName_ += "|";
-   this->srName_ += this->srPinName_;
-
-   this->type_ = type;
+   this->Set( pszInstName, pszPinName, type );
 } 
 
 //===========================================================================//
@@ -372,17 +423,10 @@ void TNO_InstPin_c::Set(
       const char*         pszPinName,
             TC_TypeMode_t type )
 {
-   this->srInstName_ = TIO_PSZ_STR( pszInstName );
-   this->srPortName_ = "";
-   this->srPinName_ = TIO_PSZ_STR( pszPinName );
+   const char* pszPortName = 0;
 
-   this->srName_ = "";
-   this->srName_ += this->srInstName_;
-   this->srName_ += "|";
-   this->srName_ += this->srPinName_;
-
-   this->type_ = type;
-} 
+   this->Set( pszInstName, pszPortName, pszPinName, type );
+}
 
 //===========================================================================//
 // Method         : Clear
@@ -397,5 +441,7 @@ void TNO_InstPin_c::Clear(
    this->srInstName_ = "";
    this->srPortName_ = "";
    this->srPinName_ = "";
+   this->portIndex_ = TNO_PORT_INDEX_INVALID;
+   this->pinIndex_ = TNO_PIN_INDEX_INVALID;
    this->type_ = TC_TYPE_UNDEFINED;
 }
