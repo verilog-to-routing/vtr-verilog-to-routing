08a6fbb4ab4773dcfddaf56ced79f45fbfc41626
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-04-26 15:30:33 +0000

    Version 4.426
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@2082 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- toro/TOS_OptionsStore/TOS_RulesSwitches.cxx
+++ toro/TOS_OptionsStore/TOS_RulesSwitches.cxx
@@ -10,7 +10,7 @@
 //===========================================================================//
 
 //---------------------------------------------------------------------------//
-// Copyright (C) 2012 Jeff Rudolph, Texas Instruments (jrudolph@ti.com)      //
+// Copyright (C) 2012-2013 Jeff Rudolph, Texas Instruments (jrudolph@ti.com) //
 //                                                                           //
 // This program is free software; you can redistribute it and/or modify it   //
 // under the terms of the GNU General Public License as published by the     //
@@ -83,25 +83,33 @@ void TOS_RulesSwitches_c::Print(
 
    TIO_PrintHandler_c& printHandler = TIO_PrintHandler_c::GetInstance( );
 
-   printHandler.WriteCenter( pfile, spaceLen, szSep,           lenSep, "//", "//" );
-   printHandler.WriteCenter( pfile, spaceLen, "Pack Options",  lenSep, "//", "//" );
-   printHandler.WriteCenter( pfile, spaceLen, szSep,           lenSep, "//", "//" );
+   printHandler.WriteCenter( pfile, spaceLen, szSep,            lenSep, "//", "//" );
+   printHandler.WriteCenter( pfile, spaceLen, "Fabric Options", lenSep, "//", "//" );
+   printHandler.WriteCenter( pfile, spaceLen, szSep,            lenSep, "//", "//" );
+
+   printHandler.Write( pfile, spaceLen, "\n" );
+   this->fabricOptions.Print( pfile, spaceLen );
+   printHandler.Write( pfile, spaceLen, "\n" );
+
+   printHandler.WriteCenter( pfile, spaceLen, szSep,            lenSep, "//", "//" );
+   printHandler.WriteCenter( pfile, spaceLen, "Pack Options",   lenSep, "//", "//" );
+   printHandler.WriteCenter( pfile, spaceLen, szSep,            lenSep, "//", "//" );
 
    printHandler.Write( pfile, spaceLen, "\n" );
    this->packOptions.Print( pfile, spaceLen );
    printHandler.Write( pfile, spaceLen, "\n" );
 
-   printHandler.WriteCenter( pfile, spaceLen, szSep,           lenSep, "//", "//" );
-   printHandler.WriteCenter( pfile, spaceLen, "Place Options", lenSep, "//", "//" );
-   printHandler.WriteCenter( pfile, spaceLen, szSep,           lenSep, "//", "//" );
+   printHandler.WriteCenter( pfile, spaceLen, szSep,            lenSep, "//", "//" );
+   printHandler.WriteCenter( pfile, spaceLen, "Place Options",  lenSep, "//", "//" );
+   printHandler.WriteCenter( pfile, spaceLen, szSep,            lenSep, "//", "//" );
 
    printHandler.Write( pfile, spaceLen, "\n" );
    this->placeOptions.Print( pfile, spaceLen );
    printHandler.Write( pfile, spaceLen, "\n" );
 
-   printHandler.WriteCenter( pfile, spaceLen, szSep,           lenSep, "//", "//" );
-   printHandler.WriteCenter( pfile, spaceLen, "Route Options", lenSep, "//", "//" );
-   printHandler.WriteCenter( pfile, spaceLen, szSep,           lenSep, "//", "//" );
+   printHandler.WriteCenter( pfile, spaceLen, szSep,            lenSep, "//", "//" );
+   printHandler.WriteCenter( pfile, spaceLen, "Route Options",  lenSep, "//", "//" );
+   printHandler.WriteCenter( pfile, spaceLen, szSep,            lenSep, "//", "//" );
 
    printHandler.Write( pfile, spaceLen, "\n" );
    this->routeOptions.Print( pfile, spaceLen );
@@ -116,10 +124,18 @@ void TOS_RulesSwitches_c::Print(
 // Version history
 // 05/01/12 jeffr : Original
 // 01/15/13 jeffr : Added support for placeOptions' relativePlace members
+// 01/18/13 jeffr : Added support for placeOptions' prePlaced members
+// 02/21/13 jeffr : Added support for routeOptions' preRouted members
+// 04/24/13 jeffr : Added support for fabricOptions' SB and CB members
 //===========================================================================//
 void TOS_RulesSwitches_c::Init(
       void )
 {
+   this->fabricOptions.blocks.override = false;
+   this->fabricOptions.switchBoxes.override = false;
+   this->fabricOptions.connectionBlocks.override = false;
+   this->fabricOptions.channels.override = false;
+
    this->packOptions.algorithmMode = TOS_PACK_ALGORITHM_AAPACK;
    this->packOptions.clusterNetsMode = TOS_PACK_CLUSTER_NETS_MIN_CONNECTIONS;
    this->packOptions.areaWeight = 0.75;
@@ -131,6 +147,7 @@ void TOS_RulesSwitches_c::Init(
    this->packOptions.fixedDelay = 0.0;
 
    this->placeOptions.algorithmMode = TOS_PLACE_ALGORITHM_ANNEALING;
+   this->placeOptions.channelWidth = 100;
    this->placeOptions.randomSeed = 1;
    this->placeOptions.initTemp = 0.0;
    this->placeOptions.initTempFactor = 20.0;
@@ -153,6 +170,7 @@ void TOS_RulesSwitches_c::Init(
    this->placeOptions.relativePlace.rotateEnable = true;
    this->placeOptions.relativePlace.maxPlaceRetryCt = TOS_PLACE_RELATIVE_INITIAL_PLACE_RETRY;
    this->placeOptions.relativePlace.maxMacroRetryCt = TOS_PLACE_RELATIVE_INITIAL_MACRO_RETRY;
+   this->placeOptions.prePlaced.enable = false;
 
    this->routeOptions.algorithmMode = TOS_ROUTE_ALGORITHM_PATHFINDER;
    this->routeOptions.abstractMode = TOS_ROUTE_ABSTRACT_DETAILED;
@@ -169,6 +187,9 @@ void TOS_RulesSwitches_c::Init(
    this->routeOptions.timingMaxCriticality = 0.99;
    this->routeOptions.slackCriticality = 1.0;
    this->routeOptions.timingAStarFactor = 1.2;
+
+   this->routeOptions.preRouted.enable = false;
+   this->routeOptions.preRouted.orderMode = TOS_ROUTE_ORDER_FIRST;
 } 
 
 //===========================================================================//
