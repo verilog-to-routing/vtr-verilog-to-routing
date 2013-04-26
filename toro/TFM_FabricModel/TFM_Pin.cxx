82194bcafc6a0f44ef16a5f70070b83fb162e9a4
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-04-26 15:15:21 +0000

    Version 4.426
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@1975 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- toro/TFM_FabricModel/TFM_Pin.cxx
+++ toro/TFM_FabricModel/TFM_Pin.cxx
@@ -3,14 +3,14 @@
 //
 //           Public methods include:
 //           - TFM_Pin_c, ~TFM_Pin_c
-//           - operator=
+//           - operator=, operator<
 //           - operator==, operator!=
 //           - Print
 //
 //===========================================================================//
 
 //---------------------------------------------------------------------------//
-// Copyright (C) 2012 Jeff Rudolph, Texas Instruments (jrudolph@ti.com)      //
+// Copyright (C) 2012-2013 Jeff Rudolph, Texas Instruments (jrudolph@ti.com) //
 //                                                                           //
 // This program is free software; you can redistribute it and/or modify it   //
 // under the terms of the GNU General Public License as published by the     //
@@ -42,8 +42,10 @@
 TFM_Pin_c::TFM_Pin_c( 
       void )
       :
+      type( TC_TYPE_UNDEFINED ),
       side( TC_SIDE_UNDEFINED ),
-      offset( 0.0 ),
+      offset( 0 ),
+      delta( 0.0 ),
       width( 0.0 ),
       slice( 0 )
 {
@@ -52,14 +54,18 @@ TFM_Pin_c::TFM_Pin_c(
 //===========================================================================//
 TFM_Pin_c::TFM_Pin_c( 
       const string&       srName,
+            TC_TypeMode_t type_,
             TC_SideMode_t side_,
-            double        offset_,
+            int           offset_,
+            double        delta_,
             double        width_,
             unsigned int  slice_ )
       :
       TPO_Pin_t( srName ),
+      type( type_ ),
       side( side_ ),
       offset( offset_ ),
+      delta( delta_ ),
       width( width_ ),
       slice( slice_ )
 {
@@ -68,14 +74,18 @@ TFM_Pin_c::TFM_Pin_c(
 //===========================================================================//
 TFM_Pin_c::TFM_Pin_c( 
       const char*         pszName,
+            TC_TypeMode_t type_,
             TC_SideMode_t side_,
-            double        offset_,
+            int           offset_,
+            double        delta_,
             double        width_,
             unsigned int  slice_ )
       :
       TPO_Pin_t( pszName ),
+      type( type_ ),
       side( side_ ),
       offset( offset_ ),
+      delta( delta_ ),
       width( width_ ),
       slice( slice_ )
 {
@@ -86,8 +96,10 @@ TFM_Pin_c::TFM_Pin_c(
       const TFM_Pin_c& pin )
       :
       TPO_Pin_t( pin ),
+      type( pin.type ),
       side( pin.side ),
       offset( pin.offset ),
+      delta( pin.delta ),
       width( pin.width ),
       slice( pin.slice ),
       connectionPattern( pin.connectionPattern )
@@ -119,13 +131,62 @@ TFM_Pin_c& TFM_Pin_c::operator=(
    if( &pin != this )
    {
       TPO_Pin_t::operator=( pin );
+      this->type = pin.type;
       this->side = pin.side;
       this->offset = pin.offset;
+      this->delta = pin.delta;
       this->width = pin.width;
       this->slice = pin.slice;
       this->connectionPattern = pin.connectionPattern;
    }
    return( *this );
+}
+
+//===========================================================================//
+// Method         : operator<
+// Author         : Jeff Rudolph
+//---------------------------------------------------------------------------//
+// Version history
+// 05/15/12 jeffr : Original
+//===========================================================================//
+bool TFM_Pin_c::operator<( 
+      const TFM_Pin_c& pin ) const
+{
+   bool isLessThan = false;
+
+   if( this->slice < pin.slice )
+   {
+      isLessThan = true;
+   }
+   else if( this->slice == pin.slice )
+   {
+      if( this->side < pin.side )
+      {
+         isLessThan = true;
+      }
+      else if( this->side == pin.side )
+      {
+         if( this->offset < pin.offset )
+         {
+            isLessThan = true;
+         }
+         else if( this->offset == pin.offset )
+         {
+            if( TCTF_IsLT( this->delta, pin.delta ))
+            {
+               isLessThan = true;
+            }
+            else if( TCTF_IsEQ( this->delta, pin.delta ))
+            {
+               if( TC_CompareStrings( this->GetName( ), pin.GetName( )) < 0 )
+               {
+                  isLessThan = true;
+               }
+            }
+         }
+      }
+   }
+   return( isLessThan );
 }
 
 //===========================================================================//
@@ -172,13 +233,21 @@ void TFM_Pin_c::Print(
 
    printHandler.Write( pfile, spaceLen, "<pin name=\"%s\"",
                                         TIO_PSZ_STR( TPO_Pin_t::GetName( )));
+   if( this->type != TC_TYPE_UNDEFINED )
+   {
+      string srType;
+      TC_ExtractStringTypeMode( this->type, &srType );
+      printHandler.Write( pfile, 0, " type=\"%s\"", 
+                                    TIO_SR_STR( srType ));
+   }
    if( this->side != TC_SIDE_UNDEFINED )
    {
       string srSide;
       TC_ExtractStringSideMode( this->side, &srSide );
-      printHandler.Write( pfile, 0, " side=\"%s\" offset=\"%0.*f\" width=\"%0.*f\" slice=\"%u\"", 
+      printHandler.Write( pfile, 0, " side=\"%s\" offset=\"%d\" delta=\"%0.*f\" width=\"%0.*f\" slice=\"%u\"", 
                                     TIO_SR_STR( srSide ),
-                                    precision, this->offset,
+                                    this->offset,
+                                    precision, this->delta,
                                     precision, this->width,
                                     this->slice );
    }
