2369da6b1ac201afd6b25bc8c0cfae7c5a4504f1
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-04-26 15:15:44 +0000

    Version 4.426
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@1985 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- toro/TFV_FabricView/TFV_FabricPin.cxx
+++ toro/TFV_FabricView/TFV_FabricPin.cxx
@@ -10,7 +10,7 @@
 //===========================================================================//
 
 //---------------------------------------------------------------------------//
-// Copyright (C) 2012 Jeff Rudolph, Texas Instruments (jrudolph@ti.com)      //
+// Copyright (C) 2012-2013 Jeff Rudolph, Texas Instruments (jrudolph@ti.com) //
 //                                                                           //
 // This program is free software; you can redistribute it and/or modify it   //
 // under the terms of the GNU General Public License as published by the     //
@@ -42,10 +42,56 @@
 TFV_FabricPin_c::TFV_FabricPin_c( 
       void )
       :
-      slice_( 0 ),
+      type_( TC_TYPE_UNDEFINED ),
       side_( TC_SIDE_UNDEFINED ),
-      offset_( 0.0 ),
+      offset_( 0 ),
+      delta_( 0.0 ),
       width_( 0.0 ),
+      slice_( 0 ),
+      channelCount_( 0 ),
+      connectionList_( TFV_CONNECTION_LIST_DEF_CAPACITY )
+{
+} 
+
+//===========================================================================//
+TFV_FabricPin_c::TFV_FabricPin_c( 
+      const string&       srName,
+            TC_TypeMode_t type,
+            TC_SideMode_t side,
+            int           offset,
+            double        delta,
+            double        width,
+            unsigned int  slice )
+      :
+      srName_( srName ),
+      type_( type ),
+      side_( side ),
+      offset_( offset ),
+      delta_( delta ),
+      width_( width ),
+      slice_( slice ),
+      channelCount_( 0 ),
+      connectionList_( TFV_CONNECTION_LIST_DEF_CAPACITY )
+{
+} 
+
+//===========================================================================//
+TFV_FabricPin_c::TFV_FabricPin_c( 
+      const char*         pszName,
+            TC_TypeMode_t type,
+            TC_SideMode_t side,
+            int           offset,
+            double        delta,
+            double        width,
+            unsigned int  slice )
+      :
+      srName_( TIO_PSZ_STR( pszName )),
+      type_( type ),
+      side_( side ),
+      offset_( offset ),
+      delta_( delta ),
+      width_( width ),
+      slice_( slice ),
       channelCount_( 0 ),
       connectionList_( TFV_CONNECTION_LIST_DEF_CAPACITY )
 {
@@ -55,15 +101,15 @@ TFV_FabricPin_c::TFV_FabricPin_c(
 TFV_FabricPin_c::TFV_FabricPin_c( 
       const string&       srName,
             unsigned int  slice,
-            TC_SideMode_t side,
-            double        offset,
-            double        width )
+            TC_SideMode_t side )
       :
       srName_( srName ),
-      slice_( slice ),
+      type_( TC_TYPE_UNDEFINED ),
       side_( side ),
-      offset_( offset ),
-      width_( width ),
+      offset_( 0 ),
+      delta_( 0.0 ),
+      width_( 0.0 ),
+      slice_( slice ),
       channelCount_( 0 ),
       connectionList_( TFV_CONNECTION_LIST_DEF_CAPACITY )
 {
@@ -73,15 +119,15 @@ TFV_FabricPin_c::TFV_FabricPin_c(
 TFV_FabricPin_c::TFV_FabricPin_c( 
       const char*         pszName,
             unsigned int  slice,
-            TC_SideMode_t side,
-            double        offset,
-            double        width )
+            TC_SideMode_t side )
       :
       srName_( TIO_PSZ_STR( pszName )),
-      slice_( slice ),
+      type_( TC_TYPE_UNDEFINED ),
       side_( side ),
-      offset_( offset ),
-      width_( width ),
+      offset_( 0 ),
+      delta_( 0.0 ),
+      width_( 0.0 ),
+      slice_( slice ),
       channelCount_( 0 ),
       connectionList_( TFV_CONNECTION_LIST_DEF_CAPACITY )
 {
@@ -92,10 +138,12 @@ TFV_FabricPin_c::TFV_FabricPin_c(
       const TFV_FabricPin_c& fabricPin )
       :
       srName_( fabricPin.srName_ ),
-      slice_( fabricPin.slice_ ),
+      type_( fabricPin.type_ ),
       side_( fabricPin.side_ ),
       offset_( fabricPin.offset_ ),
+      delta_( fabricPin.delta_ ),
       width_( fabricPin.width_ ),
+      slice_( fabricPin.slice_ ),
       channelCount_( fabricPin.channelCount_ ),
       connectionList_( fabricPin.connectionList_ )
 {
@@ -126,10 +174,12 @@ TFV_FabricPin_c& TFV_FabricPin_c::operator=(
    if( &fabricPin != this )
    {
       this->srName_ = fabricPin.srName_;
-      this->slice_ = fabricPin.slice_;
+      this->type_ = fabricPin.type_;
       this->side_ = fabricPin.side_;
       this->offset_ = fabricPin.offset_;
+      this->delta_ = fabricPin.delta_;
       this->width_ = fabricPin.width_;
+      this->slice_ = fabricPin.slice_;
       this->channelCount_ = fabricPin.channelCount_;
       this->connectionList_ = fabricPin.connectionList_;
    }
@@ -214,27 +264,40 @@ void TFV_FabricPin_c::Print(
    TC_MinGrid_c& MinGrid = TC_MinGrid_c::GetInstance( );
    unsigned int precision = MinGrid.GetPrecision( );
 
+   string srType;
+   TC_ExtractStringTypeMode( this->type_, &srType );
+
    string srSide;
    TC_ExtractStringSideMode( this->side_, &srSide );
 
-   printHandler.Write( pfile, spaceLen, "\"%s\" %u [%s %0.*f %0.*f]",
+   printHandler.Write( pfile, spaceLen, "\"%s\" %s [%s %d %0.*f %0.*f %u]",
                                         TIO_SR_STR( this->srName_ ),
-                                        this->slice_,
+                                        TIO_SR_STR( srType ),
                                         TIO_SR_STR( srSide ),
-                                        precision, this->offset_,
-                                        precision, this->width_ );
+                                        this->offset_,
+                                        precision, this->delta_,
+                                        precision, this->width_,
+                                        this->slice_ );
    if( this->connectionList_.IsValid( ))
    {
-      printHandler.Write( pfile, 0, " (" );
+      printHandler.Write( pfile, 0, " ->" );
       for( size_t i = 0; i < this->connectionList_.GetLength( ); ++i )
       {
          const TFV_Connection_t& connection = *this->connectionList_[i];
-         printHandler.Write( pfile, 0, "%s%d",
-                                       i == 0 ? "" : " ",
+
+         const char* pszSide = "?";
+         switch( connection.GetSide( ))
+         {
+         case TC_SIDE_LEFT:  pszSide = "L"; break;
+         case TC_SIDE_RIGHT: pszSide = "R"; break;
+         case TC_SIDE_LOWER: pszSide = "B"; break;
+         case TC_SIDE_UPPER: pszSide = "T"; break;
+         default:                           break;
+         }
+         printHandler.Write( pfile, 0, " (%s %d)",
+                                       TIO_PSZ_STR( pszSide ),
                                        connection.GetIndex( ));
       }
-      printHandler.Write( pfile, 0, ")" );
-
    }
    printHandler.Write( pfile, 0, "\n" );
 }
