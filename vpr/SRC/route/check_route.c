af2aa75e45d794da71434e5e1ee1c90495969230
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-05-28 15:47:51 +0000

    Added support for s_type_descriptor's new 'width' member. This new member extends the VPR variable block height capability with variable block width.
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@2193 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- vpr/SRC/route/check_route.c
+++ vpr/SRC/route/check_route.c
@@ -487,43 +487,41 @@ static int pin_and_chan_adjacent(int pin_node, int chan_node) {
 	 * nodes are adjacent and 0 if they are not (any other value means there's *
 	 * a bug in this routine).                                                 */
 
-	int num_adj, pin_xlow, pin_ylow, pin_xhigh, pin_yhigh, chan_xlow, chan_ylow,
-			chan_xhigh, chan_yhigh;
-	int pin_ptc, i;
-	t_rr_type chan_type;
-	t_type_ptr pin_grid_type;
+	int num_adj = 0;
 
-	num_adj = 0;
-	pin_xlow = rr_node[pin_node].xlow;
-	pin_ylow = rr_node[pin_node].ylow;
-	pin_xhigh = rr_node[pin_node].xhigh;
-	pin_yhigh = rr_node[pin_node].yhigh;
-	pin_grid_type = grid[pin_xlow][pin_ylow].type;
-	pin_ptc = rr_node[pin_node].ptc_num;
-	chan_type = rr_node[chan_node].type;
-	chan_xlow = rr_node[chan_node].xlow;
-	chan_ylow = rr_node[chan_node].ylow;
-	chan_xhigh = rr_node[chan_node].xhigh;
-	chan_yhigh = rr_node[chan_node].yhigh;
+	int pin_xlow = rr_node[pin_node].xlow;
+	int pin_ylow = rr_node[pin_node].ylow;
+	int pin_xhigh = rr_node[pin_node].xhigh;
+	int pin_yhigh = rr_node[pin_node].yhigh;
+	t_type_ptr pin_grid_type = grid[pin_xlow][pin_ylow].type;
+	int pin_ptc = rr_node[pin_node].ptc_num;
+
+	int chan_type = rr_node[chan_node].type;
+	int chan_xlow = rr_node[chan_node].xlow;
+	int chan_ylow = rr_node[chan_node].ylow;
+	int chan_xhigh = rr_node[chan_node].xhigh;
+	int chan_yhigh = rr_node[chan_node].yhigh;
 
 	if (chan_type == CHANX) {
-		if (chan_ylow == pin_yhigh) { /* CHANX above CLB */
-			if (pin_grid_type->pinloc[pin_grid_type->height - 1][TOP][pin_ptc]
-					== 1 && pin_xlow <= chan_xhigh && pin_xhigh >= chan_xlow)
-				num_adj++;
-		} else if (chan_ylow == pin_ylow - 1) { /* CHANX below CLB */
-			if (pin_grid_type->pinloc[0][BOTTOM][pin_ptc] == 1
-					&& pin_xlow <= chan_xhigh && pin_xhigh >= chan_xlow)
-				num_adj++;
+		for (int width = 0; width < pin_grid_type->width; ++width) {
+			if (chan_ylow == pin_yhigh) { /* CHANX above CLB */
+				if (pin_grid_type->pinloc[width][pin_grid_type->height - 1][TOP][pin_ptc] == 1
+						&& pin_xlow <= chan_xhigh && pin_xhigh >= chan_xlow)
+					num_adj++;
+			} else if (chan_ylow == pin_ylow - 1) { /* CHANX below CLB */
+				if (pin_grid_type->pinloc[width][0][BOTTOM][pin_ptc] == 1
+						&& pin_xlow <= chan_xhigh && pin_xhigh >= chan_xlow)
+					num_adj++;
+			}
 		}
 	} else if (chan_type == CHANY) {
-		for (i = 0; i < pin_grid_type->height; i++) {
+		for (int height = 0; height < pin_grid_type->height; ++height) {
 			if (chan_xlow == pin_xhigh) { /* CHANY to right of CLB */
-				if (pin_grid_type->pinloc[i][RIGHT][pin_ptc] == 1
+				if (pin_grid_type->pinloc[pin_grid_type->width-1][height][RIGHT][pin_ptc] == 1
 						&& pin_ylow <= chan_yhigh && pin_yhigh >= chan_ylow)
 					num_adj++;
 			} else if (chan_xlow == pin_xlow - 1) { /* CHANY to left of CLB */
-				if (pin_grid_type->pinloc[i][LEFT][pin_ptc] == 1
+				if (pin_grid_type->pinloc[0][height][LEFT][pin_ptc] == 1
 						&& pin_ylow <= chan_yhigh && pin_yhigh >= chan_ylow)
 					num_adj++;
 			}
