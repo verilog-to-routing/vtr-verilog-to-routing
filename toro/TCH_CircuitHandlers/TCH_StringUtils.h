be9aec7d0e269be59ed92589a9d969e81f5f6b01
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-05-29 18:53:50 +0000

    Updated to version 1.501.0
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@2270 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- toro/TCH_CircuitHandlers/TCH_StringUtils.h
+++ toro/TCH_CircuitHandlers/TCH_StringUtils.h
@@ -38,6 +38,7 @@ using namespace std;
 //===========================================================================//
 
 void TCH_ExtractStringStatusMode( TCH_PlaceStatusMode_t mode, string* psrMode );
+void TCH_ExtractStringStatusMode( TCH_RouteStatusMode_t mode, string* psrMode );
 
 void TCH_ExtractStringVPR_Type( t_rr_type type, string* psrType );
 
