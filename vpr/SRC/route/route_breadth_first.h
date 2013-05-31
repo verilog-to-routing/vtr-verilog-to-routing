fea1b8583f5c84298c79d6984458737051374467
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-05-31 18:46:15 +0000

    Added new try_breadth_first_route_net() function. This new function consolidates resuable code and supports the new optional pre-routed nets functionality.
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@2348 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- vpr/SRC/route/route_breadth_first.h
+++ vpr/SRC/route/route_breadth_first.h
@@ -1,2 +1,4 @@
 boolean try_breadth_first_route(struct s_router_opts router_opts,
 		t_ivec ** clb_opins_used_locally, int width_fac);
+boolean try_breadth_first_route_net(int inet, int itry, float pres_fac, 
+		struct s_router_opts router_opts);
