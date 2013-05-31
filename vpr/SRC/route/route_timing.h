a3c486e7f5f58d98358aab34a243a5efb2b1e583
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-05-31 18:48:04 +0000

    Added new try_timing_driven_route_net() function. This new function consolidates resuable code and supports the new optional pre-routed nets functionality. Extended the timing_driven_route_net() function to handle 'itry' parameter (needed to handle optional pre-routed nets).
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@2350 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- vpr/SRC/route/route_timing.h
+++ vpr/SRC/route/route_timing.h
@@ -1,7 +1,12 @@
 boolean try_timing_driven_route(struct s_router_opts router_opts,
 		float **net_delay, t_slack * slacks, t_ivec ** clb_opins_used_locally,
 		boolean timing_analysis_enabled);
-boolean timing_driven_route_net(int inet, float pres_fac, float max_criticality,
+boolean try_timing_driven_route_net(int inet, int itry, float pres_fac, 
+		struct s_router_opts router_opts,
+		float* pin_criticality, int* sink_order,
+		t_rt_node** rt_node_of_sink, float** net_delay, t_slack* slacks);
+
+boolean timing_driven_route_net(int inet, int itry, float pres_fac, float max_criticality,
 		float criticality_exp, float astar_fac, float bend_cost,
 		float *pin_criticality, int *sink_order, t_rt_node ** rt_node_of_sink, 
 		float *net_delay, t_slack * slacks);
