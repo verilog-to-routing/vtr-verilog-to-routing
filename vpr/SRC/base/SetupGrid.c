3ccf4d89fee348e5eb2ae9bd7731183a22584985
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-05-31 18:05:55 +0000

    Added support for user-defined fabric grid definition using the Toro front-end. See #ifdef TORO_FABRIC_GRID_OVERRIDE. Added support for user-defined fabric block dimensions using the Toro front-end. See #ifdef TORO_FABRIC_BLOCK_OVERRIDE.
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@2331 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- vpr/SRC/base/SetupGrid.c
+++ vpr/SRC/base/SetupGrid.c
@@ -15,12 +15,38 @@
 #include "SetupGrid.h"
 #include "read_xml_arch_file.h"
 
+static void CheckGrid(void);
+static t_type_ptr find_type_col(INP int x);
+
+#ifdef TORO_FABRIC_GRID_OVERRIDE 
+//===========================================================================//
+#include "TFH_FabricGridHandler.h"
+
+static void alloc_and_load_block_override_ios(
+		t_grid_tile** vpr_grid, int vpr_nx, int vpr_ny );
+//===========================================================================//
+#endif
+
+#ifdef TORO_FABRIC_BLOCK_OVERRIDE 
+//===========================================================================//
+#include "TFH_FabricBlockHandler.h"
+
+static t_type_ptr alloc_and_load_block_override_type(
+		t_type_ptr type, int x, int y);
+
+static void alloc_and_load_block_override_blocks(
+		t_grid_tile** vpr_grid, int vpr_nx, int vpr_ny);
+static void alloc_and_load_block_override_blocks_invalid(
+		t_grid_tile** vpr_grid, int vpr_nx, int vpr_ny);
+static void alloc_and_load_block_override_blocks_empty(
+		t_grid_tile** vpr_grid, int vpr_nx, int vpr_ny,
+		const TFH_GridBlockList_t& gridBlockList);
+//===========================================================================//
+#endif
+
 static void alloc_and_load_num_instances_type(
 		t_grid_tile** L_grid, int L_nx, int L_ny,
 		int* L_num_instances_type, int L_num_types);
-
-static void CheckGrid(void);
-static t_type_ptr find_type_col(INP int x);
 
 /* Create and fill FPGA architecture grid.         */
 void alloc_and_load_grid(INOUTP int *num_instances_type) {
@@ -93,6 +119,10 @@ void alloc_and_load_grid(INOUTP int *num_instances_type) {
 			if (grid[i][j].width_offset > 0 || grid[i][j].height_offset > 0)
 				continue;
 
+#ifdef TORO_FABRIC_BLOCK_OVERRIDE 
+			type = alloc_and_load_block_override_type(type, i, j);
+#endif
+
 			if (i + type->width - 1 <= nx && j + type->height - 1 <= ny) {
 				for (int i_offset = 0; i_offset < type->width; ++i_offset) {
 					for (int j_offset = 0; j_offset < type->height; ++j_offset) {
@@ -113,6 +143,14 @@ void alloc_and_load_grid(INOUTP int *num_instances_type) {
 			}
 		}
 	}
+
+#ifdef TORO_FABRIC_GRID_OVERRIDE 
+	alloc_and_load_block_override_ios(grid, nx, ny);
+#endif
+
+#ifdef TORO_FABRIC_BLOCK_OVERRIDE 
+	alloc_and_load_block_override_blocks(grid, nx, ny);
+#endif
 
 	// And, refresh (ie. reset and update) the "num_instances_type" array
 	// (while also forcing any remaining INVALID blocks to EMPTY_TYPE)
@@ -135,21 +173,156 @@ void alloc_and_load_grid(INOUTP int *num_instances_type) {
 #endif
 }
 
-void freeGrid(void) {
+#ifdef TORO_FABRIC_GRID_OVERRIDE 
+//===========================================================================//
+static void alloc_and_load_block_override_ios(
+		t_grid_tile** vpr_grid, int vpr_nx, int vpr_ny ) {
 
-	int i, j;
-	if (grid == NULL) {
-		return;
-	}
+	const TFH_FabricGridHandler_c& fabricGridHandler = TFH_FabricGridHandler_c::GetInstance();
+	if (fabricGridHandler.IsValid()) {
 
-	for (i = 0; i <= (nx + 1); ++i) {
-		for (j = 0; j <= (ny + 1); ++j) {
-			free(grid[i][j].blocks);
+		/* Iterate over the existing grid (IOs and CLBs) to adjust IO ring (as needed) */
+		const TGO_Polygon_c& ioPolygon = fabricGridHandler.GetPolygon( );
+
+		/* First pass, iterate grid to force type EMPTY for any locations outside the IO ring */
+		for (int x = 0; x <= (vpr_nx + 1); ++x) {
+			for (int y = 0; y <= (vpr_ny + 1); ++y) {
+				if (ioPolygon.IsWithin(x, y)) 
+					continue;
+				if (ioPolygon.IsEdge(x, y) && !ioPolygon.IsConvexCorner(x, y)) 
+					continue;
+
+				free(grid[x][y].blocks);
+				grid[x][y].type = EMPTY_TYPE;
+				grid[x][y].blocks = (int *) my_malloc(sizeof(int));
+				grid[x][y].blocks[0] = EMPTY;
+			}
+		}
+	
+		// Second pass, iterate grid to force type IO for any locations on the IO ring */
+		for (int x = 0; x <= (vpr_nx + 1); ++x) {
+			for (int y = 0; y <= (vpr_ny + 1); ++y) {
+				if (!ioPolygon.IsEdge(x, y) || ioPolygon.IsConvexCorner(x, y))
+					continue;
+
+				free(grid[x][y].blocks);
+				grid[x][y].type = IO_TYPE;
+				grid[x][y].blocks = (int *) my_malloc(sizeof(int) * IO_TYPE->capacity);
+				for (int i = 0; i < IO_TYPE->capacity; ++i) {
+					grid[x][y].blocks[i] = EMPTY;
+				}
+			}
 		}
 	}
-	free_matrix(grid, 0, nx + 1, 0, sizeof(struct s_grid_tile));
-	grid = NULL;
 }
+//===========================================================================//
+#endif
+
+#ifdef TORO_FABRIC_BLOCK_OVERRIDE 
+//===========================================================================//
+static t_type_ptr alloc_and_load_block_override_type(
+		t_type_ptr type, int x, int y) {
+
+	const TFH_FabricBlockHandler_c& fabricBlockHandler = TFH_FabricBlockHandler_c::GetInstance();
+	if (fabricBlockHandler.IsValid()) {
+
+		vpr_printf(TIO_MESSAGE_INFO, "Overriding architecture block[%d][%d] based on fabric %s block...\n", x, y, type->name);
+
+		// Search for override type based on given block grid list
+		const TFH_GridBlockList_t& gridBlockList = fabricBlockHandler.GetGridBlockList( );
+		TFH_GridBlock_c gridBlock(x, y);
+		if( gridBlockList.Find(gridBlock, &gridBlock))
+		{
+			int i = gridBlock.GetTypeIndex( );
+			type = &type_descriptors[i];
+		}
+	}
+	return(type);
+}
+
+//===========================================================================//
+static void alloc_and_load_block_override_blocks(
+		t_grid_tile** vpr_grid, int vpr_nx, int vpr_ny ) {
+
+	const TFH_FabricBlockHandler_c& fabricBlockHandler = TFH_FabricBlockHandler_c::GetInstance();
+	if (fabricBlockHandler.IsValid()) {
+
+		// Iterate VPR's grid to initially force all blocks to INVALID
+		alloc_and_load_block_override_blocks_invalid(vpr_grid, vpr_nx, vpr_ny);
+
+		// Then, iterate valid grid list to force unused blocks to EMPTY
+		const TFH_GridBlockList_t& gridBlockList = fabricBlockHandler.GetGridBlockList( );
+		alloc_and_load_block_override_blocks_empty(vpr_grid, vpr_nx, vpr_ny,
+				gridBlockList);
+	}
+}
+
+//===========================================================================//
+static void alloc_and_load_block_override_blocks_invalid(
+		t_grid_tile** vpr_grid, int vpr_nx, int vpr_ny) {
+
+	// Iterate grid array to force all blocks to INVALID
+	for (int x = 0; x <= (vpr_nx + 1); ++x) {
+		for (int y = 0; y <= (vpr_ny + 1); ++y) {
+
+			if (!vpr_grid[x][y].type)
+				continue;
+
+			if (vpr_grid[x][y].type == EMPTY_TYPE)
+				continue;
+
+			for (int z = 0; z < vpr_grid[x][y].type->capacity; ++z) {
+				vpr_grid[x][y].blocks[z] = INVALID;
+			}
+		}
+	}
+}
+
+//===========================================================================//
+static void alloc_and_load_block_override_blocks_empty(
+		t_grid_tile** vpr_grid, int vpr_nx, int vpr_ny,
+		const TFH_GridBlockList_t& gridBlockList) {
+
+	// Iterate optional valid grid list to force selected blocks to EMPTY
+	for (size_t i = 0; i < gridBlockList.GetLength( ); ++i) {
+
+		const TFH_GridBlock_c& gridBlock = *gridBlockList[i];
+		TFH_BlockType_t blockType = gridBlock.GetBlockType( );
+		const TGO_Point_c& gridPoint = gridBlock.GetGridPoint( );
+
+		int x = gridPoint.x;
+		int y = gridPoint.y;
+		int z = gridPoint.z;
+
+		if (x < 0 || x > (vpr_nx + 1) || y < 0 || y > (vpr_ny + 1))
+			continue;
+
+		if (!vpr_grid[x][y].type )
+			continue;
+
+		if (z < 0 || z > vpr_grid[x][y].type->capacity - 1)
+			continue;
+
+		if (vpr_grid[x][y].type == EMPTY_TYPE)
+			continue;
+
+		if (blockType == TFH_BLOCK_INPUT_OUTPUT && vpr_grid[x][y].type != IO_TYPE)
+			continue;
+
+		if (blockType != TFH_BLOCK_INPUT_OUTPUT && vpr_grid[x][y].type == IO_TYPE)
+			continue;
+
+		int dx = vpr_grid[x][y].type->width;
+		int dy = vpr_grid[x][y].type->height;
+		for (x = gridPoint.x; x <= min( gridPoint.x + dx - 1, nx + 1 ); ++x) {
+			for (y = gridPoint.y ; y <= min( gridPoint.y + dy - 1, ny + 1 ); ++y) {
+				vpr_grid[x][y].blocks[z] = EMPTY;
+			}
+		}
+	}
+}
+//===========================================================================//
+#endif
 
 //===========================================================================//
 static void alloc_and_load_num_instances_type(
@@ -200,6 +373,22 @@ static void alloc_and_load_num_instances_type(
 			}
 		}
 	}
+}
+
+void freeGrid(void) {
+
+	int i, j;
+	if (grid == NULL) {
+		return;
+	}
+
+	for (i = 0; i <= (nx + 1); ++i) {
+		for (j = 0; j <= (ny + 1); ++j) {
+			free(grid[i][j].blocks);
+		}
+	}
+	free_matrix(grid, 0, nx + 1, 0, sizeof(struct s_grid_tile));
+	grid = NULL;
 }
 
 static void CheckGrid(void) {
