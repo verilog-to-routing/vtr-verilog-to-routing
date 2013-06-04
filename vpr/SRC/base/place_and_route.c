7fd13c6c5b0d8c0da2891060d7430f1d0cf171b5
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-06-04 19:02:25 +0000

    Extended build_rr_graph() function to support 'trim_obs_channel' parameter.
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@2385 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- vpr/SRC/base/place_and_route.c
+++ vpr/SRC/base/place_and_route.c
@@ -506,7 +506,9 @@ static int binary_search_place_and_route(struct s_placer_opts placer_opts,
 			det_routing_arch.global_route_switch,
 			det_routing_arch.delayless_switch, timing_inf,
 			det_routing_arch.wire_to_ipin_switch, 
-			router_opts.base_cost_type, router_opts.empty_channel_trim,
+			router_opts.base_cost_type,
+			router_opts.trim_empty_channels,
+			router_opts.trim_obs_channels,
 			directs, num_directs, FALSE,
 			&warnings);
 
