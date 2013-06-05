e77a7396a6c5e48660c602099a8193ef60b558a5
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-06-05 20:09:09 +0000

    Updated to version 1.605.0
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@2408 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- libcommon_c++/SRC/TC_Common/TC_Bit.h
+++ libcommon_c++/SRC/TC_Common/TC_Bit.h
@@ -2,15 +2,32 @@
 // Purpose : Declaration and inline definitions for a (regexp) TC_Bit class.
 //
 //           Inline methods include:
+//           - IsTrue
+//           - IsFalse
 //           - IsValid
 //
 //===========================================================================//
 
+//---------------------------------------------------------------------------//
+// Copyright (C) 2012-2013 Jeff Rudolph, Texas Instruments (jrudolph@ti.com) //
+//                                                                           //
+// This program is free software; you can redistribute it and/or modify it   //
+// under the terms of the GNU General Public License as published by the     //
+// Free Software Foundation; version 3 of the License, or any later version. //
+//                                                                           //
+// This program is distributed in the hope that it will be useful, but       //
+// WITHOUT ANY WARRANTY; without even an implied warranty of MERCHANTABILITY //
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License   //
+// for more details.                                                         //
+//                                                                           //
+// You should have received a copy of the GNU General Public License along   //
+// with this program; if not, see <http://www.gnu.org/licenses>.             //
+//---------------------------------------------------------------------------//
+
 #ifndef TC_BIT_H
 #define TC_BIT_H
 
-#include <stdio.h>
-
+#include <cstdio>
 #include <string>
 using namespace std;
 
@@ -45,6 +62,9 @@ public:
    void SetValue( TC_BitMode_t value );
    void SetValue( char value );
 
+   bool IsTrue( void ) const;
+   bool IsFalse( void ) const;
+
    bool IsValid( void ) const;
 
 private:
@@ -58,6 +78,20 @@ private:
 //---------------------------------------------------------------------------//
 // Version history
 // 05/15/12 jeffr : Original
+//===========================================================================//
+inline bool TC_Bit_c::IsTrue( 
+      void ) const
+{
+   return( this->value_ == TC_BIT_TRUE ? true : false );
+}
+
+//===========================================================================//
+inline bool TC_Bit_c::IsFalse( 
+      void ) const
+{
+   return( this->value_ == TC_BIT_FALSE ? true : false );
+}
+
 //===========================================================================//
 inline bool TC_Bit_c::IsValid( 
       void ) const
