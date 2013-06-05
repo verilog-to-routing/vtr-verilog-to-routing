493606156065cb344c20b620a8693730e718d7e1
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-06-05 20:25:45 +0000

    Updated to 1.605.0
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@2440 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- libcommon_c++/SRC/TC_Common/TC_SideIndex.cxx
+++ libcommon_c++/SRC/TC_Common/TC_SideIndex.cxx
@@ -3,13 +3,30 @@
 //
 //           Public methods include:
 //           - TC_SideIndex_c, ~TC_SideIndex_c
-//           - operator=
+//           - operator=, operator<
 //           - operator==, operator!=
 //           - Print
 //           - ExtractString
 //           - Set
+//           - Clear
 //
 //===========================================================================//
+
+//---------------------------------------------------------------------------//
+// Copyright (C) 2012-2013 Jeff Rudolph, Texas Instruments (jrudolph@ti.com) //
+//                                                                           //
+// This program is free software; you can redistribute it and/or modify it   //
+// under the terms of the GNU General Public License as published by the     //
+// Free Software Foundation; version 3 of the License, or any later version. //
+//                                                                           //
+// This program is distributed in the hope that it will be useful, but       //
+// WITHOUT ANY WARRANTY; without even an implied warranty of MERCHANTABILITY //
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License   //
+// for more details.                                                         //
+//                                                                           //
+// You should have received a copy of the GNU General Public License along   //
+// with this program; if not, see <http://www.gnu.org/licenses>.             //
+//---------------------------------------------------------------------------//
 
 #include "TIO_PrintHandler.h"
 
@@ -72,12 +89,36 @@ TC_SideIndex_c::~TC_SideIndex_c(
 TC_SideIndex_c& TC_SideIndex_c::operator=( 
       const TC_SideIndex_c& sideIndex )
 {
-   if ( &sideIndex != this )
+   if( &sideIndex != this )
    {
       this->side_ = sideIndex.side_;
       this->index_ = sideIndex.index_;
    }
    return( *this );
+}
+
+//===========================================================================//
+// Method         : operator<
+// Author         : Jeff Rudolph
+//---------------------------------------------------------------------------//
+// Version history
+// 05/15/12 jeffr : Original
+//===========================================================================//
+bool TC_SideIndex_c::operator<( 
+      const TC_SideIndex_c& sideIndex ) const
+{
+   bool isLessThan = false;
+   
+   if( this->side_ < sideIndex.side_ )
+   {
+      isLessThan = true;
+   }
+   else if(( this->side_ == sideIndex.side_ ) && 
+           ( this->index_ < sideIndex.index_ ))
+   {
+      isLessThan = true;
+   }
+   return( isLessThan );
 }
 
 //===========================================================================//
@@ -134,21 +175,26 @@ void TC_SideIndex_c::Print(
 // 05/15/12 jeffr : Original
 //===========================================================================//
 void TC_SideIndex_c::ExtractString( 
-      string* psrSideIndex ) const
+      string* psrSideIndex,
+      size_t  sideLength ) const
 {
-   if ( psrSideIndex )
+   if( psrSideIndex )
    {
-      if ( this->IsValid( ))
+      if( this->IsValid( ))
       {
-	 string srSide;
+         string srSide;
          TC_ExtractStringSideMode( this->side_, &srSide );
 
+         if( sideLength )
+         {
+            srSide = srSide.substr( 0, sideLength );
+         }
          *psrSideIndex = srSide;
 
-	 if ( this->index_ != SIZE_MAX )
-	 {
-   	    char szIndex[ TIO_FORMAT_STRING_LEN_VALUE ];
-            sprintf( szIndex, "%lu", static_cast<unsigned long>( this->index_ ));
+         if( this->index_ != SIZE_MAX )
+         {
+            char szIndex[TIO_FORMAT_STRING_LEN_VALUE];
+            sprintf( szIndex, "%lu", static_cast< unsigned long >( this->index_ ));
 
             *psrSideIndex += " ";
             *psrSideIndex += szIndex;
@@ -174,4 +220,18 @@ void TC_SideIndex_c::Set(
 {
    this->side_ = side;
    this->index_ = index;
+} 
+
+//===========================================================================//
+// Method         : Clear
+// Author         : Jeff Rudolph
+//---------------------------------------------------------------------------//
+// Version history
+// 05/15/12 jeffr : Original
+//===========================================================================//
+void TC_SideIndex_c::Clear( 
+      void )
+{
+   this->side_ = TC_SIDE_UNDEFINED;
+   this->index_ = UINT_MAX;
 } 
