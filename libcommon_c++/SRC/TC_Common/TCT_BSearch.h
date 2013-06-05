4d3d426143cf7a4bab585c7f56bf16985fbd1b46
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-06-05 20:08:35 +0000

    Updated to version 1.605.0
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@2394 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- libcommon_c++/SRC/TC_Common/TCT_BSearch.h
+++ libcommon_c++/SRC/TC_Common/TCT_BSearch.h
@@ -8,6 +8,22 @@
 //
 //===========================================================================//
 
+//---------------------------------------------------------------------------//
+// Copyright (C) 2012-2013 Jeff Rudolph, Texas Instruments (jrudolph@ti.com) //
+//                                                                           //
+// This program is free software; you can redistribute it and/or modify it   //
+// under the terms of the GNU General Public License as published by the     //
+// Free Software Foundation; version 3 of the License, or any later version. //
+//                                                                           //
+// This program is distributed in the hope that it will be useful, but       //
+// WITHOUT ANY WARRANTY; without even an implied warranty of MERCHANTABILITY //
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License   //
+// for more details.                                                         //
+//                                                                           //
+// You should have received a copy of the GNU General Public License along   //
+// with this program; if not, see <http://www.gnu.org/licenses>.             //
+//---------------------------------------------------------------------------//
+
 #ifndef TCT_BSEARCH_H
 #define TCT_BSEARCH_H
 
@@ -85,21 +101,21 @@ template< class T > T* TCT_BSearchIterate(
    T* pdata = 0;
 
    // Iterate to perform simple binary-search until match found or exhausted
-   while ( i <= j )
+   while( i <= j )
    {
       long mid = (( j - i ) / 2 ) + i;
 
       int c = TCT_BSearchCompare( &data, padata + mid, pfxCompare );
-      if ( c == 0 )
+      if( c == 0 )
       {
          pdata = const_cast< T* >( padata + mid );
          break;
       }
-      else if ( c < 0 )
+      else if( c < 0 )
       {
          j = mid - 1;
       }
-      else if ( c > 0 )
+      else if( c > 0 )
       {
          i = mid + 1;
       }
