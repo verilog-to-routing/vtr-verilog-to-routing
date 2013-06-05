6dda84d08d3f46dd97bf0b793f90496e96b5521c
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-06-05 20:25:50 +0000

    Updated to 1.605.0
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@2442 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- libcommon_c++/SRC/TC_Common/TC_SideName.cxx
+++ libcommon_c++/SRC/TC_Common/TC_SideName.cxx
@@ -3,13 +3,30 @@
 //
 //           Public methods include:
 //           - TC_SideName_c, ~TC_SideName_c
-//           - operator=
+//           - operator=, operator<
 //           - operator==, operator!=
 //           - Print
 //           - ExtractString
 //           - Set
+//           - Clear
 //
 //===========================================================================//
+
+//---------------------------------------------------------------------------//
+// Copyright (C) 2012-2013 Jeff Rudolph, Texas Instruments (jrudolph@ti.com) //
+//                                                                           //
+// This program is free software; you can redistribute it and/or modify it   //
+// under the terms of the GNU General Public License as published by the     //
+// Free Software Foundation; version 3 of the License, or any later version. //
+//                                                                           //
+// This program is distributed in the hope that it will be useful, but       //
+// WITHOUT ANY WARRANTY; without even an implied warranty of MERCHANTABILITY //
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License   //
+// for more details.                                                         //
+//                                                                           //
+// You should have received a copy of the GNU General Public License along   //
+// with this program; if not, see <http://www.gnu.org/licenses>.             //
+//---------------------------------------------------------------------------//
 
 #include "TIO_PrintHandler.h"
 
@@ -46,7 +63,7 @@ TC_SideName_c::TC_SideName_c(
       const char*         pszName )
       :
       side_( side ),
-      srName_( pszName ? pszName : "" )
+      srName_( TIO_PSZ_STR( pszName ))
 {
 } 
 
@@ -81,12 +98,36 @@ TC_SideName_c::~TC_SideName_c(
 TC_SideName_c& TC_SideName_c::operator=( 
       const TC_SideName_c& sideName )
 {
-   if ( &sideName != this )
+   if( &sideName != this )
    {
       this->side_ = sideName.side_;
       this->srName_ = sideName.srName_;
    }
    return( *this );
+}
+
+//===========================================================================//
+// Method         : operator<
+// Author         : Jeff Rudolph
+//---------------------------------------------------------------------------//
+// Version history
+// 05/15/12 jeffr : Original
+//===========================================================================//
+bool TC_SideName_c::operator<( 
+      const TC_SideName_c& sideName ) const
+{
+   bool isLessThan = false;
+   
+   if( this->side_ < sideName.side_ )
+   {
+      isLessThan = true;
+   }
+   else if( this->side_ == sideName.side_ )
+   {
+      isLessThan = (( TC_CompareStrings( this->srName_, sideName.srName_ ) < 0 ) ? 
+                    true : false );
+   }
+   return( isLessThan );
 }
 
 //===========================================================================//
@@ -145,19 +186,21 @@ void TC_SideName_c::Print(
 void TC_SideName_c::ExtractString( 
       string* psrSideName ) const
 {
-   if ( psrSideName )
+   if( psrSideName )
    {
-      if ( this->IsValid( ))
+      if( this->IsValid( ))
       {
-	 string srSide;
+         string srSide;
          TC_ExtractStringSideMode( this->side_, &srSide );
 
          *psrSideName = srSide;
 
-	 if ( this->srName_.length( ))
-	 {
+         if( this->srName_.length( ))
+         {
             *psrSideName += " ";
+            *psrSideName += "\"";
             *psrSideName += this->srName_;
+            *psrSideName += "\"";
          }
       }
       else
@@ -188,5 +231,19 @@ void TC_SideName_c::Set(
       const char*         pszName )
 {
    this->side_ = side;
-   this->srName_ = ( pszName ? pszName : "" );
+   this->srName_ = TIO_PSZ_STR( pszName );
+} 
+
+//===========================================================================//
+// Method         : Clear
+// Author         : Jeff Rudolph
+//---------------------------------------------------------------------------//
+// Version history
+// 05/15/12 jeffr : Original
+//===========================================================================//
+void TC_SideName_c::Clear( 
+      void )
+{
+   this->side_ = TC_SIDE_UNDEFINED;
+   this->srName_ = "";
 } 
