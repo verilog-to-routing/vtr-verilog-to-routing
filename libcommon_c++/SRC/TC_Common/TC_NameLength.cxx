79031709e3106f21718e0275097c12af86fcaa5a
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-06-05 20:09:25 +0000

    Updated to version 1.605.0
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@2413 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- libcommon_c++/SRC/TC_Common/TC_NameLength.cxx
+++ libcommon_c++/SRC/TC_Common/TC_NameLength.cxx
@@ -8,8 +8,25 @@
 //           - Print
 //           - ExtractString
 //           - Set
+//           - Clear
 //
 //===========================================================================//
+
+//---------------------------------------------------------------------------//
+// Copyright (C) 2012-2013 Jeff Rudolph, Texas Instruments (jrudolph@ti.com) //
+//                                                                           //
+// This program is free software; you can redistribute it and/or modify it   //
+// under the terms of the GNU General Public License as published by the     //
+// Free Software Foundation; version 3 of the License, or any later version. //
+//                                                                           //
+// This program is distributed in the hope that it will be useful, but       //
+// WITHOUT ANY WARRANTY; without even an implied warranty of MERCHANTABILITY //
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License   //
+// for more details.                                                         //
+//                                                                           //
+// You should have received a copy of the GNU General Public License along   //
+// with this program; if not, see <http://www.gnu.org/licenses>.             //
+//---------------------------------------------------------------------------//
 
 #include "TIO_PrintHandler.h"
 
@@ -45,7 +62,7 @@ TC_NameLength_c::TC_NameLength_c(
       const char*        pszName,
             unsigned int length )
       :
-      srName_( pszName ? pszName : "" ),
+      srName_( TIO_PSZ_STR( pszName )),
       length_( length )
 {
 } 
@@ -81,7 +98,7 @@ TC_NameLength_c::~TC_NameLength_c(
 TC_NameLength_c& TC_NameLength_c::operator=( 
       const TC_NameLength_c& nameLength )
 {
-   if ( &nameLength != this )
+   if( &nameLength != this )
    {
       this->srName_ = nameLength.srName_;
       this->length_ = nameLength.length_;
@@ -159,15 +176,17 @@ void TC_NameLength_c::Print(
 void TC_NameLength_c::ExtractString( 
       string* psrNameLength ) const
 {
-   if ( psrNameLength )
+   if( psrNameLength )
    {
-      if ( this->IsValid( ))
+      if( this->IsValid( ))
       {
-         *psrNameLength = this->srName_;
+         *psrNameLength = "\"";
+         *psrNameLength += this->srName_;
+         *psrNameLength += "\"";
 
-	 if ( this->length_ != UINT_MAX )
-	 {
-   	    char szLength[ TIO_FORMAT_STRING_LEN_VALUE ];
+         if( this->length_ != UINT_MAX )
+         {
+            char szLength[TIO_FORMAT_STRING_LEN_VALUE];
             sprintf( szLength, "%d", this->length_ );
 
             *psrNameLength += " ";
@@ -201,6 +220,20 @@ void TC_NameLength_c::Set(
       const char*        pszName,
             unsigned int length )
 {
-   this->srName_ = ( pszName ? pszName : "" );
+   this->srName_ = TIO_PSZ_STR( pszName );
    this->length_ = length;
+} 
+
+//===========================================================================//
+// Method         : Clear
+// Author         : Jeff Rudolph
+//---------------------------------------------------------------------------//
+// Version history
+// 05/15/12 jeffr : Original
+//===========================================================================//
+void TC_NameLength_c::Clear( 
+      void )
+{
+   this->srName_ = "";
+   this->length_ = UINT_MAX;
 } 
