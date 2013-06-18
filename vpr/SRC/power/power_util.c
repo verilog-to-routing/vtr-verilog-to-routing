300062a18b1727d4903d847e081f96d572eb9e40
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-06-18 17:11:35 +0000

    Replaced <string.h> with <cstring>. Removed obsolete 'std::' on min/max() functions.
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@2671 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- vpr/SRC/power/power_util.c
+++ vpr/SRC/power/power_util.c
@@ -20,8 +20,8 @@
  */
 
 /************************* INCLUDES *********************************/
-#include <string.h>
-#include <assert.h>
+#include <cstring>
+using namespace std;
 
 #include "power_util.h"
 #include "globals.h"
@@ -466,8 +466,7 @@ void output_logs(FILE * fp, t_log * logs, int num_logs) {
 }
 
 float power_buffer_size_from_logical_effort(float C_load) {
-	return std::max(1.0f,
-			C_load / g_power_commonly_used->INV_1X_C_in
+	return max(1.0f, C_load / g_power_commonly_used->INV_1X_C_in
 					/ (2 * g_power_arch->logical_effort_factor));
 }
 
