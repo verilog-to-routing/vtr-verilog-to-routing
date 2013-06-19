015c129069f3bbde36ab3c2c0e92de31fac81736
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-06-19 15:39:12 +0000

    Updated to version 1.618.0
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@2736 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- toro/TC_Common/TC_StringUtils.h
+++ toro/TC_Common/TC_StringUtils.h
@@ -51,6 +51,13 @@ bool TC_FormatStringDateTimeStamp( char* pszDateTimeStamp,
                                    const char* pszPrefix = 0,
                                    const char* pszPostfix = 0 );
 
+bool TC_FormatStringFileNameLineNum( char* pszFileNameLineNum, 
+                                     size_t lenFileNameLineNum,
+                                     const char* pszFileName,
+                                     unsigned int fileNum,
+                                     const char* pszPrefix = 0,
+                                     const char* pszPostfix = 0 );
+
 void TC_ExtractStringSideMode( TC_SideMode_t sideMode, string* psrSideMode );
 void TC_ExtractStringTypeMode( TC_TypeMode_t typeMode, string* psrTypeMode );
 
