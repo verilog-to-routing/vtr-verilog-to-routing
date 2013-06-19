1d50d58d7733963f5c1a7b287d311c6a2507f25b
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-06-19 15:39:46 +0000

    Updated to version 1.618.0
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@2741 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- toro/TIO_InputOutputHandlers/TIO_PrintHandlerExtern.h
+++ toro/TIO_InputOutputHandlers/TIO_PrintHandlerExtern.h
@@ -57,18 +57,30 @@ extern "C"
 {
 #endif
 
-void PrintHandlerNew( char* pszLogFileName );
+void PrintHandlerNew( const char* pszLogFileName );
 void PrintHandlerDelete( void );
 void PrintHandlerInit( unsigned char enableTimeStamps,
+                       unsigned char enableFileLines,
                        unsigned long maxWarningCount,
                        unsigned long maxErrorCount );
 int PrintHandlerExists( void );
 void PrintHandlerFilter( TIO_MessageMode_t messageMode,
                          TIO_FilterMode_t filterMode,
-                         char* pszFilter );
+                         const char* pszFilter );
+
 unsigned char PrintHandlerMessage( TIO_MessageMode_t messageMode,
-                                   char* pszMessage,
-                                   ... );
+                                   const char* pszMessage, ... );
+
+void PrintHandlerInfo( const char* pszMessage, ... );
+unsigned char PrintHandlerWarning( const char* pszFileName,
+                                   unsigned int lineNum,
+                                   const char* pszMessage, ... );
+unsigned char PrintHandlerError( const char* pszFileName,
+                                 unsigned int lineNum,
+                                 const char* pszMessage, ... );
+void PrintHandlerTrace( const char* pszMessage, ... );
+void PrintHandlerDirect( const char* pszMessage, ... );
+
 #ifdef __cplusplus
 }
 #endif
