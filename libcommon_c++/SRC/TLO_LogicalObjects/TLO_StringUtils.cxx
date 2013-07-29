289eb6f28b67a1e4f5becb7ce1e3d6fbb4728a77
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-07-29 14:59:42 +0000

    Updated to version 1.729.0
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@3154 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- libcommon_c++/SRC/TLO_LogicalObjects/TLO_StringUtils.cxx
+++ libcommon_c++/SRC/TLO_LogicalObjects/TLO_StringUtils.cxx
@@ -3,11 +3,12 @@
 //
 //           Functions include:
 //           - TLO_ExtractStringCellSource
+//           - TLO_ExtractStringPowerType
 //
 //===========================================================================//
 
 //---------------------------------------------------------------------------//
-// Copyright (C) 2012 Jeff Rudolph, Texas Instruments (jrudolph@ti.com)      //
+// Copyright (C) 2012-2013 Jeff Rudolph, Texas Instruments (jrudolph@ti.com) //
 //                                                                           //
 // This program is free software; you can redistribute it and/or modify it   //
 // under the terms of the GNU General Public License as published by the     //
@@ -32,18 +33,47 @@
 // 05/15/12 jeffr : Original
 //===========================================================================//
 void TLO_ExtractStringCellSource(
-      TLO_CellSource_t source,
-      string*          psrSource )
+      TLO_CellSource_t cellSource,
+      string*          psrCellSource )
 {
-   if( psrSource )
+   if( psrCellSource )
    {
-      *psrSource = "";
+      *psrCellSource = "";
 
-      switch( source )
+      switch( cellSource )
       {
-      case TLO_CELL_SOURCE_STANDARD: *psrSource = "standard"; break;
-      case TLO_CELL_SOURCE_CUSTOM:   *psrSource = "custom";   break;
-      default:                       *psrSource = "?";        break;
+      case TLO_CELL_SOURCE_STANDARD: *psrCellSource = "standard"; break;
+      case TLO_CELL_SOURCE_CUSTOM:   *psrCellSource = "custom";   break;
+      default:                       *psrCellSource = "?";        break;
+      }
+   }
+}
+
+//===========================================================================//
+// Function       : TLO_ExtractStringPowerType
+// Author         : Jeff Rudolph
+//---------------------------------------------------------------------------//
+// Version history
+// 07/17/13 jeffr : Original
+//===========================================================================//
+void TLO_ExtractStringPowerType(
+      TLO_PowerType_t cellSource,
+      string*         psrPowerType )
+{
+   if( psrPowerType )
+   {
+      *psrPowerType = "";
+
+      switch( cellSource )
+      {
+      case TLO_POWER_TYPE_IGNORED:         *psrPowerType = "ignored";         break;
+      case TLO_POWER_TYPE_NONE:            *psrPowerType = "none";            break;
+      case TLO_POWER_TYPE_AUTO:            *psrPowerType = "auto";            break;
+      case TLO_POWER_TYPE_CAP:             *psrPowerType = "cap";             break;
+      case TLO_POWER_TYPE_RELATIVE_LENGTH: *psrPowerType = "relative_length"; break;
+      case TLO_POWER_TYPE_ABSOLUTE_LENGTH: *psrPowerType = "absoute_length";  break;
+      case TLO_POWER_TYPE_ABSOLUTE_SIZE:   *psrPowerType = "absolute_size";   break; 
+      default:                             *psrPowerType = "?";               break;
       }
    }
 }
