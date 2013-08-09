33614dbb5a5bf350e098568a4468803cb4e5b6e4
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-08-09 11:44:21 +0000

    Updated to version 1.809.0
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@3280 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- toro/TIO_InputOutputHandlers/TIO_StringText.h
+++ toro/TIO_InputOutputHandlers/TIO_StringText.h
@@ -27,7 +27,7 @@
 //---------------------------------------------------------------------------//
 
 #define TIO_SZ_PROGRAM_MAJOR "1"
-#define TIO_SZ_PROGRAM_MINOR "802"
+#define TIO_SZ_PROGRAM_MINOR "809"
 #define TIO_SZ_PROGRAM_PATCH "0"
 
 #if defined( SUN8 )
