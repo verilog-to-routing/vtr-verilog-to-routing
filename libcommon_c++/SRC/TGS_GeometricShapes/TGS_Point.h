02b86629603b099aa223749ef40459bf2d5598f5
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-08-15 21:18:53 +0000

    Updated to version 1.815.0
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@3948 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- libcommon_c++/SRC/TGS_GeometricShapes/TGS_Point.h
+++ libcommon_c++/SRC/TGS_GeometricShapes/TGS_Point.h
@@ -4,25 +4,29 @@
 //
 //           Inline methods include:
 //           - GetDx, GetDy, GetDz
-//           - IsLeft, IsRight, IsLower, IsUpper
-//           - IsLowerLeft, IsLowerRight, IsUpperLeft, IsUpperRight
 //
 //===========================================================================//
 
 //---------------------------------------------------------------------------//
-// Copyright (C) 2012 Jeff Rudolph, Texas Instruments (jrudolph@ti.com)      //
+// Copyright (C) 2012-2013 Jeff Rudolph, Texas Instruments (jrudolph@ti.com) //
 //                                                                           //
-// This program is free software; you can redistribute it and/or modify it   //
-// under the terms of the GNU General Public License as published by the     //
-// Free Software Foundation; version 3 of the License, or any later version. //
+// Permission is hereby granted, free of charge, to any person obtaining a   //
+// copy of this software and associated documentation files (the "Software"),//
+// to deal in the Software without restriction, including without limitation //
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,  //
+// and/or sell copies of the Software, and to permit persons to whom the     //
+// Software is furnished to do so, subject to the following conditions:      //
 //                                                                           //
-// This program is distributed in the hope that it will be useful, but       //
-// WITHOUT ANY WARRANTY; without even an implied warranty of MERCHANTABILITY //
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License   //
-// for more details.                                                         //
+// The above copyright notice and this permission notice shall be included   //
+// in all copies or substantial portions of the Software.                    //
 //                                                                           //
-// You should have received a copy of the GNU General Public License along   //
-// with this program; if not, see <http://www.gnu.org/licenses>.             //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS   //
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                //
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN // 
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,  //
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR     //
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE //
+// USE OR OTHER DEALINGS IN THE SOFTWARE.                                    //
 //---------------------------------------------------------------------------//
 
 #ifndef TGS_POINT_H
@@ -136,70 +140,6 @@ inline int TGS_Point_c::GetDz(
       const TGS_Point_c& point ) const
 {
    return( abs( this->z - point.z ));
-}
-
-//===========================================================================//
-inline bool TGS_Point_c::IsLeft( 
-      const TGS_Point_c& point ) const
-{
-   return( TCTF_IsLT( this->x, point.x ) && TCTF_IsEQ( this->y, point.y ) ?
-           true : false );
-}
-
-//===========================================================================//
-inline bool TGS_Point_c::IsRight( 
-      const TGS_Point_c& point ) const
-{
-   return( TCTF_IsGT( this->x, point.x ) && TCTF_IsEQ( this->y, point.y ) ?
-           true : false );
-}
-
-//===========================================================================//
-inline bool TGS_Point_c::IsLower( 
-      const TGS_Point_c& point ) const
-{
-   return( TCTF_IsEQ( this->x, point.x ) && TCTF_IsLT( this->y, point.y ) ?
-           true : false );
-}
-
-//===========================================================================//
-inline bool TGS_Point_c::IsUpper( 
-      const TGS_Point_c& point ) const
-{
-   return( TCTF_IsEQ( this->x, point.x ) && TCTF_IsGT( this->y, point.y ) ?
-           true : false );
-}
-
-//===========================================================================//
-inline bool TGS_Point_c::IsLowerLeft( 
-      const TGS_Point_c& point ) const
-{
-   return( TCTF_IsLT( this->x, point.x ) && TCTF_IsLT( this->y, point.y ) ?
-           true : false );
-}
-
-//===========================================================================//
-inline bool TGS_Point_c::IsLowerRight( 
-      const TGS_Point_c& point ) const
-{
-   return( TCTF_IsGT( this->x, point.x ) && TCTF_IsLT( this->y, point.y ) ?
-           true : false );
-}
-
-//===========================================================================//
-inline bool TGS_Point_c::IsUpperLeft( 
-      const TGS_Point_c& point ) const
-{
-   return( TCTF_IsLT( this->x, point.x ) && TCTF_IsGT( this->y, point.y ) ?
-           true : false );
-}
-
-//===========================================================================//
-inline bool TGS_Point_c::IsUpperRight( 
-      const TGS_Point_c& point ) const
-{
-   return( TCTF_IsGT( this->x, point.x ) && TCTF_IsGT( this->y, point.y ) ?
-           true : false );
 }
 
 #endif
