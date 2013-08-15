408cac49e1e2d5ff7b483c2edebf6bd68e7be228
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-08-15 21:21:00 +0000

    Updated to version 1.815.0
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@3989 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- libcommon_c++/SRC/TNO_NetObjects/TNO_StringUtils.cxx
+++ libcommon_c++/SRC/TNO_NetObjects/TNO_StringUtils.cxx
@@ -4,25 +4,29 @@
 //           Functions include:
 //           - TNO_ExtractStringStatusMode
 //           - TNO_ExtractStringNodeType
-//           - TNO_FormatNameIndex
-//           - TNO_ParseNameIndex
 //
 //===========================================================================//
 
 //---------------------------------------------------------------------------//
 // Copyright (C) 2012-2013 Jeff Rudolph, Texas Instruments (jrudolph@ti.com) //
 //                                                                           //
-// This program is free software; you can redistribute it and/or modify it   //
-// under the terms of the GNU General Public License as published by the     //
-// Free Software Foundation; version 3 of the License, or any later version. //
+// Permission is hereby granted, free of charge, to any person obtaining a   //
+// copy of this software and associated documentation files (the "Software"),//
+// to deal in the Software without restriction, including without limitation //
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,  //
+// and/or sell copies of the Software, and to permit persons to whom the     //
+// Software is furnished to do so, subject to the following conditions:      //
 //                                                                           //
-// This program is distributed in the hope that it will be useful, but       //
-// WITHOUT ANY WARRANTY; without even an implied warranty of MERCHANTABILITY //
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License   //
-// for more details.                                                         //
+// The above copyright notice and this permission notice shall be included   //
+// in all copies or substantial portions of the Software.                    //
 //                                                                           //
-// You should have received a copy of the GNU General Public License along   //
-// with this program; if not, see <http://www.gnu.org/licenses>.             //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS   //
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                //
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN // 
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,  //
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR     //
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE //
+// USE OR OTHER DEALINGS IN THE SOFTWARE.                                    //
 //---------------------------------------------------------------------------//
 
 #include "TNO_StringUtils.h"
@@ -77,92 +81,3 @@ void TNO_ExtractStringNodeType(
       }
    }
 } 
-
-//===========================================================================//
-// Function       : TNO_FormatNameIndex
-// Author         : Jeff Rudolph
-//---------------------------------------------------------------------------//
-// Version history
-// 02/21/12 jeffr : Original
-//===========================================================================//
-void TNO_FormatNameIndex(
-      const char*   pszName,
-            size_t  index,
-            string* psrNameIndex ) 
-{
-   if( psrNameIndex )
-   {  
-      *psrNameIndex = "";
-
-      if( pszName )
-      {
-         *psrNameIndex += pszName;
-      }
-      if( index != SIZE_MAX )
-      {
-         char szIndex[TIO_FORMAT_STRING_LEN_VALUE];
-         sprintf( szIndex, "%lu", static_cast< unsigned long >( index ));
-         *psrNameIndex += "[";
-         *psrNameIndex += szIndex;
-         *psrNameIndex += "]";
-      }
-   }
-}
-
-//===========================================================================//
-void TNO_FormatNameIndex(
-      const string& srName,
-            size_t  index,
-            string* psrNameIndex )
-{
-   const char* pszName = ( srName.length( ) ? srName.data( ) : 0 );
-   TNO_FormatNameIndex( pszName, index, psrNameIndex );
-}
-
-//===========================================================================//
-// Function       : TNO_ParseNameIndex
-// Author         : Jeff Rudolph
-//---------------------------------------------------------------------------//
-// Version history
-// 02/21/12 jeffr : Original
-//===========================================================================//
-void TNO_ParseNameIndex(
-      const char*   pszNameIndex,
-            string* psrName,
-            size_t* pindex )
-{
-   string srName( "" );
-   size_t index = SIZE_MAX;
-
-   if( pszNameIndex )
-   {
-      srName = pszNameIndex;
-      size_t left = srName.find( '[' );
-      size_t right = srName.find( ']' );
-      if(( left != string::npos ) && ( right != string::npos ))
-      {
-         string srIndex( srName.substr( left + 1, right - left - 1 ));
-         index = atoi( srIndex.data( ));
-         srName = srName.substr( 0, left );
-      }
-   }
-
-   if( psrName )
-   {
-      *psrName = srName;
-   }
-   if( pindex )
-   {
-      *pindex = index;
-   }
-}
-
-//===========================================================================//
-void TNO_ParseNameIndex(
-      const string& srNameIndex,
-            string* psrName,
-            size_t* pindex )
-{
-   const char* pszNameIndex = ( srNameIndex.length( ) ? srNameIndex.data( ) : 0 );
-   TNO_ParseNameIndex( pszNameIndex, psrName, pindex );
-}
