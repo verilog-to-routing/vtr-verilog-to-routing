75f032d7aa07e9e51d81d60e4c5bcd6f479e3802
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-08-15 21:16:52 +0000

    Updated to version 1.815.0
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@3909 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- libcommon_c++/SRC/TFH_FabricHandlers/TFH_FabricConnectionBlockHandler.cxx
+++ libcommon_c++/SRC/TFH_FabricHandlers/TFH_FabricConnectionBlockHandler.cxx
@@ -35,17 +35,23 @@
 //---------------------------------------------------------------------------//
 // Copyright (C) 2013 Jeff Rudolph, Texas Instruments (jrudolph@ti.com)      //
 //                                                                           //
-// This program is free software; you can redistribute it and/or modify it   //
-// under the terms of the GNU General Public License as published by the     //
-// Free Software Foundation; version 3 of the License, or any later version. //
+// Permission is hereby granted, free of charge, to any person obtaining a   //
+// copy of this software and associated documentation files (the "Software"),//
+// to deal in the Software without restriction, including without limitation //
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,  //
+// and/or sell copies of the Software, and to permit persons to whom the     //
+// Software is furnished to do so, subject to the following conditions:      //
 //                                                                           //
-// This program is distributed in the hope that it will be useful, but       //
-// WITHOUT ANY WARRANTY; without even an implied warranty of MERCHANTABILITY //
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License   //
-// for more details.                                                         //
+// The above copyright notice and this permission notice shall be included   //
+// in all copies or substantial portions of the Software.                    //
 //                                                                           //
-// You should have received a copy of the GNU General Public License along   //
-// with this program; if not, see <http://www.gnu.org/licenses>.             //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS   //
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                //
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN // 
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,  //
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR     //
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE //
+// USE OR OTHER DEALINGS IN THE SOFTWARE.                                    //
 //---------------------------------------------------------------------------//
 
 #include "TC_MemoryUtils.h"
@@ -367,7 +373,7 @@ void TFH_FabricConnectionBlockHandler_c::LoadConnectionMapList_(
       this->FindChannelOrigin_( vpr_blockOrigin, pinSide, 
                                 &vpr_channelOrigin, &channelOrient );
 
-      for( size_t j = 0; j < connectionPattern.GetLength(); ++j ) 
+      for( size_t j = 0; j < connectionPattern.GetLength( ); ++j ) 
       {
          const TC_Bit_c& connectionBit = *connectionPattern[j];
          if( !connectionBit.IsTrue( ))
@@ -472,6 +478,8 @@ bool TFH_FabricConnectionBlockHandler_c::OverrideGraphTracksToInputPins_(
 //---------------------------------------------------------------------------//
 // Version history
 // 04/24/13 jeffr : Original
+// 08/01/13 jeffr : Modified connection pattern list iteration, changing from
+//                  0..len to len..0, to make fabric output match fabric file
 //===========================================================================//
 bool TFH_FabricConnectionBlockHandler_c::ConnectGraphOutputPins_(
             void*                      vpr_rrNodeArray,
@@ -508,13 +516,13 @@ bool TFH_FabricConnectionBlockHandler_c::ConnectGraphOutputPins_(
                                    &vpr_channelOrigin, &channelOrient );
 
          const TFH_BitPattern_t& connectionPattern = pconnectionBlock->GetConnectionPattern( );
-         for( size_t j = 0; j < connectionPattern.GetLength(); ++j ) 
+         for( size_t j = connectionPattern.GetLength( ); j > 0; --j ) 
          {
-            const TC_Bit_c& connectionBit = *connectionPattern[j];
+            const TC_Bit_c& connectionBit = *connectionPattern[j-1];
             if( !connectionBit.IsTrue( ))
                continue;
 
-            int vpr_trackIndex = static_cast< int >( j );
+            int vpr_trackIndex = static_cast< int >( j-1 );
             TFH_ConnectionMap_c connectionMap( vpr_channelOrigin, channelOrient, vpr_trackIndex );
 
             size_t k = connectionMapList.FindIndex( connectionMap );
@@ -578,6 +586,8 @@ void TFH_FabricConnectionBlockHandler_c::DisconnectGraphOutputPins_(
 //---------------------------------------------------------------------------//
 // Version history
 // 04/24/13 jeffr : Original
+// 08/01/13 jeffr : Modified connection pattern list iteration, changing from
+//                  0..len to len..0, to make fabric output match fabric file
 //===========================================================================//
 bool TFH_FabricConnectionBlockHandler_c::ConnectGraphInputPins_(
             void*                      vpr_rrNodeArray,
@@ -614,13 +624,13 @@ bool TFH_FabricConnectionBlockHandler_c::ConnectGraphInputPins_(
                                    &vpr_channelOrigin, &channelOrient );
 
          const TFH_BitPattern_t& connectionPattern = pconnectionBlock->GetConnectionPattern( );
-         for( size_t j = 0; j < connectionPattern.GetLength(); ++j ) 
+         for( size_t j = connectionPattern.GetLength( ); j > 0; --j ) 
          {
-            const TC_Bit_c& connectionBit = *connectionPattern[j];
+            const TC_Bit_c& connectionBit = *connectionPattern[j-1];
             if( !connectionBit.IsTrue( ))
                continue;
 
-            int vpr_trackIndex = static_cast< int >( j );
+            int vpr_trackIndex = static_cast< int >( j-1 );
             TFH_ConnectionMap_c connectionMap( vpr_channelOrigin, channelOrient, vpr_trackIndex );
 
             size_t k = connectionMapList.FindIndex( connectionMap );
