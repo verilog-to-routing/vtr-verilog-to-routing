f968bbacf78453926a4d72da8c1e06e92f165926
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-08-15 21:18:56 +0000

    Updated to version 1.815.0
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@3949 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- libcommon_c++/SRC/TGS_GeometricShapes/TGS_Rect.cxx
+++ libcommon_c++/SRC/TGS_GeometricShapes/TGS_Rect.cxx
@@ -38,19 +38,25 @@
 //===========================================================================//
 
 //---------------------------------------------------------------------------//
-// Copyright (C) 2012 Jeff Rudolph, Texas Instruments (jrudolph@ti.com)      //
+// Copyright (C) 2012-2013 Jeff Rudolph, Texas Instruments (jrudolph@ti.com) //
 //                                                                           //
-// This program is free software; you can redistribute it and/or modify it   //
-// under the terms of the GNU General Public License as published by the     //
-// Free Software Foundation; version 3 of the License, or any later version. //
+// Permission is hereby granted, free of charge, to any person obtaining a   //
+// copy of this software and associated documentation files (the "Software"),//
+// to deal in the Software without restriction, including without limitation //
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,  //
+// and/or sell copies of the Software, and to permit persons to whom the     //
+// Software is furnished to do so, subject to the following conditions:      //
 //                                                                           //
-// This program is distributed in the hope that it will be useful, but       //
-// WITHOUT ANY WARRANTY; without even an implied warranty of MERCHANTABILITY //
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License   //
-// for more details.                                                         //
+// The above copyright notice and this permission notice shall be included   //
+// in all copies or substantial portions of the Software.                    //
 //                                                                           //
-// You should have received a copy of the GNU General Public License along   //
-// with this program; if not, see <http://www.gnu.org/licenses>.             //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS   //
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                //
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN // 
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,  //
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR     //
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE //
+// USE OR OTHER DEALINGS IN THE SOFTWARE.                                    //
 //---------------------------------------------------------------------------//
 
 #if defined( SUN8 ) || defined( SUN10 )
@@ -483,28 +489,28 @@ void TGS_Rect_c::FindRect(
       { 
       case TC_SIDE_PREV:
          if( this->IsWide( ) && !this->IsTall( ))
-	 {
-	    side = TC_SIDE_LEFT;
-	 }
+         {
+            side = TC_SIDE_LEFT;
+         }
          if( this->IsTall( ) && !this->IsWide( ))
-	 {
-	    side = TC_SIDE_LOWER;
-	 }
-	 break;
+         {
+            side = TC_SIDE_LOWER;
+         }
+         break;
 
       case TC_SIDE_NEXT:
          if( this->IsWide( ) && !this->IsTall( ))
-	 {
-	    side = TC_SIDE_RIGHT;
-	 }
+         {
+            side = TC_SIDE_RIGHT;
+         }
          if( this->IsTall( ) && !this->IsWide( ))
-	 {
-	    side = TC_SIDE_UPPER;
-	 }
-	 break;
+         {
+            side = TC_SIDE_UPPER;
+         }
+         break;
 
       default:
-	 break;
+         break;
       }
 
       switch( side )
@@ -537,7 +543,7 @@ void TGS_Rect_c::FindRect(
          break;
 
       default:
-	 break;
+         break;
       }
    }
 }
@@ -751,8 +757,8 @@ void TGS_Rect_c::ApplyScale(
    thisBox.ApplyScale( dx, dy, 0, snap );
 
    this->Set( thisBox.lowerLeft.z, 
-	      thisBox.lowerLeft.x, thisBox.lowerLeft.y,
-	      thisBox.upperRight.x, thisBox.upperRight.y );
+              thisBox.lowerLeft.x, thisBox.lowerLeft.y,
+              thisBox.upperRight.x, thisBox.upperRight.y );
 }
 
 //===========================================================================//
@@ -775,8 +781,8 @@ void TGS_Rect_c::ApplyScale(
    thisBox.ApplyScale( scale, orient, snap );
 
    this->Set( thisBox.lowerLeft.z, 
-	      thisBox.lowerLeft.x, thisBox.lowerLeft.y,
-	      thisBox.upperRight.x, thisBox.upperRight.y );
+              thisBox.lowerLeft.x, thisBox.lowerLeft.y,
+              thisBox.upperRight.x, thisBox.upperRight.y );
 }
 
 //===========================================================================//
@@ -791,8 +797,8 @@ void TGS_Rect_c::ApplyScale(
    thisBox.ApplyScale( scale, side, snap );
 
    this->Set( thisBox.lowerLeft.z, 
-	      thisBox.lowerLeft.x, thisBox.lowerLeft.y,
-	      thisBox.upperRight.x, thisBox.upperRight.y );
+              thisBox.lowerLeft.x, thisBox.lowerLeft.y,
+              thisBox.upperRight.x, thisBox.upperRight.y );
 }
 
 //===========================================================================//
@@ -817,8 +823,8 @@ void TGS_Rect_c::ApplyUnion(
    thisBox.ApplyUnion( rectBoxA, rectBoxB );
 
    this->Set( thisBox.lowerLeft.z, 
-	      thisBox.lowerLeft.x, thisBox.lowerLeft.y,
-	      thisBox.upperRight.x, thisBox.upperRight.y );
+              thisBox.lowerLeft.x, thisBox.lowerLeft.y,
+              thisBox.upperRight.x, thisBox.upperRight.y );
 }
 
 //===========================================================================//
@@ -850,8 +856,8 @@ void TGS_Rect_c::ApplyIntersect(
    thisBox.ApplyIntersect( rectBoxA, rectBoxB );
 
    this->Set( thisBox.lowerLeft.z, 
-	      thisBox.lowerLeft.x, thisBox.lowerLeft.y,
-	      thisBox.upperRight.x, thisBox.upperRight.y );
+              thisBox.lowerLeft.x, thisBox.lowerLeft.y,
+              thisBox.upperRight.x, thisBox.upperRight.y );
 }
 
 //===========================================================================//
@@ -882,8 +888,8 @@ void TGS_Rect_c::ApplyOverlap(
    thisBox.ApplyOverlap( rectBoxA, rectBoxB );
 
    this->Set( thisBox.lowerLeft.z, 
-	      thisBox.lowerLeft.x, thisBox.lowerLeft.y,
-	      thisBox.upperRight.x, thisBox.upperRight.y );
+              thisBox.lowerLeft.x, thisBox.lowerLeft.y,
+              thisBox.upperRight.x, thisBox.upperRight.y );
 }
 
 //===========================================================================//
@@ -1027,7 +1033,7 @@ bool TGS_Rect_c::IsAdjacent(
 {
    bool isAdjacent = (( rect.layer == this->layer ) &&
                       ( rect.region.IsAdjacent( this->region )) ?
-		      true : false );
+                      true : false );
    return( isAdjacent );
 }
 
@@ -1047,7 +1053,7 @@ bool TGS_Rect_c::IsIntersecting(
 {
    bool isIntersecting = (( rect.layer == this->layer ) &&
                           ( rect.region.IsIntersecting( this->region )) ?
-	    	          true : false );
+                          true : false );
    return( isIntersecting );
 }
 
