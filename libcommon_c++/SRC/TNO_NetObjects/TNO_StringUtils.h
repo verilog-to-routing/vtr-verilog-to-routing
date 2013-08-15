c244cc9e54a4f87ec49f79f2cbf40e1ad779aa8d
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-08-15 21:21:04 +0000

    Updated to version 1.815.0
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@3990 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- libcommon_c++/SRC/TNO_NetObjects/TNO_StringUtils.h
+++ libcommon_c++/SRC/TNO_NetObjects/TNO_StringUtils.h
@@ -6,17 +6,23 @@
 //---------------------------------------------------------------------------//
 // Copyright (C) 2012-2013 Jeff Rudolph, Texas Instruments (jrudolph@ti.com) //
 //                                                                           //
-// This program is free software; you can redistribute it and/or modify it   //
-// under the terms of the GNU General Public License as published by the     //
-// Free Software Foundation; version 3 of the License, or any later version. //
+// Permission is hereby granted, free of charge, to any person obtaining a   //
+// copy of this software and associated documentation files (the "Software"),//
+// to deal in the Software without restriction, including without limitation //
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,  //
+// and/or sell copies of the Software, and to permit persons to whom the     //
+// Software is furnished to do so, subject to the following conditions:      //
 //                                                                           //
-// This program is distributed in the hope that it will be useful, but       //
-// WITHOUT ANY WARRANTY; without even an implied warranty of MERCHANTABILITY //
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License   //
-// for more details.                                                         //
+// The above copyright notice and this permission notice shall be included   //
+// in all copies or substantial portions of the Software.                    //
 //                                                                           //
-// You should have received a copy of the GNU General Public License along   //
-// with this program; if not, see <http://www.gnu.org/licenses>.             //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS   //
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                //
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN // 
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,  //
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR     //
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE //
+// USE OR OTHER DEALINGS IN THE SOFTWARE.                                    //
 //---------------------------------------------------------------------------//
 
 #ifndef TNO_STRING_UTILS_H
@@ -37,17 +43,4 @@ using namespace std;
 void TNO_ExtractStringStatusMode( TNO_StatusMode_t mode, string* psrMode );
 void TNO_ExtractStringNodeType( TNO_NodeType_t type, string* psrType );
 
-void TNO_FormatNameIndex( const char* pszName, 
-                          size_t index,
-                          string* psrNameIndex );
-void TNO_FormatNameIndex( const string& srName,
-                          size_t index,
-                          string* psrNameIndex );
-
-void TNO_ParseNameIndex( const char* pszNameIndex,
-                         string* psrName,
-                         size_t* pindex );
-void TNO_ParseNameIndex( const string& srNameIndex,
-                         string* psrName,
-                         size_t* pindex );
 #endif 
