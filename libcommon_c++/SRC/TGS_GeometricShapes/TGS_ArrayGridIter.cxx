8a386dbc778a3f91055b5b330a6a8e392b25f3c1
Jeff Rudolph <jrudolph.at.work@gmail.com>
2013-08-15 21:18:26 +0000

    Updated to version 1.815.0
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@3939 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- libcommon_c++/SRC/TGS_GeometricShapes/TGS_ArrayGridIter.cxx
+++ libcommon_c++/SRC/TGS_GeometricShapes/TGS_ArrayGridIter.cxx
@@ -10,19 +10,25 @@
 //===========================================================================//
 
 //---------------------------------------------------------------------------//
-// Copyright (C) 2012 Jeff Rudolph, Texas Instruments (jrudolph@ti.com)      //
+// Copyright (C) 2012-2013 Jeff Rudolph, Texas Instruments (jrudolph@ti.com) //
 //                                                                           //
-// This program is free software; you can redistribute it and/or modify it   //
-// under the terms of the GNU General Public License as published by the     //
-// Free Software Foundation; version 3 of the License, or any later version. //
+// Permission is hereby granted, free of charge, to any person obtaining a   //
+// copy of this software and associated documentation files (the "Software"),//
+// to deal in the Software without restriction, including without limitation //
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,  //
+// and/or sell copies of the Software, and to permit persons to whom the     //
+// Software is furnished to do so, subject to the following conditions:      //
 //                                                                           //
-// This program is distributed in the hope that it will be useful, but       //
-// WITHOUT ANY WARRANTY; without even an implied warranty of MERCHANTABILITY //
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License   //
-// for more details.                                                         //
+// The above copyright notice and this permission notice shall be included   //
+// in all copies or substantial portions of the Software.                    //
 //                                                                           //
-// You should have received a copy of the GNU General Public License along   //
-// with this program; if not, see <http://www.gnu.org/licenses>.             //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS   //
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                //
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN // 
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,  //
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR     //
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE //
+// USE OR OTHER DEALINGS IN THE SOFTWARE.                                    //
 //---------------------------------------------------------------------------//
 
 #include "TGS_ArrayGridIter.h"
@@ -136,23 +142,23 @@ bool TGS_ArrayGridIter_c::Next(
       if( TCTF_IsLE( this->x_, this->arrayRegion_.x2 ) || 
           TCTF_IsLE( this->y_, this->arrayRegion_.y2 ))
       {
-	 this->x_ += this->pitch_.dx;
+         this->x_ += this->pitch_.dx;
 
          if( TCTF_IsGT( this->x_, this->arrayRegion_.x2 ))
-	 {
+         {
             this->x_ = this->arrayRegion_.x1;
             this->y_ += this->pitch_.dy;
 
             if( TCTF_IsGT( this->y_, this->arrayRegion_.y2 ))
-	    {
-	       this->x_ = TC_FLT_MAX;
-	       this->y_ = TC_FLT_MAX;
+            {
+               this->x_ = TC_FLT_MAX;
+               this->y_ = TC_FLT_MAX;
             }
          }
       }
       else
       {
-	 this->x_ = TC_FLT_MAX;
+         this->x_ = TC_FLT_MAX;
          this->y_ = TC_FLT_MAX;
       }
 
