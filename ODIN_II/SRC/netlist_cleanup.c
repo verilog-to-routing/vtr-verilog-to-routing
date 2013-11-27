459839d132a35fece8718a4cce9c2ef8acdd9ceb
Conor McCullough <conor.mccullough@gmail.com>
2013-11-27 04:18:07 +0000

    Fixed adder/subtractor chain reporting.
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@4199 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- ODIN_II/SRC/netlist_cleanup.c
+++ ODIN_II/SRC/netlist_cleanup.c
@@ -30,10 +30,32 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 
 /* Used in the nnode_t.node_data field to mark if the node was already visited
- * during a forward or backward sweep traversal */
-int _visited_forward, _visited_backward;
+ * during a forward or backward sweep traversal or the removal phase */
+int _visited_forward, _visited_backward, _visited_removal;
 #define VISITED_FORWARD ((void*)&_visited_forward)
 #define VISITED_BACKWARD ((void*)&_visited_backward)
+#define VISITED_REMOVAL ((void*)&_visited_removal)
+
+
+/* Simple linked list of nodes structure */
+typedef struct node_list_t_t{
+	nnode_t *node;
+	struct node_list_t_t *next;
+} node_list_t;
+
+node_list_t* insert_node_list(node_list_t* node_list, nnode_t *node){
+	node_list->node = node;
+	node_list->next = (node_list_t*)calloc(1, sizeof(node_list_t));
+	return node_list->next;
+}
+
+node_list_t useless_nodes; // List of the nodes to be removed
+node_list_t *removal_list_next = &useless_nodes; // Tail of the nodes to be removed
+
+node_list_t addsub_nodes; // List of the adder/subtractor nodes
+node_list_t *addsub_list_next = &addsub_nodes; // Tail of the adder/subtractor node list
+
+
 
 /* Traverse the netlist backwards, moving from outputs to inputs */
 void traverse_backward(nnode_t *node){
@@ -47,38 +69,16 @@ void traverse_backward(nnode_t *node){
 	}
 }
 
-/* Simple linked list to keep track of which nodes should be removed at the end */
-typedef struct removal_list_t_t{
-	nnode_t *node;
-	struct removal_list_t_t *next;
-} removal_list_t;
-
-removal_list_t useless_nodes; // List of the nodes to be removed
-removal_list_t *removal_list_next; // Tail of the nodes to be removed
-
-/* Since we are traversing the entire netlist anyway, we can use this
- * opportunity to collect statistics about hard adders/subtractors */
-int adder_chain_count =0;
-int longest_adder_chain =0;
-int total_adders = 0;
-
-int subtractor_chain_count = 0;
-int longest_subtractor_chain = 0;
-int total_subtractors = 0;
-
 /* Traverse the netlist forward, moving from inputs to outputs.
  * Adds nodes that do not affect any outputs to the useless_nodes list 
  * Arguments:
  * 	node: the current node in the netlist
  * 	toplevel: are we at one of the top-level nodes? (GND, VCC, PAD or INPUT)
  * 	remove_me: should the current node be removed?
- * 	prev_type: the type of the parent node
- *  depth: how deep in a chain this node is (for adders and subtractors) 
- * Returns: TRUE if the child will be removed, FALSE otherwise
  * */
-int traverse_forward(nnode_t *node, int toplevel, int remove_me, operation_list prev_type, int depth){
-	if(node == NULL) return FALSE; // Shouldn't happen, but check just in case
-	if(node->node_data == VISITED_FORWARD) return FALSE; // Already visited, shouldn't happen anyway
+void traverse_forward(nnode_t *node, int toplevel, int remove_me){
+	if(node == NULL) return; // Shouldn't happen, but check just in case
+	if(node->node_data == VISITED_FORWARD) return; // Already visited, shouldn't happen anyway
 	
 	/* We want to remove this node if either its parent was removed, 
 	 * or if it was not visited on the backwards sweep */
@@ -89,18 +89,15 @@ int traverse_forward(nnode_t *node, int toplevel, int remove_me, operation_list 
 	
 	if(remove_me) {
 		/* Add this node to the list of nodes to remove */
-		removal_list_next->node = node;
-		removal_list_next->next = (removal_list_t*)calloc(1, sizeof(removal_list_t));
-		removal_list_next = removal_list_next->next;
+		removal_list_next = insert_node_list(removal_list_next, node);
 	}
 	
-	/* If this node has the same type as its parent, we're still in a potential chain,
-	 * so increase the depth */
-	if(node->type == prev_type) depth += 1;
-	else depth = 0;
-	
-	/* TRUE if this node has a child with the same type */
-	int has_similar_child = 0;
+	if(node->type == ADD || node->type == MINUS){
+		/* Check if we've found the head of an adder or subtractor chain */
+		if(node->input_pins[node->num_input_pins-1]->net->driver_pin->node->type == PAD_NODE){
+			addsub_list_next = insert_node_list(addsub_list_next, node);
+		}
+	}
 	
 	/* Iterate through every fanout node */
 	int i, j;
@@ -110,50 +107,17 @@ int traverse_forward(nnode_t *node, int toplevel, int remove_me, operation_list 
 				if(node->output_pins[i]->net->fanout_pins[j]){
 					nnode_t *child = node->output_pins[i]->net->fanout_pins[j]->node;
 					if(child){
-						/* Ensure we visit the head of an adder or subtractor chain first,
-						 * before any successive nodes. The carry in (last input pin) of a
-						 * hard adder/subtractor will be  unconnected (i.e. PAD_NODE) if 
-						 * and only if it is the head of a chain. */ 
-						if(node->type != ADD && child->type == ADD && child->input_pins[child->num_input_pins-1]->net->driver_pin->node->type != PAD_NODE){
-							continue;
-						}
-						if(node->type != MINUS && child->type == MINUS && child->input_pins[child->num_input_pins-1]->net->driver_pin->node->type != PAD_NODE){
-							continue;
-						}
-						
 						/* If this child hasn't already been visited, visit it now */
 						if(child->node_data != VISITED_FORWARD){
-							int child_used = !traverse_forward(child, FALSE, remove_me, node->type, depth);
-							if(child->type == node->type && child_used) {
-								has_similar_child = 1;
-							}
+							traverse_forward(child, FALSE, remove_me);
 						}
 					}
 				}
 			}
 		}
 	}
-	
-	if(!remove_me){
-		/* Update the adder and subtractor statistics */
-		
-		/* If the current node is an adder but has no children that are 
-		 * adders, then it must be the tail of a chain */
-		if(node->type == ADD && !has_similar_child){
-			adder_chain_count++;
-			if(depth > longest_adder_chain) longest_adder_chain = depth + 1;
-			total_adders += depth + 1;
-		}
-	
-		if(node->type == MINUS && !has_similar_child){
-			subtractor_chain_count++;
-			if(depth > longest_subtractor_chain) longest_subtractor_chain = depth + 1;
-			total_subtractors += depth + 1;
-		}
-	}
-	
-	return remove_me;
 }
+
 
 /* Start at each of the top level output nodes and traverse backwards to the inputs
 to determine which nodes have an effect on the outputs */
@@ -167,29 +131,77 @@ void mark_output_dependencies(netlist_t *netlist){
 /* Traversed the netlist forward from the top level inputs and special nodes 
 (VCC, GND, PAD) */
 void identify_unused_nodes(netlist_t *netlist){
+
 	useless_nodes.node = NULL;
 	useless_nodes.next = NULL;
-	removal_list_next = &useless_nodes;
 	
-	traverse_forward(netlist->gnd_node, TRUE, FALSE, GND_NODE, 0);
-	traverse_forward(netlist->vcc_node, TRUE, FALSE, VCC_NODE, 0);
-	traverse_forward(netlist->pad_node, TRUE, FALSE, PAD_NODE, 0);
+	addsub_nodes.node = NULL;
+	addsub_nodes.next = NULL;
+		
+	traverse_forward(netlist->gnd_node, TRUE, FALSE);
+	traverse_forward(netlist->vcc_node, TRUE, FALSE);
+	traverse_forward(netlist->pad_node, TRUE, FALSE);
 	int i;
 	for(i = 0; i < netlist->num_top_input_nodes; i++){
-		traverse_forward(netlist->top_input_nodes[i], TRUE, FALSE, INPUT_NODE, 0);
+		traverse_forward(netlist->top_input_nodes[i], TRUE, FALSE);
 	}
 }
 
 /* Note: This does not actually free the unused logic, but simply detaches
 it from the rest of the circuit */
-void remove_unused_nodes(removal_list_t *remove){
+void remove_unused_nodes(node_list_t *remove){
 	while(remove != NULL && remove->node != NULL){
-		int i;
+		int i;		
 		for(i = 0; i < remove->node->num_input_pins; i++){
 			npin_t *input_pin = remove->node->input_pins[i];
 			input_pin->net->fanout_pins[input_pin->pin_net_idx] = NULL; // Remove the fanout pin from the net
 		}
+		remove->node->node_data = VISITED_REMOVAL;
 		remove = remove->next;
+	}
+}
+
+/* Since we are traversing the entire netlist anyway, we can use this
+ * opportunity to keep track of the heads of adder/subtractors chains
+ * and then compute statistics on them */
+int adder_chain_count = 0;
+int longest_adder_chain = 0;
+int total_adders = 0;
+
+int subtractor_chain_count = 0;
+int longest_subtractor_chain = 0;
+int total_subtractors = 0;
+
+void calculate_addsub_statistics(node_list_t *addsub){
+	while(addsub != NULL && addsub->node != NULL){
+		int found_tail = FALSE;
+		nnode_t *node = addsub->node;
+		int chain_depth = 0;
+		while(!found_tail){
+			if(node->node_data == VISITED_REMOVAL){
+				found_tail = TRUE;
+				break;
+			}
+			chain_depth += 1;
+			
+			/* Carry out is always output pin 0 */
+			nnet_t *carry_out_net = node->output_pins[0]->net;
+			if(carry_out_net == NULL || carry_out_net->fanout_pins[0] == NULL) found_tail = TRUE;
+			else node = carry_out_net->fanout_pins[0]->node;
+		}
+		if(chain_depth > 0){
+			if(node->type == ADD){
+				adder_chain_count += 1;
+				total_adders += chain_depth;
+				if(chain_depth > longest_adder_chain) longest_adder_chain = chain_depth;
+			}
+			else if(node->type == MINUS){
+				subtractor_chain_count += 1;
+				total_subtractors += chain_depth;
+				if(chain_depth > longest_subtractor_chain) longest_subtractor_chain = chain_depth;
+			}
+		}
+		addsub = addsub->next;
 	}
 }
 
@@ -198,4 +210,5 @@ void remove_unused_logic(netlist_t *netlist){
 	mark_output_dependencies(netlist);
 	identify_unused_nodes(netlist);
 	remove_unused_nodes(&useless_nodes);
+	calculate_addsub_statistics(&addsub_nodes);
 }
