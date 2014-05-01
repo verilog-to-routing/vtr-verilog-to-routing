8deba00b594e76f13cf3cf1553108321aeb4b621
Kevin Murray <k.murray@mail.utoronto.ca>
2014-05-01 16:54:08 +0000

    Add extern prototype for yyparse(), since some versions of bision do not define its prototype.

--- EXE/SRC/include/libvqm/vqm_dll.cpp
+++ EXE/SRC/include/libvqm/vqm_dll.cpp
@@ -19,6 +19,11 @@
 #include "vqm_parser.tab.h"
 #include "vqm_common.h"
 
+//Some versions of bison (e.g. v2.5) to do not prototype yyparse()
+//in the parser.tab.h, while others do (e.g. v3.0.2)
+extern int yyparse (t_parse_info* parse_info);
+
+
 /*Bool APIENTRY DllMain( HANDLE hModule, 
                        DWORD  ul_reason_for_call, 
                        LPVOID lpReserved
