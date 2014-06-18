505788f7b103b46ea6021dfc78321332e3e5eedd
Javeed Shaikh <javeed@syscrash.ca>
2014-06-18 03:36:05 +0000

    Adding support for (1) running vpr in stages and (2) a flow that doesn't pass region constraints to the placer
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/branches/javeed@4346 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- vpr/vpr_jvds.py
+++ vpr/vpr_jvds.py
@@ -26,9 +26,27 @@ CLB_HYPERGRAPH_FILE='clb_hypergraph.txt'
 CLB_BLOCKS_FILE='clb_blocks.txt'
 
 PLACEMENT_CONSTRAINTS_ONLY=False
+PASS_CONSTRAINTS_TO_PLACER=False
+
+def read_stage():
+    stage = 0
+    try:
+        with open("stage.txt") as f:
+            for line in f:
+                line = line.strip()
+                if len(line) > 0:
+                    stage = int(line)
+    except IOError:
+        pass
+    return stage
+
+def write_stage(stage):
+    with open("stage.txt", "w") as f:
+        f.write("%d\n" % stage)
 
 def main():
     parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--staged', dest='staged', action='store_true', default=False)
     parser.add_argument('--ub-factor', type=int, default=3)
     parser.add_argument('--graph-model', choices=['clique', 'star', 'none'], default='clique')
     parser.add_argument('--graph-edge-weight', choices=['1/f', '1/f2', '1'], default='1/f')
@@ -57,67 +75,83 @@ def main():
         vpr_args_list.pop(seed_idx)
     vpr_pack_args = " ".join(vpr_args_list)
 
+    if args.staged:
+        stage = read_stage()
+        print "read stage %s" % stage
+    else:
+        stage = -1
+
     # run VPR --pack
     vpr_cmd = "%s --pack --net_file packed.net %s" % (VPR_BIN, vpr_pack_args)
-    log = open(PACK_LOG_FILE, 'a')
-    log.write(vpr_cmd + "\n")
-    log.close()
-    status = os.system(vpr_cmd)
-    if status != 0:
-        sys.exit(status)
-
-    if args.graph_model != "none":
-        if PLACEMENT_CONSTRAINTS_ONLY:
-            input_hypergraph = CLB_HYPERGRAPH_FILE
-            input_blocks = CLB_BLOCKS_FILE
-        else:
-            input_hypergraph = LOGICAL_HYPERGRAPH_FILE
-            input_blocks = LOGICAL_BLOCKS_FILE
-        # convert hypergraph to graph
-        hypergraph_to_graph.run(blocks_file=input_blocks, input_hypergraph=input_hypergraph, output_graph='justgraph.txt', graph_model=args.graph_model, graph_edge_weight=args.graph_edge_weight)
-
-        # run metis
-        status = os.system("%s -ufactor=%d justgraph.txt 2" % (METIS_BIN, args.ub_factor * 10))
+    if stage in [-1, 0]:
+        log = open(PACK_LOG_FILE, 'a')
+        log.write(vpr_cmd + "\n")
+        log.close()
+        status = os.system(vpr_cmd)
         if status != 0:
             sys.exit(status)
 
-        # get the hyperedge cut
-        hec = compute_hyperedge_cut.run(input_hypergraph, 'justgraph.txt.part.2')
-        with open('hyperedge_cut.txt', 'w') as f:
-            f.write("%d\n" % hec)
+    if stage in [-1, 1]:
+        if args.graph_model != "none":
+            if PLACEMENT_CONSTRAINTS_ONLY:
+                input_hypergraph = CLB_HYPERGRAPH_FILE
+                input_blocks = CLB_BLOCKS_FILE
+            else:
+                input_hypergraph = LOGICAL_HYPERGRAPH_FILE
+                input_blocks = LOGICAL_BLOCKS_FILE
+            # convert hypergraph to graph
+            hypergraph_to_graph.run(blocks_file=input_blocks, input_hypergraph=input_hypergraph, output_graph='justgraph.txt', graph_model=args.graph_model, graph_edge_weight=args.graph_edge_weight)
 
-        # get nx and ny
-        nx = -1
-        ny = -1
-        with open('vpr_stdout.log') as f:
-            for line in f:
-                if "mapped into" in line:
-                    parts = line.split()
-                    nx = int(parts[7])
-                    ny = int(parts[9])
-                    break
-        assert(nx != -1)
-        assert(ny != -1)
+            # run metis
+            status = os.system("%s -ufactor=%d justgraph.txt 2" % (METIS_BIN, args.ub_factor * 10))
+            if status != 0:
+                sys.exit(status)
 
-        if PLACEMENT_CONSTRAINTS_ONLY:
-            # update net file with regions
-            update_net_file.run(input_net_file='packed.net', output_net_file='packed_partitioned.net', partitions_file='justgraph.txt.part.2', total_partitions=2, nx=nx, ny=ny, blocks=input_blocks)
+            # get the hyperedge cut
+            hec = compute_hyperedge_cut.run(input_hypergraph, 'justgraph.txt.part.2')
+            with open('hyperedge_cut.txt', 'w') as f:
+                f.write("%d\n" % hec)
+
+            # get nx and ny
+            nx = -1
+            ny = -1
+            with open('vpr_stdout.log') as f:
+                for line in f:
+                    if "mapped into" in line:
+                        parts = line.split()
+                        nx = int(parts[7])
+                        ny = int(parts[9])
+                        break
+            assert(nx != -1)
+            assert(ny != -1)
+
+            if PLACEMENT_CONSTRAINTS_ONLY:
+                # update net file with regions
+                update_net_file.run(input_net_file='packed.net', output_net_file='packed_partitioned.net', partitions_file='justgraph.txt.part.2', total_partitions=2, nx=nx, ny=ny, blocks=input_blocks)
+            else:
+                generate_logical_block_placement_constraints.run(output_partitions='placement_regions.txt', partitions_file='justgraph.txt.part.2', total_partitions=2, nx=nx,ny=nx)
+                # run the damn thing again, hoping it uses the new placement regions file
+                status = os.system(vpr_cmd)
+                if PASS_CONSTRAINTS_TO_PLACER:
+                    shutil.copyfile('packed.net', 'packed_partitioned.net')
+                else:
+                    update_net_file.remove_regions('packed.net', 'packed_partitioned.net')
         else:
-            generate_logical_block_placement_constraints.run(output_partitions='placement_regions.txt', partitions_file='justgraph.txt.part.2', total_partitions=2, nx=nx,ny=nx)
-            # run the damn thing again, hoping it uses the new placement regions file
-            status = os.system(vpr_cmd)
             shutil.copyfile('packed.net', 'packed_partitioned.net')
-    else:
-        shutil.copyfile('packed.net', 'packed_partitioned.net')
 
-    status = os.system("%s --place --route --net_file packed_partitioned.net %s" % (VPR_BIN, vpr_args))
-    if status != 0:
-        sys.exit(status)
+    if stage in [-1, 2]:
+        status = os.system("%s --place --route --net_file packed_partitioned.net %s" % (VPR_BIN, vpr_args))
+        if status != 0:
+            sys.exit(status)
 
-    # get the placement compliance
-    place_file = glob.glob('*.place')[0]
-    count_good, count_bad = compute_placement_compliance.run(place_file, 'packed_partitioned.net')
-    with open('placement_compliance.txt', 'w') as f:
-        f.write("%f\n" % (float(count_good)/float(count_good+count_bad)))
+        # get the placement compliance
+        place_file = glob.glob('*.place')[0]
+        count_good, count_bad = compute_placement_compliance.run(place_file, 'packed_partitioned.net')
+        with open('placement_compliance.txt', 'w') as f:
+            f.write("%f\n" % (float(count_good)/float(count_good+count_bad)))
+
+    if args.staged:
+        # set stage for next run
+        write_stage(stage + 1)
 
 main()
