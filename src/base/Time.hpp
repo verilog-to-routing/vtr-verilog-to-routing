f537ccc783a1f2135ea829cbf3e0ff139564c579
kmurray <k.murray@utoronto.ca>
2014-11-26 22:41:49 -0500

    Switch to generalized Time class for arrival/required computations.

--- src/base/Time.hpp
+++ src/base/Time.hpp
@@ -11,9 +11,32 @@ class Time {
 
         float value() const {return time_;}
         void set_value(float time) {time_ = time;}
+
+        void max(const Time& other) { time_ = std::max(time_, other.time_); }
+        void min(const Time& other) { time_ = std::min(time_, other.time_); }
+
+        bool valid() const { return !isnan(time_); }
+
+        Time& operator+=(const Time& rhs) {
+            time_ += rhs.time_;
+            return *this;
+        }
+
+        Time& operator-=(const Time& rhs) {
+            time_ -= rhs.time_;
+            return *this;
+        }
     private:
         float time_;
 };
+
+inline Time operator+(Time lhs, const Time& rhs) {
+    return lhs += rhs;
+}
+
+inline Time operator-(Time lhs, const Time& rhs) {
+    return lhs -= rhs;
+}
 
 std::ostream& operator<<(std::ostream& os, const Time& time);
 
--- src/timing_analyzer/SerialTimingAnalyzer.cpp
+++ src/timing_analyzer/SerialTimingAnalyzer.cpp
@@ -26,27 +26,19 @@ void SerialTimingAnalyzer::pre_traversal(TimingGraph& timing_graph) {
      *   - Initialize required times on primary outputs
      */
     //We perform a BFS from primary inputs to initialize the timing graph
-    //std::cout << "Pre Traversal: " << std::endl;
     for(int level_idx = 0; level_idx < timing_graph.num_levels(); level_idx++) {
-        //std::cout << "Level " << level_idx << ": ";
         for(int node_id : timing_graph.level(level_idx)) {
-            //std::cout << node_id << " ";
             pre_traverse_node(timing_graph, node_id, level_idx);
         }
-        //std::cout << std::endl;
     }
 }
 
 void SerialTimingAnalyzer::forward_traversal(TimingGraph& timing_graph) {
     //Forward traversal (arrival times)
-    //std::cout << "Fwd Traversal: " << std::endl;
     for(int level_idx = 1; level_idx < timing_graph.num_levels(); level_idx++) {
-        //std::cout << "Level " << level_idx << ": ";
         for(NodeId node_id : timing_graph.level(level_idx)) {
-            //std::cout << node_id << " ";
             forward_traverse_node(timing_graph, node_id);
         }
-        //std::cout << std::endl;
     }
 }
     
@@ -54,14 +46,10 @@ void SerialTimingAnalyzer::backward_traversal(TimingGraph& timing_graph) {
     //Backward traversal (required times)
     //  Since we only store edges in the forward direction we calculate required times by
     //  setting the values for the current level based on the one below
-    //std::cout << "Bck Traversal: " << std::endl;
     for(int level_idx = timing_graph.num_levels() - 2; level_idx >= 0; level_idx--) {
-        //std::cout << "Level " << level_idx << ": ";
         for(NodeId node_id : timing_graph.level(level_idx)) {
-            //std::cout << node_id << " ";
             backward_traverse_node(timing_graph, node_id);
         }
-        //std::cout << std::endl;
     }
 }
 
@@ -85,45 +73,43 @@ void SerialTimingAnalyzer::pre_traverse_node(TimingGraph& tg, NodeId node_id, in
 void SerialTimingAnalyzer::forward_traverse_node(TimingGraph& tg, NodeId node_id) {
     //From upstream sources to current node
 
-    float new_arrival_time = NAN;
+    Time arr_time;
     for(int edge_idx = 0; edge_idx < tg.num_node_in_edges(node_id); edge_idx++) {
         EdgeId edge_id = tg.node_in_edge(node_id, edge_idx);
-        float edge_delay = tg.edge_delay(edge_id).value();
 
         int src_node_id = tg.edge_src_node(edge_id);
 
-        //TODO: Generalize this to support a more general Time class (e.g. vector of timing corners)
-        float src_arrival_time = tg.node_arr_time(src_node_id).value();
+        const Time& src_arr_time = tg.node_arr_time(src_node_id);
+        const Time& edge_delay = tg.edge_delay(edge_id);
 
-        if(edge_idx == 0) {
-            new_arrival_time = src_arrival_time + edge_delay;
+        if(!arr_time.valid()) {
+            arr_time = src_arr_time + edge_delay;
         } else {
-            new_arrival_time = std::max(new_arrival_time, src_arrival_time + edge_delay);
+            arr_time.max(src_arr_time + edge_delay);
         }
 
     }
-    tg.set_node_arr_time(node_id, Time(new_arrival_time));
+    tg.set_node_arr_time(node_id, arr_time);
 }
 
 void SerialTimingAnalyzer::backward_traverse_node(TimingGraph& tg, NodeId node_id) {
     //From downstream sinks to current node
-    float required_time = tg.node_req_time(node_id).value();
+    Time req_time = tg.node_req_time(node_id);
 
     for(int edge_idx = 0; edge_idx < tg.num_node_out_edges(node_id); edge_idx++) {
         EdgeId edge_id = tg.node_out_edge(node_id, edge_idx);
-
         int sink_node_id = tg.edge_sink_node(edge_id);
 
         //TODO: Generalize this to support a general Time class (e.g. vector of corners or statistical etc.)
-        float sink_required_time = tg.node_req_time(sink_node_id).value();
-        float edge_delay = tg.edge_delay(edge_id).value();
+        const Time& sink_req_time = tg.node_req_time(sink_node_id);
+        const Time& edge_delay = tg.edge_delay(edge_id);
 
-        if(isnan(required_time)) {
-            required_time = sink_required_time - edge_delay;
+        if(!req_time.valid()) {
+            req_time = sink_req_time - edge_delay;
         } else {
-            required_time = std::min(required_time, sink_required_time - edge_delay);
+            req_time.min(sink_req_time - edge_delay);
         }
 
     }
-    tg.set_node_req_time(node_id, Time(required_time));
+    tg.set_node_req_time(node_id, Time(req_time));
 }
