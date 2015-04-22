b132b63e6d148785e953e7acb1dc6c705c2dc19f
kmurray <k.murray@utoronto.ca>
2015-04-22 17:56:02 -0400

    Move util related functions into sta_util

--- src/libtatum/base/sta_util.cpp
+++ src/libtatum/base/sta_util.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <iomanip>
 #include <algorithm>
+#include <cassert>
 #include "sta_util.hpp"
 
 float time_sec(struct timespec start, struct timespec end) {
@@ -56,4 +57,66 @@ void print_histogram(const std::vector<float>& values, int nbuckets) {
     std::cout.flags(saved_flags);
     std::cout.precision(prec);
     std::cout.width(width);
+}
+
+float relative_error(float A, float B) {
+    if (A == B) {
+        return 0.;
+    }
+
+    if (fabs(B) > fabs(A)) {
+        return fabs((A - B) / B);
+    } else {
+        return fabs((A - B) / A);
+    }
+}
+
+void print_level_histogram(const TimingGraph& tg, int nbuckets) {
+    std::cout << "Levels Width Histogram" << std::endl;
+
+    std::vector<float> level_widths;
+    for(int i = 0; i < tg.num_levels(); i++) {
+        level_widths.push_back(tg.level(i).size());
+    }
+    print_histogram(level_widths, nbuckets);
+}
+
+void print_node_fanin_histogram(const TimingGraph& tg, int nbuckets) {
+    std::cout << "Node Fan-in Histogram" << std::endl;
+
+    std::vector<float> fanin;
+    for(NodeId i = 0; i < tg.num_nodes(); i++) {
+        fanin.push_back(tg.num_node_in_edges(i));
+    }
+
+    std::sort(fanin.begin(), fanin.end(), std::greater<float>());
+    print_histogram(fanin, nbuckets);
+}
+
+void print_node_fanout_histogram(const TimingGraph& tg, int nbuckets) {
+    std::cout << "Node Fan-out Histogram" << std::endl;
+
+    std::vector<float> fanout;
+    for(NodeId i = 0; i < tg.num_nodes(); i++) {
+        fanout.push_back(tg.num_node_out_edges(i));
+    }
+
+    std::sort(fanout.begin(), fanout.end(), std::greater<float>());
+    print_histogram(fanout, nbuckets);
+}
+
+
+void print_timing_graph(const TimingGraph& tg) {
+    for(NodeId node_id = 0; node_id < tg.num_nodes(); node_id++) {
+        std::cout << "Node: " << node_id << " Type: " << tg.node_type(node_id) <<  " Out Edges: " << tg.num_node_out_edges(node_id) << std::endl;
+        for(int out_edge_idx = 0; out_edge_idx < tg.num_node_out_edges(node_id); out_edge_idx++) {
+            EdgeId edge_id = tg.node_out_edge(node_id, out_edge_idx);
+            NodeId from_node_id = tg.edge_src_node(edge_id);
+            assert(from_node_id == node_id);
+
+            NodeId sink_node_id = tg.edge_sink_node(edge_id);
+
+            std::cout << "\tEdge src node: " << node_id << " sink node: " << sink_node_id << " Delay: " << tg.edge_delay(edge_id).value() << std::endl;
+        }
+    }
 }
--- src/libtatum/base/sta_util.hpp
+++ src/libtatum/base/sta_util.hpp
@@ -1,5 +1,12 @@
 #pragma once
+#include "TimingGraph.hpp"
 
 float time_sec(struct timespec start, struct timespec end);
 
 void print_histogram(const std::vector<float>& values, int nbuckets);
+
+float relative_error(float A, float B);
+void print_level_histogram(const TimingGraph& tg, int nbuckets);
+void print_node_fanin_histogram(const TimingGraph& tg, int nbuckets);
+void print_node_fanout_histogram(const TimingGraph& tg, int nbuckets);
+void print_timing_graph(const TimingGraph& tg);
