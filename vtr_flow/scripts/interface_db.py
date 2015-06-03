00d018ddafac8860f0bc4d33bd8e75f2c53cfc72
Johnson Zhong <johnson9510@gmail.com>
2015-06-03 22:39:23 +0000

    Functionally complete interface_db
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@4636 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- vtr_flow/scripts/interface_db.py
+++ vtr_flow/scripts/interface_db.py
@@ -34,6 +34,57 @@ def describe_tasks(tasks, dbname = "results.db"):
             shared_params = params
     return shared_params
     
+# can only have 1 x param but arbitrary amounts of y_params and filters
+def retrieve_data(x_param, y_param, filters, task, dbname = "results.db"):
+    db = connect_db(dbname)
+
+    cols_to_select = x_param + ',' + y_param
+
+    select_command = "SELECT {} FROM {} ".format(cols_to_select, task_name(task))
+    sql_val_args = []
+
+    if filters:
+        filter_command = "WHERE "
+        for f in range(len(filters)):
+
+            filter_command += str(filters[f])
+            sql_val_args.extend(filters[f].args)
+
+            if f < len(filters) - 1:
+                filter_command += " AND "
+
+        select_command += filter_command
+
+    select_command += ';'
+
+    print(select_command)
+    cursor = db.cursor()
+    cursor.execute(select_command, sql_val_args)
+        
+    return cursor.fetchall()
+
+
+# give back metainformation about a parameter to allow easier filtering
+# param would be an element of the list returned by describe_tasks
+def describe_param(param, task, dbname = "results.db"):
+    db = connect_db(dbname)
+    cursor = db.cursor()
+
+    (param_name, param_type) = param.split()
+    # categorical data, return a list of all distinct values
+    if param_type == "TEXT":
+        cursor.execute("SELECT DISTINCT {} FROM {};".format(param_name, task_name(task)))
+        return [row[0] for row in cursor.fetchall()]
+    # ranged data, return (min, max)
+    else:
+        cursor.execute("SELECT MIN({}) FROM {};".format(param_name, task_name(task)))
+        param_min = cursor.fetchone()[0]
+        cursor.execute("SELECT MAX({}) FROM {};".format(param_name, task_name(task)))
+        param_max = cursor.fetchone()[0]
+        return (param_min, param_max)
+
+
+
 # attempt a connection, exiting with 1 if dbname does not exist, else return with db connection
 def connect_db(dbname = "results.db"):
     if not os.path.isfile(dbname):
@@ -44,8 +95,35 @@ def connect_db(dbname = "results.db"):
     return db
 
 
+# filter object
+valid_filter_methods = {"IN", "BETWEEN", "LIKE", "=", "<>", "!=", ">", "<", ">=", "<="}
+class Task_filter:
+    def __init__(self, param, method, args):
+        self.param = param
+        if method.upper() in valid_filter_methods:
+            self.method = method.upper()
+        else:
+            print(method, "is not a supported filter method")
+            raise ValueError
+        self.args = args
+    def __str__(self):
+        substitutions = sql_substitute(self.args)
+        if self.method == "BETWEEN":
+            substitutions = "? AND ?"
+        elif self.method == "IN":
+            substitutions = '('+substitutions+')'
+        return "({} {} {})".format(self.param, self.method, substitutions)
+        
+
 # utilities
+def task_name(task):
+    return '['+task+']'
 def intersection(first, other):
     intersection_set = set.intersection(set(first), set(other))
     # reimpose order
     return [item for item in first if item in intersection_set]
+
+def sql_substitute(args):
+    return ('?,'*len(args)).rstrip(',')
+
+
