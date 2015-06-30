1205b9eb1d1785c342caf23fda95abcbd1436c86
Johnson Zhong <johnson9510@gmail.com>
2015-06-30 20:34:22 +0000

    Script updated to not remove .svn (littering is illegal) by old svn on buildbot
    git-svn-id: https://vtr-verilog-to-routing.googlecode.com/svn/trunk@4668 8e3573b8-cf2c-4f14-ef6d-137439e28b8b

--- vtr_flow/scripts/update_benchtracker.sh
+++ vtr_flow/scripts/update_benchtracker.sh
@@ -1,6 +1,8 @@
 chmod -R 777 benchtracker
-rm -rf benchtracker
+mv benchtracker_moved
 git clone https://github.com/LemonPi/benchtracker.git
 rm -rf benchtracker/.git
 rm -rf benchtracker/.gitignore
+mv benchtracker_moved/.svn benchtracker
+rm -rf bentracker_moved
 sed -i "s/app.run(debug=True, port=port)/app.run(host='0.0.0.0', port=port)/" benchtracker/server_db.py
