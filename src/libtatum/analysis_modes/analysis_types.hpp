91ac855bf01bde387c35e1f785dec51c94ae65da
kmurray <k.murray@utoronto.ca>
2015-08-21 16:54:41 -0400

    Add docs to analysis_types.hpp

--- src/libtatum/analysis_modes/analysis_types.hpp
+++ src/libtatum/analysis_modes/analysis_types.hpp
@@ -1,18 +1,18 @@
 #include "SetupAnalysisMode.hpp"
 #include "HoldAnalysisMode.hpp"
 
-/*
+/** \file
  * Useful shorthands for the supported analysis types
  *
  * Used as template parameters to specify timing analysis
  * mode for TimingAnalyzer and derived classes
  */
 
-//Setup (max) analysis
+///Setup (max) analysis
 using SetupAnalysis = SetupAnalysisMode<>;
 
-//Hold (min) analysis
+///Hold (min) analysis
 using HoldAnalysis = HoldAnalysisMode<>;
 
-//Combined Setup AND Hold analysis
+///Combined Setup *and* Hold analysis
 using SetupHoldAnalysis = HoldAnalysisMode<SetupAnalysisMode<>>;
