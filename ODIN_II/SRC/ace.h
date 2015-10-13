7dd416714b230ad478cc10befc98edc53a529adb
srseeley <srseeley@unb.ca>
2015-10-13 16:08:39 -0300

    Initial commit for ACtivity Estimation (ace) based upon ace2 for ODIN. Data structures only for now.

---
+++ ODIN_II/SRC/ace.h
@@ -0,0 +1,31 @@
+/*
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+#ifndef __ACE_ACE_H__
+#define __ACE_ACE_H__
+
+#include "types.h"
+
+void alloc_and_init_ace_structs (netlist_t * net);
+ace_obj_info_t* alloc_and_init_ace_info_object ();
+
+#endif
