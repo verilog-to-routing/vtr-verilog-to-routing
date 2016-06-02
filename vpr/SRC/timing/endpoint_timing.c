5c5b27ed16427ffa9e2f4398bd11378d7e26f99e
kmurray <k.murray@utoronto.ca>
2016-06-02 17:10:33 -0400

    Fixes to endpoint timing output format

--- vpr/SRC/timing/endpoint_timing.c
+++ vpr/SRC/timing/endpoint_timing.c
@@ -15,11 +15,22 @@ void print_endpoint_timing(char* filename) {
     fprintf(fp, "{\n");
     fprintf(fp, "  \"endpoint_timing\": [\n");
 
+    std::vector<int> outpad_sink_tnodes;
+
     for(int inode = 0; inode < num_tnodes; inode++) {
         if(tnode[inode].type == TN_OUTPAD_SINK) {
-            char* identifier = block[tnode[inode].block].name + 4; //Trim out:
-            print_tnode_info(fp, inode, identifier);
+            outpad_sink_tnodes.push_back(inode);
         }
+    }
+    for(size_t i = 0; i < outpad_sink_tnodes.size(); ++i) {
+        int inode = outpad_sink_tnodes[i];
+        char* identifier = block[tnode[inode].block].name + 4; //Trim out:
+        print_tnode_info(fp, inode, identifier);
+
+        if(i != outpad_sink_tnodes.size() - 1) {
+            fprintf(fp, ",");
+        }
+        fprintf(fp, "\n");
     }
 
     fprintf(fp, "  ]\n");
@@ -33,17 +44,17 @@ void print_endpoint_timing(char* filename) {
 
 void print_tnode_info(FILE* fp, int inode, char* identifier) {
     fprintf(fp, "    {\n");
-    fprintf(fp, "      \"node_identifier\": \"%s\"\n", identifier);
-    fprintf(fp, "      \"tnode_id\": \"%d\"\n", inode);
+    fprintf(fp, "      \"node_identifier\": \"%s\",\n", identifier);
+    fprintf(fp, "      \"tnode_id\": \"%d\",\n", inode);
 
     if(tnode[inode].type == TN_OUTPAD_SINK) {
-        fprintf(fp, "      \"tnode_type\": \"TN_OUTPAD_SINK\"\n");
+        fprintf(fp, "      \"tnode_type\": \"TN_OUTPAD_SINK\",\n");
     } else {
         assert(tnode[inode].type == TN_FF_SINK);
-        fprintf(fp, "      \"tnode_type\": \"TN_FF_SINK\"\n");
+        fprintf(fp, "      \"tnode_type\": \"TN_FF_SINK\",\n");
     }
-    fprintf(fp, "      \"T_arr\": \"%g\"\n", tnode[inode].T_arr);
+    fprintf(fp, "      \"T_arr\": \"%g\",\n", tnode[inode].T_arr);
     fprintf(fp, "      \"T_req\": \"%g\"\n", tnode[inode].T_req);
      
-    fprintf(fp, "    }\n");
+    fprintf(fp, "    }");
 }
