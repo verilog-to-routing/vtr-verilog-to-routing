95d0369a1fe6663e23c74bc025d336895f86903e
kmurray <k.murray@utoronto.ca>
2016-09-12 13:56:45 -0400

    Move doc source files into doc/src

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -27,7 +27,7 @@ except ImportError:
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.append(os.path.abspath('_exts'))
+sys.path.append(os.path.abspath('../_exts'))
 
 # -- General configuration ------------------------------------------------
 
