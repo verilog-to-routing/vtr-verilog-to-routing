100c086675668a68a950033e47ca0f45dab390ae
kmurray <k.murray@utoronto.ca>
2016-09-13 14:18:52 -0400

    Add include guard to errors.h

--- ODIN_II/SRC/errors.h
+++ ODIN_II/SRC/errors.h
@@ -1,3 +1,7 @@
+#ifndef ERRORS_H
+#define ERRORS_H
 
 void error_message(short error_type, int line_number, int file, const char *message, ...);
 void warning_message(short error_type, int line_number, int file, const char *message, ...);
+
+#endif
