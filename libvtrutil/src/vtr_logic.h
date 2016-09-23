e9a2d09b430289e5d8d346baab2bb7676c26b561
kmurray <k.murray@utoronto.ca>
2016-09-23 14:26:27 -0400

    Add logic types

---
+++ libvtrutil/src/vtr_logic.h
@@ -0,0 +1,15 @@
+#ifndef VTR_LOGIC_H
+#define VTR_LOGIC_H
+
+namespace vtr {
+
+    enum class LogicValue {
+        FALSE = 0,
+        TRUE = 1,
+        DONT_CARE = 2,
+        UNKOWN = 3
+    };
+
+} //namespace
+
+#endif
