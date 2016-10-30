85de0e0d019a2e2988334f41b0d534ce5ed46bec
kmurray <k.murray@utoronto.ca>
2016-10-30 19:48:33 -0400

    Update sweep script with error checking

--- scripts/sweep_benchmarks.sh
+++ scripts/sweep_benchmarks.sh
@@ -1,7 +1,19 @@
-for benchmark in $(ls /project/work/timing_analysis/skew/*/vpr_timing_graph.echo) 
+#!/bin/bash
+
+for benchmark in $(ls -hSr /project/work/timing_analysis/skew/*/vpr_timing_graph.echo) 
 do
     echo "==============================================================="
     echo $benchmark
 
     $1 $benchmark
+
+    exit_code=$?
+
+    if [ $exit_code -ne 0 ]; then
+        echo "Failed"
+        exit 1
+    fi
 done
+
+echo "Passed"
+exit 0
