bae942b0ad249edc20d1bd9a53aa8f34db144184
kmurray <k.murray@utoronto.ca>
2016-11-07 16:49:08 -0500

    Add support for reading from stdin & NopCallback

--- libtatumparse/main.cpp
+++ libtatumparse/main.cpp
@@ -99,11 +99,68 @@ class PrintCallback : public tp::Callback {
         int line_no_;
 };
 
+class NopCallback : public tp::Callback {
+    public:
+        //Start of parsing
+        void start_parse() override {}
+
+        //Sets current filename
+        void filename(std::string fname) override { filename_ = fname; }
+
+        //Sets current line number
+        void lineno(int /*line_num*/) override {}
+
+        void start_graph() override {}
+        void add_node(int /*node_id*/, tp::NodeType /*type*/, std::vector<int> /*in_edge_ids*/, std::vector<int> /*out_edge_ids*/) override {}
+        void add_edge(int /*edge_id*/, int /*src_node_id*/, int /*sink_node_id*/) override {}
+        void finish_graph() override {}
+
+        void start_constraints() override {}
+        void add_clock_domain(int /*domain_id*/, std::string /*name*/) override {}
+        void add_clock_source(int /*node_id*/, int /*domain_id*/) override {}
+        void add_input_constraint(int /*node_id*/, float /*constraint*/) override {}
+        void add_output_constraint(int /*node_id*/, float /*constraint*/) override {}
+        void add_setup_constraint(int /*src_domain_id*/, int /*sink_domain_id*/, float /*constraint*/) override {}
+        void add_hold_constraint(int /*src_domain_id*/, int /*sink_domain_id*/, float /*constraint*/) override {}
+        void finish_constraints() override {}
+
+        void start_delay_model() override {}
+        void add_edge_delay(int /*edge_id*/, float /*min_delay*/, float /*max_delay*/) override {}
+        void finish_delay_model() override {}
+
+        void start_results() override {}
+        void add_tag(tp::TagType /*type*/, int /*node_id*/, int /*domain_id*/, float /*arr*/, float /*req*/) override {}
+        void finish_results() override {}
+
+        //End of parsing
+        void finish_parse() override {}
+
+        //Error during parsing
+        void parse_error(const int curr_lineno, const std::string& near_text, const std::string& msg) override {
+            fprintf(stderr, "%s:%d: Parse error: %s", filename_.c_str(), curr_lineno, msg.c_str());
+            if(!near_text.empty()) {
+                fprintf(stderr, " near '%s'", near_text.c_str());
+                fprintf(stderr, "\n");
+            } else {
+                if(msg.find('\n') == std::string::npos) {
+                    fprintf(stderr, "\n");
+                }
+            }
+        }
+    private:
+        std::string filename_;
+};
+
 int main(int argc, char** argv) {
 
-    PrintCallback cb;
+    //PrintCallback cb;
+    NopCallback cb;
     if(argc == 2) {
-        tp::tatum_parse_filename(argv[1], cb);  
+        if(std::string(argv[1]) == "-") {
+            tp::tatum_parse_file(stdin, cb);  
+        } else {
+            tp::tatum_parse_filename(argv[1], cb);  
+        }
     } else {
         fprintf(stderr, "Invalid command line\n");
         return 1;
