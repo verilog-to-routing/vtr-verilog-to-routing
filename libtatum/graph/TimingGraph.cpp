81c813ad4345962b4a355d8db2d6680e1c32b6ce
kmurray <k.murray@utoronto.ca>
2016-11-09 11:24:30 -0500

    Add output operator for new node types

--- libtatum/graph/TimingGraph.cpp
+++ libtatum/graph/TimingGraph.cpp
@@ -334,6 +334,10 @@ std::ostream& operator<<(std::ostream& os, const NodeType type) {
     else if (type == NodeType::CLOCK_SOURCE)        os << "CLOCK_SOURCE";
     else if (type == NodeType::CLOCK_OPIN)          os << "CLOCK_OPIN";
     else if (type == NodeType::CONSTANT_GEN_SOURCE) os << "CONSTANT_GEN_SOURCE";
+    else if (type == NodeType::SOURCE)              os << "SOURCE";
+    else if (type == NodeType::SINK)                os << "SINK";
+    else if (type == NodeType::IPIN)                os << "IPIN";
+    else if (type == NodeType::OPIN)                os << "OPIN";
     else if (type == NodeType::UNKOWN)              os << "UNKOWN";
     else throw std::domain_error("Unrecognized NodeType");
     return os;
