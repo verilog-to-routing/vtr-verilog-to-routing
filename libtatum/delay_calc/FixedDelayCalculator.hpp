959bff3eb395affc3e28f8635248c2329b75ad88
kmurray <k.murray@utoronto.ca>
2016-11-10 12:22:37 -0500

    Add support for different setup/hold edge delays in FixedDelayCalculator

--- libtatum/delay_calc/FixedDelayCalculator.hpp
+++ libtatum/delay_calc/FixedDelayCalculator.hpp
@@ -16,17 +16,35 @@ class FixedDelayCalculator {
 
         ///Initializes the edge delays
         ///\param edge_delays A vector specifying the delay for every edge
-        FixedDelayCalculator(std::vector<float> edge_delays) {
-            edge_delays_.reserve(edge_delays.size());
-            for(float delay : edge_delays) {
-                edge_delays_.emplace_back(delay);
+        FixedDelayCalculator(const std::vector<float>& setup_edge_delays, const std::vector<float>& hold_edge_delays=std::vector<float>()) {
+            setup_edge_delays_.reserve(setup_edge_delays.size());
+            for(float delay : setup_edge_delays) {
+                setup_edge_delays_.emplace_back(delay);
+            }
+
+            if(hold_edge_delays.empty()) {
+                hold_edge_delays_ = setup_edge_delays_;
+            } else {
+                hold_edge_delays_.reserve(hold_edge_delays.size());
+                for(float delay : hold_edge_delays) {
+                    hold_edge_delays_.emplace_back(delay);
+                }
+
             }
         }
 
-        Time min_edge_delay(const TimingGraph& /*tg*/, EdgeId edge_id) const { TATUM_ASSERT(size_t(edge_id) < edge_delays_.size()); return edge_delays_[size_t(edge_id)]; };
-        Time max_edge_delay(const TimingGraph& /*tg*/, EdgeId edge_id) const { TATUM_ASSERT(size_t(edge_id) < edge_delays_.size()); return edge_delays_[size_t(edge_id)]; };
+        Time min_edge_delay(const TimingGraph& /*tg*/, EdgeId edge_id) const { 
+            TATUM_ASSERT(size_t(edge_id) < hold_edge_delays_.size()); 
+            return hold_edge_delays_[size_t(edge_id)]; 
+        };
+
+        Time max_edge_delay(const TimingGraph& /*tg*/, EdgeId edge_id) const { 
+            TATUM_ASSERT(size_t(edge_id) < setup_edge_delays_.size()); 
+            return setup_edge_delays_[size_t(edge_id)]; 
+        };
     private:
-        std::vector<Time> edge_delays_;
+        std::vector<Time> setup_edge_delays_;
+        std::vector<Time> hold_edge_delays_;
 };
 
 } //namepsace
