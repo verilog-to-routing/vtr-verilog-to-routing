7b0639c0d6d4cdc8aae4304caee2eb0dbc9f0012
kmurray <k.murray@utoronto.ca>
2016-11-10 17:57:48 -0500

    Separate-out setup/hold edges in delay model echo

--- tatum_sta/output.hpp
+++ tatum_sta/output.hpp
@@ -13,9 +13,17 @@ template<class DelayCalc>
 void write_delay_model(std::ostream& os, const tatum::TimingGraph& tg, const DelayCalc& dc) {
     os << "delay_model:\n";
     for(auto edge_id : tg.edges()) {
+        tatum::NodeId src_node = tg.edge_src_node(edge_id);
+        tatum::NodeId sink_node = tg.edge_sink_node(edge_id);
+
         os << " edge: " << size_t(edge_id);
-        os << " min_delay: " << dc.min_edge_delay(tg, edge_id).value();
-        os << " max_delay: " << dc.max_edge_delay(tg, edge_id).value();
+        if(tg.node_type(src_node) == tatum::NodeType::CPIN && tg.node_type(sink_node) == tatum::NodeType::SINK) {
+            os << " setup_time: " << dc.setup_time(tg, edge_id).value();
+            os << " hold_time: " << dc.hold_time(tg, edge_id).value();
+        } else {
+            os << " min_delay: " << dc.min_edge_delay(tg, edge_id).value();
+            os << " max_delay: " << dc.max_edge_delay(tg, edge_id).value();
+        }
         os << "\n";
     }
     os << "\n";
