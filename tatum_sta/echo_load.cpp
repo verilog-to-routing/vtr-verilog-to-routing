ebf7eddbd9f4da9d9936ed2db5617241dbefea9c
kmurray <k.murray@utoronto.ca>
2016-11-10 18:32:28 -0500

    Add basic echo file loader

---
+++ tatum_sta/echo_load.cpp
@@ -0,0 +1,84 @@
+#include "echo_load.hpp"
+
+void EchoLoader::add_node(int node_id, tatumparse::NodeType type, std::vector<int> /*in_edge_ids*/, std::vector<int> /*out_edge_ids*/) {
+    tatum::NodeId created_node_id = tg_.add_node(to_tatum_node_type(type));
+    TATUM_ASSERT(created_node_id == tatum::NodeId(node_id));
+}
+
+void EchoLoader::add_edge(int edge_id, int src_node_id, int sink_node_id) {
+    tatum::EdgeId created_edge_id = tg_.add_edge(tatum::NodeId(src_node_id), tatum::NodeId(sink_node_id));
+    TATUM_ASSERT(created_edge_id == tatum::EdgeId(edge_id));
+}
+
+void EchoLoader::finish_graph() { 
+    max_delay_edges_.resize(tg_.edges().size()); 
+    min_delay_edges_.resize(tg_.edges().size()); 
+}
+
+void EchoLoader::add_clock_domain(int domain_id, std::string name) {
+    tatum::DomainId created_domain_id = tc_.create_clock_domain(name);
+    TATUM_ASSERT(created_domain_id == tatum::DomainId(domain_id));
+}
+
+void EchoLoader::add_clock_source(int node_id, int domain_id) {
+    tc_.set_clock_domain_source(tatum::NodeId(node_id), tatum::DomainId(domain_id));
+}
+
+void EchoLoader::add_constant_generator(int node_id) {
+    tc_.set_constant_generator(tatum::NodeId(node_id));    
+}
+
+void EchoLoader::add_input_constraint(int node_id, int domain_id, float constraint) {
+    tc_.set_input_constraint(tatum::NodeId(node_id), tatum::DomainId(domain_id), constraint);
+}
+
+void EchoLoader::add_output_constraint(int node_id, int domain_id, float constraint) {
+    tc_.set_output_constraint(tatum::NodeId(node_id), tatum::DomainId(domain_id), constraint);
+}
+
+void EchoLoader::add_setup_constraint(int src_domain_id, int sink_domain_id, float constraint) {
+    tc_.set_setup_constraint(tatum::DomainId(src_domain_id), tatum::DomainId(sink_domain_id), constraint);
+}
+
+void EchoLoader::add_hold_constraint(int src_domain_id, int sink_domain_id, float constraint) {
+    tc_.set_hold_constraint(tatum::DomainId(src_domain_id), tatum::DomainId(sink_domain_id), constraint);
+}
+
+void EchoLoader::add_edge_setup_hold_time(int edge_id, float setup_time, float hold_time) {
+    setup_times_[tatum::EdgeId(edge_id)] = tatum::Time(setup_time);
+    hold_times_[tatum::EdgeId(edge_id)] = tatum::Time(hold_time);
+}
+
+void EchoLoader::add_edge_delay(int edge_id, float min_delay, float max_delay) {
+    min_delay_edges_[tatum::EdgeId(edge_id)] = tatum::Time(min_delay);
+    max_delay_edges_[tatum::EdgeId(edge_id)] = tatum::Time(max_delay);
+}
+
+tatum::TimingGraph EchoLoader::timing_graph() {
+    return std::move(tg_);
+}
+
+tatum::TimingConstraints EchoLoader::timing_constraints() {
+    return std::move(tc_);
+}
+
+tatum::FixedDelayCalculator EchoLoader::delay_calculator() {
+    return tatum::FixedDelayCalculator(max_delay_edges_, setup_times_, min_delay_edges_, hold_times_);
+}
+
+tatum::NodeType EchoLoader::to_tatum_node_type(tatumparse::NodeType type) {
+    if(type == tatumparse::NodeType::SOURCE) {
+        return tatum::NodeType::SOURCE;
+    } else if(type == tatumparse::NodeType::SINK) {
+        return tatum::NodeType::SINK;
+    } else if(type == tatumparse::NodeType::IPIN) {
+        return tatum::NodeType::IPIN;
+    } else {
+        TATUM_ASSERT(type == tatumparse::NodeType::OPIN);
+        return tatum::NodeType::OPIN;
+    }
+}
+
+void EchoLoader::parse_error(const int /*curr_lineno*/, const std::string& /*near_text*/, const std::string& /*msg*/) {
+    TATUM_ASSERT_MSG(false, "Parse error");
+}
---
+++ tatum_sta/echo_load.hpp
@@ -0,0 +1,72 @@
+#ifndef TATUMSTA_ECHO_LOAD
+#define TATUMSTA_ECHO_LOAD
+
+#include "TimingGraph.hpp"
+#include "TimingConstraints.hpp"
+#include "FixedDelayCalculator.hpp"
+
+#include "tatumparse.hpp"
+
+class EchoLoader : public tatumparse::Callback {
+public:
+    //Start of parsing
+    void start_parse() override { }
+
+    //Sets current filename
+    void filename(std::string /*fname*/) override { }
+
+    //Sets current line number
+    void lineno(int /*line_num*/) override { }
+
+    void start_graph() override { }
+    void add_node(int node_id, tatumparse::NodeType type, std::vector<int> in_edge_ids, std::vector<int> out_edge_ids) override;
+    void add_edge(int edge_id, int src_node_id, int sink_node_id) override;
+    void finish_graph() override;
+
+    void start_constraints() override { }
+    void add_clock_domain(int domain_id, std::string name) override;
+    void add_clock_source(int node_id, int domain_id) override;
+    void add_constant_generator(int node_id) override;
+    void add_input_constraint(int node_id, int domain_id, float constraint) override;
+    void add_output_constraint(int node_id, int domain_id, float constraint) override;
+    void add_setup_constraint(int src_domain_id, int sink_domain_id, float constraint) override;
+    void add_hold_constraint(int src_domain_id, int sink_domain_id, float constraint) override;
+    void finish_constraints() override { tc_.print(); }
+
+    void start_delay_model() override { }
+    void add_edge_delay(int edge_id, float min_delay, float max_delay) override;
+    void add_edge_setup_hold_time(int edge_id, float setup_time, float hold_time) override;
+    void finish_delay_model() override { }
+
+    void start_results() override { }
+    void add_tag(tatumparse::TagType /*type*/, int /*node_id*/, int /*domain_id*/, float /*arr*/, float /*req*/) override { }
+    void finish_results() override { }
+
+    //End of parsing
+    void finish_parse() override { }
+
+    //Error during parsing
+    void parse_error(const int curr_lineno, const std::string& near_text, const std::string& msg) override;
+public:
+    tatum::TimingGraph timing_graph();
+
+    tatum::TimingConstraints timing_constraints();
+
+    tatum::FixedDelayCalculator delay_calculator();
+
+private:
+    tatum::NodeType to_tatum_node_type(tatumparse::NodeType type);
+
+private: //Data
+
+    tatum::TimingGraph tg_;
+    tatum::TimingConstraints tc_;
+
+    tatum::util::linear_map<tatum::EdgeId,tatum::Time> max_delay_edges_;
+    tatum::util::linear_map<tatum::EdgeId,tatum::Time> min_delay_edges_;
+    tatum::util::linear_map<tatum::EdgeId,tatum::Time> setup_times_;
+    tatum::util::linear_map<tatum::EdgeId,tatum::Time> hold_times_;
+};
+
+
+#endif
