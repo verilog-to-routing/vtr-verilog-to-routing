3f6f5796fba11c2d5c606e4179381976509c49da
kmurray <k.murray@utoronto.ca>
2016-11-14 17:16:20 -0500

    Fix reseting bug

--- libtatum/graph_visitors/CommonAnalysisOps.hpp
+++ libtatum/graph_visitors/CommonAnalysisOps.hpp
@@ -18,8 +18,8 @@ class CommonAnalysisOps {
     public:
         CommonAnalysisOps(size_t num_tags)
             : data_tags_(num_tags, NUM_DATA_TAGS_RESERVE)
-            , clock_launch_tags_(num_tags, NUM_CLK_TAGS_RESERVE)
-            , clock_capture_tags_(num_tags, NUM_CLK_TAGS_RESERVE) {}
+            , clock_launch_tags_(num_tags, NUM_CLOCK_TAGS_RESERVE)
+            , clock_capture_tags_(num_tags, NUM_CLOCK_TAGS_RESERVE) {}
 
         TimingTags& get_data_tags(const NodeId node_id) { return data_tags_[node_id]; }
         TimingTags& get_launch_clock_tags(const NodeId node_id) { return clock_launch_tags_[node_id]; }
@@ -29,14 +29,14 @@ class CommonAnalysisOps {
         const TimingTags& get_capture_clock_tags(const NodeId node_id) const { return clock_capture_tags_[node_id]; }
 
         void reset() { 
-            data_tags_.clear(); 
-            clock_launch_tags_.clear(); 
-            clock_capture_tags_.clear(); 
+            data_tags_ = tatum::util::linear_map<NodeId,TimingTags>(data_tags_.size(), NUM_DATA_TAGS_RESERVE);
+            clock_launch_tags_ = tatum::util::linear_map<NodeId,TimingTags>(clock_launch_tags_.size(), NUM_CLOCK_TAGS_RESERVE);
+            clock_capture_tags_ = tatum::util::linear_map<NodeId,TimingTags>(clock_capture_tags_.size(), NUM_CLOCK_TAGS_RESERVE);
         }
 
     private:
         constexpr static size_t NUM_DATA_TAGS_RESERVE = 1;
-        constexpr static size_t NUM_CLK_TAGS_RESERVE = 0;
+        constexpr static size_t NUM_CLOCK_TAGS_RESERVE = 0;
         tatum::util::linear_map<NodeId,TimingTags> data_tags_;
         tatum::util::linear_map<NodeId,TimingTags> clock_launch_tags_;
         tatum::util::linear_map<NodeId,TimingTags> clock_capture_tags_;
