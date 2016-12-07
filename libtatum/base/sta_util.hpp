5121be773cf1605b1e040b710e15f9fb9125ddbb
kmurray <k.murray@utoronto.ca>
2016-12-07 11:02:56 -0500

    Squashed 'libtatum/' changes from 151a9c9..84fbf05
    84fbf05 Make place analyzer last in optional arguments to dot writer
    559d999 Add code to identify combinational loops using Tarjan's algorithm
    2754459 Include edge IDs in DOT output
    742a873 Allow dumping DOT file with a null analyzer
    a5c1b91 Add SCC detection
    4425691 Add basic combinational loop detection
    d6da06e Fix dot file drawing when loops exist in graph
    
    git-subtree-dir: libtatum
    git-subtree-split: 84fbf05cb66bd73bfc5f58811d3f276cb826b9f8

--- libtatum/base/sta_util.hpp
+++ libtatum/base/sta_util.hpp
@@ -38,8 +38,8 @@ void find_transitive_fanin_nodes(const TimingGraph& tg, std::vector<NodeId>& nod
 template<class DelayCalc=FixedDelayCalculator>
 void write_dot_file_setup(std::string filename, 
                           const TimingGraph& tg, 
-                          std::shared_ptr<const TimingAnalyzer> analyzer = std::shared_ptr<const TimingAnalyzer>(), 
                           std::shared_ptr<DelayCalc> delay_calc = std::shared_ptr<DelayCalc>(),
+                          std::shared_ptr<const TimingAnalyzer> analyzer = std::shared_ptr<const TimingAnalyzer>(), 
                           std::vector<NodeId> nodes = std::vector<NodeId>()) {
 
     if(tg.nodes().size() > 1000 && nodes.empty()) {
@@ -50,7 +50,6 @@ void write_dot_file_setup(std::string filename,
     std::ofstream os(filename);
 
     auto setup_analyzer = std::dynamic_pointer_cast<const SetupTimingAnalyzer>(analyzer);
-    if(!setup_analyzer) return;
 
     //Write out a dot file of the timing graph
     os << "digraph G {" <<std::endl;
@@ -65,29 +64,31 @@ void write_dot_file_setup(std::string filename,
         os << "\tnode" << size_t(inode);
         os << "[label=\"";
         os << "{" << inode << " (" << tg.node_type(inode) << ")";
-        for(const TimingTag& tag : setup_analyzer->setup_tags(inode)) {
-            os << " | {";
-            os << tag.type() << "\\n";
-            if(!tag.launch_clock_domain()) {
-                os << "*";
-            } else {
-                os << tag.launch_clock_domain();
+        if(setup_analyzer) {
+            for(const TimingTag& tag : setup_analyzer->setup_tags(inode)) {
+                os << " | {";
+                os << tag.type() << "\\n";
+                if(!tag.launch_clock_domain()) {
+                    os << "*";
+                } else {
+                    os << tag.launch_clock_domain();
+                }
+                os << " to ";
+                if(!tag.capture_clock_domain()) {
+                    os << "*";
+                } else {
+                    os << tag.capture_clock_domain();
+                }
+                if(tag.type() == TagType::CLOCK_LAUNCH || tag.type() == TagType::CLOCK_CAPTURE || tag.type() == TagType::DATA_ARRIVAL) {
+                    os << " from ";
+                } else {
+                    os << " for ";
+                }
+                os << tag.origin_node();
+                os << "\\n";
+                os << " time: " << tag.time().value();
+                os << "}";
             }
-            os << " to ";
-            if(!tag.capture_clock_domain()) {
-                os << "*";
-            } else {
-                os << tag.capture_clock_domain();
-            }
-            if(tag.type() == TagType::CLOCK_LAUNCH || tag.type() == TagType::CLOCK_CAPTURE || tag.type() == TagType::DATA_ARRIVAL) {
-                os << " from ";
-            } else {
-                os << " for ";
-            }
-            os << tag.origin_node();
-            os << "\\n";
-            os << " time: " << tag.time().value();
-            os << "}";
         }
         os << "}\"]";
         os <<std::endl;
@@ -106,27 +107,23 @@ void write_dot_file_setup(std::string filename,
     }
 
     //Add edges with delays annoated
-    for(const LevelId ilevel : tg.levels()) {
-        for(NodeId node_id : tg.level_nodes(ilevel)) {
-            for(EdgeId edge_id : tg.node_out_edges(node_id)) {
+    for(EdgeId edge_id : tg.edges()) {
+        NodeId node_id = tg.edge_src_node(edge_id);
+        NodeId sink_node_id = tg.edge_sink_node(edge_id);
 
-                NodeId sink_node_id = tg.edge_sink_node(edge_id);
-
-                if(std::binary_search(nodes.begin(), nodes.end(), node_id)
-                   && std::binary_search(nodes.begin(), nodes.end(), sink_node_id)) {
-                    os << "\tnode" << size_t(node_id) << " -> node" << size_t(sink_node_id);
-                    if(delay_calc) {
-                        if(tg.node_type(node_id) == NodeType::CPIN && tg.node_type(sink_node_id) == NodeType::SINK) {
-                            os << " [ label=\"" << -delay_calc->setup_time(tg, edge_id) << " (-tsu)\" ]";
-                        } else if(tg.node_type(node_id) == NodeType::CPIN && tg.node_type(sink_node_id) == NodeType::SOURCE) {
-                            os << " [ label=\"" << delay_calc->max_edge_delay(tg, edge_id) << " (tcq)\" ]";
-                        } else {
-                            os << " [ label=\"" << delay_calc->max_edge_delay(tg, edge_id) << "\" ]";
-                        }
-                    }
-                    os << ";" <<std::endl;
+        if(std::binary_search(nodes.begin(), nodes.end(), node_id)
+           && std::binary_search(nodes.begin(), nodes.end(), sink_node_id)) {
+            os << "\tnode" << size_t(node_id) << " -> node" << size_t(sink_node_id);
+            if(delay_calc) {
+                if(tg.node_type(node_id) == NodeType::CPIN && tg.node_type(sink_node_id) == NodeType::SINK) {
+                    os << " [ label=\"" << edge_id << "\n"<< -delay_calc->setup_time(tg, edge_id) << " (-tsu)\" ]";
+                } else if(tg.node_type(node_id) == NodeType::CPIN && tg.node_type(sink_node_id) == NodeType::SOURCE) {
+                    os << " [ label=\"" << edge_id << "\n" << delay_calc->max_edge_delay(tg, edge_id) << " (tcq)\" ]";
+                } else {
+                    os << " [ label=\"" << edge_id << "\n" << delay_calc->max_edge_delay(tg, edge_id) << "\" ]";
                 }
             }
+            os << ";" <<std::endl;
         }
     }
 
@@ -136,19 +133,18 @@ void write_dot_file_setup(std::string filename,
 template<class DelayCalc=FixedDelayCalculator>
 void write_dot_file_hold(std::string filename, 
                          const TimingGraph& tg, 
-                         std::shared_ptr<const TimingAnalyzer> analyzer = std::shared_ptr<const TimingAnalyzer>(), 
                          std::shared_ptr<DelayCalc> delay_calc = std::shared_ptr<DelayCalc>(),
+                         std::shared_ptr<const TimingAnalyzer> analyzer = std::shared_ptr<const TimingAnalyzer>(), 
                          std::vector<NodeId> nodes = std::vector<NodeId>()) {
 
     if(tg.nodes().size() > 1000 && nodes.empty()) {
-        std::cout << "Skipping setup dot file due to large timing graph size\n"; 
+        std::cout << "Skipping hold dot file due to large timing graph size\n"; 
         return;
     }
 
     std::ofstream os(filename);
 
     auto hold_analyzer = std::dynamic_pointer_cast<const HoldTimingAnalyzer>(analyzer);
-    if(!hold_analyzer) return;
 
     //Write out a dot file of the timing graph
     os << "digraph G {" <<std::endl;
@@ -163,29 +159,31 @@ void write_dot_file_hold(std::string filename,
         os << "\tnode" << size_t(inode);
         os << "[label=\"";
         os << "{" << inode << " (" << tg.node_type(inode) << ")";
-        for(const TimingTag& tag : hold_analyzer->hold_tags(inode)) {
-            os << " | {";
-            os << tag.type() << "\\n";
-            if(!tag.launch_clock_domain()) {
-                os << "*";
-            } else {
-                os << tag.launch_clock_domain();
+        if(hold_analyzer) {
+            for(const TimingTag& tag : hold_analyzer->hold_tags(inode)) {
+                os << " | {";
+                os << tag.type() << "\\n";
+                if(!tag.launch_clock_domain()) {
+                    os << "*";
+                } else {
+                    os << tag.launch_clock_domain();
+                }
+                os << " to ";
+                if(!tag.capture_clock_domain()) {
+                    os << "*";
+                } else {
+                    os << tag.capture_clock_domain();
+                }
+                if(tag.type() == TagType::CLOCK_LAUNCH || tag.type() == TagType::CLOCK_CAPTURE || tag.type() == TagType::DATA_ARRIVAL) {
+                    os << " from ";
+                } else {
+                    os << " for ";
+                }
+                os << tag.origin_node();
+                os << "\\n";
+                os << " time: " << tag.time().value();
+                os << "}";
             }
-            os << " to ";
-            if(!tag.capture_clock_domain()) {
-                os << "*";
-            } else {
-                os << tag.capture_clock_domain();
-            }
-            if(tag.type() == TagType::CLOCK_LAUNCH || tag.type() == TagType::CLOCK_CAPTURE || tag.type() == TagType::DATA_ARRIVAL) {
-                os << " from ";
-            } else {
-                os << " for ";
-            }
-            os << tag.origin_node();
-            os << "\\n";
-            os << " time: " << tag.time().value();
-            os << "}";
         }
         os << "}\"]";
         os <<std::endl;
@@ -204,27 +202,23 @@ void write_dot_file_hold(std::string filename,
     }
 
     //Add edges with delays annoated
-    for(const LevelId ilevel : tg.levels()) {
-        for(NodeId node_id : tg.level_nodes(ilevel)) {
-            for(EdgeId edge_id : tg.node_out_edges(node_id)) {
+    for(EdgeId edge_id : tg.edges()) {
+        NodeId node_id = tg.edge_src_node(edge_id);
+        NodeId sink_node_id = tg.edge_sink_node(edge_id);
 
-                NodeId sink_node_id = tg.edge_sink_node(edge_id);
-
-                if(std::binary_search(nodes.begin(), nodes.end(), node_id)
-                   && std::binary_search(nodes.begin(), nodes.end(), sink_node_id)) {
-                    os << "\tnode" << size_t(node_id) << " -> node" << size_t(sink_node_id);
-                    if(delay_calc) {
-                        if(tg.node_type(node_id) == NodeType::CPIN && tg.node_type(sink_node_id) == NodeType::SINK) {
-                            os << " [ label=\"" << delay_calc->hold_time(tg, edge_id) << " (thld)\" ]";
-                        } else if(tg.node_type(node_id) == NodeType::CPIN && tg.node_type(sink_node_id) == NodeType::SOURCE) {
-                            os << " [ label=\"" << delay_calc->min_edge_delay(tg, edge_id) << " (tcq)\" ]";
-                        } else {
-                            os << " [ label=\"" << delay_calc->min_edge_delay(tg, edge_id) << "\" ]";
-                        }
-                    }
-                    os << ";" <<std::endl;
+        if(std::binary_search(nodes.begin(), nodes.end(), node_id)
+           && std::binary_search(nodes.begin(), nodes.end(), sink_node_id)) {
+            os << "\tnode" << size_t(node_id) << " -> node" << size_t(sink_node_id);
+            if(delay_calc) {
+                if(tg.node_type(node_id) == NodeType::CPIN && tg.node_type(sink_node_id) == NodeType::SINK) {
+                    os << " [ label=\"" << edge_id << "\n" << delay_calc->hold_time(tg, edge_id) << " (thld)\" ]";
+                } else if(tg.node_type(node_id) == NodeType::CPIN && tg.node_type(sink_node_id) == NodeType::SOURCE) {
+                    os << " [ label=\"" << edge_id << "\n" << delay_calc->min_edge_delay(tg, edge_id) << " (tcq)\" ]";
+                } else {
+                    os << " [ label=\"" << edge_id << "\n" << delay_calc->min_edge_delay(tg, edge_id) << "\" ]";
                 }
             }
+            os << ";" <<std::endl;
         }
     }
 
