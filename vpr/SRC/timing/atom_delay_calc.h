7d515b83b98de4a48598a622a86833ebbe8f6940
kmurray <k.murray@utoronto.ca>
2017-01-31 15:52:36 -0500

    Remove unsued caching atom and clb delay calculators
    Caching is now handled by the main delay calculator

--- vpr/SRC/timing/atom_delay_calc.h
+++ vpr/SRC/timing/atom_delay_calc.h
@@ -19,35 +19,6 @@ class AtomDelayCalc {
         const AtomMap& netlist_map_;
 };
 
-//A version of AtomDelayCalc which caches edge delays for improved performance.
-//
-// If an edge delay changes it must be invalidated before the new delay value is returned.
-class CachingAtomDelayCalc {
-    public:
-        CachingAtomDelayCalc(const AtomNetlist& netlist, const AtomMap& netlist_map);
-        float atom_combinational_delay(const AtomPinId src_pin, const AtomPinId sink_pin) const;
-        float atom_setup_time(const AtomPinId clk_pin, const AtomPinId input_pin) const;
-        float atom_clock_to_q_delay(const AtomPinId clk_pin, const AtomPinId output_pin) const;
-
-        void invalidate_delay(const AtomPinId src_pin, const AtomPinId sink_pin);
-        void invalidate_all_delays();
-    private:
-        struct KeyHasher {
-            size_t operator()(const std::tuple<AtomPinId,AtomPinId> key) const {
-                size_t hash = 0;
-                vtr::hash_combine(hash, std::get<0>(key));
-                vtr::hash_combine(hash, std::get<1>(key));
-                return hash;
-            }
-        };
-        using Cache = std::unordered_map<std::tuple<AtomPinId,AtomPinId>,float,KeyHasher>;
-    private:
-        AtomDelayCalc raw_delay_calc_;
-
-        mutable Cache delay_cache_;
-
-};
-
 #include "atom_delay_calc.inl"
 
 #endif
--- vpr/SRC/timing/clb_delay_calc.h
+++ vpr/SRC/timing/clb_delay_calc.h
@@ -34,42 +34,6 @@ class ClbDelayCalc {
         IntraLbPbPinLookup intra_lb_pb_pin_lookup_;
 };
 
-class CachingClbDelayCalc {
-    public:
-        //Delay from a CLB input (driven outside the CLB) to specified internal sink pin
-        float clb_input_to_internal_sink_delay(const t_net_pin* clb_input_pin, int internal_sink_pin) const;
-
-        //Delay from an internal driver to the CLB's output pin
-        float internal_src_to_clb_output_delay(int internal_src_pin, const t_net_pin* clb_output_pin) const;
-
-        //Delay from an internal driver to an internal sink within the same CLB
-        float internal_src_to_internal_sink_delay(int clb, int internal_src_pin, int internal_sink_pin) const;
-
-        //Delay of a route-through connection from a CLB input to output
-        float clb_input_to_clb_output_delay(const t_net_pin* clb_input_pin, const t_net_pin* clb_output_pin) const;
-
-        void invalidate_delay(int clb, int src_pb_route_pin, int sink_pb_route_pin);
-        void invalidate_all_delays();
-    private:
-        float trace_max_delay(int clb, int src_pb_route_pin, int sink_pb_route_pin) const;
-    private:
-        struct KeyHasher {
-            size_t operator()(const std::tuple<int,int,int> key) const {
-                size_t hash = 0;
-                vtr::hash_combine(hash, std::get<0>(key));
-                vtr::hash_combine(hash, std::get<1>(key));
-                vtr::hash_combine(hash, std::get<2>(key));
-                return hash;
-            }
-        };
-        using Cache = std::unordered_map<std::tuple<int,int,int>,float,KeyHasher>;
-    private:
-        ClbDelayCalc raw_delay_calc_;
-
-
-        mutable Cache delay_cache_;
-};
-
 #include "clb_delay_calc.inl"
 
 #endif
