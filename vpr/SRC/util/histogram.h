ecb868e82ab9e41096c04caaf6e0525b4f43a599
kmurray <k.murray@utoronto.ca>
2017-02-01 18:56:08 -0500

    Improve histogram formatting

--- vpr/SRC/util/histogram.h
+++ vpr/SRC/util/histogram.h
@@ -5,14 +5,14 @@
 #include <vector>
 
 struct HistogramBucket {
-    HistogramBucket(float min_val, float max_val, float init_count = 0.)
+    HistogramBucket(float min_val, float max_val, float init_count = 0)
         : min_value(min_val)
         , max_value(max_val)
         , count(init_count) {}
 
     float min_value = std::numeric_limits<float>::quiet_NaN();
     float max_value = std::numeric_limits<float>::quiet_NaN();
-    float count = std::numeric_limits<float>::quiet_NaN();
+    size_t count = 0;
 
 };
 
