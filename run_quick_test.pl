7e0ba81ca027ca39e81affd5b0197065d68dd226
kmurray <k.murray@utoronto.ca>
2017-03-23 15:29:18 -0400

    Remove legacy quick_test; use the standard regression testing infrastructure

--- run_quick_test.pl
+++ run_quick_test.pl
@@ -18,42 +18,5 @@
 use strict;
 use Cwd;
 
-print "=============================================\n";
-print "Test Verilog-to-Routing Build Infrastructure\n";
-print "=============================================\n\n";
-
-system("rm -f quick_test/depth_split.blif");
-system("rm -f quick_test/abc_out.blif");
-system("rm -f quick_test/route.out");
-
-
-chdir ("quick_test") or die "Failed to change to directory ./quick_test: $!";
-
-print "Testing ODIN II: ";
-system("../ODIN_II/odin_II.exe -c depth_split.xml > odin_ii_out.txt 2>&1");
-if(-f "depth_split.blif") {
-	print " [PASSED]\n\n";
-} else {
-	print " [FAILED]\n\n";
-}
-
-print "Testing ABC: ";
-system("../abc_with_bb_support/abc -c \"read abc_test.blif;sweep;write_hie abc_test.blif abc_out.blif\" > abc_out.txt  2>&1" );
-if(-f "abc_out.blif") {
-	print " [PASSED]\n\n";
-} else {
-	print " [FAILED]\n\n";
-}
-
-print "Testing VPR: ";
-system("../vpr/vpr sample_arch.xml vpr_test -blif_file vpr_test.blif -route_file route.out -nodisp > vpr_out.txt 2>&1");
-if(-f "route.out") {
-	print " [PASSED]\n\n";
-} else {
-	print " [FAILED]\n\n";
-}
-
-chdir ("..");
-
-print "Test complete\n";
+system("./run_reg_test.pl vtr_reg_basic");
 
