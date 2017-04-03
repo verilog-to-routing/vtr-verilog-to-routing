62ac3cf5be4285188cc75cdfa2f3d1c1e94346da
kmurray <k.murray@utoronto.ca>
2017-04-03 11:19:32 -0400

    Make test build script more command-line driven

--- test_buld_configs.py
+++ test_buld_configs.py
@@ -8,13 +8,12 @@ from collections import OrderedDict
 
 DEFAULT_TARGETS_TO_BUILD=["vpr"]
 
-GNU_COMPILER_VERSIONS=["4.9", "5", "6"]
-CLANG_COMPILER_VERSIONS=[]
-#CLANG_COMPILER_VERSIONS=["3.6", "3.8"]
+DEFAULT_GNU_COMPILER_VERSIONS=["4.9", "5", "6"]
+DEFAULT_CLANG_COMPILER_VERSIONS=["3.6", "3.8"]
 
-EASYGL_CONFIGS = ["OFF", "ON"]
-VTR_ASSERT_LEVELS= ["0", "1", "2", "3"]
-TATUM_PARALLEL_CONFIGS = ["ON", "OFF"]
+DEFAULT_EASYGL_CONFIGS = ["OFF", "ON"]
+DEFAULT_TATUM_PARALLEL_CONFIGS = ["ON", "OFF"]
+DEFAULT_VTR_ASSERT_LEVELS= ["2", "3"]#, "1", "0"]
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Test building the specified targets with multiple compilers and build configurations")
@@ -22,11 +21,40 @@ def parse_args():
     parser.add_argument("targets", 
                         nargs="*",
                         default=DEFAULT_TARGETS_TO_BUILD,
-                        help="What targets to build (default: {})".format(DEFAULT_TARGETS_TO_BUILD))
+                        help="What targets to build (default: %(default)s)")
+    parser.add_argument("--gnu_versions", 
+                        nargs="*",
+                        default=DEFAULT_GNU_COMPILER_VERSIONS,
+                        metavar="GNU_VERSION",
+                        help="What versions of gcc/g++ to test (default: %(default)s)")
+    parser.add_argument("--clang_versions", 
+                        nargs="*",
+                        metavar="CLANG_VERSION",
+                        default=DEFAULT_CLANG_COMPILER_VERSIONS,
+                        help="What versions of clang/clang++ to test (default: %(default)s)")
+    parser.add_argument("--easygl_configs", 
+                        nargs="*",
+                        default=DEFAULT_EASYGL_CONFIGS,
+                        metavar="EASYGL_CONFIG",
+                        help="What EaysGL configurations to test (default: %(default)s)")
+    parser.add_argument("--tatum_parallel_configs", 
+                        nargs="*",
+                        default=DEFAULT_TATUM_PARALLEL_CONFIGS,
+                        metavar="TATUM_PARALLEL_CONFIG",
+                        help="What parallel tatum configurations to test (default: %(default)s)")
+    parser.add_argument("--vtr_assert_levels", 
+                        nargs="*",
+                        default=DEFAULT_VTR_ASSERT_LEVELS,
+                        metavar="VTR_ASSERT_LEVEL",
+                        help="What VTR assert levels to test (default: %(default)s)")
     parser.add_argument("-j", 
                         type=int,
                         default=1,
                         help="How many parallel build jobs to allow (passed to make)")
+    parser.add_argument("--exit_on_failure",
+                        action="store_true",
+                        default=False,
+                        help="Exit on first failure intead of continuing")
 
     return parser.parse_args()
 
@@ -36,23 +64,23 @@ def main():
 
     compilers_to_test = []
 
-    for gnu_version in GNU_COMPILER_VERSIONS:
+    for gnu_version in args.gnu_versions:
         cc = "gcc-" + gnu_version
         cxx = "g++-" + gnu_version
         compilers_to_test.append((cc, cxx))
 
-    for clang_version in CLANG_COMPILER_VERSIONS:
-        cc = "clang-" + gnu_version
-        cxx = "clang++-" + gnu_version
+    for clang_version in args.clang_versions:
+        cc = "clang-" + clang_version
+        cxx = "clang++-" + clang_version
         compilers_to_test.append((cc, cxx))
 
     #Test all the compilers with the default build config
     num_failed = 0
     num_configs = 0
     for cc, cxx in compilers_to_test:
-        for easygl_config in EASYGL_CONFIGS: 
-            for tatum_parallel_config in TATUM_PARALLEL_CONFIGS: 
-                for vtr_assert_level in VTR_ASSERT_LEVELS: 
+        for vtr_assert_level in args.vtr_assert_levels: 
+            for easygl_config in args.easygl_configs: 
+                for tatum_parallel_config in args.tatum_parallel_configs: 
                     num_configs += 1
                     config = OrderedDict()
                     config["EASYGL_ENABLE_GRAPHICS"] = easygl_config
@@ -64,49 +92,65 @@ def main():
                     if not success:
                         num_failed += 1
 
+                        if args.exit_on_failure:
+                            sys.exit(1)
+
     if num_failed != 0:
         print "Failed to build {} of {} configurations".format(num_failed, num_configs)
 
     sys.exit(num_failed)
 
 def build_config(args, cc, cxx, config):
-    print "Building with CC={} CXX={}:".format(cc, cxx)
-
-    build_dir = "build"
-
-    shutil.rmtree(build_dir, ignore_errors=True)
-
-    os.mkdir(build_dir)
-
-    #Copy the os environment
-    new_env = {}
-    new_env.update(os.environ)
-
-    #Override CC and CXX
-    new_env['CC'] = cc
-    new_env['CXX'] = cxx
-
-    #Run CMAKE
-
-    cmake_cmd = ["cmake"]
+    config_strs = []
     for key, value in config.iteritems():
-        cmake_cmd += ["-D{}={}".format(key, value)]
-    cmake_cmd += [".."]
+        config_strs += ["{}={}".format(key, value)]
 
-    print "  " + ' '.join(cmake_cmd)
-    subprocess.check_output(cmake_cmd, cwd=build_dir, env=new_env)
+    log_file = "build_{}_{}_{}.log".format(cc, cxx, '__'.join(config_strs))
 
-    #Run Make
-    build_cmd = ["make"]
-    build_cmd += ["-j", "{}".format(args.j)]
-    build_cmd += args.targets
+    with open(log_file, 'w') as f:
+        print "Building with CC={} CXX={}:".format(cc, cxx)
+        print >>f, "Building with CC={} CXX={}:".format(cc, cxx)
+        f.flush()
 
-    try:
+        build_dir = "build"
+
+        shutil.rmtree(build_dir, ignore_errors=True)
+
+        os.mkdir(build_dir)
+
+        #Copy the os environment
+        new_env = {}
+        new_env.update(os.environ)
+
+        #Override CC and CXX
+        new_env['CC'] = cc
+        new_env['CXX'] = cxx
+
+        #Run CMAKE
+
+        cmake_cmd = ["cmake"]
+        for key, value in config.iteritems():
+            cmake_cmd += ["-D{}={}".format(key, value)]
+        cmake_cmd += [".."]
+
+        print "  " + ' '.join(cmake_cmd)
+        print >>f, "  " + ' '.join(cmake_cmd)
+        f.flush()
+        subprocess.check_call(cmake_cmd, cwd=build_dir, stdout=f, stderr=f, env=new_env)
+
+        #Run Make
+        build_cmd = ["make"]
+        build_cmd += ["-j", "{}".format(args.j)]
+        build_cmd += args.targets
+
         print "  " + ' '.join(build_cmd)
-        output = subprocess.check_output(build_cmd, env=new_env)
-    except subprocess.CalledProcessError as e:
-        print "\tERROR"
-        return False
+        print >>f, "  " + ' '.join(build_cmd)
+        f.flush()
+        try:
+            subprocess.check_call(build_cmd, cwd=build_dir, stdout=f, stderr=f, env=new_env)
+        except subprocess.CalledProcessError as e:
+            print "\tERROR: see {}".format(log_file)
+            return False
 
     print "\tOK"
     return True
