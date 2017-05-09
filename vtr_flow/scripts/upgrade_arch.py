860ca94e4f7de540cacdb948cd00a695d559c06b
kmurray <k.murray@utoronto.ca>
2017-05-09 12:37:25 -0400

    Unify scripts for updating architecture files into upgrade_arch.py
    This script will try to upgrade legacy VTR architecture files to be
    compatible with the current VTR.
    
    Future changes to the architecture file format which can be
    automatically upgraded should be added to this script.

--- vtr_flow/scripts/upgrade_arch.py
+++ vtr_flow/scripts/upgrade_arch.py
@@ -21,10 +21,22 @@ class ModelTiming:
         self.sequential_ports = set()
         self.comb_edges = {}
 
+#To add support for new upgrades/features add the identifier string here
+#and added a guarded call in main()
+supported_upgrades = [
+    "add_model_timing",
+    "upgrade_fc_overrides"
+]
+
 def parse_args():
-    parser = argparse.ArgumentParser(description="Update VTR architecture files to use the updated <fc_override> tags")
+    parser = argparse.ArgumentParser(description="Upgrade Legacy VTR architecture files")
 
     parser.add_argument("xml_file", help="xml_file to update (modified in place)")
+    parser.add_argument("--features",
+                        nargs="*",
+                        help="List of features/upgrades to apply (default: %(default)s)",
+                        choices=supported_upgrades,
+                        default=supported_upgrades)
     parser.add_argument("--debug", default=False, action="store_true", help="Print to stdout instead of modifying file inplace")
     parser.add_argument("--pretty", default=False, action="store_true", help="Pretty print the output?")
 
@@ -37,16 +49,95 @@ def main():
     parser = ET.XMLParser()
     root = ET.parse(args.xml_file, parser)
 
-
-    changed = False
-
     root_tags = root.findall(".")
     assert len(root_tags) == 1
     arch = root_tags[0]
     assert arch.tag == "architecture"
 
-    fc_tags = root.findall(".//fc")
+    modified = False
+    if "add_model_timing" in args.features:
+        modified = modified or add_model_timing(arch)
 
+    if "upgrade_fc_overrides" in args.features:
+        modified = modified or upgrade_fc_overrides(arch)
+
+    if modified:
+        if args.debug:
+            root.write(sys.stdout, pretty_print=args.pretty)
+        else:
+            with open(args.xml_file, "w") as f:
+                root.write(f, pretty_print=args.pretty)
+
+def add_model_timing(arch):
+    models = arch.findall("./models/model")
+
+    #Find all primitive pb types
+    prim_pbs = arch.findall(".//pb_type[@blif_model]")
+
+    #Build up the timing specifications from 
+    default_models = frozenset([".input", ".output", ".latch", ".names"])
+    primitive_timing_specs = {}
+    for prim_pb in prim_pbs:
+
+        blif_model = prim_pb.attrib['blif_model']
+
+        if blif_model in default_models:
+            continue
+
+        assert blif_model.startswith(".subckt ")
+        blif_model = blif_model[len(".subckt "):]
+
+        if blif_model not in primitive_timing_specs:
+            primitive_timing_specs[blif_model] = ModelTiming()
+
+        #Find combinational edges
+        for delay_const in prim_pb.findall("./delay_constant"):
+            iports = get_port_names(delay_const.attrib['in_port'])
+            oports = get_port_names(delay_const.attrib['out_port'])
+
+            for iport in iports:
+                if iport not in primitive_timing_specs[blif_model].comb_edges:
+                    primitive_timing_specs[blif_model].comb_edges[iport] = set()
+
+                for oport in oports:
+                    primitive_timing_specs[blif_model].comb_edges[iport].add(oport)
+
+        #Find sequential ports
+        for xpath_pattern in ["./T_setup", "./T_clock_to_Q"]:
+            for seq_tag in prim_pb.findall(xpath_pattern):
+                ports = get_port_names(seq_tag.attrib['port'])
+                for port in ports:
+                    clk = seq_tag.attrib['clock']
+
+                    primitive_timing_specs[blif_model].sequential_ports.add((port, clk))
+
+    changed = False
+    for model in models:
+        if model.attrib['name'] not in primitive_timing_specs:
+            print "Warning: no timing specifications found for {}".format(mode.attrib['name'])
+            continue
+
+        model_timing = primitive_timing_specs[model.attrib['name']]
+
+        #Mark model ports as sequential
+        for port_name, clock_name in model_timing.sequential_ports:
+            port = model.find(".//port[@name='{}']".format(port_name))
+            port.attrib['clock'] = clock_name
+            changed = True
+
+        #Mark combinational edges from sources to sink ports
+        for src_port, sink_ports in model_timing.comb_edges.iteritems():
+            xpath_pattern = "./input_ports/port[@name='{}']".format(src_port)
+            port = model.find(xpath_pattern)
+            port.attrib['combinational_sink_ports'] = ' '.join(sink_ports)
+            changed = True
+
+    return changed
+
+def upgrade_fc_overrides(arch):
+    fc_tags = arch.findall(".//fc")
+
+    changed = False
     for fc_tag in fc_tags:
         assert fc_tag.tag == "fc"
 
@@ -110,14 +201,9 @@ def main():
                 fc_override = ET.SubElement(fc_tag, "fc_override", attrib=new_attrib)
         
             changed = True
-        
-    if changed:
-        if args.debug:
-            root.write(sys.stdout, pretty_print=args.pretty)
-        else:
-            with open(args.xml_file, "w") as f:
-                root.write(f, pretty_print=args.pretty)
+    return changed
 
+        
 def get_port_names(string):
     ports = []
 
