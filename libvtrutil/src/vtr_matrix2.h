f74f6cf926c5b989a9adcf9d056b3e4fe9b3ad70
kmurray <k.murray@utoronto.ca>
2017-05-12 12:03:37 -0400

    Add specization for N=1 to reduce code duplication

--- libvtrutil/src/vtr_matrix2.h
+++ libvtrutil/src/vtr_matrix2.h
@@ -31,8 +31,8 @@ class DimRange {
 //Each instance of this class peels off one-dimension and returns a MatrixProxy representing
 //the resulting sub-matrix. This is repeated recursively until we hit the 1-dimensional base-case.
 //
-//Since this expansion happens at compiler time all the proxy classes get optimized away, high performance
-//despite the generality.
+//Since this expansion happens at compiler time all the proxy classes get optimized away, 
+//yielding both high performance and generality.
 //
 //Recursive case: N-dimensional array
 template<typename T, size_t N>
@@ -118,6 +118,133 @@ class MatrixProxy<T,1> {
         T* start_;
 };
 
+//Base class for an n-dimensional matrix supporting arbitrary index ranges per dimension.
+//This class implements all of the matrix handling (lifetime etc.) except for indexing 
+//(which is implemented in the Matrix class). Indexing is split out to allows specialization 
+//of indexing for N = 1.
+//
+//Implementation:
+//
+//This class uses a single linear array to store the matrix in c-style (row major) 
+//order. That is, the right-most index is laid out contiguous memory.
+//
+//This should improve memory usage (no extra pointers to store for each dimension), 
+//and cache locality (less indirection via pointers, predictable strides).
+//
+//The indicies are calculated based on the dimensions to access the appropriate elements.
+//Since the indexing calculations are visible to the compiler at compile time they can be
+//optimized to be efficient.
+template<typename T, size_t N>
+class MatrixBase {
+    public:
+        static_assert(N >= 1, "Minimum dimension 1");
+
+        //Specified dimension sizes:
+        //      [0..dim_sizes[0]-1]
+        //      [0..dim_sizes[1]-1]
+        //      ...
+        //with optional fill value
+        MatrixBase(std::array<size_t,N> dim_sizes, T value=T()) {
+            //Convert dimension to range [0..dim-1]
+            for(size_t i = 0; i < dim_sizes.size(); ++i) {
+                dim_ranges_[i] = {0, dim_sizes[i]-1};
+            }
+            alloc();
+            fill(value);
+        }
+
+        //Specified dimension index ranges:
+        //      [dim_ranges[0].min_index() ... dim_ranges[1].max_index()]
+        //      [dim_ranges[1].min_index() ... dim_ranges[1].max_index()]
+        //      ...
+        //with optional fill value
+        MatrixBase(std::array<DimRange,N> dim_ranges, T value=T())
+            : dim_ranges_(dim_ranges) {
+            alloc();
+            fill(value);
+        }
+    public: //Accessors
+        //Returns the size (number of elements) in the matrix
+        size_t size() const {
+            //Size is the product of all dimension sizes
+            size_t cnt = dim_size(0);
+            for (size_t idim = 1; idim < ndims(); ++idim) {
+                cnt *= dim_size(idim);
+            }
+            return cnt;
+        }
+
+        //Returns the number of dimensions (i.e. N)
+        size_t ndims() const {
+            return dim_ranges_.size();
+        }
+
+        //Returns the size of the ith dimension
+        size_t dim_size(size_t i) const {
+            VTR_ASSERT_SAFE(i < ndims());
+
+            return dim_ranges_[i].size();
+        }
+
+        //Returns the starting index of ith dimension
+        size_t begin_index(size_t i) const {
+            VTR_ASSERT_SAFE(i < ndims());
+
+            return dim_ranges_[i].begin_index();
+        }
+
+        //Returns the one-past-the-end index of the ith dimension
+        size_t end_index(size_t i) const {
+            VTR_ASSERT_SAFE(i < ndims());
+
+            return dim_ranges_[i].end_index();
+        }
+
+    public: //Mutators
+
+        //Set all elements to 'value'
+        void fill(T value) {
+            std::fill(data_.get(), data_.get() + size(), value);
+        }
+
+    public: //Lifetime management
+        //Copy constructor
+        MatrixBase(const MatrixBase& other)
+            : MatrixBase(other.dim_ranges_) {
+            std::copy(other.data_.get(), other.data_.get() + other.size(), data_.get());
+        }
+
+        //Move constructor
+        MatrixBase(MatrixBase&& other)
+            : MatrixBase() {
+            swap(*this, other);
+        }
+
+        //Copy/move assignment
+        //
+        //Note that rhs is taken by value (copy-swap idiom)
+        MatrixBase& operator=(MatrixBase rhs) {
+            swap(*this, rhs);
+            return *this;
+        }
+
+        //Swap two MatrixBase objects
+        friend void swap(MatrixBase<T,N>& m1, MatrixBase<T,N>& m2) {
+            using std::swap;
+            swap(m1.dim_ranges, m2.dim_ranges);
+            swap(m1.data_, m2.data_);
+        }
+
+    private:
+        //Allocate space for all the elements
+        void alloc() {
+            data_.reset(new T[size()]);
+        }
+
+    protected:
+        std::array<DimRange,N> dim_ranges_ = {};
+        std::unique_ptr<T[]> data_ = nullptr;
+};
 
 //An n-dimensional matrix supporting arbitrary index ranges per dimension
 //
@@ -150,105 +277,40 @@ class MatrixProxy<T,1> {
 //      //initialized to 42
 //      Matrix<int,2> m1({5,10}, 42);
 //
-//Implementation:
-//
-//This class uses a single linear array to store the matrix in c-style (row major) 
-//order. That is, the right-most index is laid out contiguous memory.
-//
-//This should improve memory usage (no extra pointers to store for each dimension), 
-//and cache locality (less indirection via pointers, predictable strides).
-//
-//The indicies are calculated based on the dimensions to access the appropriate elements.
-//Since the indexing calculations are visible to the compiler at compile time they can be
-//optimized to be efficient.
 template<typename T, size_t N>
-class Matrix {
+class Matrix : public MatrixBase<T,N> {
+    //General case
+    static_assert(N >= 2, "Minimum dimension 2");
+
     public:
-        static_assert(N >= 2, "Minimum dimension 2");
-
-        //Specified dimension sizes:
-        //      [0..dim_sizes[0]-1]
-        //      [0..dim_sizes[1]-1]
-        //      ...
-        //with optional fill value
-        Matrix(std::array<size_t,N> dim_sizes, T value=T()) {
-            //Convert dimension to range [0..dim-1]
-            for(size_t i = 0; i < dim_sizes.size(); ++i) {
-                dim_ranges_[i] = {0, dim_sizes[i]-1};
-            }
-            alloc();
-            fill(value);
-        }
-
-        //Specified dimension index ranges:
-        //      [dim_ranges[0].min_index() ... dim_ranges[1].max_index()]
-        //      [dim_ranges[1].min_index() ... dim_ranges[1].max_index()]
-        //      ...
-        //with optional fill value
-        Matrix(std::array<DimRange,N> dim_ranges, T value=T())
-            : dim_ranges_(dim_ranges) {
-            alloc();
-            fill(value);
-        }
-    public: //Accessors
-
+        //Use the base constructors
+        using MatrixBase<T,N>::MatrixBase;
+    public:
         //Access an element
         //
-        //Returns a proxy-object to allow chained array-style indexing 
+        //Returns a proxy-object to allow chained array-style indexing  (N >= 2 case)
+        //template<typename = typename std::enable_if<N >= 2>::type, typename T1=T>
         const MatrixProxy<T,N-1> operator[](size_t index) const {
-            VTR_ASSERT_SAFE_MSG(index >= dim_ranges_[0].begin_index(), "Index out of range (below dimension minimum)");
-            VTR_ASSERT_SAFE_MSG(index < dim_ranges_[0].end_index(), "Index out of range (above dimension maximum)");
+            VTR_ASSERT_SAFE_MSG(index >= this->dim_ranges_[0].begin_index(), "Index out of range (below dimension minimum)");
+            VTR_ASSERT_SAFE_MSG(index < this->dim_ranges_[0].end_index(), "Index out of range (above dimension maximum)");
 
             //The elements are stored in zero-indexed form, so adjust for any 
             //non-zero minimum index in this dimension
-            size_t effective_index = index - dim_ranges_[0].begin_index();
+            size_t effective_index = index - this->dim_ranges_[0].begin_index();
 
             //Calculate the stride for the current dimension
-            size_t dim_stride = size() / dim_size(0);
+            size_t dim_stride = this->size() / this->dim_size(0);
 
             //Calculate the stride for the next dimension
-            size_t next_dim_stride = dim_stride / dim_size(1);
+            size_t next_dim_stride = dim_stride / this->dim_size(1);
 
             //Peel off the first dimension
-            return MatrixProxy<T,N-1>(dim_ranges_.data(),                        //Pass the dimension information
-                                      1,                                         //Pass the next dimension
-                                      next_dim_stride,                           //Pass the stride for the next dimension
-                                      data_.get() + dim_stride*effective_index); //Advance to index in this dimension
+            return MatrixProxy<T,N-1>(this->dim_ranges_.data(),                        //Pass the dimension information
+                                      1,                                               //Pass the next dimension
+                                      next_dim_stride,                                 //Pass the stride for the next dimension
+                                      this->data_.get() + dim_stride*effective_index); //Advance to index in this dimension
         }
 
-        //Returns the size (number of elements) in the matrix
-        size_t size() const {
-            //Size is the product of all dimension sizes
-            size_t cnt = dim_size(0);
-            for (size_t idim = 1; idim < ndims(); ++idim) {
-                cnt *= dim_size(idim);
-            }
-            return cnt;
-        }
-
-        //Returns the number of dimensions (i.e. N)
-        size_t ndims() const {
-            return dim_ranges_.size();
-        }
-
-        //Returns the size of the ith dimension
-        size_t dim_size(size_t i) const {
-            VTR_ASSERT_SAFE(i < ndims());
-
-            return dim_ranges_[i].size();
-        }
-
-        //Returns the starting index of ith dimension
-        size_t begin_index(size_t i) const {
-            return dim_ranges_[i].begin_index();
-        }
-
-        //Returns the one-past-the-end index of the ith dimension
-        size_t end_index(size_t i) const {
-            return dim_ranges_[i].end_index();
-        }
-
-    public: //Mutators
         //Access an element
         //
         //Returns a proxy-object to allow chained array-style indexing 
@@ -256,51 +318,30 @@ class Matrix {
             //Call the const version, since returned by value don't need to worry about const
             return const_cast<const Matrix<T,N>*>(this)->operator[](index);
         }
- 
-
-        //Set all elements to 'value'
-        void fill(T value) {
-            std::fill(data_.get(), data_.get() + size(), value);
-        }
-
-    public: //Lifetime management
-        //Copy constructor
-        Matrix(const Matrix& other)
-            : Matrix(other.dim_ranges_) {
-            std::copy(other.data_.get(), other.data_.get() + other.size(), data_.get());
-        }
-
-        //Move constructor
-        Matrix(Matrix&& other)
-            : Matrix() {
-            swap(*this, other);
-        }
-
-        //Copy/move assignment
-        //
-        //Note that rhs is taken by value (copy-swap idiom)
-        Matrix& operator=(Matrix rhs) {
-            swap(*this, rhs);
-            return *this;
-        }
-
-        //Swap two Matrix objects
-        friend void swap(Matrix<T,N>& m1, Matrix<T,N>& m2) {
-            using std::swap;
-            swap(m1.dim_ranges, m2.dim_ranges);
-            swap(m1.data_, m2.data_);
-        }
-    private:
-        //Allocate space for all the elements
-        void alloc() {
-            data_.reset(new T[size()]);
-        }
-
-    private:
-        std::array<DimRange,N> dim_ranges_ = {};
-        std::unique_ptr<T[]> data_ = nullptr;
 };
 
+template<typename T>
+class Matrix<T,1> : public MatrixBase<T,1> {
+    //Specialization for N = 1
+    public:
+        //Use the base constructors
+        using MatrixBase<T,1>::MatrixBase;
+    public:
+        //Access an element (immutable)
+        const T& operator[](size_t index) const {
+            VTR_ASSERT_SAFE_MSG(index >= this->dim_ranges_[0].begin_index(), "Index out of range (below dimension minimum)");
+            VTR_ASSERT_SAFE_MSG(index < this->dim_ranges_[0].end_index(), "Index out of range (above dimension maximum)");
+
+            return this->data_[index];
+        }
+
+        //Access an element (mutable)
+        T& operator[](size_t index) {
+            //Call the const version, and cast away const-ness
+            return const_cast<T&>(const_cast<const Matrix<T,1>*>(this)->operator[](index));
+        }
+
+};
 
 } //namespace
 #endif
