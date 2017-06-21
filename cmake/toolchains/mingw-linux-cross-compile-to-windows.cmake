6c9375f657439bb0c6fd5a1506c699cbd46c3e50
kmurray <k.murray@utoronto.ca>
2017-06-21 14:33:32 -0500

    Add option to statically link system libraries when cross-compiling to Windows
    Using the mingw toolchain file, the option STATIC_LIBGCC_LIBSTDCPP will
    statically link libgcc and libstdc++ so that no additional DLLs are
    required on the target machine.
    
    The option defaults to off, since statically linking these libraries may
    cause issues with exception handling across shared libraries.

--- cmake/toolchains/mingw-linux-cross-compile-to-windows.cmake
+++ cmake/toolchains/mingw-linux-cross-compile-to-windows.cmake
@@ -1,7 +1,23 @@
 # the name of the target operating system
 SET(CMAKE_SYSTEM_NAME Windows)
 
+option(STATIC_LIBGCC_LIBSTDCPP "Link to libgcc and libstdc++ statically. If not set, requires libgcc and libstdc++ DLLs to be installed on the target machines. Note that static linking libgcc means exceptions will not propagate accross shared library boundaries." OFF) 
+
+#Note that if libgcc and libstdcpp are NOT statically linked the appropriate
+#DLL's should be installed on the target Windows machine, or distributed with
+#the generated executables (e.g. in the same directory as the exectuable). 
+#
+#They are usually installed under:
+#   /usr/lib/gcc/${COMPILER_PEFIX}/*-win32/libgcc*.dll
+#   /usr/lib/gcc/${COMPILER_PEFIX}/*-win32/libstdc++*.dll
+#
+#For example targetting x86_64 with MingW based on gcc 5.3
+#   /usr/lib/gcc/x86_64-w64-mingw32/5.3-win32/libstdc++*.dll
+#   /usr/lib/gcc/x86_64-w64-mingw32/5.3-win32/libgcc*.dll
+
+#
 # Choose an appropriate compiler prefix
+#
 
 # for classical mingw32
 # see http://www.mingw.org/
@@ -9,8 +25,8 @@ SET(CMAKE_SYSTEM_NAME Windows)
 
 # for 32 or 64 bits mingw-w64
 # see http://mingw-w64.sourceforge.net/
-#set(COMPILER_PREFIX "i686-w64-mingw32")
-set(COMPILER_PREFIX "x86_64-w64-mingw32")
+#set(COMPILER_PREFIX "i686-w64-mingw32") #32-bit
+set(COMPILER_PREFIX "x86_64-w64-mingw32") #64-bit
 
 # which compilers to use for C and C++
 find_program(CMAKE_RC_COMPILER NAMES ${COMPILER_PREFIX}-windres)
@@ -21,7 +37,9 @@ find_program(CMAKE_CXX_COMPILER NAMES ${COMPILER_PREFIX}-g++)
 #SET(CMAKE_CXX_COMPILER ${COMPILER_PREFIX}-g++)
 
 
-# here is the target environment located
+#
+# Set the target environment location
+#
 SET(USER_ROOT_PATH )
 SET(CMAKE_FIND_ROOT_PATH  /usr/${COMPILER_PREFIX} ${USER_ROOT_PATH})
 
@@ -32,8 +50,14 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-#The current version of ABC does some unsafe conversions 
+#The current version of ABC does some unsafe casting 
 #(e.g. 64-bit pointer to 32-bit unsigned), since it assumes
-#an LP64 OS, but Windows is LLP64 (this is fixed in the latest ABC).
+#an LP64 OS, but Windows is LLP64.
 #For now, just turn on permissive mode to let things compile
+#TODO: these issues are fixed in the latest ABC, so this option should be removed when ABC is upgraded
 add_compile_options(-fpermissive)
+
+if (STATIC_LIBGCC_LIBSTDCPP)
+    #Statically link to libgcc and libstdc++ to avoid DLL dependencies
+    link_libraries(-static-libgcc -static-libstdc++)
+endif()
