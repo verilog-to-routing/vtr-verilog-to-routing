dc01de68aa3d5d066b8b0983a6ab1369feec4853
kmurray <k.murray@utoronto.ca>
2017-06-21 17:09:56 -0500

    Allow MinGW compilers to be overridden when cross-compiling for Windows

--- cmake/toolchains/mingw-linux-cross-compile-to-windows.cmake
+++ cmake/toolchains/mingw-linux-cross-compile-to-windows.cmake
@@ -18,30 +18,27 @@ option(STATIC_LIBGCC_LIBSTDCPP "Link to libgcc and libstdc++ statically. If not 
 #
 # Choose an appropriate compiler prefix
 #
+# for classical mingw32 (http://www.mingw.org/) set:
+#       "i586-mingw32msvc"
+#
+# for 32 or 64 bit mingw-w64 (http://mingw-w64.sourceforge.net/) set:
+#       "i686-w64-mingw32" or "x86_64-w64-mingw32"
+set(COMPILER_PREFIX "x86_64-w64-mingw32" CACHE STRING "Default prefix for MinGW gcc/g++/windres")
 
-# for classical mingw32
-# see http://www.mingw.org/
-#set(COMPILER_PREFIX "i586-mingw32msvc")
-
-# for 32 or 64 bits mingw-w64
-# see http://mingw-w64.sourceforge.net/
-#set(COMPILER_PREFIX "i686-w64-mingw32") #32-bit
-set(COMPILER_PREFIX "x86_64-w64-mingw32") #64-bit
+set(MINGW_ROOT_PATH /usr/${COMPILER_PREFIX} CACHE STRING "MinGW path to target environment directory (containing include and lib)")
+set(MINGW_RC ${COMPILER_PREFIX}-windres CACHE STRING "MinGW RC Compiler")
+set(MINGW_CC ${COMPILER_PREFIX}-gcc CACHE STRING "MinGW C Compiler")
+set(MINGW_CXX ${COMPILER_PREFIX}-g++ CACHE STRING "MinGW C++ Compiler")
 
 # which compilers to use for C and C++
-find_program(CMAKE_RC_COMPILER NAMES ${COMPILER_PREFIX}-windres)
-find_program(CMAKE_C_COMPILER NAMES ${COMPILER_PREFIX}-gcc)
-find_program(CMAKE_CXX_COMPILER NAMES ${COMPILER_PREFIX}-g++)
-#SET(CMAKE_RC_COMPILER ${COMPILER_PREFIX}-windres)
-#SET(CMAKE_C_COMPILER ${COMPILER_PREFIX}-gcc)
-#SET(CMAKE_CXX_COMPILER ${COMPILER_PREFIX}-g++)
-
+find_program(CMAKE_RC_COMPILER NAMES ${MINGW_RC})
+find_program(CMAKE_C_COMPILER NAMES ${MINGW_CC})
+find_program(CMAKE_CXX_COMPILER NAMES ${MINGW_CXX})
 
 #
 # Set the target environment location
 #
-SET(USER_ROOT_PATH )
-SET(CMAKE_FIND_ROOT_PATH  /usr/${COMPILER_PREFIX} ${USER_ROOT_PATH})
+SET(CMAKE_FIND_ROOT_PATH ${MINGW_ROOT_PATH})
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
