bd4dd3a5de8fff9731947fcf64d3928d6aa61780
jeanlego <jeanphilippe.legaul@gmail.com>
2017-06-23 23:44:09 +0000

    remove deleted files from include

--- ODIN_II/SRC/odin_ii.c
+++ ODIN_II/SRC/odin_ii.c
@@ -22,10 +22,12 @@ OTHER DEALINGS IN THE SOFTWARE.
 */ 
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
+#include <sstream>
 
 #include "vtr_error.h"
+
+#include "argparse.hpp"
 
 #include "globals.h"
 #include "types.h"
@@ -34,7 +36,6 @@ OTHER DEALINGS IN THE SOFTWARE.
 #include "parse_making_ast.h"
 #include "netlist_create_from_ast.h"
 #include "ast_util.h"
-#include "netlist_optimizations.h"
 #include "read_xml_config_file.h"
 #include "read_xml_arch_file.h"
 #include "partial_map.h"
@@ -59,7 +60,7 @@ global_args_t global_args;
 t_type_descriptor* type_descriptors;
 int block_tag;
 
-void set_default_options();
+void set_default_config();
 void get_options(int argc, char **argv);
 void print_usage();
 
@@ -78,7 +79,7 @@ int main(int argc, char **argv)
 	printf("Email: jamieson.peter@gmail.com and ken@unb.ca for support issues\n\n");
 
 	/* Set up the global arguments to their default. */
-	set_default_options();
+	set_default_config();
 	
 	/* get the command line options */
 	get_options(argc, argv);
@@ -104,9 +105,9 @@ int main(int argc, char **argv)
 	/* do High level Synthesis */
 	if (!global_args.blif_file)
 	{
-		
+
 		double elaboration_time = wall_time();
-	
+
 		printf("--------------------------------------------------------------------\n");
 		printf("High-level synthesis Begin\n");
 		/* Perform any initialization routines here */
@@ -114,6 +115,7 @@ int main(int argc, char **argv)
 		find_hard_adders();
 		//find_hard_adders_for_sub();
 		register_hard_blocks();
+	
 		global_param_table_sc = sc_new_string_cache();
 	
 		/* parse to abstract syntax tree */
@@ -124,10 +126,8 @@ int main(int argc, char **argv)
 		 * function void next_parsed_verilog_file(ast_node_t *file_items_list) */
 	
 		/* after the ast is made potentially do tagging for downstream links to verilog */
-		if (global_args.high_level_block != NULL)
-		{
+		if (global_args.high_level_block)
 			add_tag_data();
-		}
 	
 		/* Now that we have a parse tree (abstract syntax tree [ast]) of
 		 * the Verilog we want to make into a netlist. */
@@ -137,12 +137,11 @@ int main(int argc, char **argv)
 	
 		// Can't levelize yet since the large muxes can look like combinational loops when they're not
 		check_netlist(verilog_netlist);
-	
+
 		//START ################# NETLIST OPTIMIZATION ############################
-		
+	
 			/* point for all netlist optimizations. */
 			printf("Performing Optimizations of the Netlist\n");
-			netlist_optimizations_top(verilog_netlist);
 			/* Perform a splitting of the multipliers for hard block mults */
 		    reduce_operations(verilog_netlist, MULTIPLY);
 			iterate_multipliers(verilog_netlist);
@@ -161,15 +160,11 @@ int main(int argc, char **argv)
 			reduce_operations(verilog_netlist, MINUS);
 			iterate_adders_for_sub(verilog_netlist);
 			clean_adders_for_sub();
-			
-		//END ################# NETLIST OPTIMIZATION ############################
 		
+		//END ################# NETLIST OPTIMIZATION ############################
 	
 		if (configuration.output_netlist_graphs )
-		{
-			/* Path is where we are */
-			graphVizOutputNetlist(configuration.debug_output_path, "optimized", 1, verilog_netlist);
-		}
+			graphVizOutputNetlist(configuration.debug_output_path, "optimized", 1, verilog_netlist); /* Path is where we are */
 	
 		//*******
 	
@@ -182,7 +177,7 @@ int main(int argc, char **argv)
 	
 		/* point for outputs.  This includes soft and hard mapping all structures to the
 		 * target format.  Some of these could be considred optimizations */
-		printf("Outputting the netlist to blif output format\n");
+		printf("Outputting the netlist to the specified output format\n");
 		output_blif(global_args.output_file, verilog_netlist);
 	
 		elaboration_time = wall_time() - elaboration_time;
@@ -194,7 +189,6 @@ int main(int argc, char **argv)
 	
 		// FIXME: free contents?
 		sc_free_string_cache(global_param_table_sc);
-		
 		
 	}
 	else
@@ -274,143 +268,176 @@ void print_usage()
 	fflush(stdout);
 }
 
+struct ParseInitRegState {
+    int from_str(std::string str) {
+        if      (str == "0") return 0;
+        else if (str == "1") return 1;
+        else if (str == "X") return -1;
+        std::stringstream msg;
+        msg << "Invalid conversion from '" << str << "' (expected one of: " << argparse::join(default_choices(), ", ") << ")";
+        throw argparse::ArgParseConversionError(msg.str());
+    }
+
+    std::string to_str(int val) {
+        if (val == 0)       return "0";
+        else if (val == 1)  return "1";
+        else if (val == -1) return "X";
+        std::stringstream msg;
+        msg << "Invalid conversion from " << val;
+        throw argparse::ArgParseConversionError(msg.str());
+    }
+
+    std::vector<std::string> default_choices() {
+        return {"0", "1", "X"};
+    }
+};
+
 /*---------------------------------------------------------------------------------------------
  * (function: get_options)
  *-------------------------------------------------------------------------*/
-void get_options(int argc, char **argv)
-{
-	
-	/* Parse the command line options.  */
-	const char *optString = "hc:V:WREh:o:a:B:b:N:f:s:S:p:g:r:t:T:L:H:GA3U::";
-	int opt = getopt(argc, argv, optString);
-	while(opt != -1) 
-	{
-		switch(opt)
-		{
-			/* arch file */
-			case 'a':
-				global_args.arch_file = optarg;
-				configuration.arch_file = optarg;
-			break;
-			/* config file */
-			case 'c':
-				global_args.config_file = optarg;
-			break;
-			case 'V':
-				global_args.verilog_file = optarg;
-			break;
-			case 'o':
-				global_args.output_file = optarg;
-			break;
-			case 'b':
-				global_args.blif_file = optarg;
-			break;
-			case 'f':
-				warning_message(0, -1, 0, "Option -f: VPR 6.0 doesn't have this feature yet.  You'll need to deal with the output_blif.c differences wrapped by \"if (global_args.high_level_block != NULL)\"\n");
-			break;
-			case 'g':
-				global_args.sim_num_test_vectors = atoi(optarg);
-			break;
-			case 'r':
-				global_args.sim_random_seed = atoi(optarg);
-			break;
-			case '3':
-				global_args.sim_generate_three_valued_logic = 1;
-			break;
-			case 'L':
-				global_args.sim_hold_low = optarg;
-			break;
-			case 'H':
-				global_args.sim_hold_high = optarg;
-			break;
-			case 't':
-				global_args.sim_vector_input_file = optarg;
-			break;
-			case 'T':
-				global_args.sim_vector_output_file = optarg;
-			break;
-			case 'U':
-				global_args.sim_initial_value = -1;
-				if(optarg != NULL){
-					if(*optarg == '0') global_args.sim_initial_value = 0;
-					else if(*optarg == '1') global_args.sim_initial_value = 1;
-				}
-			break;
-			case 'p':
-				global_args.sim_additional_pins = optarg;
-			break;
-			case 'G':
-				configuration.output_netlist_graphs = 1;
-			break;
-			case 'A':
-				configuration.output_ast_graphs = 1;
-			break;
-			case 'W':
-				global_args.all_warnings = 1;
-			break;
-			case 'E':
-				global_args.sim_output_both_edges = 1;
-			break;
-			case 'R':
-				global_args.sim_output_rising_edge = 1;
-			break;
-			case 'h':
-			default :
-				print_usage();
-				error_message(-1, 0, -1, "Invalid arguments.\n");
-			break;
-		}
-		opt = getopt(argc, argv, optString);
-	}
+void get_options(int argc, char** argv) {
 
-	if (
-			   !global_args.config_file
-			&& !global_args.blif_file
-			&& !global_args.verilog_file
-			// VPR5 legacy code?
-			//&& ((!global_args.activation_blif_file) || (!global_args.activation_netlist_file))
-			)
-	{
-		print_usage();
-		error_message(-1,0,-1,"Must include either "
-				"a config file, a blif netlist, or a verilog file\n");
-	}
-	else if ((global_args.config_file && global_args.verilog_file) 
-			// VPR5 legacy code?
-			//|| global_args.activation_blif_file
-			)
-	{
-		warning_message(-1,0,-1, "Using command line options for verilog input file!!!\n");
-	}
+    auto parser = argparse::ArgumentParser(argv[0]);
+
+    auto& input_grp = parser.add_argument_group("input files");
+
+    input_grp.add_argument(global_args.config_file, "-c")
+            .help("Configuration file")
+            .metavar("XML_CONFIGURATION_FILE");
+
+    input_grp.add_argument(global_args.verilog_file, "-V")
+            .help("Verilog HDL file")
+            .metavar("VERILOG_FILE");
+
+    input_grp.add_argument(global_args.blif_file, "-b")
+            .help("BLIF file")
+            .metavar("BLIF_FILE");
+
+    auto& output_grp = parser.add_argument_group("output files");
+
+    output_grp.add_argument(global_args.output_file, "-o")
+            .help("Output file path")
+            .default_value("default_out.blif")
+            .metavar("OUTPUT_FILE_PATH");
+
+    auto& other_grp = parser.add_argument_group("other options");
+
+    other_grp.add_argument(global_args.show_help, "-h")
+            .help("Display this help message")
+            .action(argparse::Action::HELP);
+
+    other_grp.add_argument(global_args.arch_file, "-a")
+            .help("VTR FPGA architecture description file (XML)")
+            .metavar("ARCHITECTURE_FILE");
+
+    other_grp.add_argument(global_args.write_netlist_as_dot, "-G")
+            .help("Output netlist graph in graphviz .dot format")
+            .default_value("false")
+            .action(argparse::Action::STORE_TRUE);
+
+    other_grp.add_argument(global_args.write_ast_as_dot, "-A")
+            .help("Output AST graph in graphviz .dot format")
+            .default_value("false")
+            .action(argparse::Action::STORE_TRUE);
+
+    other_grp.add_argument(global_args.all_warnings, "-W")
+            .help("Print all warnings (can be substantial)")
+            .default_value("false")
+            .action(argparse::Action::STORE_TRUE);
+
+    auto& rand_sim_grp = parser.add_argument_group("random simulation options");
+    
+    rand_sim_grp.add_argument(global_args.sim_num_test_vectors, "-g")
+            .help("Number of random test vectors to generate")
+            .metavar("NUM_VECTORS");
+
+    rand_sim_grp.add_argument(global_args.sim_num_test_vectors, "-r")
+            .help("Random seed")
+            .default_value("0")
+            .metavar("SEED");
+
+    rand_sim_grp.add_argument(global_args.sim_hold_low, "-L")
+            .help("Comma-separated list of primary inputs to hold high at cycle 0, and low for all subsequent cycles")
+            .metavar("PRIMARY_INPUTS");
+
+    rand_sim_grp.add_argument(global_args.sim_hold_high, "-H")
+            .help("Comma-separated list of primary inputs to hold low at cycle 0, and high for all subsequent cycles")
+            .metavar("PRIMARY_INPUTS");
+
+
+    auto& vec_sim_grp = parser.add_argument_group("vector simulation options");
+
+    vec_sim_grp.add_argument(global_args.sim_vector_input_file, "-t")
+            .help("File of predefined input vectors to simulate")
+            .metavar("INPUT_VECTOR_FILE");
+
+    vec_sim_grp.add_argument(global_args.sim_vector_output_file, "-T")
+            .help("File of predefined output vectors to check against simulation")
+            .metavar("OUTPUT_VECTOR_FILE");
+
+    auto& other_sim_grp = parser.add_argument_group("other simulation options");
+
+    other_sim_grp.add_argument(global_args.sim_generate_three_valued_logic, "-3")
+            .help("Generate three valued logic, instead of binary")
+            .default_value("false")
+            .action(argparse::Action::STORE_TRUE);
+
+    other_sim_grp.add_argument<int,ParseInitRegState>(global_args.sim_initial_value, "-U")
+            .help("Default initial register state")
+            .default_value("X")
+            .metavar("INIT_REG_STATE");
+
+    other_sim_grp.add_argument(global_args.sim_output_both_edges, "-E")
+            .help("Output after both edges of the clock (Default only after the falling edge)")
+            .default_value("false")
+            .action(argparse::Action::STORE_TRUE);
+
+    other_sim_grp.add_argument(global_args.sim_output_both_edges, "-R")
+            .help("Output after rising edges of the clock only (Default only after the falling edge)")
+            .default_value("false")
+            .action(argparse::Action::STORE_TRUE);
+
+    other_sim_grp.add_argument(global_args.sim_additional_pins, "-p")
+            .help("Comma-separated list of additional pins/nodes to monitor during simulation.\n"
+                  "Eg: \"-p input~0,input~1\" monitors pin 0 and 1 of input, \n"
+                  "  or \"-p input\" monitors all pins of input as a single port. \n"
+                  "  or \"-p input~\" monitors all pins of input as separate ports. (split) \n"
+                  "- Note: Non-existent pins are ignored. \n"
+                  "- Matching is done via strstr so general strings will match \n"
+                  "  all similar pins and nodes.\n"
+                  "    (Eg: FF_NODE will create a single port with all flipflops) \n")
+            .metavar("PINS_TO_MONITOR");
+
+    parser.parse_args(argc, argv);
+
+    //Check required options
+    if (   !global_args.config_file
+        && !global_args.blif_file
+        && !global_args.verilog_file) {
+        parser.print_usage();
+        error_message(-1,0,-1,"Must include either "
+                              "a config file, a blif netlist, or a verilog file\n");
+    } else if (global_args.config_file && global_args.verilog_file) {
+        warning_message(-1,0,-1, "Using command line options for verilog input file when a config file was specified!\n");
+    }
+
+    //Allow some config values to be overriden from command line
+    if (global_args.arch_file.provenance() == argparse::Provenance::SPECIFIED) {
+        configuration.arch_file = global_args.arch_file;
+    }
+    if (global_args.write_netlist_as_dot.provenance() == argparse::Provenance::SPECIFIED) {
+        configuration.output_netlist_graphs = global_args.write_netlist_as_dot;
+    }
+    if (global_args.write_ast_as_dot.provenance() == argparse::Provenance::SPECIFIED) {
+        configuration.output_ast_graphs = global_args.write_ast_as_dot;
+    }
 }
 
 /*---------------------------------------------------------------------------
  * (function: set_default_options)
  *-------------------------------------------------------------------------*/
-void set_default_options()
+void set_default_config()
 {
-/* Set up the global arguments to their default. */
-	global_args.config_file = NULL;
-	global_args.verilog_file = NULL;
-	global_args.blif_file = NULL;
-	global_args.output_file = vtr::strdup("./default_out.blif");
-	global_args.arch_file = NULL;
-	global_args.activation_blif_file = NULL;
-	global_args.activation_netlist_file = NULL;
-	global_args.high_level_block = NULL;
-	global_args.sim_vector_input_file = NULL;
-	global_args.sim_vector_output_file = NULL;
-	global_args.sim_additional_pins = NULL;
-	global_args.sim_num_test_vectors = 0;
-	global_args.sim_generate_three_valued_logic = 0;
-	global_args.sim_hold_low = NULL;
-	global_args.sim_hold_high = NULL;
-	global_args.sim_output_both_edges = 0;
-	global_args.sim_output_rising_edge = 0;
-	global_args.sim_initial_value = -1;
-	global_args.sim_random_seed = 0;
-	global_args.all_warnings = 0;
-
 	/* Set up the global configuration. */
 	configuration.list_of_file_names = NULL;
 	configuration.num_list_of_file_names = 0;
@@ -435,4 +462,3 @@ void set_default_options()
 	configuration.soft_logic_memory_width_threshold = 0;
 	configuration.soft_logic_memory_depth_threshold = 0;
 }
-
