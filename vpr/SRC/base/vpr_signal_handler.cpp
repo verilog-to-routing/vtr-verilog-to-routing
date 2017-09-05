d932bcb0626d2c45cbb15fff6e2d815789aee8a9
kmurray <k.murray@utoronto.ca>
2017-09-04 21:57:03 -0400

    vpr: add missing signal handler files

---
+++ vpr/SRC/base/vpr_signal_handler.cpp
@@ -0,0 +1,57 @@
+/*
+ * This file defines a signal handler used by VPR to catch SIGINT signals
+ * (ctrl-C from terminal) on POSIX systems. It is only active if
+ * VPR_USE_SIGACTION is defined.
+ *
+ * If a SIGINT occur the handler sets the 'forced_pause' flag of the VPR
+ * context.  If 'forced_pause' is still true when another SIGINT occurs an
+ * exception is thrown (typically ending the program).
+ */
+#include "vtr_log.h"
+
+#include "vpr_signal_handler.h"
+#include "vpr_error.h"
+#include "globals.h"
+
+#ifdef VPR_USE_SIGACTION
+#include <csignal>
+#endif
+
+void vpr_signal_handler(int signal);
+
+#ifdef VPR_USE_SIGACTION
+
+    void vpr_install_signal_handler() {
+        //Install the signal handler for SIGINT (i.e. ctrl+C from the terminal)
+        struct sigaction sa;
+        sa.sa_handler = vpr_signal_handler;
+        sigemptyset(&sa.sa_mask);
+        sigaction(SIGINT, &sa, nullptr);
+    }
+
+    void vpr_signal_handler(int signal) {
+        VTR_ASSERT_MSG(signal == SIGINT, "Expected to handle only SIGINT");
+
+        bool already_paused = g_vpr_ctx.forced_pause();
+
+        if (!already_paused) {
+            //Ask VPR to pause at the next conveneient point
+            g_vpr_ctx.set_forced_pause(true);
+        } else {
+            //Really stop
+            VPR_THROW(VPR_ERROR_INTERRUPTED, "Interrupted by user");
+        }
+    }
+
+#else
+    //No signal support all operations are no-ops
+
+    void vpr_install_signal_handler() {
+        //Do nothing
+    }
+
+    void vpr_signal_handler(int /*signal*/) {
+        //Do nothing
+    }
+
+#endif
---
+++ vpr/SRC/base/vpr_signal_handler.h
@@ -0,0 +1,6 @@
+#ifndef SIGNAL_HANLDER_H
+#define SIGNAL_HANLDER_H
+
+void vpr_install_signal_handler();
+
+#endif
