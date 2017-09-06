e636a61148aee9febde1f4917625b864d25d0497
kmurray <k.murray@utoronto.ca>
2017-09-06 14:27:51 -0400

    vpr: Fix incorrect pin names in clock generate warning message

--- vpr/SRC/timing/timing_graph_builder.cpp
+++ vpr/SRC/timing/timing_graph_builder.cpp
@@ -308,7 +308,7 @@ void TimingGraphBuilder::add_block_to_timing_graph(const AtomBlockId blk) {
                             //Do not create the edge
                             vtr::printf_warning(__FILE__, __LINE__, 
                                 "Timing edge from %s to %s will not be created since %s has been identified as a clock generator\n",
-                                netlist_.pin_name(src_pin).c_str(), netlist_.pin_name(src_pin).c_str(), netlist_.pin_name(src_pin).c_str());
+                                netlist_.pin_name(src_pin).c_str(), netlist_.pin_name(sink_pin).c_str(), netlist_.pin_name(sink_pin).c_str());
                         } else {
                             //Unknown
                             VPR_THROW(VPR_ERROR_TIMING, "Unable to find matching sink tnode for timing edge from %s to %s",
