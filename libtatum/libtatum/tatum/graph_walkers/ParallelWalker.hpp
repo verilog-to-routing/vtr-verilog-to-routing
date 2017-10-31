10c44f2b763bc4cfb3a5b74caecbac6e19ece151
kmurray <k.murray@utoronto.ca>
2017-10-31 18:09:16 -0400

    Remove old subtree libaries in root; now located in libs/EXTERNAL

--- libtatum/libtatum/tatum/graph_walkers/ParallelWalker.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-//ParallelWalker is an alias for ParallelLevelizedCilkWalker 
-#include "tatum/graph_walkers_fwd.hpp"
-
-
-//Include the def'n of ParallelLevelizedCilkWalker
-#include "ParallelLevelizedCilkWalker.hpp"
