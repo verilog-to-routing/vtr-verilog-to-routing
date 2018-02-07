a8b2f2de401639365a3928cd30fcdb1c9f5a5ec8
kmurray <k.murray@utoronto.ca>
2018-02-07 10:20:03 -0500

    vpr: Move non-file-format specific code into read_circuit.cpp/.h
    BLIF/EBLIF specific code remains in read_blif.cpp/.h

--- vpr/src/base/read_blif.h
+++ vpr/src/base/read_blif.h
@@ -2,21 +2,11 @@
 #define READ_BLIF_H
 #include "logic_types.h"
 #include "atom_netlist_fwd.h"
+#include "read_circuit.h"
 
-enum class e_circuit_format {
-    AUTO, //Infer from file extension
-    BLIF, //Strict structural BLIF
-    EBLIF //Structural blif with extensions
-};
-
-AtomNetlist read_and_process_circuit(const e_circuit_format circuit_format,
-                                     const char *blif_file, 
-                                     const t_model *user_models, 
-                                     const t_model *library_models,
-                                     bool should_absorb_buffers, 
-                                     bool should_sweep_dangling_primary_ios, 
-                                     bool should_sweep_dangling_nets,
-                                     bool should_sweep_dangling_blocks,
-                                     bool should_sweep_constant_primary_outputs);
+AtomNetlist read_blif(e_circuit_format circuit_format,
+                      const char* blif_file, 
+                      const t_model* user_models, 
+                      const t_model* library_models);
 
 #endif /*READ_BLIF_H*/
