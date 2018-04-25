8312a7238c9b549d62c3a13252188be7c7f4928a
kmurray <k.murray@utoronto.ca>
2018-04-25 15:44:24 -0400

    vpr: Show longline attribute in RR node info

--- vpr/src/route/rr_graph.cpp
+++ vpr/src/route/rr_graph.cpp
@@ -2293,9 +2293,10 @@ std::string describe_rr_node(int inode) {
 
         int seg_index = device_ctx.rr_indexed_data[cost_index].seg_index;
 
-        msg += vtr::string_fmt(" track: %d len: %d seg_type: %s dir: %s",
+        msg += vtr::string_fmt(" track: %d len: %d longline: %d seg_type: %s dir: %s",
                     rr_node.track_num(),
                     rr_node.length(),
+                    device_ctx.arch.Segments[seg_index].longline,
                     device_ctx.arch.Segments[seg_index].name,
                     rr_node.direction_string());
     } else if (rr_node.type() == IPIN || rr_node.type() == OPIN) {
