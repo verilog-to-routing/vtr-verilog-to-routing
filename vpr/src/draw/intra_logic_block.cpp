verilog-to-routing/vtr-verilog-to-routing

commit 670111a7302c37a21cfb1366a0326dc565b83dcd

vpr: Fix potential null dereference warnings found with gcc-7

vpr/src/draw/intra_logic_block.cpp
@@ -453,7 +453,7 @@ static void draw_internal_pb(const ClusterBlockId clb_index, t_pb* pb, const t_b
 			t_pb_type* pb_child_type = child_pb->pb_graph_node->pb_type;
 
 			// don't go farther if 0 modes
-			if (pb_child_type == nullptr && pb_child_type->num_modes == 0) {
+			if (pb_child_type == nullptr || pb_child_type->num_modes == 0) {
 				continue;
 			}
 

vpr/src/base/vpr_types.h
@@ -154,7 +154,11 @@ struct t_pb {
 	}
 
 	int get_num_children_of_type(int type_index) const {
-		return get_mode()->pb_type_children[type_index].num_pb;
+        t_mode* mode_ptr = get_mode();
+        if (mode_ptr) {
+            return mode_ptr->pb_type_children[type_index].num_pb;
+        }
+        return 0; //No mode
 	}
 
 	t_mode* get_mode() const {

vpr/src/pack/pb_type_graph.cpp
@@ -1041,60 +1041,61 @@ static bool realloc_and_load_pb_graph_pin_ptrs_at_var(const int line_num,
 		}
 	} else {
 		//Children pb_types
-		if (mode == nullptr ) {
+		if (mode) {
+            for (i = 0; i < mode->num_pb_type_children; i++) {
+                VTR_ASSERT(&mode->pb_type_children[i] == pb_graph_children_nodes[i][0].pb_type);
+                if (0 == strcmp(mode->pb_type_children[i].name,	tokens[*token_index].data)) {
+                    pb_node_array = pb_graph_children_nodes[i];
+                    max_pb_node_array = mode->pb_type_children[i].num_pb;
+                    found = true;
+                    (*token_index)++;
+
+                    if (tokens[*token_index].type == TOKEN_OPEN_SQUARE_BRACKET) {
+                        (*token_index)++;
+                        if (!checkTokenType(tokens[*token_index], TOKEN_INT)) {
+                            return false;
+                        }
+                        pb_msb = vtr::atoi(tokens[*token_index].data);
+                        VTR_ASSERT_MSG(pb_msb >= 0, "Pin most-siginificant-bit must be non-negative");
+                        (*token_index)++;
+                        if (!checkTokenType(tokens[*token_index], TOKEN_COLON)) {
+                            if (!checkTokenType(tokens[*token_index],
+                                    TOKEN_CLOSE_SQUARE_BRACKET)) {
+                                return false;
+                            }
+                            pb_lsb = pb_msb;
+                            (*token_index)++;
+                        } else {
+                            (*token_index)++;
+                            if (!checkTokenType(tokens[*token_index], TOKEN_INT)) {
+                                return false;
+                            }
+                            pb_lsb = vtr::atoi(tokens[*token_index].data);
+                            VTR_ASSERT_MSG(pb_lsb >= 0, "Pin most-siginificant-bit must be non-negative");
+                            (*token_index)++;
+                            if (!checkTokenType(tokens[*token_index],
+                                    TOKEN_CLOSE_SQUARE_BRACKET)) {
+                                return false;
+                            }
+                            (*token_index)++;
+                        }
+                        /* Check range of children pb */
+                        if (pb_lsb < 0 || pb_lsb >= max_pb_node_array ||
+                            pb_msb < 0 || pb_msb >= max_pb_node_array) {
+                            vpr_throw(VPR_ERROR_ARCH, get_arch_file_name(), line_num, 
+                                "Mode '%s' -> pb '%s' [%d,%d] out of range [%d,%d]", mode->name,
+                                mode->pb_type_children[i].name, pb_msb, pb_lsb, max_pb_node_array - 1, 0);
+                        }
+                    } else {
+                        pb_msb = pb_node_array[0].pb_type->num_pb - 1;
+                        pb_lsb = 0;
+                    }
+                    break; //found pb_type_children, no need to keep traversing
+                }
+            }
+        } else {
 			vpr_throw(VPR_ERROR_ARCH, get_arch_file_name(), line_num, 
 				"Parent pb node '%s' missing", pb_graph_parent_node->pb_type->name);
-		}
-		for (i = 0; i < mode->num_pb_type_children; i++) {
-			VTR_ASSERT(&mode->pb_type_children[i] == pb_graph_children_nodes[i][0].pb_type);
-			if (0 == strcmp(mode->pb_type_children[i].name,	tokens[*token_index].data)) {
-				pb_node_array = pb_graph_children_nodes[i];
-				max_pb_node_array = mode->pb_type_children[i].num_pb;
-				found = true;
-				(*token_index)++;
-
-				if (tokens[*token_index].type == TOKEN_OPEN_SQUARE_BRACKET) {
-					(*token_index)++;
-					if (!checkTokenType(tokens[*token_index], TOKEN_INT)) {
-						return false;
-					}
-					pb_msb = vtr::atoi(tokens[*token_index].data);
-                    VTR_ASSERT_MSG(pb_msb >= 0, "Pin most-siginificant-bit must be non-negative");
-					(*token_index)++;
-					if (!checkTokenType(tokens[*token_index], TOKEN_COLON)) {
-						if (!checkTokenType(tokens[*token_index],
-								TOKEN_CLOSE_SQUARE_BRACKET)) {
-							return false;
-						}
-						pb_lsb = pb_msb;
-						(*token_index)++;
-					} else {
-						(*token_index)++;
-						if (!checkTokenType(tokens[*token_index], TOKEN_INT)) {
-							return false;
-						}
-						pb_lsb = vtr::atoi(tokens[*token_index].data);
-                        VTR_ASSERT_MSG(pb_lsb >= 0, "Pin most-siginificant-bit must be non-negative");
-						(*token_index)++;
-						if (!checkTokenType(tokens[*token_index],
-								TOKEN_CLOSE_SQUARE_BRACKET)) {
-							return false;
-						}
-						(*token_index)++;
-					}
-					/* Check range of children pb */
-					if (pb_lsb < 0 || pb_lsb >= max_pb_node_array ||
-						pb_msb < 0 || pb_msb >= max_pb_node_array) {
-						vpr_throw(VPR_ERROR_ARCH, get_arch_file_name(), line_num, 
-							"Mode '%s' -> pb '%s' [%d,%d] out of range [%d,%d]", mode->name,
-							mode->pb_type_children[i].name, pb_msb, pb_lsb, max_pb_node_array - 1, 0);
-					}
-				} else {
-					pb_msb = pb_node_array[0].pb_type->num_pb - 1;
-					pb_lsb = 0;
-				}
-				break; //found pb_type_children, no need to keep traversing
-			}
 		}
 	}
     

vpr/src/power/PowerSpicedComponent.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 #include "vtr_assert.h"
 
+#include "vpr_error.h"
 #include "PowerSpicedComponent.h"
 
 bool sorter_PowerCallibSize(PowerCallibSize * a, PowerCallibSize * b);
@@ -171,10 +172,15 @@ float PowerSpicedComponent::scale_factor(int num_inputs,
 		size_lower = inputs_lower->get_entry_bound(true, transistor_size);
 		size_upper = inputs_lower->get_entry_bound(false, transistor_size);
 
-		perc_upper = (transistor_size - size_lower->transistor_size)
-				/ (size_upper->transistor_size - size_lower->transistor_size);
-		factor_lower = perc_upper * size_upper->factor
-				+ (1 - perc_upper) * size_lower->factor;
+        if (size_lower && size_upper) {
+
+            perc_upper = (transistor_size - size_lower->transistor_size)
+                    / (size_upper->transistor_size - size_lower->transistor_size);
+            factor_lower = perc_upper * size_upper->factor
+                    + (1 - perc_upper) * size_lower->factor;
+        } else {
+            VPR_THROW(VPR_ERROR_POWER, "Failed to interpolate transitor size");
+        }
 	}
 
 	if (inputs_upper) {
@@ -183,10 +189,14 @@ float PowerSpicedComponent::scale_factor(int num_inputs,
 		size_lower = inputs_upper->get_entry_bound(true, transistor_size);
 		size_upper = inputs_upper->get_entry_bound(false, transistor_size);
 
-		perc_upper = (transistor_size - size_lower->transistor_size)
-				/ (size_upper->transistor_size - size_lower->transistor_size);
-		factor_upper = perc_upper * size_upper->factor
-				+ (1 - perc_upper) * size_lower->factor;
+        if (size_lower && size_upper) {
+            perc_upper = (transistor_size - size_lower->transistor_size)
+                    / (size_upper->transistor_size - size_lower->transistor_size);
+            factor_upper = perc_upper * size_upper->factor
+                    + (1 - perc_upper) * size_lower->factor;
+        } else {
+            VPR_THROW(VPR_ERROR_POWER, "Failed to interpolate transitor size");
+        }
 	}
 
 	if (!inputs_lower) {

vpr/src/route/route_timing.cpp
@@ -1828,10 +1828,11 @@ void Connection_based_routing_resources::convert_sink_nodes_to_net_pins(vector<i
     for (size_t s = 0; s < rr_sink_nodes.size(); ++s) {
 
         auto mapping = node_to_pin_mapping.find(rr_sink_nodes[s]);
-        // should always expect it find a pin mapping for its own net
-        VTR_ASSERT_SAFE(mapping != node_to_pin_mapping.end());
-
-        rr_sink_nodes[s] = mapping->second;
+        if (mapping != node_to_pin_mapping.end()) {
+            rr_sink_nodes[s] = mapping->second;
+        } else {
+            VTR_ASSERT_SAFE_MSG(false, "Should always expect it find a pin mapping for its own net");
+        }
     }
 }
 
@@ -1848,10 +1849,12 @@ void Connection_based_routing_resources::put_sink_rt_nodes_in_net_pins_lookup(co
 
     for (t_rt_node* rt_node : sink_rt_nodes) {
         auto mapping = node_to_pin_mapping.find(rt_node->inode);
-        // element should be able to find itself
-        VTR_ASSERT_SAFE(mapping != node_to_pin_mapping.end());
 
-        rt_node_of_sink[mapping->second] = rt_node;
+        if (mapping != node_to_pin_mapping.end()) {
+            rt_node_of_sink[mapping->second] = rt_node;
+        } else {
+            VTR_ASSERT_SAFE_MSG(false, "element should be able to find itself");
+        }
     }
 }
 

commit 2af1365c6431de2ee03f86f507ba63dd44f4e6a3

vpr: Fix bug causing block internals not to be drawn
Originally introduced in commit 670111a.

vpr/src/draw/intra_logic_block.cpp
@@ -149,7 +149,7 @@ void draw_internal_draw_subblk() {
 			if (device_ctx.grid[i][j].width_offset > 0 || device_ctx.grid[i][j].height_offset > 0) 
 				continue;
 
-			/* Don't draw if tile is empty. This includes corners. */
+			/* Don't draw if tile is empty. */
 			if (device_ctx.grid[i][j].type == device_ctx.EMPTY_TYPE)
 				continue;
 
@@ -396,7 +396,7 @@ static void draw_internal_pb(const ClusterBlockId clb_index, t_pb* pb, const t_b
 			int tot_len = type_len + name_len;
 			char* blk_tag = (char *)vtr::malloc((tot_len + 8) * sizeof(char));
 
-			sprintf (blk_tag, "%s(%s)", pb_type->name, pb->name);
+			sprintf (blk_tag, "%s (%s)", pb_type->name, pb->name);
 
 			drawtext(
 				t_point(abs_bbox.get_xcenter(), abs_bbox.get_ycenter()),
@@ -453,9 +453,9 @@ static void draw_internal_pb(const ClusterBlockId clb_index, t_pb* pb, const t_b
 			t_pb_type* pb_child_type = child_pb->pb_graph_node->pb_type;
 
 			// don't go farther if 0 modes
-			if (pb_child_type == nullptr || pb_child_type->num_modes == 0) {
-				continue;
-			}
+            if (pb_child_type == nullptr) {
+                continue;
+            }
 
 			// now recurse
 			draw_internal_pb(clb_index, child_pb, abs_bbox, type);