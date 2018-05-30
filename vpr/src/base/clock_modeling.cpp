ed12623d2740a61997c9ce384c3fa753ce591317
Mustafa Abbas <mustafa.abbas@bucknell.edu>
2018-05-30 19:02:35 -0400

    Added a function to reassign clock pins as non-globals
    (cherry picked from commit 03ea307ec792311d8c94915daa600e83d273c011)

---
+++ vpr/src/base/clock_modeling.cpp
@@ -0,0 +1,22 @@
+#include "clock_modeling.h"
+#include "globals.h"
+#include "vtr_assert.h"
+
+void ClockModeling::treat_clock_pins_as_non_globals() {
+
+    auto& device_ctx = g_vpr_ctx.device();
+
+    for (int type_idx = 0; type_idx < device_ctx.num_block_types; type_idx++) {
+
+        auto type = device_ctx.block_types[type_idx];
+        if(type.pb_type) {
+            for(auto clock_pin_idx : type.get_clock_pins_indices()) {
+
+               // clock pins should be originally considered as global when reading the architecture
+               VTR_ASSERT(type.is_global_pin[clock_pin_idx]);
+
+               type.is_global_pin[clock_pin_idx] = false;
+            }
+        }
+    }
+}
--- vpr/src/base/clock_modeling.h
+++ vpr/src/base/clock_modeling.h
@@ -1,10 +1,21 @@
 #ifndef CLOCK_MODELING_H
 #define CLOCK_MODELING_H
 
+#include "clustered_netlist_fwd.h"
+
 enum e_clock_modeling_method {
     IDEAL_CLOCK, //Treat the clock pins as ideal (i.e. not routed)
     ROUTED_CLOCK //Treat the clock pins as normal nets (i.e. routed)
 };
+
+
+namespace ClockModeling
+{
+    /* Removes global pin flag from clock pins.
+       This causes clock nets to also be treated as non-global;
+       therefore, they will be routed using inter-block routing */
+    void treat_clock_pins_as_non_globals();
+}
 
 #endif
 
