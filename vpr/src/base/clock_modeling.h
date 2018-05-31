1610b4c704595d3b2002951008e508329de08e36
Mustafa Abbas <mustafa.abbas@bucknell.edu>
2018-05-31 10:31:26 -0400

    Removed unnecessary include from clock_modeling.h
    (cherry picked from commit e24f3e9b9e8b75f33812e7514ec0188a9b4c8648)

--- vpr/src/base/clock_modeling.h
+++ vpr/src/base/clock_modeling.h
@@ -1,8 +1,6 @@
 #ifndef CLOCK_MODELING_H
 #define CLOCK_MODELING_H
 
-#include "clustered_netlist_fwd.h"
-
 enum e_clock_modeling_method {
     IDEAL_CLOCK, //Treat the clock pins as ideal (i.e. not routed)
     ROUTED_CLOCK //Treat the clock pins as normal nets (i.e. routed)
