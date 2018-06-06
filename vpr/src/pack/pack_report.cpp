ab90db2225af9b3a9b1f9d729484359546206d0e
kmurray <k.murray@utoronto.ca>
2018-06-06 15:44:26 -0400

    vpr: Add pin usage histogram to post-pack pin usage report

--- vpr/src/pack/pack_report.cpp
+++ vpr/src/pack/pack_report.cpp
@@ -4,6 +4,7 @@
 
 #include "vpr_types.h"
 #include "vpr_utils.h"
+#include "histogram.h"
 
 #include <iostream>
 #include <iomanip>
@@ -26,8 +27,8 @@ void report_packing_pin_usage(std::ostream& os, const VprContext& ctx) {
         total_output_pins[type] = pb_type->num_output_pins;
     }
 
-    std::map<t_type_ptr,std::vector<size_t>> inputs_used;
-    std::map<t_type_ptr,std::vector<size_t>> outputs_used;
+    std::map<t_type_ptr,std::vector<float>> inputs_used;
+    std::map<t_type_ptr,std::vector<float>> outputs_used;
 
     for (auto blk : cluster_ctx.clb_nlist.blocks()) {
         t_type_ptr type = cluster_ctx.clb_nlist.block_type(blk);
@@ -45,12 +46,12 @@ void report_packing_pin_usage(std::ostream& os, const VprContext& ctx) {
         if (is_empty_type(type)) continue;
         if (!inputs_used.count(type)) continue;
 
-        size_t max_inputs = *std::max_element(inputs_used[type].begin(), inputs_used[type].end());
-        size_t min_inputs = *std::min_element(inputs_used[type].begin(), inputs_used[type].end());
+        float max_inputs = *std::max_element(inputs_used[type].begin(), inputs_used[type].end());
+        float min_inputs = *std::min_element(inputs_used[type].begin(), inputs_used[type].end());
         float avg_inputs = std::accumulate(inputs_used[type].begin(), inputs_used[type].end(), 0) / float(inputs_used[type].size());
 
-        size_t max_outputs = *std::max_element(outputs_used[type].begin(), outputs_used[type].end());
-        size_t min_outputs = *std::min_element(outputs_used[type].begin(), outputs_used[type].end());
+        float max_outputs = *std::max_element(outputs_used[type].begin(), outputs_used[type].end());
+        float min_outputs = *std::min_element(outputs_used[type].begin(), outputs_used[type].end());
         float avg_outputs = std::accumulate(outputs_used[type].begin(), outputs_used[type].end(), 0) / float(outputs_used[type].size());
 
         os << "Type: " << type->name << "\n";
@@ -59,10 +60,23 @@ void report_packing_pin_usage(std::ostream& os, const VprContext& ctx) {
         os << "\t\tMax: " << max_inputs << " (" << max_inputs / float(total_input_pins[type]) << ")" << "\n";
         os << "\t\tAvg: " << avg_inputs << " (" << avg_inputs / float(total_input_pins[type]) << ")" << "\n";
         os << "\t\tMin: " << min_inputs << " (" << min_inputs / float(total_input_pins[type]) << ")" << "\n";
+        os << "\t\tHistogram:\n";
+
+        auto input_histogram = build_histogram(inputs_used[type], 10, 0, total_input_pins[type]);
+        for (auto line : format_histogram(input_histogram)) {
+            os << "\t\t" << line << "\n";
+        }
 
         os << "\tOutput Pin Usage:\n";
         os << "\t\tMax: " << max_outputs << " (" << max_outputs / float(total_output_pins[type]) << ")" << "\n";
         os << "\t\tAvg: " << avg_outputs << " (" << avg_outputs / float(total_output_pins[type]) << ")" << "\n";
         os << "\t\tMin: " << min_outputs << " (" << min_outputs / float(total_output_pins[type]) << ")" << "\n";
+        os << "\t\tHistogram:\n";
+
+        auto output_histogram = build_histogram(outputs_used[type], 10, 0, total_output_pins[type]);
+        for (auto line : format_histogram(output_histogram)) {
+            os << "\t\t" << line << "\n";
+        }
+        os << "\n";
     }
 }
