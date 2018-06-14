fad5afba7f4b9c14cf12650ca7e4799203114a9d
kmurray <k.murray@utoronto.ca>
2018-06-14 12:03:32 -0400

    vtrutil: Add keys() member to vtr::vector
    This makes it easy to loop through a vtr::vector's keys with a
    range-based for loop.
    
    For example:
    
        vtr::vector<AtomNetId,int> my_vec;
    
        //Initialze my_vec...
    
        for (AtomNetId net_id : my_vec.keys()) {
            //Do something...
        }
    
    This is implemented by defining a key_iterator type which is convertable
    to the actual key type, and knows how to walk through the valid ID range.

--- libs/libvtrutil/src/vtr_vector.h
+++ libs/libvtrutil/src/vtr_vector.h
@@ -2,18 +2,27 @@
 #define VTR_VECTOR
 #include <vector>
 #include <cstddef>
+#include <iterator>
+#include "vtr_range.h"
 
 namespace vtr {
 
 //A std::vector container which is indexed by K (instead of size_t).
 //
 //The main use of this container is to behave like a std::vector which is
-//indexed by a vtr::StrongId.
+//indexed by a vtr::StrongId. It assumes that K is explicitly convertable to size_t
+//(i.e. via operator size_t()), and can be explicitly constructed from a size_t.
 //
 //If you need more std::map-like (instead of std::vector-like) behaviour see
 //vtr::vector_map.
 template<typename K, typename V>
 class vector : private std::vector<V> {
+    public:
+        typedef K key_type;
+
+        class key_iterator;
+        typedef vtr::Range<key_iterator> key_range;
+
     public:
         //Pass through std::vector's types
         using typename std::vector<V>::value_type;
@@ -66,23 +75,67 @@ class vector : private std::vector<V> {
 
 
         //Don't include operator[] and at() from std::vector,
-        //since we redine them to take K instead of size_t
-        reference operator[](const K id) {
+        //since we redine them to take key_type instead of size_t
+        reference operator[](const key_type id) {
             auto i = size_t(id);
             return std::vector<V>::operator[](i);
         }
-        const_reference operator[](const K id) const {
+        const_reference operator[](const key_type id) const {
             auto i = size_t(id);
             return std::vector<V>::operator[](i);
         }
-        reference at(const K id) {
+        reference at(const key_type id) {
             auto i = size_t(id);
             return std::vector<V>::at(i);
         }
-        const_reference at(const K id) const {
+        const_reference at(const key_type id) const {
             auto i = size_t(id);
             return std::vector<V>::at(i);
         }
+
+        //Returns a range containing the keys
+        key_range keys() const {
+            return vtr::make_range(key_begin(), key_end());
+        }
+    public:
+
+        //Iterator class which is convertable to the key_type
+        //This allows end-users to call the parent class's keys() member
+        //to iterate through the keys with a range-based for loop
+        class key_iterator : public std::iterator<std::bidirectional_iterator_tag, key_type> {
+            public:
+                using typename std::iterator<std::bidirectional_iterator_tag, K>::value_type; 
+                using typename std::iterator<std::bidirectional_iterator_tag, K>::iterator; 
+                using typename std::iterator<std::bidirectional_iterator_tag, K>::pointer; 
+                using typename std::iterator<std::bidirectional_iterator_tag, K>::reference; 
+
+                key_iterator(value_type init): value_(init) {}
+
+                //vtr::vector assumes that the key time is convertable to size_t and
+                //that all the underlying IDs are zero-based and contiguous. That means
+                //we can just increment the underlying Id to build the next key.
+                key_iterator operator++() {
+                    value_ = value_type(size_t(value_) + 1);
+                    return *this; 
+                }
+                key_iterator operator--() {
+                    value_ = value_type(size_t(value_) - 1);
+                    return *this;
+                }
+                reference operator*() { return value_; }
+                pointer operator->() { return &value_; }
+
+                friend bool operator== (const key_iterator lhs, const key_iterator rhs) { return lhs.value_ == rhs.value_; }
+                friend bool operator!= (const key_iterator lhs, const key_iterator rhs) { return !(lhs == rhs); }
+
+            private:
+                value_type value_;
+        };
+
+    private:
+        key_iterator key_begin() const { return key_iterator(key_type(0)); }
+        key_iterator key_end() const { return key_iterator(key_type(size())); }
+
 };
 
 
