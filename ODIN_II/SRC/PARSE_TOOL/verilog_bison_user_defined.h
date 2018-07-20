d6d6276003167214dbadf677333b73507712cf14
jeanlego <jeanphilippe.legault@unb.ca>
2018-07-20 09:54:38 -0300

    blifexplorer: fix odin depency problem

--- ODIN_II/SRC/PARSE_TOOL/verilog_bison_user_defined.h
+++
@@ -1,4 +0,0 @@
-#ifndef VERILOG_BISON_USER_DEFINED
-#define VERILOG_BISON_USER_DEFINED
-int yyparse (void);
-#endif
--- ODIN_II/SRC/lib_blif/output_blif.h
+++
@@ -1,7 +0,0 @@
-#ifndef OUTPUT_BLIF_H
-#define OUTPUT_BLIF_H
-
-void output_blif(char *file_name, netlist_t *netlist);
-
-#endif
-
