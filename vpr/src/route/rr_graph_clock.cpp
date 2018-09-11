51057605a379a14226b63b4ef67850ed8f5924e1
Mustafa Abbas <mustafa.abbas@bucknell.edu>
2018-09-11 10:42:27 -0400

    Added Spine and Rib wires rr_node generation code - TODO: -- H-tree wires rr_node generation -- Inserting clock network rr_nodes into a reverse lookup to find    the source and tap locations -- Creating edges for clock network rr_nodes

--- vpr/src/route/rr_graph_clock.cpp
+++ vpr/src/route/rr_graph_clock.cpp
@@ -5,6 +5,7 @@
 
 #include "vtr_assert.h"
 #include "vtr_log.h"
+#include "vpr_error.h"
 
 void ClockRRGraph::create_and_append_clock_rr_graph() {
 
@@ -15,114 +16,237 @@ void ClockRRGraph::create_and_append_clock_rr_graph() {
     //       functions
     //create_star_model_network();
 
+
+    // Clocks
+    std::vector<std::unique_ptr<ClockNetwork>> clock_networks;
+
+    // Clock Ribs
+    clock_networks.emplace_back(new ClockRib());
+    ClockRib* rib = dynamic_cast<ClockRib*>(clock_networks.back().get());
     
-    //create_clock_networks_wires();
-    //create_clock_networks_switches();
+    rib->set_num_instance(1);
+    rib->set_clock_name("rib1");
+    rib->set_metal_layer(0, 0);
+    rib->set_initial_wire_location(0, 10, 0);
+    rib->set_wire_repeat(10, 1);
+    rib->set_drive_location(5);
+    rib->set_tap_location(0,1);
+
+    // Clock Spines
+    clock_networks.emplace_back(new ClockSpine());
+    ClockSpine* spine = dynamic_cast<ClockSpine*>(clock_networks.back().get());
+
+    spine->set_num_instance(1);
+    spine->set_clock_name("spine1");
+    spine->set_metal_layer(0,0);
+    spine->set_initial_wire_location(0,10,5);
+    spine->set_wire_repeat(10,10);
+    spine->set_drive_location(5);
+    spine->set_tap_location(0,1);
+
+    ClockRRGraph clock_graph = ClockRRGraph();
+    clock_graph.create_clock_networks_wires(clock_networks);
+
+    // Add a shrink to fit for the rr_nodes
 
     float elapsed_time = (float) (clock() - begin) / CLOCKS_PER_SEC;
     vtr::printf_info("Building clock network resource graph took %g seconds\n", elapsed_time);
 }
 
-//void ClockRRGraph::create_clock_networks_wires(
-//        std::vector<t_clock_network_description>& clock_networks) {
-//    for (auto const& clock_network : clock_networks) {
-//       create_clock_network_wires(clock_network); 
-//    }
-//}
+void ClockRRGraph::create_clock_networks_wires(
+        std::vector<std::unique_ptr<ClockNetwork>>& clock_networks) {
+    for (auto& clock_network : clock_networks) {
+        clock_network->create_rr_nodes_for_clock_network_wires(*this);
+    }
+}
 
-void ClockRRGraph::create_star_model_network() {
 
-    vtr::printf_info("Creating a clock network in the form of a star model\n");
+void ClockNetwork::create_rr_nodes_for_clock_network_wires(ClockRRGraph& clock_graph) {
+    for(int i = 0; i < get_num_inst(); i++){
+        create_rr_nodes_for_one_instance(clock_graph);
+    }
+}
+
+void ClockRib::create_rr_nodes_for_one_instance(ClockRRGraph&clock_graph) {
+ 
+    auto& device_ctx = g_vpr_ctx.mutable_device();
+    auto& rr_nodes = device_ctx.rr_nodes;
+    auto& grid = device_ctx.grid;
+
+    for(unsigned x_start = x_chan_wire.start, x_end = x_chan_wire.end;
+        x_end < grid.width();
+        x_start += repeat.x, x_end += repeat.x) {
+        for(unsigned y = x_chan_wire.position; y < grid.height(); y += repeat.y) {
+            auto rr_node_index = create_rr_node_for_one_rib(x_start, x_end, y, rr_nodes);
+            record_switch_point_locations_for_rr_node(x_start, x_end, y, rr_node_index, clock_graph);
+        }
+    }
+}
+
+int ClockRib::create_rr_node_for_one_rib(
+        int x_start,
+        int x_end,
+        int y,
+        std::vector<t_rr_node>& rr_nodes) {
+
+    rr_nodes.emplace_back();
+    auto node_index = rr_nodes.size() - 1;
+    rr_nodes[node_index].set_coordinates(x_start, y, x_end, y);
+    rr_nodes[node_index].set_type(CHANX);
+    rr_nodes[node_index].set_capacity(1);
+
+    return node_index;
+}
+
+void ClockRib::record_switch_point_locations_for_rr_node(
+        int x_start,
+        int x_end,
+        int y,
+        int rr_node_index,
+        ClockRRGraph& clock_graph) {
+
+}
+
+void ClockSpine::create_rr_nodes_for_one_instance(ClockRRGraph& clock_graph) {
 
     auto& device_ctx = g_vpr_ctx.mutable_device();
     auto& rr_nodes = device_ctx.rr_nodes;
-    auto& rr_node_indices = device_ctx.rr_node_indices;
-
-    // 1) Create the clock source wire (located at the center of the chip)
-
-    // a) Find the center of the chip
     auto& grid = device_ctx.grid;
-    auto x_mid_dim = grid.width() / 2;
-    auto y_mid_dim = grid.height() / 2;
 
-    // b) Create clock source wire node at at the center of the chip
-    rr_nodes.emplace_back();
-    auto clock_source_idx = rr_nodes.size() - 1; // last inserted node
-    rr_nodes[clock_source_idx].set_coordinates(x_mid_dim, x_mid_dim, y_mid_dim, y_mid_dim);
-    rr_nodes[clock_source_idx].set_type(CHANX);
-    rr_nodes[clock_source_idx].set_capacity(1);
-
-    // Find all I/O inpads and connect all I/O output pins to the clock source wire
-    // through a switch.
-    // Resize the rr_nodes array to 2x the number of I/O input pins
-    for (size_t i = 0; i < grid.width(); ++i) {
-        for (size_t j = 0; j < grid.height(); j++) {
-
-            auto type = grid[i][j].type;
-            auto width_offset = grid[i][j].width_offset;
-            auto height_offset = grid[i][j].height_offset;
-            for (e_side side : SIDES) {
-                //Don't connect pins which are not adjacent to channels around the perimeter
-                if (   (i == 0 && side != RIGHT)
-                    || (i == int(grid.width() - 1) && side != LEFT)
-                    || (j == 0 && side != TOP)
-                    || (j == int(grid.width() - 1) && side != BOTTOM)) {
-                    continue;
-                }
-
-                for (int pin_index = 0; pin_index < type->num_pins; pin_index++) {
-                    /* We only are working with opins so skip non-drivers */
-                    if (type->class_inf[type->pin_class[pin_index]].type != DRIVER) {
-                        continue;
-                    }
-
-                    /* Can't do anything if pin isn't at this location */
-                    if (0 == type->pinloc[width_offset][height_offset][side][pin_index]) {
-                        continue;
-                    }
-                    vtr::printf_info("%s \n",type->pb_type->name);
-                    if (strcmp(type->pb_type->name, "io") != 0) {
-                        continue;
-                    }
-
-                    auto node_index = get_rr_node_index(rr_node_indices, i, j, OPIN, pin_index, side);
-                    rr_nodes[node_index].add_edge(clock_source_idx, 0);
-                    vtr::printf_info("At %d,%d output pin node %d\n", i, j, node_index);
-
-
-                }
-                for (auto pin_index : type->get_clock_pins_indices()) {
-
-
-                    /* Can't do anything if pin isn't at this location */
-                    if (0 == type->pinloc[width_offset][height_offset][side][pin_index]) {
-                        continue;
-                    }
-
-
-                    auto node_index = get_rr_node_index(rr_node_indices, i, j, IPIN, pin_index, side);
-                    rr_nodes[clock_source_idx].add_edge(node_index, 1);
-                    vtr::printf_info("At %d,%d input pin node %d\n", i, j, node_index);
-
-                }
-
-            }
-
-            // Loop over ports
-                // Is the port class clock_source
-                    // Assert pin is an output (check)
-                    // find node using t_rr_node_index (check))
-                    // get_rr_node_index(rr_nodes, i, j, OPIN, side) (check)
-                    // create an edge to clock source wire (check)
-                // Is the port a clock
-                    // find pin using t_rr_node_indices (rr_type is a IPIN)
-                    // create an edge from the clock source wire to the
+    for(unsigned y_start = y_chan_wire.start, y_end = y_chan_wire.end;
+        y_end < grid.height();
+        y_start += repeat.y, y_end += repeat.y) {
+        for(unsigned x = y_chan_wire.position; x < grid.width(); x += repeat.x) {
+            auto rr_node_index = create_rr_node_for_one_spine(y_start, y_end, x, rr_nodes);
+            record_switch_point_locations_for_rr_node(y_start, y_end, x, rr_node_index, clock_graph);
         }
     }
-
-    // Find all clock pins and connect them to the clock source wire
-
-
-    vtr::printf_info("Finished creating star model clock network\n");
 }
+
+int ClockSpine::create_rr_node_for_one_spine(
+        int y_start,
+        int y_end,
+        int x,
+        std::vector<t_rr_node>& rr_nodes) {
+
+    rr_nodes.emplace_back();
+    auto node_index = rr_nodes.size() - 1;
+    rr_nodes[node_index].set_coordinates(x, y_start, x, y_end);
+    rr_nodes[node_index].set_type(CHANY);
+    rr_nodes[node_index].set_capacity(1);
+
+    return node_index;
+}
+
+void ClockSpine::record_switch_point_locations_for_rr_node(
+        int y_start,
+        int y_end,
+        int x,
+        int rr_node_index,
+        ClockRRGraph& clock_graph) {
+
+}
+
+//TODO: Implement clock Htree generation code
+void ClockHTree::create_rr_nodes_for_one_instance(ClockRRGraph& clock_graph) {
+    auto unused(clock_graph); //Removes unused parameter warning
+    vpr_throw(VPR_ERROR_ROUTE, __FILE__, __LINE__, "HTrees are not yet supported.\n");
+}
+
+
+//void ClockRRGraph::create_star_model_network() {
+//
+//    vtr::printf_info("Creating a clock network in the form of a star model\n");
+//
+//    auto& device_ctx = g_vpr_ctx.mutable_device();
+//    auto& rr_nodes = device_ctx.rr_nodes;
+//    auto& rr_node_indices = device_ctx.rr_node_indices;
+//
+//    // 1) Create the clock source wire (located at the center of the chip)
+//
+//    // a) Find the center of the chip
+//    auto& grid = device_ctx.grid;
+//    auto x_mid_dim = grid.width() / 2;
+//    auto y_mid_dim = grid.height() / 2;
+//
+//    // b) Create clock source wire node at at the center of the chip
+//    rr_nodes.emplace_back();
+//    auto clock_source_idx = rr_nodes.size() - 1; // last inserted node
+//    rr_nodes[clock_source_idx].set_coordinates(x_mid_dim, x_mid_dim, y_mid_dim, y_mid_dim);
+//    rr_nodes[clock_source_idx].set_type(CHANX);
+//    rr_nodes[clock_source_idx].set_capacity(1);
+//
+//    // Find all I/O inpads and connect all I/O output pins to the clock source wire
+//    // through a switch.
+//    // Resize the rr_nodes array to 2x the number of I/O input pins
+//    for (size_t i = 0; i < grid.width(); ++i) {
+//        for (size_t j = 0; j < grid.height(); j++) {
+//
+//            auto type = grid[i][j].type;
+//            auto width_offset = grid[i][j].width_offset;
+//            auto height_offset = grid[i][j].height_offset;
+//            for (e_side side : SIDES) {
+//                //Don't connect pins which are not adjacent to channels around the perimeter
+//                if (   (i == 0 && side != RIGHT)
+//                    || (i == int(grid.width() - 1) && side != LEFT)
+//                    || (j == 0 && side != TOP)
+//                    || (j == int(grid.width() - 1) && side != BOTTOM)) {
+//                    continue;
+//                }
+//
+//                for (int pin_index = 0; pin_index < type->num_pins; pin_index++) {
+//                    /* We only are working with opins so skip non-drivers */
+//                    if (type->class_inf[type->pin_class[pin_index]].type != DRIVER) {
+//                        continue;
+//                    }
+//
+//                    /* Can't do anything if pin isn't at this location */
+//                    if (0 == type->pinloc[width_offset][height_offset][side][pin_index]) {
+//                        continue;
+//                    }
+//                    vtr::printf_info("%s \n",type->pb_type->name);
+//                    if (strcmp(type->pb_type->name, "io") != 0) {
+//                        continue;
+//                    }
+//
+//                    auto node_index = get_rr_node_index(rr_node_indices, i, j, OPIN, pin_index, side);
+//                    rr_nodes[node_index].add_edge(clock_source_idx, 0);
+//                    vtr::printf_info("At %d,%d output pin node %d\n", i, j, node_index);
+//
+//
+//                }
+//                for (auto pin_index : type->get_clock_pins_indices()) {
+//
+//
+//                    /* Can't do anything if pin isn't at this location */
+//                    if (0 == type->pinloc[width_offset][height_offset][side][pin_index]) {
+//                        continue;
+//                    }
+//
+//
+//                    auto node_index = get_rr_node_index(rr_node_indices, i, j, IPIN, pin_index, side);
+//                    rr_nodes[clock_source_idx].add_edge(node_index, 1);
+//                    vtr::printf_info("At %d,%d input pin node %d\n", i, j, node_index);
+//
+//                }
+//
+//            }
+//
+//            // Loop over ports
+//                // Is the port class clock_source
+//                    // Assert pin is an output (check)
+//                    // find node using t_rr_node_index (check))
+//                    // get_rr_node_index(rr_nodes, i, j, OPIN, side) (check)
+//                    // create an edge to clock source wire (check)
+//                // Is the port a clock
+//                    // find pin using t_rr_node_indices (rr_type is a IPIN)
+//                    // create an edge from the clock source wire to the
+//        }
+//    }
+//
+//    // Find all clock pins and connect them to the clock source wire
+//
+//
+//    vtr::printf_info("Finished creating star model clock network\n");
+//}
 
--- vpr/src/route/rr_graph_clock.h
+++ vpr/src/route/rr_graph_clock.h
@@ -3,82 +3,266 @@
 
 #include <string>
 #include <vector>
+#include <memory>
 #include <unordered_map>
-#include <utility>
 
-struct t_coordinates {
+#include "globals.h"
+
+class ClockRRGraph;
+
+enum class ClockType {
+    SPINE,
+    RIB,
+    H_TREE
+};
+
+enum class ClockSwitchType {
+    DRIVE,
+    TAP
+};
+
+struct Coordinates {
     int x;
     int y;
 };
 
-enum e_clock_type {
-    spine,
-    rib,
-    h_tree
+struct MetalLayer {
+    float r_metal;
+    float c_metal;
 };
 
-enum e_switch_type {
-    source,
-    tap
+struct Wire {
+    MetalLayer layer;
+    int start;
+    int end;
+    int position;
 };
 
-struct t_metal_layer {
-    float Rmetal;
-    float Cmetal;
+struct WireRepeat {
+    int x;
+    int y;
 };
 
-struct t_clock_connections {
+struct DriveLocation {
+    Coordinates offset;
+};
+
+struct TapLocations {
+    Coordinates offset;
+    Coordinates increment;
+};
+
+/* Data Structure that contatin*/
+struct ClockConnection {
+
     std::string from;
     std::string to;
+    int iswitch;
     int fc;
+
+    // used only to connect from local routing to the clock drive point
+    Coordinates switch_location;
 };
 
-class t_clock_network_description {
+class ClockNetwork {
     protected:
-        enum e_clock_type type;
         std::string clock_name;
         int num_inst;
-        int repeat_x;
-        int repeat_y;
     public:
-        virtual ~t_clock_network_description() = 0;
-        virtual void create_rr_nodes_for_clock_network_wires() = 0;
+        /*
+         * Destructor
+         */
+        virtual ~ClockNetwork() {};
+
+        /*
+         * Getters
+         */
+        int get_num_inst() {return num_inst;}
+        std::string get_clock_name() const {return this->clock_name;}
+        virtual ClockType get_network_type() const = 0;
+
+        /*
+         * Setters
+         */
+        void set_clock_name(std::string clock_name1) {clock_name = clock_name1;} 
+        void set_num_instance(int num_inst1) {num_inst = num_inst1;}
+
+        /*
+         * Member funtions
+         */
+        /* Creates the RR nodes for the clock network wires and adds them to the reverse lookup
+           in ClockRRGraph. The reverse lookup maps the nodes to their switch point locations */
+        void create_rr_nodes_for_clock_network_wires(ClockRRGraph& clock_graph);
+        virtual void create_rr_nodes_for_one_instance(ClockRRGraph& clock_graph) = 0;
 };
 
-class t_clock_ribs_description : private t_clock_network_description {
+class ClockRib : public ClockNetwork {
     private:
-        t_metal_layer metal_layer;
-        int y_pos;
-        int start_x;
-        int end_x;
-        int source_x_offset;
-        int tap_x_offset;
-        int tap_x_incr;
+
+        // start and end x and position in the y
+        Wire x_chan_wire;
+        WireRepeat repeat;
+
+        // offset in the x
+        DriveLocation drive;
+
+        // offset and incr in the x
+        TapLocations tap;
+
     public:
-        void create_rr_nodes_for_clock_network_wires();
+        /** Constructor**/
+        ClockRib() {}; // default
+        ClockRib(Wire wire1, WireRepeat repeat1, DriveLocation drive1, TapLocations tap1):
+                 x_chan_wire(wire1), repeat(repeat1), drive(drive1), tap(tap1) {};
+        /*
+         * Getters
+         */
+        ClockType get_network_type() const {return ClockType::RIB;}
+
+        /*
+         * Setters
+         */
+        void set_metal_layer(int r_metal, int c_metal) {
+            x_chan_wire.layer.r_metal = r_metal;
+            x_chan_wire.layer.c_metal = c_metal;
+        }
+        void set_initial_wire_location(int start_x, int end_x, int y) {
+            x_chan_wire.start = start_x;
+            x_chan_wire.end = end_x;
+            x_chan_wire.position = y;
+        }
+        void set_wire_repeat(int repeat_x, int repeat_y) {
+            repeat.x = repeat_x;
+            repeat.y = repeat_y;
+        }
+        void set_drive_location(int offset_x) {
+            drive.offset.x = offset_x;
+            drive.offset.y = -1;
+        }
+        void set_tap_location(int offset_x, int increment_x) {
+            tap.offset.x = offset_x;
+            tap.offset.y = -1;
+            tap.increment.x = increment_x;
+            tap.increment.y = -1;
+        }
+
+        /*
+         * Member functions
+         */
+        void create_rr_nodes_for_one_instance(ClockRRGraph& clock_graph);
+        int create_rr_node_for_one_rib(
+                int x_start,
+                int x_end,
+                int y,
+                std::vector<t_rr_node>& rr_nodes);
+        void record_switch_point_locations_for_rr_node(
+                int x_start,
+                int x_end,
+                int y,
+                int rr_node_index,
+                ClockRRGraph& clock_graph);
 };
 
-class t_clock_spines_description : private t_clock_network_description {
+class ClockSpine : public ClockNetwork {
     private:
-        t_metal_layer metal_layer;
-        int x_pos;
-        int start_y;
-        int end_y;
-        int source_y_offset;
-        int tap_y_offset;
-        int tap_y_incr;
+
+        // start and end y and position in the x
+        Wire y_chan_wire;
+        WireRepeat repeat;
+
+        // offset in the y
+        DriveLocation drive;
+
+        // offset and incr in the y
+        TapLocations tap;
+
     public:
-        void create_rr_nodes_for_clock_network_wires();
+        ClockType get_network_type() const {return ClockType::SPINE;}
+
+        /*
+         * Setters
+         */
+        void set_metal_layer(int r_metal, int c_metal) {
+            y_chan_wire.layer.r_metal = r_metal;
+            y_chan_wire.layer.c_metal = c_metal;
+        }
+        void set_initial_wire_location(int start_y, int end_y, int x) {
+            y_chan_wire.start = start_y;
+            y_chan_wire.end = end_y;
+            y_chan_wire.position = x;
+        }
+        void set_wire_repeat(int repeat_x, int repeat_y) {
+            repeat.x = repeat_x;
+            repeat.y = repeat_y;
+        }
+        void set_drive_location(int offset_y) {
+            drive.offset.x = -1;
+            drive.offset.y = offset_y;
+        }
+        void set_tap_location(int offset_y, int increment_y) {
+            tap.offset.x = -1;
+            tap.offset.y = offset_y;
+            tap.increment.x = -1;
+            tap.increment.y = increment_y;
+        }
+
+        /*
+         * Member functions
+         */
+        void create_rr_nodes_for_one_instance(ClockRRGraph& clock_graph);
+        int create_rr_node_for_one_spine(
+                int y_start,
+                int y_end,
+                int x,
+                std::vector<t_rr_node>& rr_nodes);
+        void record_switch_point_locations_for_rr_node(
+                int y_start,
+                int y_end,
+                int x,
+                int rr_node_index,
+                ClockRRGraph& clock_graph);
+
+};
+
+class ClockHTree : private ClockNetwork {
+    private:
+
+        // position not needed since it changes with every root of the tree
+        Wire x_chan_wire;
+        Wire y_chan_wire;
+        WireRepeat repeat;
+
+        DriveLocation drive;
+
+        TapLocations tap;
+
+    public:
+        ClockType get_network_type() const {return ClockType::H_TREE;}
+        // TODO: Unimplemented member function
+        void create_rr_nodes_for_one_instance(ClockRRGraph& clock_graph);
+};
+
+class SwitchPoint {
+    private:
+        // [grid_width][grid_height][nodes]
+        std::vector<std::vector<std::vector<int>>> rr_node_indices;
+        std::vector<Coordinates> locations;
+};
+
+class SwitchPoints {
+    private:
+        std::unordered_map<std::string, SwitchPoint> switch_name_to_switch_location;
+    public:
+        /** Getters **/
+
+        /** Setters **/
 };
 
 class ClockRRGraph {
     public:
         /* Reverse lookup for to find the clock source and tap locations for each clock_network
-           The map key is the the clock network name and the switch type (source or sink).
-           The map value is a map of all of the coordinates to the number of times they repeat */
-//        std::unordered_map<std::pair<std::string, enum e_switch_type>,
-//            std::unordered_map<t_coordinates, int>>
-//                clock_tap_and_source_locations;
+           The map key is the the clock network name and value are all the switch points*/
+        std::unordered_map<std::string, SwitchPoints> clock_name_to_switch_points;
 
     public:
         /* Creates the routing resourse (rr) graph of the clock network and appends it to the
@@ -90,27 +274,19 @@ class ClockRRGraph {
         static void create_star_model_network();
 
         /* Create the wires of the clock network, supports rib and spines (TODO: suport H-trees).
-           TODO: will need to include buffers or have a sperate buffer insertion function */
-        static void create_clock_network_wires(
-                const t_clock_network_description& clock_network);
-
+            TODO: will need to include buffers or have a sperate buffer insertion function */
         /* loop over all of the clock networks and create their wires */
-        static void create_clock_networks_wires(
-                std::vector<t_clock_network_description>& clock_networks);
-
-        /* Connects the switches an connections between clock network wires */
-        static void create_clock_network_switches(
-                std::string clock_network_from,
-                std::string clock_network_to,
-                const int fc);
+        void create_clock_networks_wires(std::vector<std::unique_ptr<ClockNetwork>>& clock_networks);
 
         /* loop over all clock routing connections and create the switches and connections */
-        static void create_clock_networks_switches(
-                std::vector<t_clock_connections>& clock_connections);
+        static void create_clock_networks_switches(std::vector<ClockConnection>& clock_connections);
+
+        /* Connects the switches and connections between clock network wires */
+        static void create_clock_network_switches(ClockConnection& clock_connection);
 
         /* Connects the inter-block routing to the clock source at the specified coordinates */
         static void create_switches_from_routing_to_clock_source(
-                const t_coordinates& clock_source_coordinates,
+                const Coordinates& clock_source_coordinates,
                 std::string& clock_network_name,
                 const int fc);
 
@@ -126,8 +302,7 @@ class ClockRRGraph {
                 const int fc);
 
         /* Connects inter-block routing to the clock pins*/
-        static void create_switches_from_routing_to_clock_pins(
-                const int fc);
+        static void create_switches_from_routing_to_clock_pins(const int fc);
 
 };
 
