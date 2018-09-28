ab1e44117878a53e91c5555bf9af4df4e4a59310
Mustafa Abbas <mustafa.abbas@bucknell.edu>
2018-09-28 19:37:51 -0400

    Added support to draw edges for zero length wires in an unconventional way

--- vpr/src/draw/draw.cpp
+++ vpr/src/draw/draw.cpp
@@ -1770,6 +1770,9 @@ static void draw_chanx_to_chanx_edge(int from_node, int to_node,
 	to_xlow = device_ctx.rr_nodes[to_node].xlow();
 	to_xhigh = device_ctx.rr_nodes[to_node].xhigh();
 
+    int from_length = from_xhigh - from_xlow;
+    int to_length = to_xhigh = to_xlow;
+
 	if (to_xhigh < from_xlow) { /* From right to left */
 		/* UDSD Note by WMF: could never happen for INC wires, unless U-turn. For DEC
 		 * wires this handles well */
@@ -1790,12 +1793,16 @@ static void draw_chanx_to_chanx_edge(int from_node, int to_node,
 		if (device_ctx.rr_nodes[to_node].direction() != BI_DIRECTION) {
 			/* must connect to to_node's wire beginning at x2 */
 			if (to_track % 2 == 0) { /* INC wire starts at leftmost edge */
-				VTR_ASSERT(from_xlow < to_xlow);
+                if(from_length != 0 && to_length != 0) { // ignore assert if length zero wire 
+				    VTR_ASSERT(from_xlow < to_xlow);
+                }
 				x2 = to_chan.left();
 				/* since no U-turns from_track must be INC as well */
 				x1 = draw_coords->tile_x[to_xlow - 1] + draw_coords->get_tile_width();
 			} else { /* DEC wire starts at rightmost edge */
-				VTR_ASSERT(from_xhigh > to_xhigh);
+                if(from_length != 0 && to_length != 0) { // ignore assert if length zero wire
+				    VTR_ASSERT(from_xhigh > to_xhigh);
+                }
 				x2 = to_chan.right();
 				x1 = draw_coords->tile_x[to_xhigh + 1];
 			}
