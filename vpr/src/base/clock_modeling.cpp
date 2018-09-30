cf66303d600836197490dbfdac5109357c008e8d
Mustafa Abbas <mustafa.abbas@bucknell.edu>
2018-09-30 14:35:43 -0400

    Safer way to acess is_global pin array in treat_clock_pins_as_non_globals - Initial way would copy the pointer to the array. This was fine since   is_global_pin is an array and not a vector. But in the case of a   vector the function would only modify the vector locally. - Additionally, was initially taking a reference to device not   mutable_device. Since t_type_descriptor is not encapsulated it doen't   matter for now but it may in the future.

--- vpr/src/base/clock_modeling.cpp
+++ vpr/src/base/clock_modeling.cpp
@@ -4,18 +4,18 @@
 
 void ClockModeling::treat_clock_pins_as_non_globals() {
 
-    auto& device_ctx = g_vpr_ctx.device();
+    auto& device_ctx = g_vpr_ctx.mutable_device();
 
     for (int type_idx = 0; type_idx < device_ctx.num_block_types; type_idx++) {
 
-        auto type = device_ctx.block_types[type_idx];
-        if(type.pb_type) {
-            for(auto clock_pin_idx : type.get_clock_pins_indices()) {
+        auto* type = &(device_ctx.block_types[type_idx]);
+        if(type->pb_type) {
+            for(auto clock_pin_idx : type->get_clock_pins_indices()) {
 
                // clock pins should be originally considered as global when reading the architecture
-               VTR_ASSERT(type.is_global_pin[clock_pin_idx]);
+               VTR_ASSERT(type->is_global_pin[clock_pin_idx]);
 
-               type.is_global_pin[clock_pin_idx] = false;
+               type->is_global_pin[clock_pin_idx] = false;
             }
         }
     }
