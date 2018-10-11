b54d98acf08c5bfa7adb0c72977fa320c8228057
kmurray <k.murray@utoronto.ca>
2018-10-11 11:18:17 -0400

    vpr: Ensure rr node occupancy is reloaded when reading routing from file
    This ensures that the routing feasibility check will correctly detect
    invalid routing and VPR will exit with the correct exit code if an
    infeasible (i.e. congested) routing is loaded.

--- vpr/src/base/read_route.cpp
+++ vpr/src/base/read_route.cpp
@@ -120,6 +120,7 @@ bool read_route(const char* route_file, const t_router_opts& router_opts, bool v
             router_opts.acc_fac, true);
 
     /* Finished loading in the routing, now check it*/
+	recompute_occupancy_from_scratch();
     bool is_feasible = feasible_routing();
     if (is_feasible) {
         check_route(router_opts.route_type, device_ctx.num_rr_switches);
