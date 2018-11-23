0ba0d00272e46c43d00b9522ee8a52ad034fda65
kmurray <k.murray@utoronto.ca>
2018-11-23 17:42:46 -0500

    archfpga: Only check for timing consistency in arch file if timing enabled

--- libs/libarchfpga/src/read_xml_arch_file.cpp
+++ libs/libarchfpga/src/read_xml_arch_file.cpp
@@ -87,13 +87,18 @@ static void SetupPinLocationsAndPinClasses(pugi::xml_node Locations,
 
 /*    Process XML hiearchy */
 static void ProcessPb_Type(pugi::xml_node Parent, t_pb_type * pb_type,
-		t_mode * mode, const t_arch& arch, const pugiutil::loc_data& loc_data);
+		t_mode * mode, 
+        const bool timing_enabled,
+        const t_arch& arch,
+        const pugiutil::loc_data& loc_data);
 static void ProcessPb_TypePort(pugi::xml_node Parent, t_port * port,
 		e_power_estimation_method power_method, const bool is_root_pb_type, const pugiutil::loc_data& loc_data);
 static void ProcessPinToPinAnnotations(pugi::xml_node parent,
 		t_pin_to_pin_annotation *annotation, t_pb_type * parent_pb_type, const pugiutil::loc_data& loc_data);
 static void ProcessInterconnect(pugi::xml_node Parent, t_mode * mode, const pugiutil::loc_data& loc_data);
-static void ProcessMode(pugi::xml_node Parent, t_mode * mode, const t_arch& arch,
+static void ProcessMode(pugi::xml_node Parent, t_mode * mode,
+        const bool timing_enabled,
+        const t_arch& arch,
 		const pugiutil::loc_data& loc_data);
 static void Process_Fc_Values(pugi::xml_node Node, t_default_fc_spec &spec, const pugiutil::loc_data& loc_data);
 static void Process_Fc(pugi::xml_node Node, t_type_descriptor * Type, t_segment_inf *segments, int num_segments, const t_default_fc_spec &arch_def_fc, const pugiutil::loc_data& loc_data);
@@ -111,7 +116,9 @@ static t_grid_def ProcessGridLayout(pugi::xml_node layout_type_tag, const pugiut
 static void ProcessDevice(pugi::xml_node Node, t_arch *arch, t_default_fc_spec &arch_def_fc, const pugiutil::loc_data& loc_data);
 static void ProcessComplexBlocks(pugi::xml_node Node,
 		t_type_descriptor ** Types, int *NumTypes,
-		t_arch& arch, const t_default_fc_spec &arch_def_fc,
+		t_arch& arch,
+        const bool timing_enabled,
+        const t_default_fc_spec &arch_def_fc,
         const pugiutil::loc_data& loc_data);
 static void ProcessSwitches(pugi::xml_node Node,
 		t_arch_switch_inf **Switches, int *NumSwitches,
@@ -238,7 +245,7 @@ void XmlReadArch(const char *ArchFile, const bool timing_enabled,
 
         /* Process types */
         Next = get_single_child(architecture, "complexblocklist", loc_data);
-        ProcessComplexBlocks(Next, Types, NumTypes, *arch, arch_def_fc, loc_data);
+        ProcessComplexBlocks(Next, Types, NumTypes, *arch, timing_enabled, arch_def_fc, loc_data);
 
         /* Process directs */
         Next = get_single_child(architecture, "directlist", loc_data, OPTIONAL);
@@ -920,7 +927,10 @@ static void ProcessPb_TypePowerEstMethod(pugi::xml_node Parent, t_pb_type * pb_t
 
 /* Takes in a pb_type, allocates and loads data for it and recurses downwards */
 static void ProcessPb_Type(pugi::xml_node Parent, t_pb_type * pb_type,
-		t_mode * mode, const t_arch& arch, const pugiutil::loc_data& loc_data) {
+		t_mode * mode,
+        const bool timing_enabled,
+        const t_arch& arch,
+        const pugiutil::loc_data& loc_data) {
 	int num_ports, i, j, k, num_annotations;
 	const char *Prop;
 	pugi::xml_node Cur;
@@ -1148,7 +1158,9 @@ static void ProcessPb_Type(pugi::xml_node Parent, t_pb_type * pb_type,
 		}
 		VTR_ASSERT(j == num_annotations);
 
-        check_leaf_pb_model_timing_consistency(pb_type, arch);
+        if (timing_enabled) {
+            check_leaf_pb_model_timing_consistency(pb_type, arch);
+        }
 
 		/* leaf pb_type, if special known class, then read class lib otherwise treat as primitive */
 		if (pb_type->class_type == LUT_CLASS) {
@@ -1173,7 +1185,7 @@ static void ProcessPb_Type(pugi::xml_node Parent, t_pb_type * pb_type,
 					sizeof(t_mode));
 			pb_type->modes[i].parent_pb_type = pb_type;
 			pb_type->modes[i].index = i;
-			ProcessMode(Parent, &pb_type->modes[i], arch, loc_data);
+			ProcessMode(Parent, &pb_type->modes[i], timing_enabled, arch, loc_data);
 			i++;
 		} else {
 			pb_type->modes = (t_mode*) vtr::calloc(pb_type->num_modes,
@@ -1184,7 +1196,7 @@ static void ProcessPb_Type(pugi::xml_node Parent, t_pb_type * pb_type,
 				if (0 == strcmp(Cur.name(), "mode")) {
 					pb_type->modes[i].parent_pb_type = pb_type;
 					pb_type->modes[i].index = i;
-					ProcessMode(Cur, &pb_type->modes[i], arch, loc_data);
+					ProcessMode(Cur, &pb_type->modes[i], timing_enabled, arch, loc_data);
 
 					ret_mode_names = mode_names.insert(
 							pair<string, int>(pb_type->modes[i].name, 0));
@@ -1560,7 +1572,9 @@ static void ProcessInterconnect(pugi::xml_node Parent, t_mode * mode, const pugi
 	VTR_ASSERT(i == num_interconnect);
 }
 
-static void ProcessMode(pugi::xml_node Parent, t_mode * mode, const t_arch& arch,
+static void ProcessMode(pugi::xml_node Parent, t_mode * mode,
+        const bool timing_enabled,
+        const t_arch& arch,
 		const pugiutil::loc_data& loc_data) {
 	int i;
 	const char *Prop;
@@ -1584,7 +1598,7 @@ static void ProcessMode(pugi::xml_node Parent, t_mode * mode, const t_arch& arch
 		Cur = get_first_child(Parent, "pb_type", loc_data);
 		while (Cur != nullptr) {
 			if (0 == strcmp(Cur.name(), "pb_type")) {
-				ProcessPb_Type(Cur, &mode->pb_type_children[i], mode, arch, loc_data);
+				ProcessPb_Type(Cur, &mode->pb_type_children[i], mode, timing_enabled, arch, loc_data);
 
 				ret_pb_types = pb_type_names.insert(
 						pair<string, int>(mode->pb_type_children[i].name, 0));
@@ -2555,7 +2569,9 @@ static void ProcessChanWidthDistrDir(pugi::xml_node Node, t_chan * chan, const p
  * child type objects. */
 static void ProcessComplexBlocks(pugi::xml_node Node,
 		t_type_descriptor ** Types, int *NumTypes,
-		t_arch& arch, const t_default_fc_spec &arch_def_fc,
+		t_arch& arch,
+        const bool timing_enabled,
+        const t_default_fc_spec &arch_def_fc,
         const pugiutil::loc_data& loc_data) {
 	pugi::xml_node CurType, Prev;
 	pugi::xml_node Cur;
@@ -2600,7 +2616,7 @@ static void ProcessComplexBlocks(pugi::xml_node Node,
 		/* Load pb_type info */
 		Type->pb_type = new t_pb_type;
 		Type->pb_type->name = vtr::strdup(Type->name);
-		ProcessPb_Type(CurType, Type->pb_type, nullptr, arch, loc_data);
+		ProcessPb_Type(CurType, Type->pb_type, nullptr, timing_enabled, arch, loc_data);
 		Type->num_pins = Type->capacity
 				* (Type->pb_type->num_input_pins
 						+ Type->pb_type->num_output_pins
