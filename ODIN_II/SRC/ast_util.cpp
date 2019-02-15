024efd08b93d11c3f474206d012c48a82744ba42
ademmings <lxdemmings@gmail.com>
2019-02-15 13:40:58 -0400

    tidied up clean up

--- ODIN_II/SRC/ast_util.cpp
+++ ODIN_II/SRC/ast_util.cpp
@@ -947,14 +947,8 @@ ast_node_t *resolve_node(STRING_CACHE *local_param_table_sc, short initial, char
 			newNode->shared_node = node->shared_node;
 
 			/* clean up */
-			if (node->type == BINARY_OPERATION || node->type == UNARY_OPERATION) {
-				for (i = 0; i < node->num_children; i++) {
-					vtr::free(node->children[i]->types.number.number);
-					vtr::free(node->children[i]->types.number.binary_string);
-					vtr::free(node->children[i]);
-				}
-				vtr::free(node->children);
-				vtr::free(node);
+			if (node->type != IDENTIFIERS) {
+				node = free_whole_tree(node);
 			}
 			
 			node = newNode;
