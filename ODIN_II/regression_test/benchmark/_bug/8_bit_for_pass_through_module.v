4fbfc48c7d72b74f6a313054c820422821af6f7f
Scott Young <sy6746@gmail.com>
2019-02-19 13:18:22 -0400

    Updated module pass through in _bug folder for Odin II tests to show case the missing generate feature.

--- ODIN_II/regression_test/benchmark/_bug/8_bit_for_pass_through_module.v
+++ ODIN_II/regression_test/benchmark/_bug/8_bit_for_pass_through_module.v
@@ -2,14 +2,13 @@ module eight_bit_for_pass_through(a, c, clk, rst);
 input [7:0] a;
 input clk, rst;
 output [7:0] c;
-        pass_through pass_wire_0(a[0], c[0], clk, rst);
-        pass_through pass_wire_1(a[1], c[1], clk, rst);
-        pass_through pass_wire_2(a[2], c[2], clk, rst);
-        pass_through pass_wire_3(a[3], c[3], clk, rst);
-        pass_through pass_wire_4(a[4], c[4], clk, rst);
-        pass_through pass_wire_5(a[5], c[5], clk, rst);
-        pass_through pass_wire_6(a[6], c[6], clk, rst);
-        pass_through pass_wire_7(a[7], c[7], clk, rst);
+genvar i;
+generate
+	for(i=0; i<8; i = i+1)
+	begin
+		pass_through pass_wire_7(a[i], c[i], clk, rst);
+	end	
+endgenerate
 endmodule
 
 module pass_through(a, c, clk, rst);
