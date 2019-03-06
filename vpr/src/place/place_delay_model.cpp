d380f9445c67ca765409d8e7089568664b3bea7f
kmurray <k.murray@utoronto.ca>
2019-03-05 19:04:48 -0500

    vpr: Fix shadow warnings with gcc-4.9

--- vpr/src/place/place_delay_model.cpp
+++ vpr/src/place/place_delay_model.cpp
@@ -17,35 +17,7 @@ float DeltaDelayModel::delay(int from_x, int from_y, int /*from_pin*/, int to_x,
     int delta_x = std::abs(from_x - to_x);
     int delta_y = std::abs(from_y - to_y);
 
-    float delay = delays_[delta_x][delta_y];
-#if 0
-    auto& device_ctx = g_vpr_ctx.device();
-    auto& grid = device_ctx.grid;
-
-    t_type_ptr from_type_ptr = grid[from_x][from_y].type;
-    t_type_ptr to_type_ptr = grid[to_x][to_y].type;
-    int from_class = from_type_ptr->pin_class[from_pin];
-    int to_class = to_type_ptr->pin_class[to_pin];
-
-    int src_node = get_rr_node_index(device_ctx.rr_node_indices, from_x, from_y, SOURCE, from_class);
-    int sink_node = get_rr_node_index(device_ctx.rr_node_indices, to_x, to_y, SINK, to_class);
-
-    float actual_delay = std::numeric_limits<float>::quiet_NaN();
-    calculate_delay(src_node, sink_node, router_opts_, &actual_delay);
-
-    float rel_diff = delay / actual_delay;
-    if (delay == 0. && actual_delay == 0.) {
-        rel_diff = 1.;
-    }
-
-    VTR_LOG("delay %d,%d:%d -> %d,%d:%d delta: %d,%d:%d %s->%s (estimate: %g actual: %g abs_diff: %g rel_diff: %g)\n",
-            from_x, from_y, from_class,
-            to_x, to_y, to_class,
-            to_x - from_x, to_y - from_y, to_class - from_class,
-            from_type_ptr->name, to_type_ptr->name,
-            delay, actual_delay, delay - actual_delay, rel_diff);
-#endif
-    return delay;
+    return delays_[delta_x][delta_y];
 }
 
 void DeltaDelayModel::dump_echo(std::string filepath) const {
@@ -88,40 +60,17 @@ float OverrideDelayModel::delay(int from_x, int from_y, int from_pin, int to_x, 
     override_key.delta_x = from_x - to_x;
     override_key.delta_y = from_y - to_y;
 
-    float delay = std::numeric_limits<float>::quiet_NaN();
+    float delay_val = std::numeric_limits<float>::quiet_NaN();
     auto override_iter = delay_overrides_.find(override_key);
     if (override_iter != delay_overrides_.end()) {
         //Found an override
-        delay = override_iter->second;
+        delay_val = override_iter->second;
     } else {
         //Fall back to the base delay model if no override was found
-        delay = base_delay_model_->delay(from_x, from_y, from_pin, to_x, to_y, to_pin);
+        delay_val = base_delay_model_->delay(from_x, from_y, from_pin, to_x, to_y, to_pin);
     }
 
-#if 0
-    int from_class = from_type_ptr->pin_class[from_pin];
-    int to_class = to_type_ptr->pin_class[to_pin];
-
-    int src_node = get_rr_node_index(device_ctx.rr_node_indices, from_x, from_y, SOURCE, from_class);
-    int sink_node = get_rr_node_index(device_ctx.rr_node_indices, to_x, to_y, SINK, to_class);
-
-    float actual_delay = std::numeric_limits<float>::quiet_NaN();
-    calculate_delay(src_node, sink_node, router_opts_, &actual_delay);
-
-    float rel_diff = delay / actual_delay;
-    if (delay == 0. && actual_delay == 0.) {
-        rel_diff = 1.;
-    }
-
-    VTR_LOG("delay %d,%d:%d -> %d,%d:%d delta: %d,%d:%d %s->%s (estimate: %g actual: %g abs_diff: %g rel_diff: %g)\n",
-            from_x, from_y, from_class,
-            to_x, to_y, to_class,
-            to_x - from_x, to_y - from_y, to_class - from_class,
-            from_type_ptr->name, to_type_ptr->name,
-            delay, actual_delay, delay - actual_delay, rel_diff);
-#endif
-
-    return delay;
+    return delay_val;
 }
 
 void OverrideDelayModel::set_delay_override(int from_type, int from_class, int to_type, int to_class, int delta_x, int delta_y, float delay) {
