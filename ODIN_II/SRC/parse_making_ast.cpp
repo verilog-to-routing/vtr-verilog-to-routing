3a6e1cb6e14ab227c6f53d0f3e4b941a634321bf
Nasrin Eshraghi Ivari <nasrin.eshraghi@unb.ca>
2019-03-13 16:34:55 -0300

    change the error messages

--- ODIN_II/SRC/parse_making_ast.cpp
+++ ODIN_II/SRC/parse_making_ast.cpp
@@ -863,7 +863,6 @@ ast_node_t *newPartSelectRangeRef(char *id, ast_node_t *expression1, ast_node_t 
 	}
 	else{
 		expression2->types.number.value = expression1->types.number.value - expression2->types.number.value + 1;
-		expression1->types.number.value = expression1->types.number.value + expression2->types.number.value - 1;
 	}
 
 	if (expression1->types.number.value < 0 || expression2->types.number.value < 0){
@@ -873,18 +872,11 @@ ast_node_t *newPartSelectRangeRef(char *id, ast_node_t *expression1, ast_node_t 
 								"Odin doesn't support negative number in index : [%d%s%d].", 
 								expression1->types.number.value, direction == 1 ? "+:" : "-:",
 								expression2->types.number.value);
-
 	}
-	else if(expression1->types.number.value < expression2->types.number.value){
-
-		/*ODIN doesn't support m<n */
-	 	error_message(PARSE_ERROR, line_number, current_parse_file, "%s", "Odin doesn't support arrays declared [m:n] where m is less than n.");
-
-	}
-	else if (expression1->types.number.value > upper_limit || expression2->types.number.value > bottom_limit) {
+	else if (expression1->types.number.value < upper_limit || expression2->types.number.value < bottom_limit) {
 		/* out of original range */
 		error_message(PARSE_ERROR, line_number,current_parse_file, 
-								"This part select range (%s [%d%s%d]) is out of range. It should be in the [%d:%d] range.",
+								"This part-select range (%s [%d%s%d]) is out of range. It should be in the [%d:%d] range.",
 								id,expression1->types.number.value, direction ==1 ? "+:" : "-:",expression2->types.number.value,
 								 upper_limit,bottom_limit );
 	}
