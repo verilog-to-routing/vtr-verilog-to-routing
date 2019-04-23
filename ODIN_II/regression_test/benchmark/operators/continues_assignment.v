f058ad3e6d09d732c566bcb49443c6b4acfb9e86
Nasrin Eshraghi Ivari <nasrin.eshraghi@unb.ca>
2019-04-23 18:13:55 -0300

    Add assignment tests

---
+++ ODIN_II/regression_test/benchmark/operators/continues_assignment.v
@@ -0,0 +1,14 @@
+module ff(D,clk,sync_reset,Q);
+input D;
+input clk; 
+input sync_reset; 
+output Q; 
+reg Q;
+always @(posedge clk) 
+begin
+ if(sync_reset==1'b1)
+  Q = 1'b0; 
+ else 
+  Q = D; 
+end 
+endmodule 
