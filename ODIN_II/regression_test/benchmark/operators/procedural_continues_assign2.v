7b9ac2b80f0a2ea0c0a4922dadac03edcbe4cb15
Nasrin Eshraghi Ivari <nasrin.eshraghi@unb.ca>
2019-04-23 18:58:07 -0300

    add Procedural_assignment test

---
+++ ODIN_II/regression_test/benchmark/operators/procedural_continues_assign2.v
@@ -0,0 +1,12 @@
+
+`timescale 1ns/100ps
+module FlipflopAssign (input reset, din, clk, output qout);
+reg qout;
+always @ (reset) begin
+if (reset) assign qout <= 0;
+else deassign qout;
+end
+always @ (posedge clk) begin
+qout <= din;
+end
+endmodule
