7c0343c52dfc8ffeeccbf59fed5cb00bd5fe03aa
ademmings <lxdemmings@gmail.com>
2019-05-21 16:47:12 -0300

    can almost generate module instances

--- ODIN_II/SRC/parse_making_ast.cpp
+++ ODIN_II/SRC/parse_making_ast.cpp
@@ -1271,6 +1271,7 @@ ast_node_t *newModuleInstance(char* module_ref_name, ast_node_t *module_named_in
 		ast_node_t *module_param_list = module_named_instance->children[i]->children[2];
 		char *module_param_name = make_module_param_name(defines_for_module_sc[num_modules-num_instances], module_param_list, module_ref_name);
 		ast_node_t *symbol_node = newSymbolNode(module_param_name, line_number);
+		printf("created: %s\n", module_param_name);
 
 		// if this is a parameterised instantiation
 		if (module_param_list)
@@ -1302,7 +1303,7 @@ ast_node_t *newModuleInstance(char* module_ref_name, ast_node_t *module_named_in
 
 		/* store the module symbol name that this calls in a list that will at the end be asociated with the module node */
 		module_instantiations_instance = (ast_node_t **)vtr::realloc(module_instantiations_instance, sizeof(ast_node_t*)*(size_module_instantiations+1));
-		module_instantiations_instance[size_module_instantiations] = new_node;
+		module_instantiations_instance[size_module_instantiations] = ast_node_deep_copy(new_node);
 		size_module_instantiations++;
 
 	}
