b81aa20711d69e5a84f9b877db809c2d4fd6e5b5
Tim 'mithro' Ansell <me@mith.ro>
2019-05-28 17:50:40 -0700

    utils: Fix fasm test.
    ```
    ./utils//fasm/test/test_parameters.cpp:19: FAILED:
      CHECK_THAT( params.EmitFasmFeature("A", "0"), Equals("B=1'b0") )
    with expansion:
      "B=1'b0
      " equals: "B=1'b0"
    
    ./utils//fasm/test/test_parameters.cpp:20: FAILED:
      CHECK_THAT( params.EmitFasmFeature("INIT_0", "10100000000000000000000000000001"), Equals("INIT[31:0]=32'b10100000000000000000000000000001") )
    with expansion:
      "INIT[31:0]=32'b10100000000000000000000000000001
      " equals: "INIT[31:0]=32'b10100000000000000000000000000001"
    
    ./utils//fasm/test/test_parameters.cpp:21: FAILED:
      CHECK_THAT( params.EmitFasmFeature("INIT_1", "00010000000000000000000000001001"), Equals("INIT[63:32]=32'b00010000000000000000000000001001") )
    with expansion:
      "INIT[63:32]=32'b00010000000000000000000000001001
      " equals: "INIT[63:32]=32'b00010000000000000000000000001001"
    ```
    
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- utils/fasm/test/test_parameters.cpp
+++ utils/fasm/test/test_parameters.cpp
@@ -16,9 +16,9 @@ TEST_CASE("parameters", "[fasm]") {
     // Unmatched features returns "".
     CHECK_THAT(params.EmitFasmFeature("C", "0"), Equals(""));
 
-    CHECK_THAT(params.EmitFasmFeature("A", "0"), Equals("B=1'b0"));
-    CHECK_THAT(params.EmitFasmFeature("INIT_0", "10100000000000000000000000000001"), Equals("INIT[31:0]=32'b10100000000000000000000000000001"));
-    CHECK_THAT(params.EmitFasmFeature("INIT_1", "00010000000000000000000000001001"), Equals("INIT[63:32]=32'b00010000000000000000000000001001"));
+    CHECK_THAT(params.EmitFasmFeature("A", "0"), Equals("B=1'b0\n"));
+    CHECK_THAT(params.EmitFasmFeature("INIT_0", "10100000000000000000000000000001"), Equals("INIT[31:0]=32'b10100000000000000000000000000001\n"));
+    CHECK_THAT(params.EmitFasmFeature("INIT_1", "00010000000000000000000000001001"), Equals("INIT[63:32]=32'b00010000000000000000000000001001\n"));
 }
 
 } // namespace
