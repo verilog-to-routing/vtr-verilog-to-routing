034d34e17d7debb7af8b818791c77ba14838bb01
Tim 'mithro' Ansell <me@mith.ro>
2019-06-03 09:48:57 -0700

    infra: Make the failure output clearer.

--- .github/travis/build.sh
+++ .github/travis/build.sh
@@ -6,8 +6,17 @@ set -e
 $SPACER
 
 start_section "vtr.build" "${GREEN}Building..${NC}"
-make -k CMAKE_PARAMS="-DVTR_ASSERT_LEVEL=3 -DWITH_BLIFEXPLORER=on" -j2 || make CMAKE_PARAMS="-DVTR_ASSERT_LEVEL=3 -DWITH_BLIFEXPLORER=on" -j1
+export FAILURE=0
+make -k CMAKE_PARAMS="-DVTR_ASSERT_LEVEL=3 -DWITH_BLIFEXPLORER=on" -j2 || export FAILURE=1
 end_section "vtr.build"
+
+# When the build fails, produce the failure output in a clear way
+if [ $FAILURE -ne 0 ]; then
+	start_section "vtr.failure" "${RED}Build failure output..${NC}"
+	make CMAKE_PARAMS="-DVTR_ASSERT_LEVEL=3 -DWITH_BLIFEXPLORER=on" -j1
+	end_section "vtr.failure"
+	exit 1
+fi
 
 $SPACER
 
