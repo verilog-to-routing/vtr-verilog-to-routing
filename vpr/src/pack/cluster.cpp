a8675701f16e6cf7b174feabaeda042ee3d682ba
kmurray <k.murray@utoronto.ca>
2019-06-10 18:56:38 -0400

    vpr: Fix formatting

--- vpr/src/pack/cluster.cpp
+++ vpr/src/pack/cluster.cpp
@@ -1881,13 +1881,13 @@ static void start_new_cluster(t_cluster_placement_stats* cluster_placement_stats
         //This is a naive approach to try balancing utilization when multiple types can
         //support the same primitive(s).
         std::stable_sort(candidate_types.begin(), candidate_types.end(),
-                  [&](t_type_ptr lhs, t_type_ptr rhs) {
-                      float lhs_util = float(num_used_type_instances[lhs]) / device_ctx.grid.num_instances(lhs);
-                      float rhs_util = float(num_used_type_instances[rhs]) / device_ctx.grid.num_instances(rhs);
+                         [&](t_type_ptr lhs, t_type_ptr rhs) {
+                             float lhs_util = float(num_used_type_instances[lhs]) / device_ctx.grid.num_instances(lhs);
+                             float rhs_util = float(num_used_type_instances[rhs]) / device_ctx.grid.num_instances(rhs);
 
-                      //Lower util first
-                      return lhs_util < rhs_util;
-                  });
+                             //Lower util first
+                             return lhs_util < rhs_util;
+                         });
     }
 
     if (verbosity > 2) {
