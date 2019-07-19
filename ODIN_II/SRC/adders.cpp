8e1bcb54a651d5a7ba1c282370ff09a599b2c867
j-b-1-7 <julie.brown@unb.ca>
2019-07-19 12:45:09 -0300

    ODIN_II: Fix coverity issue CID 201067

--- ODIN_II/SRC/adders.cpp
+++ ODIN_II/SRC/adders.cpp
@@ -218,7 +218,7 @@ void instantiate_hard_adder(nnode_t *node, short mark, netlist_t * /*netlist*/)
 		{
 			len = strlen(node->name) + 20; /* 6 chars for pin idx */
 			new_name = (char*)vtr::malloc(len);
-			odin_sprintf(new_name, "%s[%ld]", node->name, node->output_pins[i]->pin_node_idx);
+			odin_sprintf(new_name, "%s[%d]", node->name, node->output_pins[i]->pin_node_idx);
 			node->output_pins[i]->name = new_name;
 		}
 	}
