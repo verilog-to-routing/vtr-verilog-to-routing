6ab8ad872a2000e7a924ed5134a1a7e49072181d
MohamedEldafrawy <mohamed.eldafrawy@mail.utoronto.ca>
2019-08-13 15:17:05 -0400

    vpr: use vtr::safe_ratio to avoid potential divide by zero

--- vpr/src/pack/cluster.cpp
+++ vpr/src/pack/cluster.cpp
@@ -1975,16 +1975,8 @@ static void start_new_cluster(t_cluster_placement_stats* cluster_placement_stats
         //support the same primitive(s).
         std::stable_sort(candidate_types.begin(), candidate_types.end(),
                          [&](t_type_ptr lhs, t_type_ptr rhs) {
-                             float lhs_util = 0.;
-                             if (device_ctx.grid.num_instances(lhs) != 0) {
-                                 lhs_util = float(num_used_type_instances[lhs]) / device_ctx.grid.num_instances(lhs);
-                             }
-
-                             float rhs_util = 0;
-                             if (device_ctx.grid.num_instances(rhs) != 0) {
-                                 rhs_util = float(num_used_type_instances[rhs]) / device_ctx.grid.num_instances(rhs);
-                             }
-
+                             float lhs_util = vtr::safe_ratio(num_used_type_instances[lhs], device_ctx.grid.num_instances(lhs));
+                             float rhs_util = vtr::safe_ratio(num_used_type_instances[rhs], device_ctx.grid.num_instances(rhs));
                              //Lower util first
                              return lhs_util < rhs_util;
                          });
