9ae9c272ff224dbf30fc3acd2ccc1fd2859f1bd7
Tina (Dingyu) Yang <dingyu.yang@mail.utoronto.ca>
2019-08-14 12:30:09 -0400

    vpr: add label to combo box, add extension to file name, add default value to all combo boxes

--- vpr/src/draw/draw.cpp
+++ vpr/src/draw/draw.cpp
@@ -302,12 +302,14 @@ void initial_setup_NO_PICTURE_to_PLACEMENT(ezgl::application* app) {
     g_signal_connect(save, "clicked", G_CALLBACK(save_graphics_dialog_box), app);
 
     GObject* search_type = (GObject*)app->get_object("SearchType");
-    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(search_type), "Block ID");
-    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(search_type), "Block Name");
-    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(search_type), "Net ID");
-    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(search_type), "Net Name");
-    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(search_type), "RR Node ID");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(search_type), "Block ID");    // index 0
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(search_type), "Block Name");  // index 1
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(search_type), "Net ID");      // index 2
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(search_type), "Net Name");    // index 3
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(search_type), "RR Node ID");  // index 4
+    gtk_combo_box_set_active((GtkComboBox*)search_type, 0); // default set to Block ID which has an index 0
 
+    
     app->create_button("Toggle Nets", 2, toggle_nets);
     app->create_button("Blk Internal", 3, toggle_blk_internal);
     app->create_button("Blk Pin Util", 4, toggle_block_pin_util);
--- vpr/src/draw/save_graphics.cpp
+++ vpr/src/draw/save_graphics.cpp
@@ -23,6 +23,7 @@ void save_graphics_from_button(GtkWidget* /*widget*/, gint response_id, gpointer
         GList* list_of_widgets = gtk_container_get_children(GTK_CONTAINER(content_area));
         GtkWidget* combo_box = NULL;
         GtkWidget* text_entry = NULL;
+        gchar * combo_box_content = NULL;
         std::string file_name;
         std::string extension;
 
@@ -42,19 +43,22 @@ void save_graphics_from_button(GtkWidget* /*widget*/, gint response_id, gpointer
 
         // get the data from the text entry and combo box
         file_name = gtk_entry_get_text(GTK_ENTRY(text_entry));
-        extension = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(combo_box));
-
+        combo_box_content = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(combo_box));
+        extension = combo_box_content;
+        
         //save the graphics
         save_graphics(extension, file_name);
 
-        // free dynamically allocated list
+        // free dynamically allocated variables
         g_list_free(list_of_widgets);
+        g_free(combo_box_content);
     }
     // free widget
     gtk_widget_destroy(dialog);
 }
 
 void save_graphics(std::string& extension, std::string& file_name) {
+    file_name = file_name + "." + extension;
     if (extension == "pdf") {
         application.get_canvas(application.get_main_canvas_id())->print_pdf(file_name.c_str(), initial_world.width(), initial_world.height());
         return;
@@ -65,7 +69,7 @@ void save_graphics(std::string& extension, std::string& file_name) {
         application.get_canvas(application.get_main_canvas_id())->print_svg(file_name.c_str(), initial_world.width(), initial_world.height());
         return;
     } else {
-        warning_dialog_box("Invalid file extension");
+        warning_dialog_box("Invalid file type");
     }
 }
 
@@ -73,7 +77,8 @@ void save_graphics_dialog_box(GtkWidget* /*widget*/, ezgl::application* /*app*/)
     GObject* main_window;
     GtkWidget* content_area;
     GtkWidget* text_entry;
-    GtkWidget* label;
+    GtkWidget* name_label;
+    GtkWidget* type_label;
     GtkWidget* dialog;
     GtkWidget* combo_box;
 
@@ -91,32 +96,37 @@ void save_graphics_dialog_box(GtkWidget* /*widget*/, ezgl::application* /*app*/)
                                          NULL);
 
     // create elements
-    label = gtk_label_new("Enter file name:");
+    name_label = gtk_label_new("File name:");
     text_entry = gtk_entry_new();
+    type_label = gtk_label_new("File format:");
     combo_box = gtk_combo_box_text_new();
 
     // set name for text entry and combo box for later data extraction
     gtk_widget_set_name(text_entry, "file_name_text_entry");
     gtk_widget_set_name(combo_box, "file_name_combo_box");
 
-    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_box), "pdf");
-    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_box), "png");
-    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_box), "svg");
-
-    g_signal_connect_swapped(GTK_DIALOG(dialog),
-                             "response",
-                             G_CALLBACK(save_graphics_from_button),
-                             GTK_DIALOG(dialog));
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_box), "pdf"); // index 0
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_box), "png"); // index 1
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_box), "svg"); // index 2
+    
+    // set default values
+    gtk_combo_box_set_active((GtkComboBox*)combo_box, 0); // default set to pdf which has an index 0
+    gtk_entry_set_text((GtkEntry*)text_entry, "vpr_graphics"); // defualt text set to vpr_graphics
 
     // attach elements to the content area of the dialog
     content_area = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
-    gtk_container_add(GTK_CONTAINER(content_area), label);
+    gtk_container_add(GTK_CONTAINER(content_area), name_label);
     gtk_container_add(GTK_CONTAINER(content_area), text_entry);
+    gtk_container_add(GTK_CONTAINER(content_area), type_label);
     gtk_container_add(GTK_CONTAINER(content_area), combo_box);
-
+    
     // show the label & child widget of the dialog
     gtk_widget_show_all(dialog);
 
+    g_signal_connect_swapped(GTK_DIALOG(dialog),
+                         "response",
+                         G_CALLBACK(save_graphics_from_button),
+                         GTK_DIALOG(dialog));
     return;
 }
 
