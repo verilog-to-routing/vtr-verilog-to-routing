b2dc9d6fc9f93a147902fd07fdeff948ec6019d7
Alessandro Comodi <acomodi@antmicro.com>
2019-09-04 11:02:12 +0200

    fix valgrind memory leak
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- libs/libarchfpga/src/arch_util.cpp
+++ libs/libarchfpga/src/arch_util.cpp
@@ -251,6 +251,14 @@ void free_type_descriptors(std::vector<t_physical_tile_type>& type_descriptors) 
         vtr::free(type.is_ignored_pin);
         vtr::free(type.is_pin_global);
         vtr::free(type.pin_class);
+
+        for (auto equivalent_site : type.equivalent_sites) {
+            vtr::free(equivalent_site.pb_type_name);
+        }
+
+        for (auto port : type.ports) {
+            vtr::free(port.name);
+        }
     }
     type_descriptors.clear();
 }
