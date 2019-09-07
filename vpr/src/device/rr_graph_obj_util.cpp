da2bd71bf66a9c8e7513b0f723c2bf241b3f5331
tangxifan <tangxifan@gmail.com>
2019-09-06 19:56:54 -0600

    fix code format problems

--- vpr/src/device/rr_graph_obj_util.cpp
+++ vpr/src/device/rr_graph_obj_util.cpp
@@ -1,16 +1,17 @@
 /****************************************************************************
- * This file include most-utilized functions that manipulate on the 
- * RRGraph object 
+ * This file include most-utilized functions that manipulate on the
+ * RRGraph object
  ***************************************************************************/
 #include <tuple>
 #include "rr_graph_obj_util.h"
 
 /****************************************************************************
- * Find the switches interconnecting two nodes 
+ * Find the switches interconnecting two nodes
  * Return a vector of switch ids
  ***************************************************************************/
 std::vector<RRSwitchId> find_rr_graph_switches(const RRGraph& rr_graph,
-                                               const RRNodeId& from_node, const RRNodeId& to_node) {
+                                               const RRNodeId& from_node,
+                                               const RRNodeId& to_node) {
     std::vector<RRSwitchId> switches;
     std::vector<RREdgeId> edges = rr_graph.find_edges(from_node, to_node);
     if (true == edges.empty()) {
@@ -19,12 +20,11 @@ std::vector<RRSwitchId> find_rr_graph_switches(const RRGraph& rr_graph,
     }
 
     /* Reach here, edge list is not empty, find switch id one by one
-     * and update the switch list  
-     */
+   * and update the switch list
+   */
     for (auto edge : edges) {
         switches.push_back(rr_graph.edge_switch(edge));
     }
 
     return switches;
 }
-
