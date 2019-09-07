2ca58db6d389c55e04657f8cd1dd16d6cce79b74
tangxifan <tangxifan@gmail.com>
2019-09-06 20:04:47 -0600

    fix code format

--- vpr/src/device/rr_graph_obj_util.h
+++ vpr/src/device/rr_graph_obj_util.h
@@ -7,7 +7,8 @@
 #include "rr_graph_obj.h"
 
 /* Get node-to-node switches in a RRGraph */
-std::vector<RRSwitchId> find_rr_graph_switches(const RRGraph& rr_graph, 
-                                               const RRNodeId& from_node, const RRNodeId& to_node);
+std::vector<RRSwitchId> find_rr_graph_switches(const RRGraph& rr_graph,
+                                               const RRNodeId& from_node,
+                                               const RRNodeId& to_node);
 
 #endif
