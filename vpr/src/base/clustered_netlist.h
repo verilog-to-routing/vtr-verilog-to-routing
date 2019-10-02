b6d373fa3a4e67add37ad9691aeab2b5fc968281
Alessandro Comodi <acomodi@antmicro.com>
2019-10-02 13:22:03 +0200

    place: added placement priority and check direct pin mappings
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- vpr/src/base/clustered_netlist.h
+++ vpr/src/base/clustered_netlist.h
@@ -254,9 +254,9 @@ class ClusteredNetlist : public Netlist<ClusterBlockId, ClusterPortId, ClusterPi
 
     //Pins
     vtr::vector_map<ClusterPinId, int> pin_physical_index_; //The physical pin index (i.e. pin index
-                                                                                                   //in t_physical_tile_type) corresponding
-                                                                                                   //to the logical pin
-    vtr::vector_map<ClusterPinId, int> pin_logical_index_; //The logical pin index of this block
+                                                            //in t_physical_tile_type) corresponding
+                                                            //to the logical pin
+    vtr::vector_map<ClusterPinId, int> pin_logical_index_;  //The logical pin index of this block
 
     //Nets
     vtr::vector_map<ClusterNetId, bool> net_is_ignored_; //Boolean mapping indicating if the net is ignored
