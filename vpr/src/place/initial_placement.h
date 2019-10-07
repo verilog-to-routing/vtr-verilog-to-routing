f3825952d7e54e6e43f377b976754603a38930a8
Alessandro Comodi <acomodi@antmicro.com>
2019-10-07 13:13:08 +0200

    place: move initial placement in separate file
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

---
+++ vpr/src/place/initial_placement.h
@@ -0,0 +1,9 @@
+#ifndef VPR_INITIAL_PLACEMENT_H
+#define VPR_INITIAL_PLACEMENT_H
+
+#include "vpr_types.h"
+
+void initial_placement(enum e_pad_loc_type pad_loc_type,
+                       const char* pad_loc_file);
+
+#endif
