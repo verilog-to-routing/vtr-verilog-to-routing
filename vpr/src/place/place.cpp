e733ff794b6f38e8c1fe0cf8aa2873e6e4606fcb
Alessandro Comodi <acomodi@antmicro.com>
2019-10-18 11:16:00 +0200

    equivalent: using physical_tiles instead of logical_block
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- vpr/src/place/place.cpp
+++ vpr/src/place/place.cpp
@@ -1722,7 +1722,6 @@ static void free_placement_structs(const t_placer_opts& placer_opts) {
     free_placement_macros_structs();
 
     /* Frees up all the data structure used in vpr_utils. */
-    free_port_pin_from_blk_pin();
     free_blk_pin_from_port_pin();
 }
 
