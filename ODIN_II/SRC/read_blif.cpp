8f420959cd56959b9b398ebf3ef66fe785646319
Seyed Alireza Damghani <s.alireza.damghani@unb.ca>
2019-10-18 14:45:26 -0300

    make duplicate and free all of them instead of last one

--- ODIN_II/SRC/read_blif.cpp
+++ ODIN_II/SRC/read_blif.cpp
@@ -677,10 +677,6 @@ void create_internal_node_and_driver(FILE *file, Hashtable *output_nets_hash)
 	{
 		skip_reading_bit_map = true;
 		free_nnode(new_node);
-		for(int i = 0; i < input_count; i++)
-		{
-			vtr::free(names[i]);
-		}
 	}
 	else
 	{
@@ -723,7 +719,7 @@ void create_internal_node_and_driver(FILE *file, Hashtable *output_nets_hash)
 		for(i = 0; i <= input_count-2; i++)
 		{
 			npin_t *new_pin = allocate_npin();
-			new_pin->name = names[i];
+			new_pin->name = vtr::strdup(names[i]);
 			new_pin->type = INPUT;
 			add_input_pin_to_node(new_node, new_pin, i);
 		}
@@ -779,9 +775,11 @@ void create_internal_node_and_driver(FILE *file, Hashtable *output_nets_hash)
 
 		output_nets_hash->add(new_node->name, new_net);
 
-		vtr::free(names[input_count-1]); 
 	}
 	/* Free the char** names */
+	for(int i = 0; i < input_count; i++)
+		vtr::free(names[i]);
+
 	vtr::free(names);
 }
 
