5f9ff61bbd3c67a64fbac4af8d63365daa1d4c58
Dusty DeWeese <dustin.deweese@gmail.com>
2019-10-23 15:03:13 -0700

    Revert "Add option for search locations."
    This reverts commit 05b8479d2f4cbd3c191d41a1d25bc0c08103839c.
    
    Signed-off-by: Dusty DeWeese <dustin.deweese@gmail.com>

--- vpr/src/route/route_timing.cpp
+++ vpr/src/route/route_timing.cpp
@@ -361,8 +361,7 @@ bool try_timing_driven_route(const t_router_opts& router_opts,
         router_opts.lookahead_type,
         router_opts.write_router_lookahead,
         router_opts.read_router_lookahead,
-        segment_inf,
-        router_opts.lookahead_search_locations);
+        segment_inf);
 
     /*
      * Routing parameters
@@ -1383,8 +1382,7 @@ std::vector<t_heap> timing_driven_find_all_shortest_paths_from_route_tree(t_rt_n
     int target_node = OPEN;
     auto router_lookahead = make_router_lookahead(e_router_lookahead::NO_OP,
                                                   /*write_lookahead=*/"", /*read_lookahead=*/"",
-                                                  /*segment_inf=*/{},
-                                                  /*lookahead_search_locations=*/"");
+                                                  /*segment_inf=*/{});
     add_route_tree_to_heap(rt_root, target_node, cost_params, *router_lookahead, router_stats);
     heap_::build_heap(); // via sifting down everything
 
@@ -1406,8 +1404,7 @@ static std::vector<t_heap> timing_driven_find_all_shortest_paths_from_heap(const
                                                                            RouterStats& router_stats) {
     auto router_lookahead = make_router_lookahead(e_router_lookahead::NO_OP,
                                                   /*write_lookahead=*/"", /*read_lookahead=*/"",
-                                                  /*segment_inf=*/{},
-                                                  /*lookahead_search_locations=*/"");
+                                                  /*segment_inf=*/{});
 
     auto& device_ctx = g_vpr_ctx.device();
     std::vector<t_heap> cheapest_paths(device_ctx.rr_nodes.size());
