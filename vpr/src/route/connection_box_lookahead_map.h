32249d92d1d67bb334b5fd7ffd6ee4b80288bca4
Dusty DeWeese <dustin.deweese@gmail.com>
2019-10-24 18:46:28 -0700

    performance improvements
    Signed-off-by: Dusty DeWeese <dustin.deweese@gmail.com>

--- vpr/src/route/connection_box_lookahead_map.h
+++ vpr/src/route/connection_box_lookahead_map.h
@@ -10,14 +10,48 @@
 
 // Keys in the RoutingCosts map
 struct RoutingCostKey {
-    // offset of the destination connection box from the starting segment
-    vtr::Point<int> delta;
+    // segment type index
+    int seg_index;
 
     // type of the destination connection box
     ConnectionBoxId box_id;
 
+    // offset of the destination connection box from the starting segment
+    vtr::Point<int> delta;
+
+    RoutingCostKey()
+        : seg_index(-1)
+        , delta(0, 0) {}
+    RoutingCostKey(int seg_index_arg, ConnectionBoxId box_id_arg, vtr::Point<int> delta_arg)
+        : seg_index(seg_index_arg)
+        , box_id(box_id_arg)
+        , delta(delta_arg) {}
+
     bool operator==(const RoutingCostKey& other) const {
-        return delta == other.delta && box_id == other.box_id;
+        return seg_index == other.seg_index && box_id == other.box_id && delta == other.delta;
+    }
+};
+
+// compressed version of RoutingCostKey
+// TODO add bounds checks
+struct CompressedRoutingCostKey {
+    uint32_t data;
+
+    CompressedRoutingCostKey() {
+        data = -1;
+    }
+    CompressedRoutingCostKey(const RoutingCostKey& key) {
+        data = key.seg_index & 0xff;
+        data <<= 8;
+        data |= size_t(key.box_id) & 0xff;
+        data <<= 8;
+        data |= key.delta.x() & 0xff;
+        data <<= 8;
+        data |= key.delta.y() & 0xff;
+    }
+
+    bool operator==(CompressedRoutingCostKey other) const {
+        return data == other.data;
     }
 };
 
@@ -30,23 +64,23 @@ struct RoutingCost {
     util::Cost_Entry cost_entry;
 };
 
-// Specialization of std::hash for RoutingCostKey
-namespace std {
-template<>
-struct hash<RoutingCostKey> {
-    typedef RoutingCostKey argument_type;
-    typedef std::size_t result_type;
-    result_type operator()(argument_type const& s) const noexcept {
-        result_type const h1(std::hash<int>{}(s.delta.x()));
-        result_type const h2(std::hash<int>{}(s.delta.y()));
-        result_type const h3(std::hash<size_t>{}(size_t(s.box_id)));
-        return h1 ^ ((h2 ^ (h3 << 1)) << 1);
+// hash implementation for RoutingCostKey
+struct HashRoutingCostKey {
+    std::size_t operator()(RoutingCostKey const& key) const noexcept {
+        uint64_t data;
+        data = key.seg_index & 0xffff;
+        data <<= 16;
+        data |= size_t(key.box_id) & 0xffff;
+        data <<= 16;
+        data |= key.delta.x() & 0xffff;
+        data <<= 16;
+        data |= key.delta.y() & 0xffff;
+        return std::hash<uint64_t>{}(data);
     }
 };
-} // namespace std
 
 // Map used to store intermediate routing costs
-typedef std::unordered_map<RoutingCostKey, RoutingCost> RoutingCosts;
+typedef std::unordered_map<RoutingCostKey, RoutingCost, HashRoutingCostKey> RoutingCosts;
 
 // Dense cost maps per source segment and destination connection box types
 class CostMap {
@@ -54,10 +88,8 @@ class CostMap {
     void set_counts(size_t seg_count, size_t box_count);
     int node_to_segment(int from_node_ind) const;
     util::Cost_Entry find_cost(int from_seg_index, ConnectionBoxId box_id, int delta_x, int delta_y) const;
-    void set_cost_map(int from_seg_index,
-                      const RoutingCosts& costs);
-    void set_cost_map(int from_seg_index, ConnectionBoxId box_id, const RoutingCosts& costs);
-    util::Cost_Entry get_nearby_cost_entry(const vtr::NdMatrix<util::Cost_Entry, 2>& matrix, int cx, int cy, const vtr::Rect<int>& bounds);
+    void set_cost_map(const RoutingCosts& costs);
+    std::pair<util::Cost_Entry, int> get_nearby_cost_entry(const vtr::NdMatrix<util::Cost_Entry, 2>& matrix, int cx, int cy, const vtr::Rect<int>& bounds);
     void read(const std::string& file);
     void write(const std::string& file) const;
     void print(int iseg) const;
@@ -67,6 +99,8 @@ class CostMap {
     vtr::NdMatrix<vtr::NdMatrix<util::Cost_Entry, 2>, 2> cost_map_;
     vtr::NdMatrix<std::pair<int, int>, 2> offset_;
     std::vector<int> segment_map_;
+    size_t seg_count_;
+    size_t box_count_;
 };
 
 // Implementation of RouterLookahead based on source segment and destination connection box types
