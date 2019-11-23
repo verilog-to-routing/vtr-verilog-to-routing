92803a9db191819a049d804adb2bf267525c60bb
Tim 'mithro' Ansell <me@mith.ro>
2019-11-23 11:13:31 -0800

    Adding the kokoro CI configuration.
    Kokoro is a Google internal service which allows significantly bigger
    and longer CI builds using Google Cloud Platform.
    
    This enables us to run all the `vtr_reg_strong`, `vtr_reg_nightly` and
    `vtr_reg_nightly` on every pull request. This will happen once the
    @kokoro-team has been given access to the repo and the web hook set up.
    
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

---
+++ .github/kokoro/run-vtr.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+set -x
+set -e
+
+# Set up the host
+source $SCRIPT_DIR/steps/hostsetup.sh
+source $SCRIPT_DIR/steps/hostinfo.sh
+
+# Output git information
+ls -l
+cd github
+ls -l
+cd vtr-verilog-to-routing
+source $SCRIPT_DIR/steps/git.sh
+
+if [ $VTR_TEST == "vtr_reg_strong" ]; then
+	source $SCRIPT_DIR/steps/vtr-min-setup.sh
+else
+	source $SCRIPT_DIR/steps/vtr-full-setup.sh
+fi
+
+# Build VtR
+source $SCRIPT_DIR/steps/vtr-build.sh
+# Run the reg test.
+source $SCRIPT_DIR/steps/vtr-test.sh
