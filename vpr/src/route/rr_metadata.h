a4ccc911cfdee861f5b787815620f8463d093b31
Keith Rothman <537074+litghost@users.noreply.github.com>
2020-01-17 13:58:27 -0800

    Some improvements to metadata storage.
    - Intern metadata strings earlier to avoid allocations.
    - Avoid second sort when building metadata map.
    
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- vpr/src/route/rr_metadata.h
+++ vpr/src/route/rr_metadata.h
@@ -6,9 +6,11 @@
 namespace vpr {
 
 const t_metadata_value* rr_node_metadata(int src_node, vtr::interned_string key);
+void add_rr_node_metadata(int src_node, vtr::interned_string key, vtr::interned_string value);
 void add_rr_node_metadata(int src_node, vtr::string_view key, vtr::string_view value);
 
 const t_metadata_value* rr_edge_metadata(int src_node, int sink_node, short switch_id, vtr::interned_string key);
+void add_rr_edge_metadata(int src_node, int sink_node, short switch_id, vtr::interned_string key, vtr::interned_string value);
 void add_rr_edge_metadata(int src_node, int sink_node, short switch_id, vtr::string_view key, vtr::string_view value);
 
 } // namespace vpr
