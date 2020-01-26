b5af8e25dfd0d71cdd17dcce4f231cb31119670c
Keith Rothman <537074+litghost@users.noreply.github.com>
2020-01-26 09:02:23 -0800

    Refactor edge storage.
    This changes edge storage from an allocation array of struct per node to
    struct of array for all edge data.
    
    Several algorithms over edges that were previous per node per edge, but
    were actually just iteration over edges are now part of rr_node_storage.
    
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- vpr/src/route/rr_node.cpp
+++ vpr/src/route/rr_node.cpp
@@ -55,12 +55,6 @@ float t_rr_node::C() const {
 
 bool t_rr_node::validate() const {
     //Check internal assumptions about RR node are valid
-    auto& node = storage_->get(id_);
-
-    if (node.num_edges_ > node.edges_capacity_) {
-        VPR_FATAL_ERROR(VPR_ERROR_ROUTE, "RR Node number of edges exceeded edge capacity");
-    }
-
     t_edge_size iedge = 0;
     for (auto edge : edges()) {
         if (edge < num_configurable_edges()) {
@@ -159,85 +153,6 @@ void t_rr_node::set_capacity(short new_capacity) {
     node.capacity_ = new_capacity;
 }
 
-void t_rr_node::set_fan_in(t_edge_size new_fan_in) {
-    auto& node = storage_->get(id_);
-    node.fan_in_ = new_fan_in;
-}
-
-t_edge_size t_rr_node::add_edge(int sink_node, int iswitch) {
-    auto& node = storage_->get(id_);
-    if (node.edges_capacity_ == node.num_edges_) {
-        constexpr size_t MAX_EDGE_COUNT = std::numeric_limits<decltype(t_rr_node_data::edges_capacity_)>::max();
-        if (node.edges_capacity_ == MAX_EDGE_COUNT) {
-            VPR_FATAL_ERROR(VPR_ERROR_ROUTE, "Maximum RR Node out-edge count (%zu) exceeded", MAX_EDGE_COUNT);
-        }
-
-        //Grow
-        size_t new_edges_capacity = std::max<size_t>(1, 2 * node.edges_capacity_);
-        new_edges_capacity = std::min(new_edges_capacity, MAX_EDGE_COUNT); //Clip to maximum count
-        auto new_edges = std::make_unique<t_rr_node_data::t_rr_edge[]>(new_edges_capacity);
-
-        //Copy
-        std::copy_n(node.edges_.get(), node.num_edges_, new_edges.get());
-
-        //Replace
-        node.edges_ = std::move(new_edges);
-        node.edges_capacity_ = new_edges_capacity;
-    }
-
-    VTR_ASSERT(node.num_edges_ < node.edges_capacity_);
-
-    node.edges_[node.num_edges_].sink_node = sink_node;
-    node.edges_[node.num_edges_].switch_id = iswitch;
-
-    ++node.num_edges_;
-
-    return node.num_edges_;
-}
-
-void t_rr_node::shrink_to_fit() {
-    //Shrink
-    auto& node = storage_->get(id_);
-    auto new_edges = std::make_unique<t_rr_node_data::t_rr_edge[]>(node.num_edges_);
-
-    //Copy
-    std::copy_n(node.edges_.get(), node.num_edges_, new_edges.get());
-
-    //Replace
-    node.edges_ = std::move(new_edges);
-    node.edges_capacity_ = node.num_edges_;
-}
-
-void t_rr_node::partition_edges() {
-    auto& device_ctx = g_vpr_ctx.device();
-    auto is_configurable = [&](const t_rr_node_data::t_rr_edge& edge) {
-        auto iswitch = edge.switch_id;
-        return device_ctx.rr_switch_inf[iswitch].configurable();
-    };
-
-    //Partition the edges so the first set of edges are all configurable, and the later are not
-    auto& node = storage_->get(id_);
-    auto first_non_config_edge = std::partition(node.edges_.get(), node.edges_.get() + node.num_edges_, is_configurable);
-
-    size_t num_conf_edges = std::distance(node.edges_.get(), first_non_config_edge);
-    size_t num_non_conf_edges = num_edges() - num_conf_edges; //Note we calculate using the size_t to get full range
-
-    //Check that within allowable range (no overflow when stored as num_non_configurable_edges_
-    if (num_non_conf_edges > std::numeric_limits<decltype(node.num_non_configurable_edges_)>::max()) {
-        VPR_FATAL_ERROR(VPR_ERROR_ROUTE, "Exceeded RR node maximum number of non-configurable edges");
-    }
-    node.num_non_configurable_edges_ = num_non_conf_edges; //Narrowing
-}
-
-void t_rr_node::set_num_edges(size_t new_num_edges) {
-    auto& node = storage_->get(id_);
-    VTR_ASSERT(new_num_edges <= std::numeric_limits<t_edge_size>::max());
-    node.num_edges_ = new_num_edges;
-    node.edges_capacity_ = new_num_edges;
-
-    node.edges_ = std::make_unique<t_rr_node_data::t_rr_edge[]>(node.num_edges_);
-}
-
 void t_rr_node::set_direction(e_direction new_direction) {
     if (type() != CHANX && type() != CHANY) {
         VPR_FATAL_ERROR(VPR_ERROR_ROUTE, "Attempted to set RR node 'direction' for non-channel type '%s'", type_string());
@@ -252,20 +167,6 @@ void t_rr_node::set_side(e_side new_side) {
     }
     auto& node = storage_->get(id_);
     node.dir_side_.side = new_side;
-}
-
-void t_rr_node::set_edge_sink_node(t_edge_size iedge, int sink_node) {
-    auto& node = storage_->get(id_);
-    VTR_ASSERT(iedge < num_edges());
-    VTR_ASSERT(sink_node >= 0);
-    node.edges_[iedge].sink_node = sink_node;
-}
-
-void t_rr_node::set_edge_switch(t_edge_size iedge, short switch_index) {
-    auto& node = storage_->get(id_);
-    VTR_ASSERT(iedge < num_edges());
-    VTR_ASSERT(switch_index >= 0);
-    node.edges_[iedge].switch_id = switch_index;
 }
 
 t_rr_rc_data::t_rr_rc_data(float Rval, float Cval) noexcept
