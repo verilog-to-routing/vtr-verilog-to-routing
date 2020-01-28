5695ad85e459a2433ad59b3f5a250a452c107769
Keith Rothman <537074+litghost@users.noreply.github.com>
2020-01-28 14:37:50 -0800

    Add "--read_edge_metadata" flag to enable using VPR to convert XML -> capnp
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- vpr/src/base/read_options.h
+++ vpr/src/base/read_options.h
@@ -135,6 +135,7 @@ struct t_options {
     argparse::ArgValue<int> min_incremental_reroute_fanout;
     argparse::ArgValue<bool> disable_check_route;
     argparse::ArgValue<bool> quick_check_route;
+    argparse::ArgValue<bool> read_edge_metadata;
 
     /* Timing-driven router options only */
     argparse::ArgValue<float> astar_fac;
