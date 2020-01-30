07e3f9a7dd3d0b360612288e1b00ac71824422f8
kmurray <k.murray@utoronto.ca>
2020-01-30 10:14:48 -0500

    vpr: Add --router_initial_timing option
    This option (currently unimplemented) will control how the initial
    connection criticality is determined for the first routing iteration.

--- vpr/src/base/SetupVPR.cpp
+++ vpr/src/base/SetupVPR.cpp
@@ -371,6 +371,7 @@ static void SetupRouterOpts(const t_options& Options, t_router_opts* RouterOpts)
     RouterOpts->lookahead_type = Options.router_lookahead_type;
     RouterOpts->max_convergence_count = Options.router_max_convergence_count;
     RouterOpts->reconvergence_cpd_threshold = Options.router_reconvergence_cpd_threshold;
+    RouterOpts->initial_timing = Options.router_initial_timing;
     RouterOpts->first_iteration_timing_report_file = Options.router_first_iteration_timing_report_file;
 
     RouterOpts->strict_checks = Options.strict_checks;
