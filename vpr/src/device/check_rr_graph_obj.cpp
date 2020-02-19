a2d1cea5f43fe408848f9567d130fdafc5e74e4a
tangxifan <tangxifan@gmail.com>
2020-02-19 14:35:55 -0700

    hotfix to avoid double look-up in edge_counter

--- vpr/src/device/check_rr_graph_obj.cpp
+++ vpr/src/device/check_rr_graph_obj.cpp
@@ -18,10 +18,10 @@ static bool check_rr_graph_node_duplicated_edges(const RRGraph& rr_graph,
 
     /* Check each input edges */
     for (const auto edge : rr_graph.node_in_edges(node)) {
-        if (0 < edge_counter.count(edge)) {
-            edge_counter[edge]++;
-        } else {
-            edge_counter[edge] = 1;
+        auto result = edge_counter.insert(std::pair<RREdgeId, size_t>(edge, 1));
+        VTR_ASSERT(edge_counter.end() != result.first);
+        if (false == result.second) {
+            result.first->second++;
         }
     }
 
