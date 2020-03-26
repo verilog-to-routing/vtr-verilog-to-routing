399468e896888fe44b45574ef063144fbee941c8
kmurray <k.murray@utoronto.ca>
2020-03-26 17:50:25 -0400

    vpr: Fix unintended mutation of save graphics file name

--- vpr/src/draw/save_graphics.h
+++ vpr/src/draw/save_graphics.h
@@ -9,7 +9,7 @@
 #    include "ezgl/application.hpp"
 #    include "ezgl/graphics.hpp"
 
-void save_graphics(std::string& extension, std::string& file_name);
+void save_graphics(std::string extension, std::string file_name);
 void save_graphics_dialog_box(GtkWidget* /*widget*/, ezgl::application* /*app*/);
 void save_graphics_from_button(GtkWidget* /*widget*/, gint response_id, gpointer data);
 
