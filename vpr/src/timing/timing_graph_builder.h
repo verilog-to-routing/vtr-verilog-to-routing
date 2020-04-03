9a8444c0a48cae3f9e614ad8b951af4b2942cafd
kmurray <k.murray@utoronto.ca>
2020-04-03 16:42:55 -0400

    vpr: Improve commenting of timing graph construction code
    Added a large top-level comment walking through how VPR models
    netlist/architectural primitives as a timing graph, and the various
    transformations used to convert the netlist into the timing graph
    (includes several ASCII diagrams).
    
    Also includes discussions about how the code goes about performing those
    transformations and some of the additional complications (like clock
    generators).

--- vpr/src/timing/timing_graph_builder.h
+++ vpr/src/timing/timing_graph_builder.h
@@ -5,6 +5,17 @@
 #include "atom_netlist_fwd.h"
 #include "atom_lookup.h"
 
+/*
+ * Class for constructing a Timing Graph (a tatum::TimingGraph, for use with the Tatum 
+ * STA engine) from the provided AtomNetlist. It also updates the provided AtomLookup 
+ * with the mapping from netlist elements to timing graph nodes.
+ *
+ * To construct a timing graph initialize this class with the netlist and lookup 
+ * (via constructor), and call the timing_graph() method.
+ *
+ * For details on how the timing graph is constructed from the netlist, see the comments 
+ * in the associated .cpp implementation file.
+ */
 class TimingGraphBuilder {
   public:
     TimingGraphBuilder(const AtomNetlist& netlist,
