763528e9e328c1998f169d09cf028aae3e7bbfcf
Alessandro Comodi <acomodi@antmicro.com>
2020-04-10 00:09:48 +0200

    place: change variable names
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- vpr/src/place/compressed_grid.cpp
+++ vpr/src/place/compressed_grid.cpp
@@ -35,15 +35,15 @@ std::vector<t_compressed_block_grid> create_compressed_block_grids() {
             // Given that a logical block can potentially be compatible with only a subset of the
             // sub tiles of a physical tile, we need to ensure which sub tile locations are part of
             // this subset.
-            for (int capacity = 0; capacity < physical_tile->capacity; capacity++) {
-                if (is_sub_tile_compatible(physical_tile, &logical_block, capacity)) {
-                    compatible_sub_tiles.push_back(capacity);
+            for (int isub_tile = 0; isub_tile < physical_tile->capacity; isub_tile++) {
+                if (is_sub_tile_compatible(physical_tile, &logical_block, isub_tile)) {
+                    compatible_sub_tiles.push_back(isub_tile);
                 }
             }
 
             // For each of physical tiles compatible with the current logical block, create add an entry
             // to the compatible sub tiles map with the physical tile index and the above generated vector.
-            compressed_block_grid.compatible_sub_tiles_for_tile_map.insert({physical_tile->index, compatible_sub_tiles});
+            compressed_block_grid.compatible_sub_tiles_for_tile.insert({physical_tile->index, compatible_sub_tiles});
         }
 
         compressed_type_grids[logical_block.index] = compressed_block_grid;
