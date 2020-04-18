92431c062ab4390d9a4b634f8a66289da40da8d2
Mohamed Elgammal <mohamed.elgammal@mail.utoronto.ca>
2020-04-18 15:07:14 -0400

    generalize the number of move generators

--- vpr/src/place/place.cpp
+++ vpr/src/place/place.cpp
@@ -679,9 +679,9 @@ void try_place(const t_placer_opts& placer_opts,
     std::vector<float> X_coord, Y_coord;
 
     //Define some variables for move generation statistics
-    std::vector<int> num_moves (4,0);
-    std::vector<int> accepted_moves (4,0);
-    std::vector<int> aborted_moves (4,0);
+    std::vector<int> num_moves (placer_opts.place_static_move_prob.size(),0);
+    std::vector<int> accepted_moves (placer_opts.place_static_move_prob.size(),0);
+    std::vector<int> aborted_moves (placer.opts.place_static_move_prob.size(),0);
 
     /* Update the starting temperature for placement annealing to a more appropriate value */
     state.t = starting_t(&state,
