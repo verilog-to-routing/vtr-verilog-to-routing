efddfbf6ea4e1f7a2693ec2e631804fbd94d0c13
Maciej Kurc <mkurc@antmicro.com>
2020-04-21 09:25:50 +0200

    Added code comment.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- vpr/src/place/place.cpp
+++ vpr/src/place/place.cpp
@@ -2348,6 +2348,11 @@ static void alloc_and_load_for_fast_cost_update(float place_cost_exp) {
 
     for (size_t high = 0; high < device_ctx.grid.height(); high++)
         for (size_t low = 0; low <= high; low++) {
+            /* Since we will divide the wiring cost by the average channel *
+             * capacity between high and low, having only 0 width channels *
+             * will result in infinite wiring capacity normalization       *
+             * factor, and extremely bad placer behaviour. Hence we change *
+             * this to a small (1 track) channel capacity instead.         */
             if (chanx_place_cost_fac[high][low] == 0.0f) {
                 VTR_LOG_WARN("CHANX place cost fac is 0 at %d %d\n", high, low);
                 chanx_place_cost_fac[high][low] = 1.0f;
@@ -2375,6 +2380,11 @@ static void alloc_and_load_for_fast_cost_update(float place_cost_exp) {
 
     for (size_t high = 0; high < device_ctx.grid.width(); high++)
         for (size_t low = 0; low <= high; low++) {
+            /* Since we will divide the wiring cost by the average channel *
+             * capacity between high and low, having only 0 width channels *
+             * will result in infinite wiring capacity normalization       *
+             * factor, and extremely bad placer behaviour. Hence we change *
+             * this to a small (1 track) channel capacity instead.         */
             if (chany_place_cost_fac[high][low] == 0.0f) {
                 VTR_LOG_WARN("CHANY place cost fac is 0 at %d %d\n", high, low);
                 chany_place_cost_fac[high][low] = 1.0f;
