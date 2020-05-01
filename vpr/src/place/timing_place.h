0d87ab3a24e62490c900cd2d1e00fa1c9ac5e567
kmurray <k.murray@utoronto.ca>
2020-05-01 11:46:22 -0400

    vpr: Move place criticality into PlacerCriticalities class

--- vpr/src/place/timing_place.h
+++ vpr/src/place/timing_place.h
@@ -1,6 +1,7 @@
 #ifndef TIMING_PLACE
 #define TIMING_PLACE
 
+#include "vtr_vec_id_set.h"
 #include "timing_info_fwd.h"
 #include "clustered_netlist_utils.h"
 #include "place_delay_model.h"
@@ -13,11 +14,36 @@ std::unique_ptr<PlaceDelayModel> alloc_lookups_and_criticalities(t_chan_width_di
                                                                  const t_direct_inf* directs,
                                                                  const int num_directs);
 
-void free_lookups_and_criticalities();
+class PlacerCriticalities {
+    public: //Types
+        typedef vtr::vec_id_set<ClusterPinId>::iterator pin_iterator;
+        typedef vtr::Range<pin_iterator> pin_range;
 
-void load_criticalities(const SetupTimingInfo& timing_info, float crit_exponent, const ClusteredPinAtomPinsLookup& pin_lookup);
+    public: //Lifetime
+        PlacerCriticalities(const ClusteredNetlist& clb_nlist);
+        ~PlacerCriticalities();
+        PlacerCriticalities(const PlacerCriticalities& clb_nlist) = delete;
+        PlacerCriticalities& operator=(const PlacerCriticalities& clb_nlist) = delete;
 
-float get_timing_place_crit(ClusterNetId net_id, int ipin);
-void set_timing_place_crit(ClusterNetId net_id, int ipin, float val);
+    public: //Accessors
+        float criticality(ClusterNetId net, int ipin) const;
+        pin_range pins_with_modified_criticality() const;
+
+    public: //Modifiers
+        void update_criticalities(const SetupTimingInfo& timing_info, float crit_exponent, const ClusteredPinAtomPinsLookup& pin_lookup);
+        void set_criticality(ClusterNetId net, int ipin, float val);
+
+    private: //Data
+        const ClusteredNetlist& clb_nlist_;
+        
+        vtr::t_chunk timing_place_crit_ch_;
+        vtr::vector<ClusterNetId, float*> timing_place_crit_; /* [0..cluster_ctx.clb_nlist.nets().size()-1][1..num_pins-1] */
+
+        //The criticality exponent when update_criticalites() was last called (used to detect if incremental update can be used)
+        float last_crit_exponent_ = std::numeric_limits<float>::quiet_NaN();
+
+        //Set of pins with criticaltites modified by last call to update_criticalities()
+        vtr::vec_id_set<ClusterPinId> cluster_pins_with_modified_criticality_;
+};
 
 #endif
