7352856868561799957a4f5fac0b5fbfc49986f3
djns1 <djns1@students.waikato.ac.nz>
2020-05-15 15:09:44 +1200

    Updated error message reporting format

--- ODIN_II/SRC/include/odin_error.h
+++ ODIN_II/SRC/include/odin_error.h
@@ -28,32 +28,25 @@ extern const char* odin_error_STR[];
 extern std::vector<std::pair<std::string, int>> include_file_names;
 extern int delayed_errors;
 // causes an interrupt in GDB
-static inline void _verbose_assert(bool condition, const char* condition_str, const char* odin_file_name, long odin_line_number, const char* odin_function_name) {
-    fflush(stdout);
-    if (!condition) {
-        fprintf(stderr, "Assertion failed(%s)@[%s]%s::%ld \n", condition_str, odin_file_name, odin_function_name, odin_line_number);
-        fflush(stderr);
-        std::abort();
-    }
-}
+void _verbose_assert(bool condition, const char* condition_str, const char* odin_file_name, long odin_line_number, const char* odin_function_name);
 
 #define oassert(condition) _verbose_assert(condition, #condition, __FILE__, __LINE__, __func__)
 
 void _log_message(odin_error error_type, long column, long line_number, long file, bool soft_error, const char* function_file_name, long function_line, const char* function_name, const char* message, ...);
 
 #define error_message(error_type, line_number, file, message, ...) \
-    _log_message(error_type, -1, line_number, file, true, __FILE__, __LINE__, __func__, message, __VA_ARGS__)
+    _log_message(error_type, -1, line_number, file, true, __FILE__, __LINE__, __PRETTY_FUNCTION__, message, __VA_ARGS__)
 
 #define warning_message(error_type, line_number, file, message, ...) \
-    _log_message(error_type, -1, line_number, file, false, __FILE__, __LINE__, __func__, message, __VA_ARGS__)
+    _log_message(error_type, -1, line_number, file, false, __FILE__, __LINE__, __PRETTY_FUNCTION__, message, __VA_ARGS__)
 
 #define possible_error_message(error_type, line_number, file, message, ...) \
-    _log_message(error_type, -1, line_number, file, !global_args.permissive.value(), __FILE__, __LINE__, __func__, message, __VA_ARGS__)
+    _log_message(error_type, -1, line_number, file, !global_args.permissive.value(), __FILE__, __LINE__, __PRETTY_FUNCTION__, message, __VA_ARGS__)
 
-#define delayed_error_message(error_type, column, line_number, file, message, ...)                                      \
-    {                                                                                                                   \
-        _log_message(error_type, column, line_number, file, false, __FILE__, __LINE__, __func__, message, __VA_ARGS__); \
-        delayed_errors += 1;                                                                                            \
+#define delayed_error_message(error_type, column, line_number, file, message, ...)                                                 \
+    {                                                                                                                              \
+        _log_message(error_type, column, line_number, file, false, __FILE__, __LINE__, __PRETTY_FUNCTION__, message, __VA_ARGS__); \
+        delayed_errors += 1;                                                                                                       \
     }
 
 void verify_delayed_error(odin_error error_type);
