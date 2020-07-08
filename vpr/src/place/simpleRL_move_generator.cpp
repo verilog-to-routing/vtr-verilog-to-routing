7fdc6613df8af2a5ad1548c51feb302d94b81bc9
MohamedElgammal <mohamed.elgammal@mail.utoronto.ca>
2020-07-08 14:12:15 -0400

    testing same normalization factors on e greedy

--- vpr/src/place/simpleRL_move_generator.cpp
+++ vpr/src/place/simpleRL_move_generator.cpp
@@ -51,12 +51,14 @@ void EpsilonGreedyAgent::set_step(float gamma, int move_lim) {
 
 void EpsilonGreedyAgent::process_outcome(double reward) {
     ++n_[last_action_];
-    if(reward_num == 1 || reward_num == 3 ){
+    if(reward_num == 1 || reward_num == 4 ){
         reward = reward / time_elapsed_per_move[last_action_];
     }
-    else if(reward_num == 2 || reward_num == 4 ){
+    else if(reward_num == 2 || reward_num == 5 ){
         reward = reward / time_elapsed_per_accepted_move[last_action_];
     }
+    else
+        reward = reward / time_elapsed[last_action_];
     //Determine step size
     float step = 0.;
     if (exp_alpha_ < 0.) {
