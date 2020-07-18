33cf9b0db1d3a96d15d6c06ca904d32720d1cc5e
emacdo12 <emacdo12@unb.ca>
2020-07-18 23:57:37 +0000

    Odin: Update defparam tests
        this commit update the defparams test to test for different overrides
        hierarchy depth

--- ODIN_II/regression_test/benchmark/verilog/keywords/defparam/defparam_depth_1.v
+++ ODIN_II/regression_test/benchmark/verilog/keywords/defparam/defparam_depth_1.v
@@ -1,11 +1,14 @@
 module simple_op(in1,in2,out1,out2);
-	parameter width1 = 3;
-	parameter width2 = 4;
+	parameter width1 = 2;
+	parameter width2 = 8;
 	
 	input  [width1-1:0] in1;
 	input  [width2-1:0] in2;
 	output [width1-1:0] out1;
 	output [width2-1:0] out2;
+	
+	defparam m1.width = 2;
+	defparam m2.width = 8;
 	
 	assg m1 (in1,out1);
 	assg m2 (in2,out2);
@@ -13,18 +16,10 @@ module simple_op(in1,in2,out1,out2);
 endmodule
 	
 module assg(in,out);
-	localparam width = 3;
+	parameter width = 3;
 		
 	input  [width-1:0] in;
 	output [width-1:0] out;
 		
 	assign out = in;
-endmodule 
-	
-module params;
-    defparam
-		simple_op.m1.width = 2,
-		simple_op.m2.width = 8,
-		simple_op.width1 = 2,
-		simple_op.width2 = 8;
 endmodule 
