4b193bc008378a9442098898e42fc538420de3a1
MohamedElgammal <mohamed.elgammal@mail.utoronto.ca>
2020-07-19 13:31:03 -0400

    print some statistics about time elapsed for each move calculation

--- vpr/src/place/place.cpp
+++ vpr/src/place/place.cpp
@@ -1001,7 +1001,7 @@ quench:
         p_runtime_ctx.f_update_td_costs_sum_nets_elapsed_sec,
         p_runtime_ctx.f_update_td_costs_total_elapsed_sec);
 
-#if 0
+#if 1
     //measure time of each move type
     VTR_LOG("time of uniform move = %f \n", time_of_moves[0]/num_of_moves[0]);
     VTR_LOG("time of median move = %f \n", time_of_moves[1]/num_of_moves[0]);
@@ -1398,14 +1398,14 @@ static e_move_result try_swap(const t_annealing_state* state,
     }
 
     //Generate a new move (perturbation) used to explore the space of possible placements
-#if 0
+#if 1
     auto start = std::chrono::high_resolution_clock::now();
 #endif
     e_create_move create_move_outcome = move_generator.propose_move(blocks_affected
       , rlim, X_coord, Y_coord, type, high_fanout_net, criticalities);
 
     ++num_moves[type];
-#if 0
+#if 1
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
 
