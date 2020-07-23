4b05c8ec7af0529d26724e76aec97712010dc855
shadtorrie <shad.torrie@gmail.com>
2020-07-23 17:26:39 -0600

    actually fixing error.py to pass pylint

--- vtr_flow/scripts/python_libs/vtr/error.py
+++ vtr_flow/scripts/python_libs/vtr/error.py
@@ -1,3 +1,6 @@
+"""
+    Module to handle VTR flow erroring.
+"""
 class VtrError(Exception):
     """
     Base class for VTR related exceptions
@@ -5,8 +8,11 @@ class VtrError(Exception):
     Attributes:
         msg -- An explanation of the error
     """
+
     def __init__(self, msg=""):
         self.msg = msg
+        super().__init__(msg=msg)
+
 
 class CommandError(VtrError):
     """
@@ -16,17 +22,19 @@ class CommandError(VtrError):
         returncode -- The return code from the command
         cmd == The command run
     """
+
     def __init__(self, msg, cmd, returncode, log=None):
         super().__init__(msg=msg)
         self.returncode = returncode
         self.cmd = cmd
         self.log = log
 
+
 class InspectError(VtrError):
     """
     Raised when some query (inspection) result is not found.
     """
+
     def __init__(self, msg, filename=None):
         super().__init__(msg=msg)
-        self.filename=filename
-
+        self.filename = filename
