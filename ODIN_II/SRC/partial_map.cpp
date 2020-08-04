f3ff95695b4298abcb99323ad472e868f79f209c
Georgiy Krylov <georgiy.krylov@unb.ca>
2020-08-03 22:57:41 -0300

    ODIN_II: enabling complex synthesis through command line arguments.
    This commit enables mixing hard and soft logic implementation as part of
    the flow by defining a global reference to the mixing optimization con-
    troller and doing changes to partial mapping stage to defer mapping of
    hard multipliers. The commit also introduces command line arguments and
    their default values, enabling to use the optimization.
    
    Signed-off-by: Georgiy Krylov <georgiy.krylov@unb.ca>

--- ODIN_II/SRC/partial_map.cpp
+++ ODIN_II/SRC/partial_map.cpp
@@ -221,12 +221,7 @@ void partial_map_node(nnode_t* node, short traverse_number, netlist_t* netlist) 
             instantiate_multi_port_mux(node, traverse_number, netlist);
             break;
         case MULTIPLY: {
-            int mult_size = std::max<int>(node->input_port_sizes[0], node->input_port_sizes[1]);
-            if (hard_multipliers && mult_size >= min_mult) {
-                instantiate_hard_multiplier(node, traverse_number, netlist);
-            } else if (!hard_adders) {
-                instantiate_simple_soft_multiplier(node, traverse_number, netlist);
-            }
+            mixer->partial_map_node(node, traverse_number, netlist);
             break;
         }
         case MEMORY: {
