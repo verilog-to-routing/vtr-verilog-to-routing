4485493a746927466833c63031870534036c9613
Mahshad <jalalif2@betzgrp-wintermute.eecg.utoronto.ca>
2020-08-26 19:05:25 -0400

    commented out all my code in place.cpp to see if the errors remain

--- vpr/src/place/place.cpp
+++ vpr/src/place/place.cpp
@@ -500,8 +500,8 @@ static void print_place_status(const size_t num_temps,
 static void print_resources_utilization();
 
 static void init_annealing_state(t_annealing_state* state, const t_annealing_sched& annealing_sched, float t, float rlim, int move_lim_max, float crit_exponent);
-void manual_move_info_from_user_and_open_window(ManualMoveInfo* /*manual_move_info*/);
-void update_manual_move_costs_and_open_window(ManualMoveInfo* manual_move_info, e_move_result& move_outcome, double delta_c, double bb_delta_c, double timing_delta_c);
+//void manual_move_info_from_user_and_open_window(ManualMoveInfo* /*manual_move_info*/);
+//void update_manual_move_costs_and_open_window(ManualMoveInfo* manual_move_info, e_move_result& move_outcome, double delta_c, double bb_delta_c, double timing_delta_c);
 
 /*****************************************************************************/
 void try_place(const t_placer_opts& placer_opts,
@@ -1412,7 +1412,7 @@ static e_move_result try_swap(float t,
 
     MoveOutcomeStats move_outcome_stats;
 
-    ManualMoveInfo* manual_move_info; //the struct that holds all relavant info (e.g block_id and to location)
+    //ManualMoveInfo* manual_move_info; //the struct that holds all relavant info (e.g block_id and to location)
 
     /* I'm using negative values of proposed_net_cost as a flag, so DO NOT   *
      * use cost functions that can go negative.                          */
@@ -1427,27 +1427,27 @@ static e_move_result try_swap(float t,
         rlim = std::numeric_limits<float>::infinity();
     }
 
-    bool manual_move = get_manual_move_flag(); //whether the manual move info has been enabled or not
+    //bool manual_move = get_manual_move_flag(); //whether the manual move info has been enabled or not
 
-    if (manual_move) {
-        //pops up the manual move window for the user to input set their move
-        manual_move_info_from_user_and_open_window(manual_move_info);
-        manual_move_info = get_manual_move_info();
-        //sends info to the move generator class
-        mmg_get_manual_move_info(*manual_move_info);
-    }
+    /*if (manual_move) {
+     * //pops up the manual move window for the user to input set their move
+     * manual_move_info_from_user_and_open_window(manual_move_info);
+     * manual_move_info = get_manual_move_info();
+     * //sends info to the move generator class
+     * mmg_get_manual_move_info(*manual_move_info);
+     * }*/
 
     //Generate a new move (perturbation) used to explore the space of possible placements
     e_create_move create_move_outcome;
-    if (!manual_move)
-        create_move_outcome = move_generator.propose_move(blocks_affected, rlim);
-    else
-        create_move_outcome = manual_move_generator.propose_move(blocks_affected, rlim);
+    //if (!manual_move)
+    //create_move_outcome = move_generator.propose_move(blocks_affected, rlim);
+    //else
+    create_move_outcome = manual_move_generator.propose_move(blocks_affected, rlim);
 
     LOG_MOVE_STATS_PROPOSED(t, blocks_affected);
     e_move_result move_outcome = ABORTED;
 
-    if (create_move_outcome == e_create_move::ABORT || !manual_move_info->valid_input) {
+    if (create_move_outcome == e_create_move::ABORT /*|| !manual_move_info->valid_input*/) {
         //Proposed move is not legal -- give up on this move
         clear_move_blocks(blocks_affected);
 
@@ -1497,9 +1497,9 @@ static e_move_result try_swap(float t,
         /* 1 -> move accepted, 0 -> rejected. */
         move_outcome = assess_swap(delta_c, t);
 
-        if (manual_move && manual_move_info->valid_input)
-            //update all the costs in the manual_move_info variable and open cost summary window
-            update_manual_move_costs_and_open_window(manual_move_info, move_outcome, delta_c, bb_delta_c, timing_delta_c);
+        /*if (manual_move && manual_move_info->valid_input)
+         * //update all the costs in the manual_move_info variable and open cost summary window
+         * update_manual_move_costs_and_open_window(manual_move_info, move_outcome, delta_c, bb_delta_c, timing_delta_c);*/
 
         if (move_outcome == ACCEPTED) {
             costs->cost += delta_c;
@@ -1562,7 +1562,7 @@ static e_move_result try_swap(float t,
     check_place(*costs, delay_model, place_algorithm);
 #endif
 
-    manual_move_info->valid_input = true;
+    //manual_move_info->valid_input = true;
     return (move_outcome);
 }
 
@@ -3042,22 +3042,22 @@ bool placer_needs_lookahead(const t_vpr_setup& vpr_setup) {
     return (vpr_setup.PlacerOpts.place_algorithm == PATH_TIMING_DRIVEN_PLACE);
 }
 
-void manual_move_info_from_user_and_open_window(ManualMoveInfo* /*manual_move_info*/) {
-    //pops up the manual move window for the user to input set their move
-    manual_move_generator_window("");
-    update_screen(ScreenUpdatePriority::MAJOR, " ", PLACEMENT, nullptr);
-    //manual_move_info = get_manual_move_info();
-    //sends info to the move generator class
-    //mmg_get_manual_move_info(*manual_move_info);
-}
+//void manual_move_info_from_user_and_open_window(ManualMoveInfo* /*manual_move_info*/) {
+//pops up the manual move window for the user to input set their move
+//manual_move_generator_window("");
+//update_screen(ScreenUpdatePriority::MAJOR, " ", PLACEMENT, nullptr);
+//manual_move_info = get_manual_move_info();
+//sends info to the move generator class
+//mmg_get_manual_move_info(*manual_move_info);
+//}
 
-void update_manual_move_costs_and_open_window(ManualMoveInfo* manual_move_info, e_move_result& move_outcome, double delta_c, double bb_delta_c, double timing_delta_c) {
-    //update all the costs in the manual_move_info variable and open cost summary window
-    manual_move_info->delta_c = delta_c;
-    manual_move_info->bb_delta_c = bb_delta_c;
-    manual_move_info->timing_delta_c = timing_delta_c;
-    manual_move_info->placer_move_outcome = move_outcome;
-    cost_summary_window();
-    update_screen(ScreenUpdatePriority::MAJOR, " ", PLACEMENT, nullptr);
-    move_outcome = manual_move_info->user_move_outcome;
-}
+//void update_manual_move_costs_and_open_window(ManualMoveInfo* manual_move_info, e_move_result& move_outcome, double delta_c, double bb_delta_c, double timing_delta_c) {
+//update all the costs in the manual_move_info variable and open cost summary window
+/*manual_move_info->delta_c = delta_c;
+ * manual_move_info->bb_delta_c = bb_delta_c;
+ * manual_move_info->timing_delta_c = timing_delta_c;
+ * manual_move_info->placer_move_outcome = move_outcome;
+ * cost_summary_window();
+ * update_screen(ScreenUpdatePriority::MAJOR, " ", PLACEMENT, nullptr);
+ * move_outcome = manual_move_info->user_move_outcome;*/
+//}
