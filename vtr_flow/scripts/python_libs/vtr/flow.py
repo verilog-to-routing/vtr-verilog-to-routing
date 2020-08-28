977c1517c891b6ef1be9ad7c43c00be6c62a83ea
shadtorrie <shad.torrie@gmail.com>
2020-08-28 12:19:33 -0600

    updating comment

--- vtr_flow/scripts/python_libs/vtr/flow.py
+++ vtr_flow/scripts/python_libs/vtr/flow.py
@@ -121,7 +121,7 @@ def run(
             Do a second-run of the incremental analysis to compare the result files
 
         check_route:
-            Check first placement by enabling VPR analysis.
+            Check first placement and routing by enabling VPR analysis
 
         check_place:
             Route existing placement by enabling VPR routing.
