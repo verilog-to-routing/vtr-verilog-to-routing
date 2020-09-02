00002816e8ae267eecb74ad75d4aefbc6ce1b1b3
Mahshad <jalalif2@betzgrp-wintermute.eecg.utoronto.ca>
2020-09-02 17:53:47 -0400

    adding all the code back. I'm tired!

--- vpr/src/place/place.cpp
+++ vpr/src/place/place.cpp
@@ -514,7 +514,7 @@ void transform_blocks_affected(t_pl_blocks_to_be_moved blocksAffected);
 static void init_annealing_state(t_annealing_state* state, const t_annealing_sched& annealing_sched, float t, float rlim, int move_lim_max, float crit_exponent);
 void stop_placement_and_check_breakopints(t_pl_blocks_to_be_moved& blocks_affected, bool& f_place_debug, e_move_result move_outcome, double delta_c, double bb_delta_c, double timing_delta_c);
 
-void manual_move_info_from_user_and_open_window(ManualMoveInfo* /*manual_move_info*/);
+void manual_move_info_from_user_and_open_window(ManualMoveInfo* manual_move_info);
 void update_manual_move_costs_and_open_window(ManualMoveInfo* manual_move_info, e_move_result& move_outcome, double delta_c, double bb_delta_c, double timing_delta_c);
 
 /*****************************************************************************/
@@ -1445,29 +1445,27 @@ static e_move_result try_swap(float t,
     }
 
     bool manual_move = get_manual_move_flag();
-    //bool manual_move = false;
     ManualMoveInfo* manual_move_info;
 
-    //if (manual_move) {
-        //manual_move_generator_window("");
-        //update_screen(ScreenUpdatePriority::MAJOR, " ", PLACEMENT, nullptr);
-        //manual_move_info = get_manual_move_info();
+    if (manual_move) {
+        manual_move_info_from_user_and_open_window(manual_move_info);
+        manual_move_info = get_manual_move_info();
         //sends info to the move generator class
-        //mmg_get_manual_move_info(*manual_move_info);
-    //}
+        mmg_get_manual_move_info(*manual_move_info);
+    }
 
     e_create_move create_move_outcome;
     //Generate a new move (perturbation) used to explore the space of possible placements
-    //if (manual_move)
-        //create_move_outcome = manual_move_generator.propose_move(blocks_affected, rlim);
-    //else
+    if (manual_move)
+        create_move_outcome = manual_move_generator.propose_move(blocks_affected, rlim);
+    else
         create_move_outcome = move_generator.propose_move(blocks_affected, rlim);
 
     LOG_MOVE_STATS_PROPOSED(t, blocks_affected);
 
     e_move_result move_outcome = ABORTED;
 
-    if (create_move_outcome == e_create_move::ABORT /*|| (manual_move && !manual_move_info->valid_input)*/) {
+    if (create_move_outcome == e_create_move::ABORT || (manual_move && !manual_move_info->valid_input)) {
         //Proposed move is not legal -- give up on this move
         clear_move_blocks(blocks_affected);
 
@@ -1517,9 +1515,9 @@ static e_move_result try_swap(float t,
         /* 1 -> move accepted, 0 -> rejected. */
         move_outcome = assess_swap(delta_c, t);
 
-        //if (manual_move && manual_move_info->valid_input)
+        if (manual_move && manual_move_info->valid_input)
             //update all the costs in the manual_move_info variable and open cost summary window
-            //update_manual_move_costs_and_open_window(manual_move_info, move_outcome, delta_c, bb_delta_c, timing_delta_c);
+            update_manual_move_costs_and_open_window(manual_move_info, move_outcome, delta_c, bb_delta_c, timing_delta_c);
 
         if (move_outcome == ACCEPTED) {
             costs->cost += delta_c;
@@ -1583,8 +1581,8 @@ static e_move_result try_swap(float t,
     //Check that each accepted swap yields a valid placement
     check_place(*costs, delay_model, place_algorithm);
 #endif
-    //if (manual_move)
-        //manual_move_info->valid_input = true;
+    if (manual_move)
+        manual_move_info->valid_input = true;
 
     return (move_outcome);
 }
