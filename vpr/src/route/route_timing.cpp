37bb70479433067c7ed7b928348c703606fde653
Alessandro Comodi <acomodi@antmicro.com>
2020-09-11 19:06:45 +0200

    lookahead: additional comments and assertions
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- vpr/src/route/route_timing.cpp
+++ vpr/src/route/route_timing.cpp
@@ -69,9 +69,14 @@ struct RoutingMetrics {
  * File-scope variables
  */
 
-//Run-time flag to control when router debug information is printed
-//Note only enables debug output if compiled with VTR_ENABLE_DEBUG_LOGGING defined
-//f_router_debug is used to stop the router when a breakpoint is reached. When a breakpoint is reached, this flag is set to true.
+/**
+ * @brief Run-time flag to control when router debug information is printed
+ * Note only enables debug output if compiled with VTR_ENABLE_DEBUG_LOGGING defined
+ * f_router_debug is used to stop the router when a breakpoint is reached. When a breakpoint is reached, this flag is set to true.
+ *
+ * In addition f_router_debug is used to print additional debug information during routing, for instance lookahead expected costs
+ * information.
+ */
 bool f_router_debug = false;
 
 //Count the number of times the router has failed
