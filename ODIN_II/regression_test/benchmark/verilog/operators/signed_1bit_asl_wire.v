8f45ccdddc38bc7a642263623597da52d94495cf
s.alireza.damghani@unb.ca <s.alireza.damghani@unb.ca>
2020-09-17 12:55:58 -0300

    * Adding new shift operation benchmarks to test the design
    * Removing old benchmarks for shift operation
    
    * Regenerating expectation results

---
+++ ODIN_II/regression_test/benchmark/verilog/operators/signed_1bit_asl_wire.v
@@ -0,0 +1,6 @@
+module simple_op ( input signed in,
+                                output out );
+
+    assign out = in <<< 1;
+
+endmodule
---
+++ ODIN_II/regression_test/benchmark/verilog/operators/unsigned_variable_sl_int_wide.v
@@ -0,0 +1,7 @@
+module simple_op ( input [7:0] shift,
+				input [31:0] in,
+                                output [31:0] out );
+
+    assign out = in << shift[4:0];
+
+endmodule
