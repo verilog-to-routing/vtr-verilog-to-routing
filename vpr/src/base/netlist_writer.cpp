95e704a9ba6b4590becadd5355b55cb8129ba5ef
tangxifan <tangxifan@gmail.com>
2020-10-27 15:33:57 -0600

    [VPR] code format fix

--- vpr/src/base/netlist_writer.cpp
+++ vpr/src/base/netlist_writer.cpp
@@ -1819,7 +1819,7 @@ class NetlistWriterVisitor : public NetlistVisitor {
                     //If there is a valid net connected in the implementation
                     AtomNetId logical_net_id = atom_ctx.nlist.port_net(port_id, orig_index);
 
-                    // Fatal error should be flagged when the net marked in implementation 
+                    // Fatal error should be flagged when the net marked in implementation
                     // does not match the net marked in input netlist
                     if (impl_input_net_id != logical_net_id) {
                         VPR_FATAL_ERROR(VPR_ERROR_IMPL_NETLIST_WRITER,
