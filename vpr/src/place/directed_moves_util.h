57d2a7089b6e2cab34c3061687edd627267306a2
MohamedElgammal <mohamed.elgammal@mail.utoronto.ca>
2020-11-21 05:16:39 -0500

    comment out the calculate_centroid_loc function

--- vpr/src/place/directed_moves_util.h
+++ vpr/src/place/directed_moves_util.h
@@ -5,6 +5,18 @@
 #include "timing_place.h"
 #include "move_generator.h"
 
+/**
+ * @brief Calculates the exact centroid location
+ *
+ * This function is very useful in centroid and weightedCentroid moves as it calculates
+ * the centroid location. It returns the calculated location in centroid.
+ *
+ *      @param b_from: The block Id of the moving block
+ *      @param timing_weights: Determines whether to calculate centroid or weighted centroid location. If true, use the timing weights (weighted centroid)
+ *      @param criticalities: A pointer to the placer criticalities which is used when calculating weighted centroid (send a NULL pointer in case of centroid)
+ * 
+ *      @return The calculated location is returned in centroid parameter that is sent by reference
+ */ 
 void calculate_centroid_loc(ClusterBlockId b_from, bool timing_weights, t_pl_loc& centroid, const PlacerCriticalities* criticalities);
 
 #endif
