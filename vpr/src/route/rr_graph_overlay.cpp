6a2eb50264e8c36b846dec1064fc00667eb0d95d
tangxifan <tangxifan@gmail.com>
2021-03-24 13:58:45 -0600

    [VPR] Add an overlay RRGraph object; Different from t_rr_graph_view, this overlay aims to provide full featured RRGraph APIs.

---
+++ vpr/src/route/rr_graph_overlay.cpp
@@ -0,0 +1,22 @@
+#include "rr_graph_overlay.h"
+
+/****************************
+ * Constructors
+ ****************************/
+RRGraphOverlay::RRGraphOverlay() {
+    node_storage_ = nullptr;
+    rr_node_indices_ = nullptr;
+}
+
+RRGraphOverlay::RRGraphOverlay(t_rr_graph_storage* node_storage,
+                               t_rr_node_indices* rr_node_indices) {
+    node_storage_ = node_storage;
+    rr_node_indices_ = rr_node_indices;
+}
+
+/****************************
+ * Accessors
+ ****************************/
+t_rr_type RRGraphOverlay::node_type(const RRNodeId& id) const {
+    return (*node_storage_).node_type(id);
+}
