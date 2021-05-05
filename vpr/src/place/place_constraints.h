f5b875069fde3088d957ca1dc97fbf54ffd6ef5d
Sarah Khalid <srhkhalid10@gmail.com>
2021-05-05 17:28:44 -0400

    Removed some unneeded code and changed some function parameters to const references

--- vpr/src/place/place_constraints.h
+++ vpr/src/place/place_constraints.h
@@ -24,20 +24,20 @@ bool is_cluster_constrained(ClusterBlockId blk_id);
 /*
  * Check if the placement location would respect floorplan constraints of the block, if it has any
  */
-bool cluster_floorplanning_legal(ClusterBlockId blk_id, t_pl_loc& loc);
+bool cluster_floorplanning_legal(ClusterBlockId blk_id, const t_pl_loc& loc);
 
 /*
  * Check whether any member of the macro has floorplan constraints
  */
-bool is_macro_constrained(t_pl_macro& pl_macro);
+bool is_macro_constrained(const t_pl_macro& pl_macro);
 
 /*
  * Returns region of valid locations for the head of the macro based on floorplan constraints
  */
-PartitionRegion constrained_macro_locs(t_pl_macro& pl_macro);
+PartitionRegion constrained_macro_locs(const t_pl_macro& pl_macro);
 
-inline bool floorplan_legal(t_pl_blocks_to_be_moved& blocks_affected) {
-    bool floorplan_legal = true;
+inline bool floorplan_legal(const t_pl_blocks_to_be_moved& blocks_affected) {
+    bool floorplan_legal;
 
     for (int i = 0; i < blocks_affected.num_moved_blocks; i++) {
         floorplan_legal = cluster_floorplanning_legal(blocks_affected.moved_blocks[i].block_num, blocks_affected.moved_blocks[i].new_loc);
