d6ede09b6b17b800ede2f72c3094c68cb8c93291
MohamedElgammal <mohamed.elgammal@mail.utoronto.ca>
2021-05-20 12:33:08 -0400

    Fix formatting

--- vtr_flow/scripts/run_vtr_task.py
+++ vtr_flow/scripts/run_vtr_task.py
@@ -225,7 +225,6 @@ def vtr_command_main(arg_list, prog=None):
 
         config_files = [find_task_config_file(task_name) for task_name in task_names]
         configs = []
-        longest_name = 0  # longest task name for use in creating prettier output
         common_task_prefix = None  # common task prefix to shorten task names
         for config_file in config_files:
             config = load_task_config(config_file)
@@ -241,10 +240,7 @@ def vtr_command_main(arg_list, prog=None):
                 longest_name = len(config.task_name)
         if args.short_task_names:
             configs = shorten_task_names(configs, common_task_prefix)
-        longest_arch_circuit = find_longest_task_description(
-            configs
-        )  # find longest task description for use in creating prettier output
-        num_failed = run_tasks(args, configs, longest_name, longest_arch_circuit)
+        num_failed = run_tasks(args, configs)
 
     except CommandError as exception:
         print("Error: {msg}".format(msg=exception.msg))
@@ -265,8 +261,6 @@ def vtr_command_main(arg_list, prog=None):
 def run_tasks(
     args,
     configs,
-    longest_name,
-    longest_arch_circuit,
 ):
     """
     Runs the specified set of tasks (configs)
