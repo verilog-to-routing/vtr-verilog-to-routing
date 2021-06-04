63507d2278c7ec6ea0813ab0fafdd4686d54cdfa
MohamedElgammal <mohamed.elgammal@mail.utoronto.ca>
2021-06-04 11:56:26 -0400

    More commenting in the submission script to explain the env variable option

--- vtr_flow/scripts/slurm/submission_template.sh
+++ vtr_flow/scripts/slurm/submission_template.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+#======================================================================================
+#Please Make a copy of this template in the same directory and edit it to fit your run.
+#Please do not submit your copy of this template to the repo.
+#======================================================================================
+
+
+#The following section controls the parameters of your run, feel free to edit:
+#=============================================================================
 #SBATCH --nodes=1                                       # Number of nodes in our pool
 #SBATCH --ntasks=80                                     # Number of parallel tasks
 #SBATCH --ntasks-per-node=40                            # maximum number of tasks simultaneously running on a node
@@ -9,6 +17,11 @@
 #SBATCH --output=parallel_%j.log                        # Determine the output log file (%j) is the jobid
 #SBATCH --error=error_%j.log                            # Determine the error log file
 #SBATCH --time=10:00:00                                 # The job time limit in hh:mm:ss
+
+#You can also overwrite the values of some of these paramters using environment variables
+#========================================================================================
+# - SBATCH_JOB_NAME instead of --job-name
+# - SBATCH_TIMELIMIT instead of --time
 
 #Preload the required environment modules
 module load gcc/9.2.0
