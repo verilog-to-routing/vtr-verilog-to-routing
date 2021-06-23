5dc28a82971cd87f785531ee3d8ddc6617720939
Paula <perdomom@betzgrp-wintermute.eecg.utoronto.ca>
2021-06-23 15:26:43 -0400

    Fixed gtk errors and changed the size of the button

--- vpr/src/draw/buttons.cpp
+++ vpr/src/draw/buttons.cpp
@@ -22,7 +22,7 @@
 
 //location of spin buttons, combo boxes, and labels on grid
 gint box_width = 1;
-gint box_height = 0.5;
+gint box_height = 1;
 gint label_left_start_col = 0;
 gint box_left_start_col = 0;
 gint button_row = 2; // 2 is the row num of the window button in main.ui, add buttons starting from this row
