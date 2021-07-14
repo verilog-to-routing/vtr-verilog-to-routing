89e29931c956cfb60546cd95ecf2e51f1f73472c
Srivatsan Srinivasan <srivat97@outlook.com>
2021-07-13 20:33:07 -0400

    initial commit: adding main files used for new feature

---
+++ utils/vqm2blif/src/base/hard_block_recog.h
@@ -0,0 +1,28 @@
+/* This file contains datastructures and functions used to
+*  filter out LUTs/flip flops used for hard block port connections
+*  and then instantiate a new hard block type in the netlist.*/
+
+
+#ifndef HARD_BLOCK_RECOG_H
+#define HARD_BLOCK_RECOG_H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
