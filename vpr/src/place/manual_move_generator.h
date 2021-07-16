11fd6aede9e0ee84b5742236f8dcba360c26f1ac
Paula <perdomom@betzgrp-wintermute.eecg.utoronto.ca>
2021-07-16 15:32:14 -0400

    Removed NO_GRAPHICS from the manual move generator.h

--- vpr/src/place/manual_move_generator.h
+++ vpr/src/place/manual_move_generator.h
@@ -1,18 +1,16 @@
 #ifndef VPR_MANUAL_MOVE_GEN_H
 #define VPR_MANUAL_MOVE_GEN_H
 
-#ifndef NO_GRAPHICS
-
-#    include "move_generator.h"
-#    include "median_move_generator.h"
-#    include "weighted_median_move_generator.h"
-#    include "weighted_centroid_move_generator.h"
-#    include "feasible_region_move_generator.h"
-#    include "uniform_move_generator.h"
-#    include "critical_uniform_move_generator.h"
-#    include "centroid_move_generator.h"
-#    include "simpleRL_move_generator.h"
-#    include <numeric>
+#include "move_generator.h"
+#include "median_move_generator.h"
+#include "weighted_median_move_generator.h"
+#include "weighted_centroid_move_generator.h"
+#include "feasible_region_move_generator.h"
+#include "uniform_move_generator.h"
+#include "critical_uniform_move_generator.h"
+#include "centroid_move_generator.h"
+#include "simpleRL_move_generator.h"
+#include <numeric>
 
 /** Manual Moves Generator, inherits from MoveGenerator class **/
 class ManualMoveGenerator : public MoveGenerator {
@@ -28,7 +26,5 @@ class ManualMoveGenerator : public MoveGenerator {
     //Evaluates if move is successful and legal or unable to do.
     e_create_move propose_move(t_pl_blocks_to_be_moved& blocks_affected, e_move_type& /*move_type*/, float /*rlim*/, const t_placer_opts& /*placer_opts*/, const PlacerCriticalities* /*criticalities*/);
 };
-
-#endif /*NO_GRAPHICS*/
 
 #endif /*VPR_MANUAL_MOVE_GEN_H */
