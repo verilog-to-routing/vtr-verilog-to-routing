d36ddc2f90ae853fe1942125a6e76c9179c938ea
Seyed Alireza Damghani <sdamghan@unb.ca>
2021-08-11 19:21:45 -0300

    [Infra]: adding yosys as a submodule to VTR repo
    Signed-off-by: Seyed Alireza Damghani <sdamghan@unb.ca>

--- .github/kokoro/steps/vtr-yosys-setup.sh
+++ .github/kokoro/steps/vtr-yosys-setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 source .github/kokoro/steps/vtr-min-setup.sh
-source .github/scripts/install_yosys.sh
-
-cd $VTR_DIR
+source .github/scripts/build-yosys.sh
--- .github/scripts/build-yosys.sh
+++ .github/scripts/build-yosys.sh
@@ -8,13 +8,17 @@ if [ ! -e $HOME/.local/bin/yosys ]; then
     echo '                        Building yosys'
     echo '================================================================'
     mkdir -p ~/.local
-    mkdir -p ~/.local/src
-    cd ~/.local/src
-    git clone https://github.com/YosysHQ/yosys.git
+    # Yosys is added to VTR fixed at the b96eb888 commit
+    # mkdir -p ~/.local/src
+    # cd ~/.local/src
+    # git clone https://github.com/YosysHQ/yosys.git
 
-    cd yosys
+    # cd to VTR_ROOT/yosys submodule
+    cd ./yosys
     make -j$(nproc) PREFIX=$HOME/.local
     make install PREFIX=$HOME/.local 
+    # cd back to VTR_ROOT
+    cd ../
 fi
 
 echo $(which yosys)
