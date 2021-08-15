926c9278fb3005a8681a0e685c72c8b2046b4368
Seyed Alireza Damghani <sdamghan@unb.ca>
2021-08-15 19:21:26 -0400

    [Infra]: add "-DODIN_USE_YOSYS=ON" to config files of GitHub action and Kokoro runners
    Signed-off-by: Seyed Alireza Damghani <sdamghan@unb.ca>

--- .github/kokoro/run-vtr.sh
+++ .github/kokoro/run-vtr.sh
@@ -17,11 +17,9 @@ source $SCRIPT_DIR/steps/hostinfo.sh
 # Output git information
 source $SCRIPT_DIR/steps/git.sh
 
-if [ $VTR_TEST == "vtr_reg_strong" ] || [ $VTR_TEST == "odin_reg_strong" ]; then
+if [ $VTR_TEST == "vtr_reg_strong" ] || [ $VTR_TEST == "odin_reg_strong" ]
+   || [ $VTR_TEST == "odin_tech_strong" ] || [ $VTR_TEST == "vtr_reg_yosys+odin" ]; then
 	source $SCRIPT_DIR/steps/vtr-min-setup.sh
-elif [ $VTR_TEST == "vtr_reg_yosys+odin" ] || [ $VTR_TEST == "odin_tech_strong" ]; then
-	# Yosys needs to be installed first and then Odin-II tech map runs
-    source $SCRIPT_DIR/steps/vtr-yosys-setup.sh
 else
 	source $SCRIPT_DIR/steps/vtr-full-setup.sh
 fi
--- .github/kokoro/steps/vtr-yosys-setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source .github/kokoro/steps/vtr-min-setup.sh
-source .github/scripts/build-yosys.sh
