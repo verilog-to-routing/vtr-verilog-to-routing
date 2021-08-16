0119d26e6ae25af05798b934899e70c2d7e9d13d
Seyed Alireza Damghani <sdamghan@unb.ca>
2021-08-16 13:29:45 -0300

    [Odin]: - merge Yosys+Odin-II micro benchmarks with Odin-II micro benchmarks         - separate simulation vectors using _yosys_input/output and _odin_input\output
    Signed-off-by: Seyed Alireza Damghani <sdamghan@unb.ca>

--- ODIN_II/regression_test/benchmark/verilog/micro/hard_adder_cin_propagation.v
+++ ODIN_II/regression_test/benchmark/verilog/micro/hard_adder_cin_propagation.v
@@ -1,12 +1,13 @@
+`define WIDTH 5
 module top_module
 (
-    input  [3:0] a, b,
-    output [4:0] out
+    input  [`WIDTH-1:0] a, b,
+    output [`WIDTH-1:0] out
 );
 
 	wire  cout;
 
-	adder add1(.a(a[1:0]), .b(b[1:0]), .cin(1'b0), .sumout(out[1:0]), .cout(cout));
-	adder add2(.a(a[3:2]), .b(b[3:2]), .cin(cout), .sumout(out[3:2]), .cout(out[4]));
+	adder #(`WIDTH) add1(.a(a[1:0]), .b(b[1:0]), .cin(1'b0), .sumout(out[1:0]), .cout(cout));
+	adder #(`WIDTH) add2(.a(a[3:2]), .b(b[3:2]), .cin(cout), .sumout(out[3:2]), .cout(out[4]));
 
 endmodule
--- ODIN_II/regression_test/benchmark/verilog/yosys_compatible_verilogs/micro/bm_DL_2_1_mux.v
+++
@@ -1,9 +0,0 @@
-/* pg 322 2 to 1 multiplexer */
-
-module bm_DL_2_1_mux  (w0, w1, s, f);
-	input w0, w1, s;
-	output f;
-	
-	assign f = s ? w1 : w0;
-		
-endmodule
