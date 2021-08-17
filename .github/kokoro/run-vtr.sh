d779c50137f3ef42a84dae438198531748b64847
Seyed Alireza Damghani <sdamghan@unb.ca>
2021-08-16 21:10:25 -0400

    [Odin]: - regenerate expectation results of arch_sweep benchmarks 	- update expectation results of mixing optimization tasks 	- regenerate expectation results of yosys+odin/full benchmarks
    Signed-off-by: Seyed Alireza Damghani <sdamghan@unb.ca>

--- .github/kokoro/run-vtr.sh
+++ .github/kokoro/run-vtr.sh
@@ -17,8 +17,8 @@ source $SCRIPT_DIR/steps/hostinfo.sh
 # Output git information
 source $SCRIPT_DIR/steps/git.sh
 
-if [ $VTR_TEST == "vtr_reg_strong" ] || [ $VTR_TEST == "odin_reg_strong" ]
-   || [ $VTR_TEST == "odin_tech_strong" ] || [ $VTR_TEST == "vtr_reg_yosys+odin" ]; then
+if [ $VTR_TEST == "vtr_reg_strong" ] || [ $VTR_TEST == "odin_reg_strong" ] \
+|| [ $VTR_TEST == "odin_tech_strong" ] || [ $VTR_TEST == "vtr_reg_yosys+odin" ]; then
 	source $SCRIPT_DIR/steps/vtr-min-setup.sh
 else
 	source $SCRIPT_DIR/steps/vtr-full-setup.sh
