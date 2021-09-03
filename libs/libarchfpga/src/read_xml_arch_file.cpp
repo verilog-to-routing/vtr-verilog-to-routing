55c5cf5b9cceecc5b8ef3bfac24b6dfc6ee2de9f
Arash Ahmadian <arash.ahmadian@mail.utoronto.ca>
2021-09-02 21:30:50 -0400

    addressing comments on PR

--- libs/libarchfpga/src/read_xml_arch_file.cpp
+++ libs/libarchfpga/src/read_xml_arch_file.cpp
@@ -3641,9 +3641,9 @@ static void ProcessSegments(pugi::xml_node Parent,
     Node = get_first_child(Parent, "segment", loc_data);
 
     /*Keep track of the type of axis of the segments loaded*/
-    bool both_axis_segs = false;
-    bool x_axis_segs = false;
-    bool y_axis_segs = false;
+
+    bool x_axis_seg_found = false;
+    bool y_axis_seg_found = false;
 
     for (i = 0; i < NumSegs; ++i) {
         /* Get segment name */
@@ -3696,15 +3696,16 @@ static void ProcessSegments(pugi::xml_node Parent,
         if (tmp) {
             if (strcmp(tmp, "x") == 0) {
                 Segs[i].parallel_axis = X_AXIS;
-                x_axis_segs = true;
+                x_axis_seg_found = true;
             } else if (strcmp(tmp, "y") == 0) {
                 Segs[i].parallel_axis = Y_AXIS;
-                y_axis_segs = true;
+                y_axis_seg_found = true;
             } else {
                 archfpga_throw(loc_data.filename_c_str(), loc_data.line(Node), "Unsopported parralel axis type: %s\n", tmp);
             }
         } else {
-            both_axis_segs = true;
+            x_axis_seg_found = true;
+            y_axis_seg_found = true;
         }
 
         /* Get Power info */
@@ -3825,9 +3826,10 @@ static void ProcessSegments(pugi::xml_node Parent,
         Node = Node.next_sibling(Node.name());
     }
     /*if no segment with default axis option isn't provided, atleast 1 segment along each x & y axes is needed */
-    if (!both_axis_segs && !(x_axis_segs && y_axis_segs && true)) {
+
+    if (!x_axis_seg_found || !y_axis_seg_found) {
         archfpga_throw(loc_data.filename_c_str(), loc_data.line(Node),
-                       "Atleast one segment per-axis needs to get specified if no segments with non-specified (default) axis attribute exists.");
+                       "Atleast one segment per-axis needs to get specified if no segments with non-specified (default) axis attribute exist.");
     }
 }
 
