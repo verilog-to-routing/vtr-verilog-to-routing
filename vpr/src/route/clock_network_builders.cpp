e0e97c582abe12ed44989dd4e2d92d764a7cea08
Muhammad Haris Zafar <muhammad.haris@lampromellon.com>
2021-10-18 17:21:07 +0500

    Fixing format

--- vpr/src/route/clock_network_builders.cpp
+++ vpr/src/route/clock_network_builders.cpp
@@ -324,7 +324,7 @@ int ClockRib::create_chanx_wire(int x_start,
     rr_graph_builder.set_node_capacity(chanx_node, 1);
     rr_graph_builder.set_node_track_num(chanx_node, ptc_num);
     rr_graph_builder.set_node_rc_index(chanx_node, find_create_rr_rc_data(
-        x_chan_wire.layer.r_metal, x_chan_wire.layer.c_metal));
+                                                       x_chan_wire.layer.r_metal, x_chan_wire.layer.c_metal));
     rr_graph_builder.set_node_direction(chanx_node, direction);
 
     short seg_index = 0;
@@ -630,7 +630,7 @@ int ClockSpine::create_chany_wire(int y_start,
     rr_graph_builder.set_node_capacity(chany_node, 1);
     rr_graph_builder.set_node_track_num(chany_node, ptc_num);
     rr_graph_builder.set_node_rc_index(chany_node, find_create_rr_rc_data(
-        y_chan_wire.layer.r_metal, y_chan_wire.layer.c_metal));
+                                                       y_chan_wire.layer.r_metal, y_chan_wire.layer.c_metal));
     rr_graph_builder.set_node_direction(chany_node, direction);
 
     short seg_index = 0;
