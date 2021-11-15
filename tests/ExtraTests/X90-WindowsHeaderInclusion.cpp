25401b4a8204fedfa9c0dc86b67b006806c22903
Srivatsan Srinivasan <srivat97@outlook.com>
2021-11-15 18:56:57 -0500

    Squashed 'libs/EXTERNAL/libcatch2/' content from commit 2287d225e
    git-subtree-dir: libs/EXTERNAL/libcatch2
    git-subtree-split: 2287d225e5595a0cbac14c69ec10b16f6b9488af

---
+++ tests/ExtraTests/X90-WindowsHeaderInclusion.cpp
@@ -0,0 +1,11 @@
+// X90-WindowsHeaderInclusion.cpp
+// Test that the Catch2 header compiles even after including windows.h
+// without defining NOMINMAX first. As an FYI, if you do that, you are
+// wrong.
+
+#include <windows.h>
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Catch2 did survive compilation with windows.h", "[compile-test]") {
+    SUCCEED();
+}
