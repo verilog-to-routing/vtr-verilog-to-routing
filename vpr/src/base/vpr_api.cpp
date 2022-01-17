ff949b9a63e04b342165d91ca38ea187aa5c8eac
Pawel Czarnecki <pczarnecki@antmicro.com>
2022-01-17 09:50:30 +0100

    vpr: base: add option for dumping merged netlist
    Signed-off-by: Pawel Czarnecki <pczarnecki@antmicro.com>

--- vpr/src/base/vpr_api.cpp
+++ vpr/src/base/vpr_api.cpp
@@ -1282,6 +1282,11 @@ void vpr_analysis(t_vpr_setup& vpr_setup, const t_arch& Arch, const RouteStatus&
                            vpr_setup.AnalysisOpts);
         }
 
+        //Write the post-syntesis merged netlist
+        if (vpr_setup.AnalysisOpts.gen_post_synthesis_merged_netlist) {
+            merged_netlist_writer(atom_ctx.nlist.netlist_name().c_str(), analysis_delay_calc);
+        }
+
         //Do power analysis
         if (vpr_setup.PowerOpts.do_power) {
             vpr_power_estimation(vpr_setup, Arch, *timing_info, route_status);
