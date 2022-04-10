c4012ab09657a62c5651ab39a1d1bdd54cdf524d
amin1377 <amin1377.mohaghegh@gmail.com>
2022-04-10 11:48:34 -0400

    add members to t_pb_graph_node to store the number of primitive pin classes

--- libs/libarchfpga/src/physical_types.h
+++ libs/libarchfpga/src/physical_types.h
@@ -1187,10 +1187,13 @@ class t_pb_graph_node {
     int* output_pin_class_size; /* Stores the number of pins that belong to a particular output pin class */
     int num_output_pin_class;   /* number of output pin classes that this pb_graph_node has */
 
+    int total_num_primitive_input_class; /* only valid for root blocks */
+    int total_num_primitive_output_class; /* only valid for root blocks */
     int max_input_pin_mode_num;
     int total_num_input_pins;
     int total_num_output_pins;
-    std::unordered_map<t_pb_graph_pin*, int> pins_vec; /* {pin, pin_idx} */
+    std::unordered_map<const t_pb_graph_pin*, int> pins_vec; /* {pin, intra_cluster_pin_idx} */
+    std::unordered_map<const t_pb_graph_pin*, int> primitive_pin_class_map; /* {primitive_pin, class_number} - only valid for top level block*/
     int total_primitive_count; /* total number of this primitive type in the cluster */
 
     /* Interconnect instances for this pb
