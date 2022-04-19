377f5e7575977f9e0bdee1979c60f7db75596537
amin1377 <amin1377.mohaghegh@gmail.com>
2022-04-19 09:41:16 -0400

    some functions still use the previous logical pin index (in arch_check.cpp) - I returned get_port_by_pin for backward compatibility, but this needs to be solved

--- libs/libarchfpga/src/physical_types_util.cpp
+++ libs/libarchfpga/src/physical_types_util.cpp
@@ -479,6 +479,19 @@ const t_physical_tile_port* get_port_by_pin(const t_sub_tile* sub_tile, int pin)
     return nullptr;
 }
 
+const t_port* get_port_by_pin(t_logical_block_type_ptr type, int pin) {
+    auto pb_type = type->pb_type;
+
+    for (int i = 0; i < pb_type->num_ports; i++) {
+        auto port = pb_type->ports[i];
+        if (pin >= port.absolute_first_pin_index && pin < port.absolute_first_pin_index + port.num_pins) {
+            return &pb_type->ports[port.index];
+        }
+    }
+
+    return nullptr;
+}
+
 const t_port* get_port_by_logical_pin_num(t_logical_block_type_ptr type, int pin) {
     auto pb_pin = type->pb_graph_head->pb_pin_idx_bimap[pin];
     return pb_pin->port;
