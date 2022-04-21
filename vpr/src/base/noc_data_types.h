72b4f96a61869c94cf898396bff23f3d04b35e33
Srivatsan Srinivasan <srivat97@outlook.com>
2022-04-20 22:14:50 -0400

    added a new strong id to index noc traffic flows

--- vpr/src/base/noc_data_types.h
+++ vpr/src/base/noc_data_types.h
@@ -2,7 +2,6 @@
 #define NOC_DATA_TYPES_H
 
 #include "vtr_strong_id.h"
-#include "atom_netlist_fwd.h"
 
 #include <string>
 
@@ -13,32 +12,9 @@ struct noc_link_id_tag;
 typedef vtr::StrongId<noc_router_id_tag, int> NocRouterId;
 typedef vtr::StrongId<noc_link_id_tag, int> NocLinkId;
 
-/*
-    Describes a traffic flow within the NoC, which is the communication between two routers. This description includes the following:
-        - The module names of the source and destination routers that are communicating with each other. The module name is the name used when instantiating a router module in the HDL design.
-        - The atom IDs of the router modules. By storing this, we can quickly lookup the router modules instead of identifying them by using their names.
-        - The bandwidth(size) of data being transferred between the two routers.
-        - The maximum allowable latency of the data transfer.
+// data type to index traffic flows within the noc
+struct noc_traffic_flow_id_tag;
 
-    This datastructure will be primarily used during placement to identify which routers inside the NoC(NocStorage) need to be routed to each other.This is important since the router modules can be moved around to different tiles on the FPGA device. Additionaly, once a the routing has been completed, the bandwidht and latency parameters can be used to calculate the cost of the placement of routers.
-*/
-struct t_noc_traffic_flow {
-
-    // router module names
-    std::string source_router_module_name;
-    std::string sink_router_module_name;
-
-    // router module atom ids
-    AtomBlockId source_router_atom_id;
-    AtomBlockId sink_router_atom_id;
-
-    // in bytes/s
-    double traffic_flow_bandwidth;
-
-    // in seconds
-    double max_traffic_flow_latency;
-
-};
-
+typedef vtr::StrongId<noc_traffic_flow_id_tag, int> NocTrafficFlowId;
 
 #endif
