7cbddc7368ffacb5185809a157cc2060c0eaa699
jmah76 <cheepycurner@gmail.com>
2022-06-30 16:23:03 -0400

    formatting

--- vpr/src/pack/cluster_feasibility_filter.cpp
+++ vpr/src/pack/cluster_feasibility_filter.cpp
@@ -339,10 +339,6 @@ static void expand_pb_graph_node_and_load_output_to_input_connections(t_pb_graph
             else{
             	reference_pin->list_of_connectable_input_pin_ptrs[depth] = new t_pb_graph_pin*[reference_pin->num_connectable_primitive_input_pins[depth]];
             }
-//            reference_pin->list_of_connectable_input_pin_ptrs[depth] = (t_pb_graph_pin**)vtr::realloc(
-//                reference_pin->list_of_connectable_input_pin_ptrs[depth],
-//                reference_pin->num_connectable_primitive_input_pins[depth]
-//                    * sizeof(t_pb_graph_pin*));
             reference_pin->list_of_connectable_input_pin_ptrs[depth][reference_pin->num_connectable_primitive_input_pins[depth]
                                                                      - 1]
                 = current_pb_graph_pin;
