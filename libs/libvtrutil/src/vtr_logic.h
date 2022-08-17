b429ef125ad437d5c4a46391f4dd78b67868d99d
tangxifan <tangxifan@gmail.com>
2022-08-17 16:35:38 -0700

    [lib] add constant string for logic value, useful for debugging messages

--- libs/libvtrutil/src/vtr_logic.h
+++ libs/libvtrutil/src/vtr_logic.h
@@ -25,8 +25,11 @@ enum class LogicValue {
     FALSE = 0,
     TRUE = 1,
     DONT_CARE = 2,
-    UNKOWN = 3
+    UNKOWN = 3,
+    NUM_LOGIC_VALUE_TYPES
 };
+
+constexpr std::array<const char*, size_t(LogicValue::NUM_LOGIC_VALUE_TYPES)> LOGIC_VALUE_STRING = {{"false", "true", "don't care", "unknown"}};
 
 } // namespace vtr
 
