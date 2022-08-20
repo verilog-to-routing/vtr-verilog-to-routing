790a7c7ae09cfafb1607e9f832ff7fe05d843b6e
Srivatsan Srinivasan <srivat97@outlook.com>
2022-08-20 15:14:43 -0400

    Converted a string to a char string before printing it. This was done so that the clang compiler would not fail

--- vpr/src/noc/noc_routing_algorithm_creator.cpp
+++ vpr/src/noc/noc_routing_algorithm_creator.cpp
@@ -10,7 +10,7 @@ NocRouting* NocRoutingAlgorithmCreator::create_routing_algorithm(std::string rou
     } else if (routing_algorithm_name == "bfs_routing") {
         noc_routing_algorithm = new BFSRouting();
     } else {
-        VPR_FATAL_ERROR(VPR_ERROR_OTHER, "The provided NoC routing algorithm '%s' is not supported.", routing_algorithm_name);
+        VPR_FATAL_ERROR(VPR_ERROR_OTHER, "The provided NoC routing algorithm '%s' is not supported.", routing_algorithm_name.c_str());
     }
 
     return noc_routing_algorithm;
