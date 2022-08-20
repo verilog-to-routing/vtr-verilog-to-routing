dc8aee4d0da098f4e6fd692ac2deb8682bf0f9cc
Srivatsan Srinivasan <srivat97@outlook.com>
2022-08-19 23:26:02 -0400

    Fixed up commenting to indicate that the noc_routing_algorithm_creator is a factory object

--- vpr/src/noc/noc_routing_algorithm_creator.h
+++ vpr/src/noc/noc_routing_algorithm_creator.h
@@ -2,18 +2,19 @@
 #define NOC_ROUTING_ALGORITHM_CREATOR
 
 /**
- * @file om)
+ * @file
  * @brief  This file defines the NocRoutingAlgorithmCreator class, which creates
  * the routing algorithm that will be used to route packets within the NoC.
  * 
  * Overview
  * ========
- * There are a number of different available NoC routing algorithms. The purpose of
- * this class is to construct the appropriate routing algorithm based on the user
- * specification in the command line. The user identifies a specific routing algorithm
- * in the command line by providing a string (which is the name of routing algorithm).
- * Then, the appripriate routing algorithm is created here based on the provided
- * string.
+ * There are a number of different available NoC routing algorithms. This class * is a factory object for the NocRouting abstract class. This class constructs 
+ * the appropriate routing algorithm based on the user specification in the
+ * command line. The user identifies a 
+ * specific routing algorithm in the command line by providing a string
+ * (which is the name of routing algorithm).
+ * Then the corresponding routing algorithm is created here based on the 
+ * provided string.
  */
 
 #include <string>
@@ -29,10 +30,10 @@ class NocRoutingAlgorithmCreator {
     ~NocRoutingAlgorithmCreator() {}
 
     /**
-     * @brief Given a string that identifies a NoC routing algorithm, this function
-     * create the corresponding routing algorithm and returns a reference to it. If
-     * the provided string does not match any available routing algorithms then an
-     * error is thrown.
+     * @brief Given a string that identifies a NoC routing algorithm, this 
+     * function creates the corresponding routing algorithm and returns a
+     * reference to it. If the provided string does not match any
+     * available routing algorithms then an error is thrown.
      * 
      * @param routing_algorithm_name A user provided string that identifies a 
      * NoC routing algorithm
