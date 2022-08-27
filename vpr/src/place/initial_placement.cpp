d490075c5234d8f252e8f4c6ce516b0cef52db83
Srivatsan Srinivasan <srivat97@outlook.com>
2022-08-27 01:27:49 -0400

    Within the initial placement function in the vpr flow integrated the NoC placement initialization. This was done in the process to integrate noc optimization during vpr placement.

--- vpr/src/place/initial_placement.cpp
+++ vpr/src/place/initial_placement.cpp
@@ -10,6 +10,13 @@
 #include "place_constraints.h"
 #include "move_utils.h"
 #include "region.h"
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+#include "directed_moves_util.h"
+=======
+>>>>>>> Within the initial placement function in the vpr flow integrated the NoC placement initialization. This was done in the process to integrate noc optimization during vpr placement.
+>>>>>>> Within the initial placement function in the vpr flow integrated the NoC placement initialization. This was done in the process to integrate noc optimization during vpr placement.
 #include "noc_place_utils.h"
 
 #include "echo_files.h"
