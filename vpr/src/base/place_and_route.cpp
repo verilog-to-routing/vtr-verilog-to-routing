be34c06f65366132a9de619a39cd8330bed4c68d
tangxifan <tangxifan@gmail.com>
2022-09-18 21:56:39 -0700

    [vpr] fixed a bug where tileable rr_graph is not considered during binary search and place

--- vpr/src/base/place_and_route.cpp
+++ vpr/src/base/place_and_route.cpp
@@ -84,6 +84,10 @@ int binary_search_place_and_route(const Netlist<>& placement_net_list,
         graph_directionality = GRAPH_BIDIR;
     } else {
         graph_type = (det_routing_arch->directionality == BI_DIRECTIONAL ? GRAPH_BIDIR : GRAPH_UNIDIR);
+        /* Branch on tileable routing */
+        if (det_routing_arch->directionality == UNI_DIRECTIONAL && det_routing_arch->tileable) {
+            graph_type = GRAPH_UNIDIR_TILEABLE;
+        }
         graph_directionality = (det_routing_arch->directionality == BI_DIRECTIONAL ? GRAPH_BIDIR : GRAPH_UNIDIR);
     }
 
