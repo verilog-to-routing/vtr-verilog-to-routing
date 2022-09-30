e3636d832fb43c0b194dd8bb58b287ba4301a280
saaramahmoudi <sin29mim@gmail.com>
2022-09-30 14:32:35 -0400

    Slave interface and traffic generator is added

--- vtr_flow/benchmarks/noc/Synthetic_Designs/shared_verilog/slave_interface.v
+++ vtr_flow/benchmarks/noc/Synthetic_Designs/shared_verilog/slave_interface.v
@@ -1,5 +1,4 @@
-module master_interface (
-	
+module slave_interface (
 	clk,
 	reset,
 	tvalid_in,
@@ -13,34 +12,36 @@ module master_interface (
 	tdest,
 	tuser,
 	tlast
-	
 );
 
 parameter noc_dw = 32; //NoC Data Width
-parameter byte_dw = 8; 
-
+parameter byte_dw = 8;
 
 /*****************INPUT/OUTPUT Definition********************/
 input wire clk;
 input wire reset;
+
 input wire tvalid_in;
 input wire [noc_dw - 1 : 0] tdata_in;
-input wire tready;
- 
+
+//future purposes
+input wire [noc_dw/byte_dw - 1 : 0] tstrb;
+input wire [noc_dw/byte_dw - 1 : 0] tkeep;
+input wire [byte_dw - 1 : 0] tid;
+input wire [byte_dw - 1 : 0] tdest;
+input wire [byte_dw - 1 : 0] tuser; 
+input wire tlast;
+
+output reg tready;
 output reg tvalid_out;
-output reg [noc_dw -1 : 0] tdata_out;
-output reg [noc_dw/byte_dw - 1 : 0] tstrb;
-output reg [noc_dw/byte_dw - 1 : 0] tkeep;
-output reg [byte_dw - 1 : 0] tid;
-output reg [byte_dw - 1 : 0] tdest;
-output reg [byte_dw - 1 : 0] tuser; 
-output reg tlast;
+output reg [noc_dw - 1 : 0] tdata_out;
 
 /*******************Internal Variables***********************/
 localparam reset_state = 1'b0;
-localparam send_data_state = 1'b1;
+localparam send_ready_state = 1'b1;
 
 reg curr_state, next_state;
+
 
 /******************Sequential Logic*************************/
 always @ (posedge clk, posedge reset)
@@ -56,48 +57,35 @@ end
 
 
 /*****************Combinational Logic**********************/
-always @ (curr_state,tvalid_in,tready)
+always @ (curr_state,tvalid_in)
 begin
-	
 	tdata_out = tdata_in;
-	//future purposes
-	tstrb = curr_state;
-	tkeep = curr_state;
-	tid = curr_state;
-	tuser = curr_state;
-	tdest = curr_state;
-	tlast = curr_state;
-
+	tvalid_out = tvalid_in; 
 	case(curr_state) 
 		reset_state: begin
 			if(tvalid_in == 1'b0)begin
 				next_state = reset_state;
-				tvalid_out = 1'b0;
+				tready = 1'b0;
 			end
 			else begin
-				next_state = send_data_state; 
-				tvalid_out = 1'b1;
+				next_state = send_ready_state; 
+				tready = 1'b1;
 			end
 		end
 	
-		send_data_state: begin
-			if(tready == 1'b1) begin 
-				next_state = reset_state;
-				tvalid_out = 1'b0;
-			end
-			else begin	
-				next_state = send_data_state;
-				tvalid_out = 1'b1;
-			end
+		send_ready_state: begin
+			next_state = reset_state;
+			tready = 1'b0;
 		end
 		
 		default: begin
 			next_state = curr_state;
-			tvalid_out = tvalid_out;
+			tready = tready;
 		end 
 		
 	endcase
 
 end
+
 
 endmodule 
