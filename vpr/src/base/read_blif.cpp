3264072d5d4b646efaef911095767d58b5647461
tangxifan <tangxifan@gmail.com>
2022-11-01 15:44:44 -0700

    [vpr] a more sophiscated version of read blif api

--- vpr/src/base/read_blif.cpp
+++ vpr/src/base/read_blif.cpp
@@ -718,19 +718,16 @@ bool is_binary_param(const std::string& param) {
     return true;
 }
 
-bool is_real_param(const std::string& param) {
-    const std::string chars = "012345678.";
-
+bool is_real_param(const std::string &param) {
     /* Must be non-empty */
     if (param.empty()) {
         return false;
     }
 
-    /* The string mustn't contain any other chars that the expected ones */
-    for (size_t i = 0; i < param.length(); ++i) {
-        if (chars.find(param[i]) == std::string::npos) {
-            return false;
-        }
+    /* The string must match the regular expression */
+    static const std::regex real_number_expr("[+-]?([0-9]*\\.[0-9]+)|([0-9]+\\.[0-9]*)");
+    if (!std::regex_match(param, real_number_expr)) {
+        return false;
     }
 
     /* This is a real number param */
