da53061e99ee7d793999f58886003190f82a11d9
saaramahmoudi <sin29mim@gmail.com>
2022-11-08 11:44:51 -0500

    verilog files for traffic processor and complex designs updated, blif files needs to be updated with tcl file as well

--- vtr_flow/benchmarks/noc/Synthetic_Designs/spread_2_noc_1D_chain/verilog/traffic_processor.v
+++ vtr_flow/benchmarks/noc/Synthetic_Designs/spread_2_noc_1D_chain/verilog/traffic_processor.v
@@ -1,6 +1,6 @@
-/* This is the traffic processor module. This module
+/* This is the traffic processor module. This
 	accepts data coming in from the NoC and 
-	accumulates it.
+	encrypts it using sha algorithm
 */
 
 module traffic_processor (
@@ -24,6 +24,7 @@ output reg [noc_dw-1:0] o_enc;
 
 /*******************Internal Variables**********************/
 wire [noc_dw - 1 : 0] sha_out;
+wire [noc_dw - 1 : 0] sha2_out;
 
 /*******************module instantiation*******************/
 sha1 sha1_module
@@ -37,12 +38,22 @@ sha1 sha1_module
 	.cmd_o()
 );
 
+sha1 sha2_module 
+(
+	.clk_i(clk),
+	.rst_i(reset),
+	.text_i(sha_out),
+	.text_o(sha2_out),
+	.cmd_i({{1{1'b0}},{2{tvalid}}}),
+	.cmd_w_i(tvalid), 
+	.cmd_o()
+);
+
 /******************Sequential Logic*************************/
 /*
 	This module will wait on the tvalid signal
 	to indicate whether data is available to read
-	in from the input. 
-
+	in from the input.
 */
 
 always @(posedge clk)
@@ -52,35 +63,10 @@ begin
 		end
 	else begin
 			if (tvalid) begin
-				o_enc <= sha_out;
+				o_enc <= sha2_out;
 			end
 		end
 end
 		
 endmodule 
 						
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
