69f6bc35bd921273cf5466bbf73475fbd0e3bed9
MohamedElgammal <mohamed.elgammal@mail.utoronto.ca>
2023-01-05 11:36:50 -0500

    Build the atom cluster lookup after the packing is done

--- vpr/src/pack/pack.cpp
+++ vpr/src/pack/pack.cpp
@@ -266,6 +266,7 @@ bool try_pack(t_packer_opts* packer_opts,
     /*       Use the re-cluster API to edit it        */
     /******************* Start *************************/
     VTR_LOG("Start the iterative improvement process\n");
+    init_clb_atoms_lookup(helper_ctx.atoms_lookup);
     //iteratively_improve_packing(*packer_opts, clustering_data, 2);
     VTR_LOG("the iterative improvement process is done\n");
 
