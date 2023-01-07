398fb7c1772e520f6b27154896cebe6f7c746a83
Dani <17553473+poname@users.noreply.github.com>
2023-01-07 14:27:29 -0400

    Ext yosys (#43)
    * Myosys
    
    * libyosys
    
    * removed libyosys from src
    
    * almost there
    
    * fixed
    
    * golden result updated
    
    * f4pga-plugins UHDM_INSTALL_DIR
    
    * golden result update vexriscv
    
    * odin libyosys incude
    
    * odin_ii fixed
    
    * install libyosys.so

--- libs/EXTERNAL/libyosys/tests/sim/dffsr.v
+++
@@ -1,9 +0,0 @@
-module dffsr( input clk, d, clr, set, output reg q );
-	always @( posedge clk, posedge set, posedge clr)
-		if ( clr )
-			q <= 0;
-		else if (set)
-			q <= 1;
-		else
-			q <= d;
-endmodule
--- libs/EXTERNAL/libyosys/tests/simple/string_format.v
+++
@@ -1,7 +0,0 @@
-module string_format_top;
-	parameter STR = "something interesting";
-	initial begin
-		$display("A: %s", STR);
-		$display("B: %0s", STR);
-	end
-endmodule
