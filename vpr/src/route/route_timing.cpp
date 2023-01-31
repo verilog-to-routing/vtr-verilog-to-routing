fcb7a36986feae9bd2ff95b099885ab5c90eb71e
amin1377 <amin1377.mohaghegh@gmail.com>
2023-01-31 18:11:26 -0500

    move get_cached_router_lookahead before filling intra-cluster lookahead resources

--- vpr/src/route/route_timing.cpp
+++ vpr/src/route/route_timing.cpp
@@ -368,7 +368,17 @@ bool try_timing_driven_route_tmpl(const Netlist<>& net_list,
 
     route_budgets budgeting_inf(net_list, is_flat);
 
+    // This needs to be called before filling intra-cluster lookahead maps to ensure that the intra-cluster lookahead maps are initialized.
+    const RouterLookahead* router_lookahead = get_cached_router_lookahead(det_routing_arch,
+                                                                          router_opts.lookahead_type,
+                                                                          router_opts.write_router_lookahead,
+                                                                          router_opts.read_router_lookahead,
+                                                                          segment_inf,
+                                                                          is_flat);
+
     if (is_flat) {
+        // If is_flat is true, the router lookahead maps related to intra-cluster resources should be initialized since
+        // they haven't been initialized when the map related to global resources was initialized.
         auto cache_key = route_ctx.router_lookahead_cache_key_;
         std::unique_ptr<RouterLookahead> mut_router_lookahead(route_ctx.cached_router_lookahead_.release());
         VTR_ASSERT(mut_router_lookahead);
@@ -379,17 +389,15 @@ bool try_timing_driven_route_tmpl(const Netlist<>& net_list,
             mut_router_lookahead->compute_intra_tile();
         }
         route_ctx.cached_router_lookahead_.set(cache_key, std::move(mut_router_lookahead));
-    }
-
-    const RouterLookahead* router_lookahead = get_cached_router_lookahead(det_routing_arch,
-                                                                          router_opts.lookahead_type,
-                                                                          router_opts.write_router_lookahead,
-                                                                          router_opts.read_router_lookahead,
-                                                                          segment_inf,
-                                                                          is_flat);
-
-    if(is_flat && !router_opts.write_intra_cluster_router_lookahead.empty()) {
-        router_lookahead->write_intra_cluster(router_opts.write_intra_cluster_router_lookahead);
+        router_lookahead = get_cached_router_lookahead(det_routing_arch,
+                                                       router_opts.lookahead_type,
+                                                       router_opts.write_router_lookahead,
+                                                       router_opts.read_router_lookahead,
+                                                       segment_inf,
+                                                       is_flat);
+        if(!router_opts.write_intra_cluster_router_lookahead.empty()) {
+            router_lookahead->write_intra_cluster(router_opts.write_intra_cluster_router_lookahead);
+        }
     }
 
     VTR_ASSERT(router_lookahead != nullptr);
