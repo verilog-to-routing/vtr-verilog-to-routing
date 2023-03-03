5993b48e82cfa460faa1ece5d63ab728fa492b2f
Tao Li <tao.xiaoxia@gmail.com>
2023-03-03 09:41:26 -0800

    Revert un-wanted changes made by editor.

--- libs/libvtrutil/src/vtr_geometry.h
+++ libs/libvtrutil/src/vtr_geometry.h
@@ -71,13 +71,13 @@ class Point {
     T y() const;
 
     ///@brief == operator
-    friend bool operator==<>(Point<T> lhs, Point<T> rhs);
+    friend bool operator== <>(Point<T> lhs, Point<T> rhs);
 
     ///@brief != operator
-    friend bool operator!=<>(Point<T> lhs, Point<T> rhs);
+    friend bool operator!= <>(Point<T> lhs, Point<T> rhs);
 
     ///@brief < operator
-    friend bool operator<<>(Point<T> lhs, Point<T> rhs);
+    friend bool operator< <>(Point<T> lhs, Point<T> rhs);
 
   public: //Mutators
     ///@brief Set x and y values
@@ -193,10 +193,10 @@ class Rect {
     bool empty() const;
 
     ///@brief == operator
-    friend bool operator==<>(const Rect<T>& lhs, const Rect<T>& rhs);
+    friend bool operator== <>(const Rect<T>& lhs, const Rect<T>& rhs);
 
     ///@brief != operator
-    friend bool operator!=<>(const Rect<T>& lhs, const Rect<T>& rhs);
+    friend bool operator!= <>(const Rect<T>& lhs, const Rect<T>& rhs);
 
   public: //Mutators
     ///@brief set xmin to a point
@@ -323,10 +323,10 @@ class RectUnion {
      *
      * Note: does not check whether the representations they are equivalent
      */
-    friend bool operator==<>(const RectUnion<T>& lhs, const RectUnion<T>& rhs);
+    friend bool operator== <>(const RectUnion<T>& lhs, const RectUnion<T>& rhs);
 
     ///@brief != operator
-    friend bool operator!=<>(const RectUnion<T>& lhs, const RectUnion<T>& rhs);
+    friend bool operator!= <>(const RectUnion<T>& lhs, const RectUnion<T>& rhs);
 
   private:
     // Note that a union of rectangles may have holes and may not be contiguous
