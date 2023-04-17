08ee834dedc1a82ea8a67109ec44099de44c7f4a
amin1377 <amin1377.mohaghegh@gmail.com>
2023-04-17 14:32:30 -0400

    revert the changes in device_grid.num_instances(): if instance_counts_ is empty, return 0

--- libs/libarchfpga/src/device_grid.cpp
+++ libs/libarchfpga/src/device_grid.cpp
@@ -14,7 +14,9 @@ DeviceGrid::DeviceGrid(std::string grid_name, vtr::NdMatrix<t_grid_tile, 3> grid
 size_t DeviceGrid::num_instances(t_physical_tile_type_ptr type, int layer_num) const {
     size_t count = 0;
     //instance_counts_ is not initialized
-    VTR_ASSERT(!instance_counts_.empty());
+    if(instance_counts_.empty()) {
+        return 0;
+    }
 
     int num_layers = (int)grid_.dim_size(0);
 
