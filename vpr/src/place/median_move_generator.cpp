81fcfd12ddb78a5208e297685d26ed30e2b43c8f
saaramahmoudi <sin29mim@gmail.com>
2023-05-11 18:17:59 -0400

    test commit - fix a comment typo

--- vpr/src/place/median_move_generator.cpp
+++ vpr/src/place/median_move_generator.cpp
@@ -47,7 +47,7 @@ e_create_move MedianMoveGenerator::propose_move(t_pl_blocks_to_be_moved& blocks_
         ClusterNetId net_id = cluster_ctx.clb_nlist.pin_net(pin_id);
         if (cluster_ctx.clb_nlist.net_is_ignored(net_id))
             continue;
-        /* To speedup the calculation, we found it is useful to ignore high fanout nets.
+        /* To speed up the calculation, we found it is useful to ignore high fanout nets.
          * Especially that in most cases, these high fanout nets are scattered in many locations of
          * the device and don't guide to a specific location. We also assuered these assumpitions experimentally.
          */
