0f7cf5120137aedd96e3e1a16c41b40a86cbece5
soheilshahrouz <soheilqs@gmail.com>
2023-08-06 13:58:45 -0400

    Add murmur3_32 to TurnModelRouting
    std::hash<vtr::StrongID>() was implemented as the identity function. The generated hash values were very small, causing west-first algorithm to almost choose the right direction when down/up was also available.

--- vpr/src/noc/turn_model_routing.h
+++ vpr/src/noc/turn_model_routing.h
@@ -33,6 +33,10 @@ class TurnModelRouting : public NocRouting {
     size_t get_hash_value(NocRouterId src_router_id, NocRouterId dst_router_id, NocRouterId curr_router_id, NocTrafficFlowId traffic_flow_id);
 
   private:
+    inline uint32_t murmur_32_scramble(uint32_t k);
+
+    uint32_t murmur3_32(const std::vector<uint32_t>& key, uint32_t seed);
+
     template<class T>
     inline void hash_combine(std::size_t& seed, T const& v);
 
@@ -53,6 +57,9 @@ class TurnModelRouting : public NocRouting {
     // get_legal_directions() return a reference to this vector to avoid allocating a new vector
     // each time it is called
     std::vector<TurnModelRouting::Direction> returned_legal_direction{4};
+
+  private:
+    std::vector<uint32_t> inputs_to_murmur3_hahser{4};
 
 };
 
