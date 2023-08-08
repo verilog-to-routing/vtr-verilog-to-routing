81155d5e6c7ba7f189178e43a91af693e8979dea
saaramahmoudi <sin29mim@gmail.com>
2023-08-08 11:11:44 -0400

    removed the update that will change rr_graph for 2d cases

--- vpr/src/route/build_switchblocks.cpp
+++ vpr/src/route/build_switchblocks.cpp
@@ -331,8 +331,9 @@ t_sb_connection_map* alloc_and_load_switchblock_permutations(const t_chan_detail
                     continue;
                 }
                 /* now we iterate over all the potential side1->side2 connections */
-                for (e_side from_side : TOTAL_SIDES) {
-                    for (e_side to_side : TOTAL_SIDES) {
+                //TODO: SM: this must change to TOTAL_SIDES INSTEAD OF SIDES
+                for (e_side from_side : SIDES) {
+                    for (e_side to_side : SIDES) {
                         /* Fill appropriate entry of the sb_conns map with vector specifying the wires
                          * the current wire will connect to */
                         compute_wire_connections(x_coord, y_coord, from_side, to_side,
