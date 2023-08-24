47c788075331b72a303767fd457f4186edd85a13
umariqbal-rs <umar.iqbal@rapidsilicon.com>
2023-08-24 12:19:40 +0500

    updating CMAKE in libencrypt to take libpugixml as linking library

--- libs/libencrypt/main.cpp
+++ libs/libencrypt/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <file_path> \n";
+        std::cerr << "Usage: " << argv[0] << " <publicKeyFile> <file_path> \n";
         return -1;
     }
 
