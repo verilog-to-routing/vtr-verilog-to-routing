65df4af3cc81fe62b89e14ea15642eb80fd01d6e
saaramahmoudi <sin29mim@gmail.com>
2023-08-29 13:46:41 -0400

    Revert "revert a test change"
    This reverts commit 26b7338347f0a084480cebf078cd32af787a463c.

--- libs/librrgraph/src/io/gen/rr_graph_uxsdcxx.h
+++ libs/librrgraph/src/io/gen/rr_graph_uxsdcxx.h
@@ -4002,6 +4002,7 @@ inline void write_node(T &in, std::ostream &os, Context &context){
 		os << "<loc";
 		os << " layer=\"" << in.get_node_loc_layer(child_context) << "\"";
 		os << " ptc=\"" << in.get_node_loc_ptc(child_context) << "\"";
+		os << "meta=\"" << "more ptcs" <<"\"";
 		if((bool)in.get_node_loc_side(child_context))
 			os << " side=\"" << lookup_loc_side[(int)in.get_node_loc_side(child_context)] << "\"";
 		if((bool)in.get_node_loc_ptc_twist(child_context))
