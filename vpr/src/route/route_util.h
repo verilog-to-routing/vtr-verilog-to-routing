2a636c79587999623573c96429119a438ccd671a
alanbu1 <alanbu1230@gmail.com>
2023-09-03 01:27:38 -0400

    Added a comment to calculate_routing_usage function in route_util.h, explaining what the function does.

--- vpr/src/route/route_util.h
+++ vpr/src/route/route_util.h
@@ -5,6 +5,13 @@
 #include "draw_global.h"
 
 vtr::Matrix<float> calculate_routing_avail(t_rr_type rr_type);
+
+/**
+ * @brief Calculates routing usage over entire grid
+ * Collects all in-use nodes and records number of used resources
+ * in each x/y channel. Takes into consideration visible layers
+ * for multi-layered architectures.
+ */
 vtr::Matrix<float> calculate_routing_usage(t_rr_type rr_type, bool is_flat);
 float routing_util(float used, float avail);
 
