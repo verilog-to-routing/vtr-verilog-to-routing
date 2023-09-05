1f95b8f174f26ae9918f1bdd62a094aa40a5a56e
alanbu1 <123594608+alanbu1@users.noreply.github.com>
2023-09-05 00:54:52 -0400

    Trying changes on new branch
    Testing to see if tests pass on this branch.

--- vpr/src/route/route_util.h
+++ vpr/src/route/route_util.h
@@ -3,6 +3,14 @@
 #include "vpr_types.h"
 
 vtr::Matrix<float> calculate_routing_avail(t_rr_type rr_type);
+
+/**
+* @brief Calculates routing usage over entire grid
+*
+* Collects all in-use nodes and records number of used resources
+* in each x/y channel. Takes into consideration visible layers for
+* multi-layered architectures. 
+*/
 vtr::Matrix<float> calculate_routing_usage(t_rr_type rr_type, bool is_flat);
 float routing_util(float used, float avail);
 
