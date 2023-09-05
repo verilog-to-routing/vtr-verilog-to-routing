72dc40a1f26450d5c8ff76e5d02e1b22b981cb61
alanbu1 <123594608+alanbu1@users.noreply.github.com>
2023-09-05 00:57:21 -0400

    Include statements
    Including so program can run

--- vpr/src/route/route_util.h
+++ vpr/src/route/route_util.h
@@ -1,6 +1,8 @@
 #ifndef VPR_ROUTE_UTIL_H
 #define VPR_ROUTE_UTIL_H
 #include "vpr_types.h"
+#include "draw_types.h"
+#include "draw_global.h"
 
 vtr::Matrix<float> calculate_routing_avail(t_rr_type rr_type);
 
