b5839fcaafffaceee0a31925f6d338292441fc95
amin1377 <amin1377.mohaghegh@gmail.com>
2023-10-11 14:48:23 -0400

    choose from.layer if centroid layer is not valid

--- vpr/src/place/centroid_move_generator.cpp
+++ vpr/src/place/centroid_move_generator.cpp
@@ -39,7 +39,7 @@ e_create_move CentroidMoveGenerator::propose_move(t_pl_blocks_to_be_moved& block
     /* Calculate the centroid location*/
     calculate_centroid_loc(b_from, false, centroid, nullptr);
 
-    to.layer = centroid.layer;
+    to.layer = (centroid.layer < 0) ? from.layer : centroid.layer;
     /* Find a location near the weighted centroid_loc */
     if (!find_to_loc_centroid(cluster_from_type, from, centroid, range_limiters, to, b_from)) {
         return e_create_move::ABORT;
