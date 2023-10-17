0d1b0396591654d484f1c00c960fef9dc1bf6ce1
amin1377 <amin1377.mohaghegh@gmail.com>
2023-10-17 15:43:13 -0400

    add ERROR_TOL to placer globals

--- vpr/src/place/placer_globals.h
+++ vpr/src/place/placer_globals.h
@@ -7,4 +7,8 @@
 #pragma once
 #include "placer_context.h"
 
+/* This defines the error tolerance for floating points variables used in *
+ * cost computation. 0.01 means that there is a 1% error tolerance.       */
+#define ERROR_TOL .01
+
 extern PlacerContext g_placer_ctx;
