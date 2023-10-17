c0af5fe3224032e7df0ce468869e9bde6f16e050
amin1377 <amin1377.mohaghegh@gmail.com>
2023-10-17 11:08:45 -0400

    alloc_and_load_for_fast_cost_update & free_fast_cost_update & wirelength_crossing_count & get_net_bounding_box_cost from place_util

--- vpr/src/place/place_util.h
+++ vpr/src/place/place_util.h
@@ -268,10 +268,4 @@ bool macro_can_be_placed(t_pl_macro pl_macro, t_pl_loc head_pos, bool check_all_
 // Array in place_ctx.
 t_pl_atom_loc get_atom_loc (AtomBlockId atom);
 
-void alloc_and_load_for_fast_cost_update(float place_cost_exp);
-
-double wirelength_crossing_count(size_t fanout);
-
-double get_net_bounding_box_cost(ClusterNetId net_id, t_bb* bb_ptr);
-
 #endif
