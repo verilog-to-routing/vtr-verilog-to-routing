fa7c5a94c186e703af4baedff637275172763199
amin1377 <amin1377.mohaghegh@gmail.com>
2023-11-27 13:28:32 -0500

    define the header for compressed router lookahead

--- vpr/src/route/router_lookahead_compressed_map.cpp
+++ vpr/src/route/router_lookahead_compressed_map.cpp
@@ -1,3 +1,34 @@
 //
 // Created by amin on 11/27/23.
 //
+
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <ctime>
+#include "router_lookahead_compressed_map.h"
+#include "connection_router_interface.h"
+#include "vpr_types.h"
+#include "vpr_error.h"
+#include "vpr_utils.h"
+#include "globals.h"
+#include "vtr_math.h"
+#include "vtr_log.h"
+#include "vtr_assert.h"
+#include "vtr_time.h"
+#include "vtr_geometry.h"
+#include "router_lookahead_map.h"
+#include "router_lookahead_map_utils.h"
+#include "rr_graph2.h"
+#include "rr_graph.h"
+#include "route_common.h"
+
+
+
+
+
+
+/******** Interface class member function definitions ********/
+CompressedMapLookahead::CompressedMapLookahead(const t_det_routing_arch& det_routing_arch, bool is_flat)
+    : det_routing_arch_(det_routing_arch)
+    , is_flat_(is_flat) {}
--- vpr/src/route/router_lookahead_compressed_map.h
+++ vpr/src/route/router_lookahead_compressed_map.h
@@ -5,4 +5,54 @@
 #ifndef VTR_ROUTER_LOOKAHEAD_SPARSE_MAP_H
 #define VTR_ROUTER_LOOKAHEAD_SPARSE_MAP_H
 
+#pragma once
+
+#include <string>
+#include <limits>
+#include "vtr_ndmatrix.h"
+#include "router_lookahead.h"
+#include "router_lookahead_map_utils.h"
+
+class CompressedMapLookahead : public RouterLookahead {
+  public:
+    explicit CompressedMapLookahead(const t_det_routing_arch& det_routing_arch, bool is_flat);
+
+  private:
+    //Look-up table from SOURCE/OPIN to CHANX/CHANY of various types
+    util::t_src_opin_delays src_opin_delays;
+    // Lookup table to store the minimum cost for each dx and dy
+    vtr::NdMatrix<util::Cost_Entry, 3> distance_based_min_cost; // [layer_num][dx][dy] -> cost
+
+    const t_det_routing_arch& det_routing_arch_;
+    bool is_flat_;
+
+  protected:
+    float get_expected_cost(RRNodeId node, RRNodeId target_node, const t_conn_cost_params& params, float R_upstream) const override;
+
+    std::pair<float, float> get_expected_delay_and_cong(RRNodeId from_node,
+                                                        RRNodeId to_node,
+                                                        const t_conn_cost_params& params,
+                                                        float R_upstream) const override;
+
+    void compute(const std::vector<t_segment_inf>& segment_inf) override;
+
+    void compute_intra_tile() override {
+        VPR_THROW(VPR_ERROR_ROUTE, "CompressedMapLookahead::compute_intra_time unimplemented");
+    }
+
+    void read(const std::string& /*file*/) override;
+
+    void read_intra_cluster(const std::string& /*file*/) {
+        VPR_THROW(VPR_ERROR_ROUTE, "CompressedMapLookahead::read_intra_cluster unimplemented");
+    }
+
+    void write(const std::string& /*file*/) const override {
+        VPR_THROW(VPR_ERROR_ROUTE, "CompressedMapLookahead::write unimplemented");
+    }
+
+    void write_intra_cluster(const std::string& /*file*/) const override {
+        VPR_THROW(VPR_ERROR_ROUTE, "CompressedMapLookahead::write_intra_cluster unimplemented");
+    }
+};
+
 #endif //VTR_ROUTER_LOOKAHEAD_SPARSE_MAP_H
