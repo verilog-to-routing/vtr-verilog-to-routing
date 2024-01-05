80752db7ce548cc44d02860d01cac396660d1509
amin1377 <amin1377.mohaghegh@gmail.com>
2024-01-05 16:50:27 -0500

    vpr: map_router_lookahead: when getting the min opin cost, only consider channels not direct connections

--- vpr/src/route/router_lookahead_map.cpp
+++ vpr/src/route/router_lookahead_map.cpp
@@ -1513,14 +1513,43 @@ static void min_opin_distance_cost_map(const util::t_src_opin_delays& src_opin_d
             for (int to_layer_num = 0; to_layer_num < num_layers; to_layer_num++) {
                 for (int dx = 0; dx < width; dx++) {
                     for (int dy = 0; dy < height; dy++) {
-                        float expected_delay_cost = std::numeric_limits<float>::infinity();
-                        float expected_cong_cost = std::numeric_limits<float>::infinity();
                         util::Cost_Entry min_cost(std::numeric_limits<float>::infinity(), std::numeric_limits<float>::infinity());
                         for (const auto& tile_opin_map : src_opin_delays[from_layer_num][tile_type_idx]) {
-                            std::tie(expected_delay_cost, expected_cong_cost) = get_cost_from_src_opin(tile_opin_map,
-                                                                                                       dx,
-                                                                                                       dy,
-                                                                                                       to_layer_num);
+                            float expected_delay_cost = std::numeric_limits<float>::infinity();
+                            float expected_cong_cost = std::numeric_limits<float>::infinity();
+
+                            for (const auto& layer_src_opin_delay_map : tile_opin_map) {
+                                float layer_expected_delay_cost = std::numeric_limits<float>::infinity();
+                                float layer_expected_cong_cost = std::numeric_limits<float>::infinity();
+                                if (layer_src_opin_delay_map.empty()) {
+                                    layer_expected_delay_cost = std::numeric_limits<float>::max() / 1e12;
+                                    layer_expected_cong_cost = std::numeric_limits<float>::max() / 1e12;
+                                } else {
+                                    for (const auto& kv : layer_src_opin_delay_map) {
+                                        const util::t_reachable_wire_inf& reachable_wire_inf = kv.second;
+                                        if (reachable_wire_inf.wire_rr_type == SINK) {
+                                            continue;
+                                        }
+                                        Cost_Entry wire_cost_entry;
+
+                                        wire_cost_entry = get_wire_cost_entry(reachable_wire_inf.wire_rr_type,
+                                                                              reachable_wire_inf.wire_seg_index,
+                                                                              reachable_wire_inf.layer_number,
+                                                                              dx,
+                                                                              dy,
+                                                                              to_layer_num);
+
+                                        float this_delay_cost = reachable_wire_inf.delay + wire_cost_entry.delay;
+                                        float this_cong_cost = reachable_wire_inf.congestion + wire_cost_entry.congestion;
+
+                                        layer_expected_delay_cost = std::min(layer_expected_delay_cost, this_delay_cost);
+                                        layer_expected_cong_cost = std::min(layer_expected_cong_cost, this_cong_cost);
+                                    }
+                                }
+                                expected_delay_cost = std::min(expected_delay_cost, layer_expected_delay_cost);
+                                expected_cong_cost = std::min(expected_cong_cost, layer_expected_cong_cost);
+                            }
+
                             if (expected_delay_cost < min_cost.delay) {
                                 min_cost.delay = expected_delay_cost;
                                 min_cost.congestion = expected_cong_cost;
