b4706e9e195837b7a7f0cfd507d9e4182960d74f
amin1377 <amin1377.mohaghegh@gmail.com>
2024-02-07 17:40:18 -0500

    vpr: to get the minimum delay among inter-layer connections, iterate over rr graph edges

--- vpr/src/util/vpr_utils.h
+++ vpr/src/util/vpr_utils.h
@@ -319,8 +319,6 @@ void add_pb_child_to_list(std::list<const t_pb*>& pb_list, const t_pb* parent_pb
  * @param wire_to_ipin_arch_sw_id
  * @return
  */
-float get_min_cross_layer_delay(const std::vector<t_arch_switch_inf>& arch_switch_inf,
-                                const std::vector<t_segment_inf>& segment_inf,
-                                const int wire_to_ipin_arch_sw_id);
+float get_min_cross_layer_delay();
 
 #endif
