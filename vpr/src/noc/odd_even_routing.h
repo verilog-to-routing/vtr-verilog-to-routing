a03716096293cb12b4597b3eb3e997ebfa0ec8a3
soheilshahrouz <soheilqs@gmail.com>
2024-02-07 17:46:26 -0500

    Add odd even routing for NoC

---
+++ vpr/src/noc/odd_even_routing.h
@@ -0,0 +1,27 @@
+#ifndef VTR_ODD_EVEN_ROUTING_H
+#define VTR_ODD_EVEN_ROUTING_H
+
+#include "turn_model_routing.h"
+
+class OddEvenRouting : public TurnModelRouting{
+  public:
+    ~OddEvenRouting() override;
+
+  private:
+    const std::vector<TurnModelRouting::Direction>& get_legal_directions(NocRouterId src_router_id,
+                                                                         NocRouterId curr_router_id,
+                                                                         NocRouterId dst_router_id,
+                                                                         const NocStorage& noc_model) override;
+
+    TurnModelRouting::Direction select_next_direction(const std::vector<TurnModelRouting::Direction>& legal_directions,
+                                                      NocRouterId src_router_id,
+                                                      NocRouterId dst_router_id,
+                                                      NocRouterId curr_router_id,
+                                                      NocTrafficFlowId traffic_flow_id,
+                                                      const NocStorage& noc_model) override;
+
+    static inline bool is_odd(int number);
+    static inline bool is_even(int number);
+};
+
+#endif //VTR_ODD_EVEN_ROUTING_H
