67a898903547f02ab04c0cdaa7a3123cf464148e
amin1377 <amin1377.mohaghegh@gmail.com>
2024-02-23 09:55:42 -0500

    make format

--- vpr/src/place/median_move_generator.cpp
+++ vpr/src/place/median_move_generator.cpp
@@ -315,7 +315,7 @@ static bool get_bb_incrementally(ClusterNetId net_id,
 
     xnew = std::max(std::min<int>(xnew, device_ctx.grid.width() - 2), 1);  //-2 for no perim channels
     ynew = std::max(std::min<int>(ynew, device_ctx.grid.height() - 2), 1); //-2 for no perim channels
-    layer_new = std::max(std::min<int>(layer_new, device_ctx.grid.get_num_layers() -1 ), 0);
+    layer_new = std::max(std::min<int>(layer_new, device_ctx.grid.get_num_layers() - 1), 0);
 
     xold = std::max(std::min<int>(xold, device_ctx.grid.width() - 2), 1);  //-2 for no perim channels
     yold = std::max(std::min<int>(yold, device_ctx.grid.height() - 2), 1); //-2 for no perim channels
