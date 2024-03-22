a3a6ee5e2ace3b0197bb2abfdcdaa3f6dc75273b
AlexandreSinger <alex.singer@mail.utoronto.ca>
2024-03-21 22:20:12 -0400

    [Warnings] Removed Unused Variables When Capnproto is Disabled
    A few variables were left unused when CAPNPROTO was disabled which were
    giving warnings on the CI. Commented these unused variables out.

--- vpr/src/route/router_lookahead_extended_map.cpp
+++ vpr/src/route/router_lookahead_extended_map.cpp
@@ -606,10 +606,10 @@ float ExtendedMapLookahead::get_expected_cost(
 
 #ifndef VTR_ENABLE_CAPNPROTO
 
-void ExtendedMapLookahead::read(const std::string& file) {
+void ExtendedMapLookahead::read(const std::string& /*file*/) {
     VPR_THROW(VPR_ERROR_ROUTE, "MapLookahead::read not implemented");
 }
-void ExtendedMapLookahead::write(const std::string& file) const {
+void ExtendedMapLookahead::write(const std::string& /*file*/) const {
     VPR_THROW(VPR_ERROR_ROUTE, "MapLookahead::write not implemented");
 }
 
