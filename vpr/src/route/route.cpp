5276017f35ae1e26c43ce8009764b32f8496b8dd
amin1377 <amin1377.mohaghegh@gmail.com>
2024-04-02 10:35:57 -0400

    vpr: fix a type. set max pres fac

--- vpr/src/route/route.cpp
+++ vpr/src/route/route.cpp
@@ -442,7 +442,7 @@ bool route(const Netlist<>& net_list,
             pres_fac *= router_opts.pres_fac_mult;
 
             /* Set the maximum pres_fac to the value passed by the command line argument */
-            pres_fac = update_draw_pres_fac(std::min(pres_fac, router_opts.pres_fac_mult));
+            pres_fac = update_draw_pres_fac(std::min(pres_fac, router_opts.max_pres_fac));
 
             // Increase short path criticality if it's having a hard time resolving hold violations due to congestion
             if (budgeting_inf.if_set()) {
