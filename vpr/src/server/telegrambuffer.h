0c42d0b401db72b7e7f02f54100d289372d26526
Oleksandr <apivovarov@quicklogic.com>
2024-04-02 15:21:20 +0300

    add doc to telegrambuffer.h

--- vpr/src/server/telegrambuffer.h
+++ vpr/src/server/telegrambuffer.h
@@ -25,15 +25,46 @@ public:
     explicit TelegramBuffer(std::size_t sizeHint): m_rawBuffer(sizeHint) {}
     ~TelegramBuffer()=default;
 
+    /**
+     * @brief Check if internal byte buffer is empty.
+     * 
+     * @return true if the internal byte buffer is empty, false otherwise.
+     */
     bool empty() { return m_rawBuffer.empty(); }
 
+    /**
+     * @brief Clear internal byte buffer.
+     */
     void clear() { m_rawBuffer.clear(); }
 
+    /**
+     * @brief Append bytes to the internal byte buffer.
+     * 
+     * @param data The byte array whose contents will be appended to internal byte buffer.
+     */
     void append(const ByteArray&);
+
+    /**
+     * @brief Extracts well-formed telegram frames from the internal byte buffer.
+     * 
+     * @param frames A reference to a vector where the extracted telegram frames will be stored.
+     */
     void takeTelegramFrames(std::vector<TelegramFramePtr>&);
+
+    /**
+     * @brief Extracts well-formed telegram frames from the internal byte buffer.
+     * 
+     * @return std::vector<TelegramFramePtr> A vector containing pointers to the extracted telegram frames.
+     */
     std::vector<TelegramFramePtr> takeTelegramFrames();
+
     void takeErrors(std::vector<std::string>&);
 
+    /**
+     * @brief Retrieves a constant reference to the internal byte buffer.
+     * 
+     * @return A constant reference to the internal byte buffer.
+     */
     const ByteArray& data() const { return m_rawBuffer; }
 
 private:
