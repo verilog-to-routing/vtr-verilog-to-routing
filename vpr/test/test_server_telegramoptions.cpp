f9ffb52b5f13df5f0d225ecfb546f0c198e07cce
Oleksandr <apivovarov@quicklogic.com>
2024-04-02 18:38:48 +0300

    add test_server_telegramoptions_get_wrong_keys TEST_CASE

--- vpr/test/test_server_telegramoptions.cpp
+++ vpr/test/test_server_telegramoptions.cpp
@@ -16,4 +16,15 @@ TEST_CASE("test_server_telegramoptions", "[vpr]") {
     REQUIRE(options.getBool("is_flat_routing", true) == false);
 }
 
+TEST_CASE("test_server_telegramoptions_get_wrong_keys", "[vpr]") {
+    server::TelegramOptions options{"int:path_num:11;string:path_type:debug;int:details_level:3;bool:is_flat_routing:0", {"_path_num", "_path_type", "_details_level", "_is_flat_routing"}};
+
+    REQUIRE(!options.errorsStr().empty());
+
+    REQUIRE(options.getString("_path_type") == "");
+    REQUIRE(options.getInt("_path_num", -1) == -1);
+    REQUIRE(options.getInt("_details_level", -1) == -1);
+    REQUIRE(options.getBool("_is_flat_routing", true) == true);
+}
+
 } // namespace
