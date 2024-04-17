edb8fd4b182f508402c5beaa8b3fb938f6c0a9e3
Oleksandr <apivovarov@quicklogic.com>
2024-04-17 20:23:54 +0300

    add documentation for tryCompress and tryDecompress methods in zlibutils

--- vpr/src/server/zlibutils.h
+++ vpr/src/server/zlibutils.h
@@ -4,7 +4,32 @@
 #include <string>
 #include <optional>
 
+/**
+* @brief Compresses the input sequence using zlib.
+*
+* This function takes a string representing the decompressed data as input
+* and compresses it using zlib. If compression is successful, the compressed
+* data is returned as an optional string. If compression fails, an empty optional
+* is returned.
+*
+* @param decompressed The input string representing the decompressed data.
+* @return An optional string containing the compressed data if compression is successful,
+*         or an empty optional if compression fails.
+*/
 std::optional<std::string> tryCompress(const std::string& decompressed);
+
+/**
+* @brief Decompresses the compressed sequence using zlib.
+*
+* This function takes a string representing the compressed data as input
+* and decompresses it using zlib. If decompression is successful, the decompressed
+* data is returned as an optional string. If decompression fails, an empty optional
+* is returned.
+*
+* @param compressed The input string representing the compressed data.
+* @return An optional string containing the decompressed data if decompression is successful,
+*         or an empty optional if decompression fails.
+*/
 std::optional<std::string> tryDecompress(const std::string& compressed);
 
 #endif /* ZLIBUTILS_H */
