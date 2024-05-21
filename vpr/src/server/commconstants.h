6e0a0dc23231a702da44b2c94f12a8d7dec5603c
Oleksandr <apivovarov@quicklogic.com>
2024-05-21 21:35:03 +0300

    remove commconstants.cpp, use inline for declaring const std::string in commconstants.h

--- vpr/src/server/commconstants.h
+++ vpr/src/server/commconstants.h
@@ -7,26 +7,26 @@
 
 namespace comm {
 
-extern const std::string KEY_JOB_ID;
-extern const std::string KEY_CMD;
-extern const std::string KEY_OPTIONS;
-extern const std::string KEY_DATA;
-extern const std::string KEY_STATUS;
-extern const std::string ECHO_DATA;
+inline const std::string KEY_JOB_ID{"JOB_ID"};
+inline const std::string KEY_CMD{"CMD"};
+inline const std::string KEY_OPTIONS{"OPTIONS"};
+inline const std::string KEY_DATA{"DATA"};
+inline const std::string KEY_STATUS{"STATUS"};
+inline const std::string ECHO_DATA{"ECHO"};
 
 const unsigned char ZLIB_COMPRESSOR_ID = 'z';
 const unsigned char NONE_COMPRESSOR_ID = '\x0';
 
-extern const std::string OPTION_PATH_NUM;
-extern const std::string OPTION_PATH_TYPE;
-extern const std::string OPTION_DETAILS_LEVEL;
-extern const std::string OPTION_IS_FLAT_ROUTING;
-extern const std::string OPTION_PATH_ELEMENTS;
-extern const std::string OPTION_HIGHLIGHT_MODE;
-extern const std::string OPTION_DRAW_PATH_CONTOUR;
+inline const std::string OPTION_PATH_NUM{"path_num"};
+inline const std::string OPTION_PATH_TYPE{"path_type"};
+inline const std::string OPTION_DETAILS_LEVEL{"details_level"};
+inline const std::string OPTION_IS_FLAT_ROUTING{"is_flat_routing"};
+inline const std::string OPTION_PATH_ELEMENTS{"path_elements"};
+inline const std::string OPTION_HIGHLIGHT_MODE{"high_light_mode"};
+inline const std::string OPTION_DRAW_PATH_CONTOUR{"draw_path_contour"};;
 
-extern const std::string KEY_SETUP_PATH_LIST;
-extern const std::string KEY_HOLD_PATH_LIST;
+inline const std::string KEY_SETUP_PATH_LIST{"setup"};
+inline const std::string KEY_HOLD_PATH_LIST{"hold"};
 
 enum CMD {
     CMD_GET_PATH_LIST_ID=0,
