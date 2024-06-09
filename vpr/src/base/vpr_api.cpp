94f03ba4548f34fbbe484f28731642b1fb17e5ef
Wang Yuanqi <101231450+Wang-Yuanqi-source@users.noreply.github.com>
2024-06-09 16:12:01 +0800

    Update vpr_api.cpp

--- vpr/src/base/vpr_api.cpp
+++ vpr/src/base/vpr_api.cpp
@@ -500,6 +500,9 @@ void vpr_create_device_grid(const t_vpr_setup& vpr_setup, const t_arch& Arch) {
     //Build the device
     float target_device_utilization = vpr_setup.PackerOpts.target_device_utilization;
     device_ctx.grid = create_device_grid(vpr_setup.device_layout, Arch.grid_layouts, num_type_instances, target_device_utilization);
+    if (Arch.is_vib_arch) {
+        device_ctx.vib_grid = create_vib_device_grid(vpr_setup.device_layout, Arch.vib_grid_layouts);
+    }
 
     VTR_ASSERT_MSG(device_ctx.grid.get_num_layers() <= MAX_NUM_LAYERS,
                    "Number of layers should be less than MAX_NUM_LAYERS. "
