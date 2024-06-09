0d549ece641743d52ff883f1ccf4bd8ae7b010f4
Wang Yuanqi <101231450+Wang-Yuanqi-source@users.noreply.github.com>
2024-06-09 16:16:17 +0800

    Update tileable_rr_graph_node_builder.cpp

--- vpr/src/tileable_rr_graph/tileable_rr_graph_node_builder.cpp
+++ vpr/src/tileable_rr_graph/tileable_rr_graph_node_builder.cpp
@@ -80,6 +80,42 @@ static size_t estimate_num_grid_rr_nodes_by_type(const DeviceGrid& grids,
     return num_grid_rr_nodes;
 }
 
+static size_t estimate_num_medium_rr_nodes(const DeviceGrid& grids,
+                                           const vtr::NdMatrix<const t_vib_inf*, 3>& vib_grid,
+                                           const size_t& layer) {
+    size_t num_grid_rr_nodes = 0;
+
+    VTR_ASSERT(grids.width() == vib_grid.dim_size(1) && grids.height() == vib_grid.dim_size(2));
+    for (size_t ix = 0; ix < grids.width(); ++ix) {
+        for (size_t iy = 0; iy < grids.height(); ++iy) {
+            
+            const t_vib_inf* vib = vib_grid[layer][ix][iy];
+            if (vib == nullptr) {
+                VTR_LOGF_ERROR(__FILE__, __LINE__,
+                               "VIB at (%d, %d) is EMPTY!\n", ix, iy);
+                exit(1);
+                continue;
+            }
+
+            size_t count = 0;
+            for (size_t i_first_stage = 0; i_first_stage < vib->first_stages.size(); i_first_stage++) {
+                auto first_stage = vib->first_stages[i_first_stage];
+                if (first_stage.froms.size() == 0) {
+                    VTR_LOGF_ERROR(__FILE__, __LINE__,
+                                   "VIB first stage '%s' at (%d, %d) has no from!\n", first_stage.mux_name, ix, iy);
+                    exit(1);
+                }
+                count++;
+            }
+
+            VTR_ASSERT(count == vib->first_stages.size());
+            num_grid_rr_nodes += count;
+        }
+    }
+
+    return num_grid_rr_nodes;
+}
+
 /************************************************************************
  * For X-direction Channel: CHANX
  * We pair each x-direction routing channel to the grid below it
@@ -302,13 +338,15 @@ static size_t estimate_num_chany_rr_nodes(const DeviceGrid& grids,
  * Estimate the number of nodes by each type in a routing resource graph
  ***********************************************************************/
 static std::vector<size_t> estimate_num_rr_nodes(const DeviceGrid& grids,
+                                                 const vtr::NdMatrix<const t_vib_inf*, 3>& vib_grid,
                                                  const size_t& layer,
                                                  const vtr::Point<size_t>& chan_width,
                                                  const std::vector<t_segment_inf>& segment_inf_x,
                                                  const std::vector<t_segment_inf>& segment_inf_y,
                                                  const DeviceGridAnnotation& device_grid_annotation,
                                                  const bool& shrink_boundary,
-                                                 const bool& through_channel) {
+                                                 const bool& through_channel,
+                                                 const bool& is_vib_arch) {
     /* Reset the OPIN, IPIN, SOURCE, SINK counter to be zero */
     std::vector<size_t> num_rr_nodes_per_type(NUM_RR_TYPES, 0);
 
@@ -319,7 +357,10 @@ static std::vector<size_t> estimate_num_rr_nodes(const DeviceGrid& grids,
     num_rr_nodes_per_type[IPIN] = estimate_num_grid_rr_nodes_by_type(grids, layer, IPIN);
     num_rr_nodes_per_type[SOURCE] = estimate_num_grid_rr_nodes_by_type(grids, layer, SOURCE);
     num_rr_nodes_per_type[SINK] = estimate_num_grid_rr_nodes_by_type(grids, layer, SINK);
-
+    if (is_vib_arch)
+        num_rr_nodes_per_type[MEDIUM] = estimate_num_medium_rr_nodes(grids, vib_grid, layer);
+    else
+        num_rr_nodes_per_type[MEDIUM] = 0;
     /**
      * 2. Assign the segments for each routing channel,
      *    To be specific, for each routing track, we assign a routing segment.
@@ -361,23 +402,27 @@ static std::vector<size_t> estimate_num_rr_nodes(const DeviceGrid& grids,
 void alloc_tileable_rr_graph_nodes(RRGraphBuilder& rr_graph_builder,
                                    vtr::vector<RRNodeId, RRSwitchId>& rr_node_driver_switches,
                                    const DeviceGrid& grids,
+                                   const vtr::NdMatrix<const t_vib_inf*, 3>& vib_grid,
                                    const size_t& layer,
                                    const vtr::Point<size_t>& chan_width,
                                    const std::vector<t_segment_inf>& segment_inf_x,
                                    const std::vector<t_segment_inf>& segment_inf_y,
                                    const DeviceGridAnnotation& device_grid_annotation,
                                    const bool& shrink_boundary,
-                                   const bool& through_channel) {
+                                   const bool& through_channel,
+                                   const bool& is_vib_arch) {
     VTR_ASSERT(0 == rr_graph_builder.rr_nodes().size());
 
     std::vector<size_t> num_rr_nodes_per_type = estimate_num_rr_nodes(grids,
+                                                                      vib_grid,
                                                                       layer,
                                                                       chan_width,
                                                                       segment_inf_x,
                                                                       segment_inf_y,
                                                                       device_grid_annotation,
                                                                       shrink_boundary,
-                                                                      through_channel);
+                                                                      through_channel,
+                                                                      is_vib_arch);
 
     /* Reserve the number of node to be memory efficient */
     size_t num_nodes = 0;
@@ -626,6 +671,41 @@ static void load_one_grid_sink_nodes_basic_info(RRGraphBuilder& rr_graph_builder
     } /* End of class enumeration */
 }
 
+static void load_one_grid_medium_nodes_basic_info(RRGraphBuilder& rr_graph_builder,
+                                                  vtr::vector<RRNodeId, RRSwitchId>& rr_node_driver_switches,
+                                                  std::vector<t_rr_rc_data>& rr_rc_data,
+                                                  const size_t& layer,
+                                                  const vtr::Point<size_t>& grid_coordinate,
+                                                  const vtr::NdMatrix<const t_vib_inf*, 3>& vib_grid) {
+
+    const t_vib_inf* vib = vib_grid[layer][grid_coordinate.x()][grid_coordinate.y()];
+    size_t num_medium_nodes = vib->first_stages.size();
+    for (size_t i_medium = 0; i_medium < num_medium_nodes; i_medium++) {
+        /* Create a new node and fill information */
+        RRNodeId node = rr_graph_builder.create_node(layer, grid_coordinate.x(), grid_coordinate.y(), MEDIUM, i_medium, SIDES[0]);
+        /* node bounding box */
+        rr_graph_builder.set_node_coordinates(node, grid_coordinate.x(),
+                                              grid_coordinate.y(),
+                                              grid_coordinate.x(),
+                                              grid_coordinate.y());
+        //rr_graph_builder.add_node_side(node, SIDES[0]);
+        rr_graph_builder.set_node_medium_num(node, i_medium);
+
+        rr_graph_builder.set_node_capacity(node, 1);
+        rr_graph_builder.set_node_layer(node, layer);
+
+        /* cost index is a FIXED value for MEDIUM */
+        rr_graph_builder.set_node_cost_index(node, RRIndexedDataId(MEDIUM_COST_INDEX));
+
+        /* Switch info */
+        rr_node_driver_switches[node] = RRSwitchId(vib->switch_idx);
+
+        /* RC data */
+        rr_graph_builder.set_node_rc_index(node, NodeRCIndex(find_create_rr_rc_data(0., 0., rr_rc_data)));
+    }
+    
+}
+
 /************************************************************************
  * Create all the rr_nodes for grids
  ***********************************************************************/
@@ -633,9 +713,11 @@ static void load_grid_nodes_basic_info(RRGraphBuilder& rr_graph_builder,
                                        vtr::vector<RRNodeId, RRSwitchId>& rr_node_driver_switches,
                                        std::vector<t_rr_rc_data>& rr_rc_data,
                                        const DeviceGrid& grids,
+                                       const vtr::NdMatrix<const t_vib_inf*, 3>& vib_grid,
                                        const size_t& layer,
                                        const RRSwitchId& wire_to_ipin_switch,
-                                       const RRSwitchId& delayless_switch) {
+                                       const RRSwitchId& delayless_switch,
+                                       const bool& is_vib_arch) {
     for (size_t iy = 0; iy < grids.height(); ++iy) {
         for (size_t ix = 0; ix < grids.width(); ++ix) {
             t_physical_tile_loc tile_loc(ix, iy, layer);
@@ -711,6 +793,30 @@ static void load_grid_nodes_basic_info(RRGraphBuilder& rr_graph_builder,
                                                 wire_to_ipin_switch);
         }
     }
+
+    if (is_vib_arch) {
+        /* Create medium nodes */
+        VTR_ASSERT(grids.width() == vib_grid.dim_size(1) && grids.height() == vib_grid.dim_size(2));
+        for (size_t iy = 0; iy < grids.height(); ++iy) {
+            for (size_t ix = 0; ix < grids.width(); ++ix) {
+            
+                t_physical_tile_loc tile_loc(ix, iy, layer);
+                VTR_ASSERT(vib_grid[layer][ix][iy]->pbtype_name == grids.get_physical_type(tile_loc)->name);
+                vtr::Point<size_t> grid_coordinate(ix, iy);
+
+                rr_graph_builder.node_lookup().reserve_nodes(layer, ix, iy, MEDIUM, vib_grid[layer][ix][iy]->first_stages.size(), SIDES[0]);
+
+                load_one_grid_medium_nodes_basic_info(rr_graph_builder,
+                                                      rr_node_driver_switches,
+                                                      rr_rc_data,
+                                                      layer, grid_coordinate,
+                                                      vib_grid);
+
+        }
+    }
+    }
+    
+
     //Copy the SOURCE/SINK nodes to all offset positions for blocks with width > 1 and/or height > 1
     // This ensures that look-ups on non-root locations will still find the correct SOURCE/SINK
     for (size_t x = 0; x < grids.width(); x++) {
@@ -1160,6 +1266,7 @@ void create_tileable_rr_graph_nodes(const RRGraphView& rr_graph,
                                     std::map<RRNodeId, std::vector<size_t>>& rr_node_track_ids,
                                     std::vector<t_rr_rc_data>& rr_rc_data,
                                     const DeviceGrid& grids,
+                                    const vtr::NdMatrix<const t_vib_inf*, 3>& vib_grid,
                                     const size_t& layer,
                                     const vtr::Point<size_t>& chan_width,
                                     const std::vector<t_segment_inf>& segment_inf_x,
@@ -1169,7 +1276,8 @@ void create_tileable_rr_graph_nodes(const RRGraphView& rr_graph,
                                     const RRSwitchId& delayless_switch,
                                     const DeviceGridAnnotation& device_grid_annotation,
                                     const bool& shrink_boundary,
-                                    const bool& through_channel) {
+                                    const bool& through_channel,
+                                    const bool& is_vib_arch) {
     /* Allocates and loads all the structures needed for fast lookups of the   *
      * index of an rr_node.  rr_node_indices is a matrix containing the index  *
      * of the *first* rr_node at a given (i,j) location.                       */
@@ -1188,9 +1296,10 @@ void create_tileable_rr_graph_nodes(const RRGraphView& rr_graph,
     load_grid_nodes_basic_info(rr_graph_builder,
                                rr_node_driver_switches,
                                rr_rc_data,
-                               grids, layer,
+                               grids, vib_grid, layer,
                                wire_to_ipin_switch,
-                               delayless_switch);
+                               delayless_switch,
+                               is_vib_arch);
 
     load_chanx_rr_nodes_basic_info(rr_graph,
                                    rr_graph_builder,
