570259aa931e254309490490ba6565d7022d5ac4
Wang Yuanqi <101231450+Wang-Yuanqi-source@users.noreply.github.com>
2024-06-15 19:30:20 +0800

    Update router_lookahead_map_utils.cpp

--- vpr/src/route/router_lookahead_map_utils.cpp
+++ vpr/src/route/router_lookahead_map_utils.cpp
@@ -420,6 +420,14 @@ t_src_opin_delays compute_router_src_opin_lookahead(bool is_flat) {
                     const std::vector<RRNodeId>& rr_nodes_at_loc = device_ctx.rr_graph.node_lookup().find_grid_nodes_at_all_sides(sample_loc.layer_num, sample_loc.x, sample_loc.y, rr_type);
                     for (RRNodeId node_id : rr_nodes_at_loc) {
                         int ptc = rr_graph.node_ptc_num(node_id);
+                        const t_vib_inf* vib;
+                        if (device_ctx.arch->is_vib_arch) {
+                            vib = device_ctx.vib_grid[sample_loc.layer_num][sample_loc.x][sample_loc.y];
+                        }
+                        else {
+                            vib = nullptr;
+                        }
+                        //const t_vib_inf* vib = device_ctx.vib_grid[sample_loc.layer_num][sample_loc.x][sample_loc.y];
                         // For the time being, we decide to not let the lookahead explore the node inside the clusters
                         if (!is_inter_cluster_node(rr_graph, node_id)) {
                             continue;
@@ -1011,7 +1019,7 @@ static void dijkstra_flood_to_wires(int itile,
                 src_opin_delays[root_layer_num][itile][ptc][curr_layer_num][seg_index].congestion = curr.congestion;
             }
 
-        } else if (curr_rr_type == SOURCE || curr_rr_type == OPIN || curr_rr_type == IPIN) {
+        } else if (curr_rr_type == SOURCE || curr_rr_type == OPIN || curr_rr_type == IPIN || curr_rr_type == MEDIUM) {
             //We allow expansion through SOURCE/OPIN/IPIN types
             auto cost_index = rr_graph.node_cost_index(curr.node);
             float incr_cong = device_ctx.rr_indexed_data[cost_index].base_cost; //Current nodes congestion cost
@@ -1022,7 +1030,17 @@ static void dijkstra_flood_to_wires(int itile,
 
                 RRNodeId next_node = rr_graph.rr_nodes().edge_sink_node(edge);
                 // For the time being, we decide to not let the lookahead explore the node inside the clusters
-
+                t_physical_tile_type_ptr physical_type = device_ctx.grid.get_physical_type({rr_graph.node_xlow(next_node),
+                                                                                            rr_graph.node_ylow(next_node),
+                                                                                            rr_graph.node_layer(next_node)});
+                const t_vib_inf* vib;
+                if (device_ctx.arch->is_vib_arch) {
+                    vib = device_ctx.vib_grid[rr_graph.node_layer(next_node)][rr_graph.node_xlow(next_node)][rr_graph.node_ylow(next_node)];
+                }
+                else {
+                    vib = nullptr;
+                }
+                //const t_vib_inf* vib = device_ctx.vib_grid[rr_graph.node_layer(next_node)][rr_graph.node_xlow(next_node)][rr_graph.node_ylow(next_node)];
                 if (!is_inter_cluster_node(rr_graph, next_node)) {
                     // Don't go inside the clusters
                     continue;
@@ -1379,7 +1397,22 @@ static void expand_dijkstra_neighbours(util::PQ_Entry parent_entry,
         RRNodeId child_node = rr_graph.edge_sink_node(parent, edge);
         // For the time being, we decide to not let the lookahead explore the node inside the clusters
 
+<<<<<<< HEAD
         if (!is_inter_cluster_node(rr_graph, child_node)) {
+=======
+        const t_vib_inf* vib;
+        if (device_ctx.arch->is_vib_arch) {
+            vib = device_ctx.vib_grid[rr_graph.node_layer(child_node)][rr_graph.node_xlow(child_node)][rr_graph.node_ylow(child_node)];
+        }
+        else {
+            vib = nullptr;
+        }
+        
+        if (!is_inter_cluster_node(physical_type,
+                                   vib,
+                                   rr_graph.node_type(child_node),
+                                   rr_graph.node_ptc_num(child_node))) {
+>>>>>>> Update router_lookahead_map_utils.cpp
             continue;
         }
         int switch_ind = size_t(rr_graph.edge_switch(parent, edge));
