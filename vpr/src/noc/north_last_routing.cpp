b3649d36fb0840e48d4f44e1e68ac64e18923c95
soheilshahrouz <soheilqs@gmail.com>
2024-07-05 19:50:58 -0400

    3d north-last routing algorithm

--- vpr/src/noc/north_last_routing.cpp
+++ vpr/src/noc/north_last_routing.cpp
@@ -42,60 +42,23 @@ const std::vector<TurnModelRouting::Direction>& NorthLastRouting::get_legal_dire
         returned_legal_direction.push_back(TurnModelRouting::Direction::DOWN);
     }
 
-    // consider north only when none of other directions are legal
-    if (returned_legal_direction.empty() && dst_router_pos.y > curr_router_pos.y) {
-        returned_legal_direction.push_back(TurnModelRouting::Direction::UP);
+    // check if the destination router is at the layer below of the current router
+    if (dst_router_pos.layer_num < curr_router_pos.layer_num) {
+        returned_legal_direction.push_back(TurnModelRouting::Direction::BELOW);
+    }
+
+    // consider north and up only when none of other directions are legal
+    if (returned_legal_direction.empty()) {
+        if (dst_router_pos.y > curr_router_pos.y) {
+            returned_legal_direction.push_back(TurnModelRouting::Direction::UP);
+        }
+
+        if (dst_router_pos.layer_num > curr_router_pos.layer_num) {
+            returned_legal_direction.push_back(TurnModelRouting::Direction::ABOVE);
+        }
     }
 
     return returned_legal_direction;
-}
-
-TurnModelRouting::Direction NorthLastRouting::select_next_direction(const std::vector<TurnModelRouting::Direction>& legal_directions,
-                                                                    NocRouterId src_router_id,
-                                                                    NocRouterId dst_router_id,
-                                                                    NocRouterId curr_router_id,
-                                                                    NocTrafficFlowId traffic_flow_id,
-                                                                    const NocStorage& noc_model) {
-    // get current and destination NoC routers
-    const auto& curr_router = noc_model.get_single_noc_router(curr_router_id);
-    const auto& dst_router = noc_model.get_single_noc_router(dst_router_id);
-
-    // get the position of current and destination NoC routers
-    const auto curr_router_pos = curr_router.get_router_physical_location();
-    const auto dst_router_pos = dst_router.get_router_physical_location();
-
-    // if there is only one legal direction, take it
-    if (legal_directions.size() == 1) {
-        return legal_directions[0];
-    }
-
-    /* If the function reaches this point,
-     * the destination routed is located at SE or SW of
-     * the current router. We adopt a similar approach to
-     * WestFirstRouting to select between south and west/east.
-     */
-
-    // compute the hash value
-    uint32_t hash_val = get_hash_value(src_router_id, dst_router_id, curr_router_id, traffic_flow_id);
-    // get the maximum value that can be represented by size_t
-    const uint32_t max_uint32_t_val = std::numeric_limits<uint32_t>::max();
-
-    // get the distance from the current router to the destination in each coordination
-    int delta_x = abs(dst_router_pos.x - curr_router_pos.x);
-    int delta_y = abs(dst_router_pos.y - curr_router_pos.y);
-
-    // compute the probability of going to the down (south) direction
-    uint32_t south_probability = delta_y * (max_uint32_t_val / (delta_x + delta_y));
-
-    TurnModelRouting::Direction selected_direction = TurnModelRouting::Direction::INVALID;
-
-    if (hash_val < south_probability) { // sometimes turn south
-        selected_direction = TurnModelRouting::Direction::DOWN;
-    } else { // if turning south was rejected, take the other option (east/west)
-        selected_direction = select_direction_other_than(legal_directions, TurnModelRouting::Direction::DOWN);
-    }
-
-    return selected_direction;
 }
 
 bool NorthLastRouting::is_turn_legal(const std::array<std::reference_wrapper<const NocRouter>, 3>& noc_routers) const {
