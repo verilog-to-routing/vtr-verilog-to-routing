75eae08e75761eb3a8da2d551b4eefb1ad5da945
soheilshahrouz <soheilqs@gmail.com>
2024-07-08 14:37:32 -0400

    3d west-first routing algorithm

--- vpr/src/noc/west_first_routing.cpp
+++ vpr/src/noc/west_first_routing.cpp
@@ -23,17 +23,28 @@ const std::vector<TurnModelRouting::Direction>& WestFirstRouting::get_legal_dire
      * as the destination router. Otherwise, we can move south, north,
      * and east adaptively.
      */
+
     if (dst_router_pos.x < curr_router_pos.x) {
         returned_legal_direction.push_back(TurnModelRouting::Direction::LEFT);
-    } else { // to the east or the same column
-        if (dst_router_pos.x > curr_router_pos.x) { // not the same column
+    }
+
+    if (dst_router_pos.y < curr_router_pos.y) {
+        returned_legal_direction.push_back(TurnModelRouting::Direction::DOWN);
+    }
+
+    if (returned_legal_direction.empty()) {
+        if (dst_router_pos.x > curr_router_pos.x) {
             returned_legal_direction.push_back(TurnModelRouting::Direction::RIGHT);
         }
 
         if (dst_router_pos.y > curr_router_pos.y) {
             returned_legal_direction.push_back(TurnModelRouting::Direction::UP);
-        } else if (dst_router_pos.y < curr_router_pos.y) {
-            returned_legal_direction.push_back(TurnModelRouting::Direction::DOWN);
+        }
+
+        if (dst_router_pos.layer_num > curr_router_pos.layer_num) {
+            returned_legal_direction.push_back(TurnModelRouting::Direction::ABOVE);
+        } else if (dst_router_pos.layer_num < curr_router_pos.layer_num) {
+            returned_legal_direction.push_back(TurnModelRouting::Direction::BELOW);
         }
     }
 
@@ -96,21 +107,16 @@ TurnModelRouting::Direction WestFirstRouting::select_next_direction(const std::v
 }
 
 bool WestFirstRouting::is_turn_legal(const std::array<std::reference_wrapper<const NocRouter>, 3>& noc_routers) const {
-    const int x1 = noc_routers[0].get().get_router_grid_position_x();
-    const int y1 = noc_routers[0].get().get_router_grid_position_y();
-
-    const int x2 = noc_routers[1].get().get_router_grid_position_x();
-    const int y2 = noc_routers[1].get().get_router_grid_position_y();
-
-    const int x3 = noc_routers[2].get().get_router_grid_position_x();
-    const int y3 = noc_routers[2].get().get_router_grid_position_y();
+    const auto [x1, y1, z1] = noc_routers[0].get().get_router_physical_location();
+    const auto [x2, y2, z2] = noc_routers[1].get().get_router_physical_location();
+    const auto [x3, y3, z3] = noc_routers[2].get().get_router_physical_location();
 
     // check if the given routers can be traversed one after another
-    VTR_ASSERT(x2 == x1 || y2 == y1);
-    VTR_ASSERT(x3 == x2 || y3 == y2);
+    VTR_ASSERT(vtr::exactly_k_conditions(2, x1 == x2, y1 == y2, z1 == z2));
+    VTR_ASSERT(vtr::exactly_k_conditions(2, x2 == x3, y2 == y3, z2 == z3));
 
     // going back to the first router is not allowed
-    if (x1 == x3 && y1 == y3) {
+    if (x1 == x3 && y1 == y3 && z1 == z3) {
         return false;
     }
 
@@ -119,7 +125,8 @@ bool WestFirstRouting::is_turn_legal(const std::array<std::reference_wrapper<con
      * towards west. Therefore, if the first link was travelling in a
      * vertical direction, the second link can't move towards left.
      */
-    if (y2 != y1 && x3 < x2) {
+    if ((z2 > z1 && x3 < x2) || (z2 < z1 && x3 < x2) || (z2 > z1 && y3 < y2) ||
+        (z2 < z1 && y3 < y2) || (y2 > y1 && x3 < x2) || (x2 > x1 && y3 > y2)) {
         return false;
     }
 
