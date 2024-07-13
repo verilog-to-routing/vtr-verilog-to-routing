72fb12cdd93aa87cda40ad7929f4a1de85b41e2a
Wang Yuanqi <101231450+Wang-Yuanqi-source@users.noreply.github.com>
2024-07-13 14:00:21 +0800

    Update check_route.cpp

--- vpr/src/route/check_route.cpp
+++ vpr/src/route/check_route.cpp
@@ -355,7 +355,7 @@ static bool check_adjacent(RRNodeId from_node, RRNodeId to_node, bool is_flat) {
 
         case OPIN:
             from_grid_type = device_ctx.grid.get_physical_type({from_xlow, from_ylow, from_layer});
-            if (to_type == CHANX || to_type == CHANY) {
+            if (to_type == CHANX || to_type == CHANY || to_type == MEDIUM) {
                 num_adj += 1; //adjacent
             } else if (is_flat) {
                 VTR_ASSERT(to_type == OPIN || to_type == IPIN); // If pin is located inside a cluster
@@ -430,6 +430,8 @@ static bool check_adjacent(RRNodeId from_node, RRNodeId to_node, bool is_flat) {
                 }
             } else if (to_type == CHANY) {
                 num_adj += chanx_chany_adjacent(from_node, to_node);
+            } else if (to_type == MEDIUM) {
+                num_adj += 1;
             } else {
                 VPR_FATAL_ERROR(VPR_ERROR_ROUTE,
                                 "in check_adjacent: %d and %d are not adjacent", from_node, to_node);
@@ -462,10 +464,26 @@ static bool check_adjacent(RRNodeId from_node, RRNodeId to_node, bool is_flat) {
                 }
             } else if (to_type == CHANX) {
                 num_adj += chanx_chany_adjacent(to_node, from_node);
+            } else if (to_type == MEDIUM) {
+                num_adj += 1;
             } else {
                 VPR_FATAL_ERROR(VPR_ERROR_ROUTE,
                                 "in check_adjacent: %d and %d are not adjacent", from_node, to_node);
             }
+            break;
+
+        case MEDIUM:
+            //from_grid_type = device_ctx.grid.get_physical_type({from_xlow, from_ylow, from_layer});
+            if (to_type == CHANX || to_type == CHANY || to_type == MEDIUM) {
+                num_adj += 1; //adjacent
+            } else if (is_flat) {
+                VTR_ASSERT(to_type == OPIN || to_type == IPIN); // If pin is located inside a cluster
+                return true;
+            } else {
+                VTR_ASSERT(to_type == IPIN); 
+                num_adj += 1;                 
+            }
+
             break;
 
         default:
