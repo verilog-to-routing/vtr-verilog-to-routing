7b956b61d0aa47c49815232e48117ad1a8d387b9
Duck Deux <duck2@protonmail.com>
2024-08-15 08:49:20 -0700

    Add flat version of sync_netlists_to_routing

--- vpr/src/pack/post_routing_pb_pin_fixup.h
+++ vpr/src/pack/post_routing_pb_pin_fixup.h
@@ -9,14 +9,28 @@
 /********************************************************************
  * Function declaration
  *******************************************************************/
+
+/********************************************************************
+ * Top-level function to synchronize a packed netlist to routing results
+ * The problem comes from a mismatch between the packing and routing results
+ * When there are equivalent input/output for any grids, the router will try
+ * to swap the net mapping among these pins so as to achieve best 
+ * routing optimization.
+ * However, it will cause the packing results out-of-date as the net mapping 
+ * of each grid remain untouched once packing is done.
+ * This function aims to fix the mess after routing so that the net mapping
+ * can be synchronized
+ *
+ * Note:
+ *   - This function SHOULD be run ONLY when routing is finished.
+ *   - This function only handles the two-stage routing results.
+ *     See \see sync_netlists_to_routing_flat() for the flat routing case.
+ *******************************************************************/
 void sync_netlists_to_routing(const Netlist<>& net_list,
                               const DeviceContext& device_ctx,
                               AtomContext& atom_ctx,
-                              const AtomLookup& atom_lookup,
                               ClusteringContext& clustering_ctx,
                               const PlacementContext& placement_ctx,
-                              const RoutingContext& routing_ctx,
-                              const bool& verbose,
-                              bool is_flat);
+                              const bool& verbose);
 
 #endif
