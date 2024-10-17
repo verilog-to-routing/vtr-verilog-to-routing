53f3b3be0aea5d4def2c4fde12591aa1c6a75ec4
amin1377 <amin1377.mohaghegh@gmail.com>
2024-10-17 17:51:40 -0400

    [vpr][route] return from set_nets_choking_spots if flat router is not selected

--- vpr/src/route/route_utils.cpp
+++ vpr/src/route/route_utils.cpp
@@ -398,13 +398,10 @@ vtr::vector<ParentNetId, std::vector<std::unordered_map<RRNodeId, int>>> set_net
         choking_spots[net_id].resize(net_list.net_pins(net_id).size());
     }
 
-    // Return if the architecture doesn't have any potential choke points
-    if (!router_opt_choke_points) {
+    // Return if the architecture doesn't have any potential choke points or flat router is not enabled
+    if (!router_opt_choke_points || !is_flat) {
         return choking_spots;
     }
-
-    // We only identify choke points if flat_routing is enabled.
-    VTR_ASSERT(is_flat);
 
     const auto& device_ctx = g_vpr_ctx.device();
     const auto& rr_graph = device_ctx.rr_graph;
