391d0443306c7ebb3a99a6c9e20011226ed1549e
Wang Yuanqi <101231450+Wang-Yuanqi-source@users.noreply.github.com>
2024-11-04 10:35:30 +0800

    Update SetupVPR.cpp

--- vpr/src/base/SetupVPR.cpp
+++ vpr/src/base/SetupVPR.cpp
@@ -49,6 +49,16 @@ static void SetupSwitches(const t_arch& Arch,
 static void SetupAnalysisOpts(const t_options& Options, t_analysis_opts& analysis_opts);
 static void SetupPowerOpts(const t_options& Options, t_power_opts* power_opts, t_arch* Arch);
 
+static void SetupVibInf(const std::vector<t_physical_tile_type>& PhysicalTileTypes, 
+                        const int num_switches,
+                        const t_arch_switch_inf* Switches, 
+                        const std::vector<t_segment_inf>& Segments, 
+                        std::vector<VibInf>& vib_infs);
+
+static void ProcessFromOrToTokens(const std::vector<std::string> Tokens, const std::vector<t_physical_tile_type>& PhysicalTileTypes, const std::vector<t_segment_inf> segments, std::vector<t_from_or_to_inf>& froms);
+static void parse_pin_name(char* src_string, int* start_pin_index, int* end_pin_index, char* pb_type_name, char* port_name);
+
+
 /**
  * @brief Identify which switch must be used for *track* to *IPIN* connections based on architecture file specification.
  * @param Arch Architecture file specification
@@ -238,6 +248,8 @@ void SetupVPR(const t_options* options,
     SetupPackerOpts(*options, packerOpts);
     routingArch->write_rr_graph_filename = options->write_rr_graph_file;
     routingArch->read_rr_graph_filename = options->read_rr_graph_file;
+
+    SetupVibInf(device_ctx.physical_tile_types, arch->num_switches, arch->Switches, arch->Segments, arch->vib_infs);
 
     for (auto has_global_routing : arch->layer_global_routing) {
         device_ctx.inter_cluster_prog_routing_resources.emplace_back(has_global_routing);
@@ -1042,6 +1054,278 @@ static void do_reachability_analysis(t_physical_tile_type* physical_tile,
                     }
                 }
             }
+        }
+    }
+}
+
+static void SetupVibInf(const std::vector<t_physical_tile_type>& PhysicalTileTypes, 
+                        const int num_switches,
+                        const t_arch_switch_inf* Switches, 
+                        const std::vector<t_segment_inf>& Segments, 
+                        std::vector<VibInf>& vib_infs) {
+    VTR_ASSERT(!vib_infs.empty());
+    for (auto& vib_inf : vib_infs) {
+        for (int i_switch = 0; i_switch < num_switches; i_switch++) {
+            if (vib_inf.get_switch_name() == Switches[i_switch].name) {
+                vib_inf.set_switch_idx(i_switch);
+                break;
+            }
+        }
+
+        std::vector<t_seg_group> seg_groups = vib_inf.get_seg_groups();
+        for (auto& seg_group : seg_groups) {
+            for (int i_seg = 0; i_seg < (int)Segments.size(); i_seg++) {
+                if (Segments[i_seg].name == seg_group.name) {
+                    seg_group.seg_index = i_seg;
+                    break;
+                }
+            }
+        }
+        vib_inf.set_seg_groups(seg_groups);
+
+        std::vector<t_first_stage_mux_inf> first_stages = vib_inf.get_first_stages();
+        for (auto& first_stage : first_stages) {
+            auto& from_tokens = first_stage.from_tokens;
+            for (const auto& from_token : from_tokens) {
+                ProcessFromOrToTokens(from_token, PhysicalTileTypes, Segments, first_stage.froms);
+            }
+        }
+        vib_inf.set_first_stages(first_stages);
+
+        auto second_stages = vib_inf.get_second_stages();
+        for (auto& second_stage : second_stages) {
+            std::vector<t_from_or_to_inf> tos;
+
+            ProcessFromOrToTokens(second_stage.to_tokens, PhysicalTileTypes, Segments, tos);
+            for (auto& to : tos) {
+                VTR_ASSERT(to.from_type == SEGMENT || to.from_type == PB);
+                second_stage.to.push_back(to);
+            }
+
+            auto from_tokens = second_stage.from_tokens;
+            for (const auto& from_token : from_tokens) {
+                ProcessFromOrToTokens(from_token, PhysicalTileTypes, Segments, second_stage.froms);
+            }            
+        }
+        vib_inf.set_second_stages(second_stages);
+
+    }
+}
+
+static void ProcessFromOrToTokens(const std::vector<std::string> Tokens, const std::vector<t_physical_tile_type>& PhysicalTileTypes, const std::vector<t_segment_inf> segments, std::vector<t_from_or_to_inf>& froms) {
+    for (int i_token = 0; i_token < (int)Tokens.size(); i_token++) {
+        std::string Token = Tokens[i_token];
+        const char* Token_char = Token.c_str();
+        auto token = vtr::split(Token, ".");
+        if (token.size() == 1) {
+            t_from_or_to_inf from_inf;
+            from_inf.type_name = token[0];
+            from_inf.from_type = MUX;
+            froms.push_back(from_inf);
+        }
+        else if (token.size() == 2) {
+            std::string from_type_name = token[0];
+            e_multistage_mux_from_or_to_type from_type;
+            for (int i_phy_type = 0; i_phy_type < (int)PhysicalTileTypes.size(); i_phy_type++) {
+                if (from_type_name == PhysicalTileTypes[i_phy_type].name) {
+                    from_type = PB;
+                    int start_pin_index, end_pin_index;
+                    char *pb_type_name, *port_name;
+                    pb_type_name = nullptr;
+                    port_name = nullptr;
+                    pb_type_name = new char[strlen(Token_char)];
+                    port_name = new char[strlen(Token_char)];
+                    parse_pin_name((char*)Token_char, &start_pin_index, &end_pin_index, pb_type_name, port_name);
+                    
+                    std::vector<int> all_sub_tile_to_tile_pin_indices;
+                    for (auto& sub_tile : PhysicalTileTypes[i_phy_type].sub_tiles) {
+                        int sub_tile_capacity = sub_tile.capacity.total();
+
+                        int start = 0;
+                        int end = 0;
+                        int i_port = 0;
+                        for (; i_port < (int)sub_tile.ports.size(); ++i_port) {
+                            if (!strcmp(sub_tile.ports[i_port].name, port_name)) {
+                                start = sub_tile.ports[i_port].absolute_first_pin_index;
+                                end = start + sub_tile.ports[i_port].num_pins - 1;
+                                break;
+                            }
+                        }
+                        if (i_port == (int)sub_tile.ports.size()) {
+                            continue;
+                        }
+                        for (int pin_num = start; pin_num <= end; ++pin_num) {
+                            VTR_ASSERT(pin_num < (int)sub_tile.sub_tile_to_tile_pin_indices.size() / sub_tile_capacity);
+                            for (int capacity = 0; capacity < sub_tile_capacity; ++ capacity) {
+                                int sub_tile_pin_index = pin_num + capacity * sub_tile.num_phy_pins / sub_tile_capacity;
+                                int physical_pin_index = sub_tile.sub_tile_to_tile_pin_indices[sub_tile_pin_index];
+                                all_sub_tile_to_tile_pin_indices.push_back(physical_pin_index);
+                            }
+                        }
+                    }
+
+                    if (start_pin_index == end_pin_index && start_pin_index < 0) {
+                        start_pin_index = 0;
+                        end_pin_index = all_sub_tile_to_tile_pin_indices.size() - 1;
+                    }
+
+                    if ((int)all_sub_tile_to_tile_pin_indices.size() <= start_pin_index || (int)all_sub_tile_to_tile_pin_indices.size() <= end_pin_index) {
+                        VTR_LOGF_ERROR(__FILE__, __LINE__,
+                                       "The index of pbtype %s : port %s exceeds its total number!\n", pb_type_name, port_name);
+                    }
+
+                    for (int i = start_pin_index; i <= end_pin_index; i++) {
+                        t_from_or_to_inf from_inf;
+                        from_inf.type_name = from_type_name;
+                        from_inf.from_type = from_type;
+                        from_inf.type_index = i_phy_type;
+                        from_inf.phy_pin_index = all_sub_tile_to_tile_pin_indices[i];
+                        froms.push_back(from_inf);
+                    }
+                    
+                    // for (auto& sub_tile : PhysicalTileTypes[i_phy_type].sub_tiles) {
+                    //     //int sub_tile_index = sub_tile.index;
+                    //     int sub_tile_capacity = sub_tile.capacity.total();
+
+                    //     int i_port = 0;
+                    //     for (; i_port < (int)sub_tile.ports.size(); ++i_port) {
+                            
+                    //         if (!strcmp(sub_tile.ports[i_port].name, port_name)) {
+                    //             if (start_pin_index == end_pin_index && start_pin_index < 0) {
+                    //                 start_pin_index = 0;
+                    //                 end_pin_index = sub_tile.ports[i_port].num_pins - 1;
+                    //             }
+                    //             start_pin_index += sub_tile.ports[i_port].absolute_first_pin_index;
+                    //             end_pin_index += sub_tile.ports[i_port].absolute_first_pin_index;
+                    //             break;
+                    //         }
+                    //     }
+
+                    //     if (i_port == (int)sub_tile.ports.size()) {
+                    //         continue;
+                    //     }
+
+                    //     for (int pin_num = start_pin_index; pin_num <= end_pin_index; ++pin_num) {
+                    //         VTR_ASSERT(pin_num < (int)sub_tile.sub_tile_to_tile_pin_indices.size() / sub_tile_capacity);
+                    //         for (int capacity = 0; capacity < sub_tile_capacity; ++ capacity) {
+                    //             int sub_tile_pin_index = pin_num + capacity * sub_tile.num_phy_pins / sub_tile_capacity;
+                    //             int physical_pin_index = sub_tile.sub_tile_to_tile_pin_indices[sub_tile_pin_index];
+                    //             t_from_or_to_inf from_inf;
+                    //             from_inf.type_name = from_type_name;
+                    //             from_inf.from_type = from_type;
+                    //             from_inf.type_index = i_phy_type;
+                    //             from_inf.phy_pin_index = physical_pin_index;
+                    //             froms.push_back(from_inf);
+                    //         }
+                    //     }
+                    // }
+                    
+                }
+            }
+            for (int i_seg_type = 0; i_seg_type < (int)segments.size(); i_seg_type++) {
+                if (from_type_name == segments[i_seg_type].name) {
+                    from_type = SEGMENT;
+                    std::string from_detail = token[1];
+                    if (from_detail.length() >= 2) {
+                        char dir = from_detail.c_str()[0];
+                        from_detail.erase(from_detail.begin());
+                        int seg_index = std::stoi(from_detail);
+
+                        t_from_or_to_inf from_inf;
+                        from_inf.type_name = from_type_name;
+                        from_inf.from_type = from_type;
+                        from_inf.type_index = i_seg_type;
+                        from_inf.seg_dir = dir;
+                        from_inf.seg_index = seg_index;
+                        froms.push_back(from_inf);
+                    }
+                    
+                    break;
+                }
+            }
+            VTR_ASSERT(from_type == PB || from_type == SEGMENT);
+            
+        }
+        else {
+            std::string msg = vtr::string_fmt("Failed to parse vib mux from information '%s'", Token.c_str());
+            VTR_LOGF_ERROR(__FILE__, __LINE__, msg.c_str());
+        }
+    }
+}
+
+static void parse_pin_name(char* src_string, int* start_pin_index, int* end_pin_index, char* pb_type_name, char* port_name) {
+    /* Parses out the pb_type_name and port_name   *
+     * If the start_pin_index and end_pin_index is specified, parse them too. *
+     * Return the values parsed by reference.                                 */
+
+    char source_string[128];
+    char* find_format = nullptr;
+    int ichar, match_count;
+
+    // parse out the pb_type and port name, possibly pin_indices
+    find_format = strstr(src_string, "[");
+    if (find_format == nullptr) {
+        /* Format "pb_type_name.port_name" */
+        *start_pin_index = *end_pin_index = -1;
+
+        
+        strcpy(source_string, src_string);
+        
+        for (ichar = 0; ichar < (int)(strlen(source_string)); ichar++) {
+            if (source_string[ichar] == '.')
+                source_string[ichar] = ' ';
+        }
+
+        match_count = sscanf(source_string, "%s %s", pb_type_name, port_name);
+        if (match_count != 2) {
+            VTR_LOG_ERROR(
+                "Invalid pin - %s, name should be in the format "
+                "\"pb_type_name\".\"port_name\" or \"pb_type_name\".\"port_name[end_pin_index:start_pin_index]\". "
+                "The end_pin_index and start_pin_index can be the same.\n",
+                src_string);
+            exit(1);
+        }
+    } else {
+        /* Format "pb_type_name.port_name[end_pin_index:start_pin_index]" */
+        strcpy(source_string, src_string);
+        for (ichar = 0; ichar < (int)(strlen(source_string)); ichar++) {
+            //Need white space between the components when using %s with
+            //sscanf
+            if (source_string[ichar] == '.')
+                source_string[ichar] = ' ';
+            if (source_string[ichar] == '[')
+                source_string[ichar] = ' ';
+        }
+
+        match_count = sscanf(source_string, "%s %s %d:%d]",
+                             pb_type_name, port_name,
+                             end_pin_index, start_pin_index);
+        if (match_count != 4) {
+            match_count = sscanf(source_string, "%s %s %d]",
+                                 pb_type_name, port_name,
+                                 end_pin_index);
+            *start_pin_index = *end_pin_index;
+            if (match_count != 3) {
+                VTR_LOG_ERROR(
+                    "Invalid pin - %s, name should be in the format "
+                    "\"pb_type_name\".\"port_name\" or \"pb_type_name\".\"port_name[end_pin_index:start_pin_index]\". "
+                    "The end_pin_index and start_pin_index can be the same.\n",
+                    src_string);
+                exit(1);
+            }            
+        }
+        if (*end_pin_index < 0 || *start_pin_index < 0) {
+            VTR_LOG_ERROR(
+                "Invalid pin - %s, the pin_index in "
+                "[end_pin_index:start_pin_index] should not be a negative value.\n",
+                src_string);
+            exit(1);
+        }
+        if (*end_pin_index < *start_pin_index) {
+            int temp;
+            temp = *end_pin_index;
+            *end_pin_index = *start_pin_index;
+            *start_pin_index = temp;
         }
     }
 }
