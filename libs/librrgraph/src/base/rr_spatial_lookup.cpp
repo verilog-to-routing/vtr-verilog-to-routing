cafd0b9d808701ee86016b927bf35fdaae3a8031
Wang Yuanqi <101231450+Wang-Yuanqi-source@users.noreply.github.com>
2024-11-04 10:29:05 +0800

    Update rr_spatial_lookup.cpp

--- libs/librrgraph/src/base/rr_spatial_lookup.cpp
+++ libs/librrgraph/src/base/rr_spatial_lookup.cpp
@@ -218,14 +218,14 @@ std::vector<RRNodeId> RRSpatialLookup::find_grid_nodes_at_all_sides(int layer,
                                                                     t_rr_type rr_type) const {
     VTR_ASSERT(rr_type == SOURCE || rr_type == OPIN || rr_type == IPIN || rr_type == SINK || rr_type == MEDIUM);
     if (rr_type == SOURCE || rr_type == SINK || rr_type == MEDIUM) {
-        return find_nodes(layer,x, y, rr_type);
+        return find_nodes(layer, x, y, rr_type);
     }
 
     std::vector<RRNodeId> nodes;
     /* Reserve space to avoid memory fragmentation */
     size_t num_nodes = 0;
     for (e_side node_side : TOTAL_2D_SIDES) {
-        num_nodes += find_nodes(layer,x, y, rr_type, node_side).size();
+        num_nodes += find_nodes(layer, x, y, rr_type, node_side).size();
     }
 
     nodes.reserve(num_nodes);
