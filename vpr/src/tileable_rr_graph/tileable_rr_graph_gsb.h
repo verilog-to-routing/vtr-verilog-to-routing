9dac8c4d6243681b967852855f573ab8b09f2877
Wang Yuanqi <101231450+Wang-Yuanqi-source@users.noreply.github.com>
2024-11-25 18:52:42 +0800

    Update tileable_rr_graph_gsb.h

--- vpr/src/tileable_rr_graph/tileable_rr_graph_gsb.h
+++ vpr/src/tileable_rr_graph/tileable_rr_graph_gsb.h
@@ -64,12 +64,20 @@ RRGSB build_one_tileable_rr_gsb(const DeviceGrid& grids,
 
 void build_edges_for_one_tileable_rr_gsb(RRGraphBuilder& rr_graph_builder,
                                          const RRGSB& rr_gsb,
-                                         const t_bend_track2track_map& sb_bend_conn,
                                          const t_track2pin_map& track2ipin_map,
                                          const t_pin2track_map& opin2track_map,
                                          const t_track2track_map& track2track_map,
                                          const vtr::vector<RRNodeId, RRSwitchId>& rr_node_driver_switches,
                                          size_t& num_edges);
+
+void build_edges_for_one_tileable_rr_gsb_vib(RRGraphBuilder& rr_graph_builder,
+                                             const RRGSB& rr_gsb,
+                                             const t_bend_track2track_map& sb_bend_conn,
+                                             const t_track2pin_map& track2ipin_map,
+                                             const t_pin2track_map& opin2track_map,
+                                             const t_track2track_map& track2track_map,
+                                             const vtr::vector<RRNodeId, RRSwitchId>& rr_node_driver_switches,
+                                             size_t& num_edges);
 
 t_track2pin_map build_gsb_track_to_ipin_map(const RRGraphView& rr_graph,
                                             const RRGSB& rr_gsb,
@@ -99,8 +107,7 @@ t_vib_map build_vib_map(const RRGraphView& rr_graph,
                         const std::vector<t_segment_inf>& segment_inf,
                         const size_t& layer,
                         const vtr::Point<size_t>& gsb_coordinate,
-                        const vtr::Point<size_t>& actual_coordinate,
-                        std::vector<std::vector<std::vector<std::map<std::string, size_t>>>> medium_mux_name2medium_index);
+                        const vtr::Point<size_t>& actual_coordinate);
 
 void build_edges_for_one_tileable_vib(RRGraphBuilder& rr_graph_builder,
                                       const t_vib_map& vib_map,
