d2438e34ff5459b0109f6d60839903e5cacb2dd0
Amir.A <amirarjmandaqai@gmail.com>
2024-12-09 22:25:48 -0400

    minor update

--- vtr_flow/scripts/python_libs/vtr/parmys/parmys.py
+++ vtr_flow/scripts/python_libs/vtr/parmys/parmys.py
@@ -232,7 +232,8 @@ def run(
         odin_config_full_path,
     )
 
-    # Set the synlig exe script path in the environment variable(handle if it is not set or system-verilog OFF)
+    # Set the synlig exe script path in the environment variable
+    # (handle if it is not set or system-verilog OFF)
     try:
         os.environ["synlig_exe_path"] = str(vtr.paths.synlig_exe_path)
     except KeyError:
