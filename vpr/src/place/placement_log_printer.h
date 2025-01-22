3be84ef2e65da53f42ff4c8f857fb5eab9f7e969
soheilshahrouz <soheilqs@gmail.com>
2025-01-22 15:58:39 -0500

    make logging in Placer method conditional

--- vpr/src/place/placement_log_printer.h
+++ vpr/src/place/placement_log_printer.h
@@ -61,6 +61,8 @@ class PlacementLogPrinter {
     void print_initial_placement_stats() const;
     /// Prints final placement metrics and generates timing reports.
     void print_post_placement_stats() const;
+    ///
+    bool quiet() const { return quiet_; }
 
   private:
     /**
