cc793dd54ef313004bff4ac24c00d31fd8476082
AlexandreSinger <alex.singer@mail.utoronto.ca>
2025-02-25 14:50:25 -0500

    [AP][DetailedPlacer] Added Detailed Placer Class
    The Detailed Placer is a pass in the AP flow which takes a legal
    placement and optimizes it, while still remaining legal.
    
    Previously in the AP flow, the annealer from the Placement stage was
    always used to optimize the placement. Now the user can choose to not
    use the annealer, by selecting the "none" Detailed Placer. Other
    Detailed Placers can now also be added to explore other Detailed
    Placement algorithms in VTR.

--- vpr/src/analytical_place/ap_flow_enums.h
+++ vpr/src/analytical_place/ap_flow_enums.h
@@ -18,3 +18,14 @@ enum class e_ap_full_legalizer {
     APPack  ///< The APPack Full Legalizer, which uses the flat placement to improve the Packer and Placer.
 };
 
+/**
+ * @brief The type of a Detailed Placer.
+ *
+ * The Analytical Placement flow may implement different Detailed Placers. This
+ * enum can select between these different Detailed Placers.
+ */
+enum class e_ap_detailed_placer {
+    Identity,           ///< The Identity Detailed Placer, which does not perform any optimizations on the legalized placement. Needed as a placeholder.
+    Annealer            ///< The Annealer Detailed Placer, which runs the annealer found in the Place part of the VPR flow (using the same options as the Placement stage).
+};
+
