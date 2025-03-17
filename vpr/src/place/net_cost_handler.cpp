b5255b45fea75d6474413291f4a9f41f8198fccb
Amin Mohaghegh <amohaghegh@quicklogic.com>
2025-03-17 15:24:08 -0700

    [vpr][place] print initial placement total wirelength estimate

--- vpr/src/place/net_cost_handler.cpp
+++ vpr/src/place/net_cost_handler.cpp
@@ -1625,6 +1625,20 @@ void NetCostHandler::recompute_costs_from_scratch(const PlaceDelayModel* delay_m
     }
 }
 
+double NetCostHandler::get_total_wirelength_estimate() const {
+    const auto& cluster_ctx = g_vpr_ctx.clustering();
+    const auto& place_move_ctx = placer_state_.move();
+
+    double estimated_wirelength = 0.0;
+    for (ClusterNetId net_id : cluster_ctx.clb_nlist.nets()) { /* for each net ... */
+        if (!cluster_ctx.clb_nlist.net_is_ignored(net_id)) {   /* Do only if not ignored. */
+                estimated_wirelength += get_net_wirelength_estimate(net_id, place_move_ctx.bb_coords[net_id]);
+        }
+    }
+
+    return estimated_wirelength;
+}
+
 void NetCostHandler::set_ts_bb_coord_(const ClusterNetId net_id) {
     auto& place_move_ctx = placer_state_.mutable_move();
     if (cube_bb_) {
