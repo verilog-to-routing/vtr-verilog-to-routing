e275a82d12c333c5306c5584f562fb38ad47fd02
amin1377 <amin1377.mohaghegh@gmail.com>
2025-05-02 11:33:49 -0400

    [vpr][CLI] add generate_net_timing_report

--- vpr/src/base/read_options.cpp
+++ vpr/src/base/read_options.cpp
@@ -3010,6 +3010,11 @@ argparse::ArgumentParser create_arg_parser(const std::string& prog_name, t_optio
     analysis_grp.add_argument(args.write_timing_summary, "--write_timing_summary")
         .help("Writes implemented design final timing summary to the specified JSON, XML or TXT file.")
         .show_in(argparse::ShowIn::HELP_ONLY);
+    
+    analysis_grp.add_argument(args.generate_net_timing_report, "--generate_net_timing_report")
+        .help("Generates a net timing report for each net in the design.")
+        .default_value("off")
+        .show_in(argparse::ShowIn::HELP_ONLY);
 
     auto& power_grp = parser.add_argument_group("power analysis options");
 
