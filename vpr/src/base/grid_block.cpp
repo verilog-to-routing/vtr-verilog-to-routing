0a0933ea1e0cf812660777c89dc243630dfa2cd0
haydar-c <haydar2000cakan@gmail.com>
2025-05-27 18:34:18 -0400

    Clean up auto types from grid blocks

--- vpr/src/base/grid_block.cpp
+++ vpr/src/base/grid_block.cpp
@@ -17,7 +17,7 @@ void GridBlock::init_grid_blocks(const DeviceGrid& device_grid) {
         for (size_t x = 0; x < grid_width; x++) {
             for (size_t y = 0; y < grid_height; y++) {
                 const t_physical_tile_loc tile_loc({(int)x, (int)y, (int)layer_num});
-                auto type = device_grid.get_physical_type(tile_loc);
+                t_physical_tile_type_ptr type = device_grid.get_physical_type(tile_loc);
                 initialized_grid_block_at_location(tile_loc, type->capacity);
             }
         }
@@ -25,16 +25,16 @@ void GridBlock::init_grid_blocks(const DeviceGrid& device_grid) {
 }
 
 void GridBlock::zero_initialize() {
-    auto& device_ctx = g_vpr_ctx.device();
+    const DeviceContext& device_ctx = g_vpr_ctx.device();
 
     /* Initialize all occupancy to zero. */
     for (int layer_num = 0; layer_num < (int)device_ctx.grid.get_num_layers(); layer_num++) {
         for (int i = 0; i < (int)device_ctx.grid.width(); i++) {
             for (int j = 0; j < (int)device_ctx.grid.height(); j++) {
-                auto tile = device_ctx.grid.get_physical_type({i, j, layer_num});
+                t_physical_tile_type_ptr tile = device_ctx.grid.get_physical_type({i, j, layer_num});
 
-                for (const auto& sub_tile : tile->sub_tiles) {
-                    auto capacity = sub_tile.capacity;
+                for (const t_sub_tile& sub_tile : tile->sub_tiles) {
+                    t_capacity_range capacity = sub_tile.capacity;
 
                     for (int k = 0; k < capacity.total(); k++) {
                         set_block_at_location({i, j, k + capacity.low, layer_num}, ClusterBlockId::INVALID());
@@ -46,8 +46,8 @@ void GridBlock::zero_initialize() {
 }
 
 void GridBlock::load_from_block_locs(const vtr::vector_map<ClusterBlockId, t_block_loc>& block_locs) {
-    auto& cluster_ctx = g_vpr_ctx.clustering();
-    auto& device_ctx = g_vpr_ctx.device();
+    const ClusteringContext& cluster_ctx = g_vpr_ctx.clustering();
+    const DeviceContext& device_ctx = g_vpr_ctx.device();
 
     zero_initialize();
 
