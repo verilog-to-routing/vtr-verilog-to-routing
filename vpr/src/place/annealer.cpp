f7c570ae163b2d83a4b57486d8a19cfc5766d49e
AlexandreSinger <alex.singer@mail.utoronto.ca>
2025-06-21 22:38:36 -0400

    [AP][DP] Added Option to Scale Init T in Annealer
    Since the AP flow produces an initial placement that has 4x improved
    estimated wirelength and 2x improved CPD, the initial temperature of the
    annealer is too high. From experimentation, I have found that reducing
    the initial temperature in half improves quality slightly and reduces
    runtime.
    
    Added an option to the Placer to scale up or down the initial
    temperature. The default flow just sets this scale to 1.0, but the AP
    flow sets this scale to 0.5 currently.
    
    I have also exposed this option to the command-line so this option can
    be swept more easily.

--- vpr/src/place/annealer.cpp
+++ vpr/src/place/annealer.cpp
@@ -206,6 +206,7 @@ PlacementAnnealer::PlacementAnnealer(const t_placer_opts& placer_opts,
                                      PlacerSetupSlacks* setup_slacks,
                                      SetupTimingInfo* timing_info,
                                      NetPinTimingInvalidator* pin_timing_invalidator,
+                                     float auto_init_t_scale,
                                      int move_lim)
     : placer_opts_(placer_opts)
     , placer_state_(placer_state)
@@ -285,7 +286,8 @@ PlacementAnnealer::PlacementAnnealer(const t_placer_opts& placer_opts,
     move_type_stats_.rejected_moves.resize({device_ctx.logical_block_types.size(), (int)e_move_type::NUMBER_OF_AUTO_MOVES}, 0);
 
     // Update the starting temperature for placement annealing to a more appropriate value
-    annealing_state_.t = estimate_starting_temperature_();
+    VTR_ASSERT_SAFE_MSG(auto_init_t_scale >= 0, "Initial temperature scale cannot be negative.");
+    annealing_state_.t = estimate_starting_temperature_() * auto_init_t_scale;
 }
 
 float PlacementAnnealer::estimate_starting_temperature_() {
