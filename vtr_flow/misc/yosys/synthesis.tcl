90e180b23e2fa28fbe6753c79412c156c838ec0c
Logan Lavigne <llavign1@unb.ca>
2025-07-03 12:30:31 -0300

    Fully implemented yosys-slang as a plugin for yosys

--- vtr_flow/misc/yosys/synthesis.tcl
+++ vtr_flow/misc/yosys/synthesis.tcl
@@ -1,12 +1,14 @@
 yosys -import
-
+#puts "Files in here: [glob -nocomplain *]"
 plugin -i parmys
-plugin -i slang
-yosys -import
 
 read_verilog -nomem2reg +/parmys/vtr_primitives.v
 setattr -mod -set keep_hierarchy 1 single_port_ram
 setattr -mod -set keep_hierarchy 1 dual_port_ram
+setattr -mod -set keep 1 dual_port_ram
+
+plugin -i slang
+yosys -import
 
 # synlig path error handling
 if {[catch {set synlig $::env(synlig_exe_path)} err]} {
@@ -25,10 +27,6 @@ if {[catch {set synlig $::env(synlig_exe_path)} err]} {
 # output file: ZZZ
 
 parmys_arch -a QQQ
-if {$env(PARSER) == "slang" } {
-	puts "Using yosys-slang read_slang command"
-	read_slang XXX
-}
 
 #if {$env(PARSER) == "surelog" } {
 #	puts "Using Synlig read_uhdm command"
@@ -37,8 +35,35 @@ if {$env(PARSER) == "slang" } {
 #} elseif {$env(PARSER) == "system-verilog" } {
 #	puts "Using Synlig read_systemverilog "
 #	exec $synlig -p "read_systemverilog XXX"
-#	} 
-elseif {$env(PARSER) == "default" } {
+#	}
+
+if {$env(PARSER) == "slang" } {
+	# Create a file list containing the name(s) of file(s)
+	# to read together with read_slang
+	set sv_files {}
+	set v_files {}
+	set readfile [file join [pwd] "filelist.txt"]
+	set fh [open $readfile "w"]
+	foreach f {XXX} {
+		set ext [string tolower [file extension $f]]
+		switch -- $ext {
+			.sv {
+				lappend $sv_files $f
+				puts $fh $f
+			}
+			.v {
+				error "Use default parser to parse .v files."
+			}
+		}
+	}
+	close $fh
+	if {[llength $sv_files] > 0} {
+		#puts "Using Yosys read_slang command"
+		#read_slang -C $readfile
+	}
+	puts "Using Yosys read_slang command"
+	read_slang -C $readfile
+} elseif {$env(PARSER) == "default" } {
 	puts "Using Yosys read_verilog command"
 	read_verilog -sv -nolatches XXX
 } else {
@@ -81,7 +106,8 @@ opt -full
 if {$env(PARSER) == "default" || $env(PARSER) == "slang"} {
     # For Verilog, use -nopass for a simpler, faster flow
     parmys -a QQQ -nopass -c CCC YYY
-} #elseif {$env(PARSER) == "slang"} {
+} 
+#elseif {$env(PARSER) == "slang"} {
     # For Slang, run additional passes to handle complexity
 #    parmys -a QQQ -c CCC YYY
 #}
