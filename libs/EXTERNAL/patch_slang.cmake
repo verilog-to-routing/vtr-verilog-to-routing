341a972f4fb697b9e8648e764ca8fed6936c76aa
Logan Lavigne <llavign1@unb.ca>
2025-07-11 15:02:56 -0300

    Added header explanation to patch_slang.cmake

--- libs/EXTERNAL/patch_slang.cmake
+++ libs/EXTERNAL/patch_slang.cmake
@@ -1,3 +1,12 @@
+# Patch step for yosys-slang
+#
+# The variable 'IN' points to vtr_root/libs/EXTERNAL/yosys-slang/src/slang_frontend.cc
+# This file contains the UndrivenPass that we want to disable
+#
+# This patch step finds the line where the UndrivenPass is called and comments out that line
+#
+# The UndrivenPass needs to be disabled due to unsupported synchronous rules in vtr_primitives.v
+
 if(NOT DEFINED IN)
 	message(FATAL_ERROR "patch_slang.cmake: IN (SLANG_FE) variable not set.")
 endif()
