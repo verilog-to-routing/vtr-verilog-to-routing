b892b6e022915c05fd8ac32b1d314cde14c08ab3
Soheil Shahrouz <soheilqs@gmail.com>
2025-09-25 15:58:32 -0400

    add doxygen comments for load_rr_switch_inf() and alloc_rr_switch_inf()

--- vpr/src/route/rr_graph_generation/rr_graph_switch_utils.cpp
+++ vpr/src/route/rr_graph_generation/rr_graph_switch_utils.cpp
@@ -8,6 +8,16 @@
 #include "vpr_error.h"
 #include "vpr_types.h"
 
+//
+// Static Function Declarations
+//
+
+/**
+ * @brief Populate rr_switch entries for all fanin variants.
+ *
+ * Iterates over each architecture switch and its fanins, creates the
+ * corresponding rr_switch entries, and fills the fanin --> rr_switch mapping.
+ */
 static void load_rr_switch_inf(RRGraphBuilder& rr_graph_builder,
                                std::vector<std::map<int, int>>& switch_fanin_remap,
                                const std::map<int, t_arch_switch_inf>& arch_sw_inf,
@@ -15,25 +25,29 @@ static void load_rr_switch_inf(RRGraphBuilder& rr_graph_builder,
                                const float R_minW_pmos,
                                const t_arch_switch_fanin& arch_switch_fanins);
 
+/**
+ * @brief Allocate rr_switch storage and determine fanin variants.
+ *
+ * Builds the list of fanins observed for each architecture switch and resizes
+ * the rr_switch array accordingly.
+ */
 static void alloc_rr_switch_inf(RRGraphBuilder& rr_graph_builder,
                                 t_arch_switch_fanin& arch_switch_fanins,
                                 const std::map<int, t_arch_switch_inf>& arch_sw_map);
 
-/* load the global device_ctx.rr_switch_inf variable. also keep track of, for each arch switch, what
- * index of the rr_switch_inf array each version of its fanin has been mapped to (through switch_fanin map) */
+//
+// Static Function Definitions
+//
+
 static void load_rr_switch_inf(RRGraphBuilder& rr_graph_builder,
                                std::vector<std::map<int, int>>& switch_fanin_remap,
                                const std::map<int, t_arch_switch_inf>& arch_sw_inf,
                                const float R_minW_nmos,
                                const float R_minW_pmos,
                                const t_arch_switch_fanin& arch_switch_fanins) {
-    if (!switch_fanin_remap.empty()) {
-        // at this stage, we rebuild the rr_graph (probably in binary search)
-        // so old device_ctx.switch_fanin_remap is obsolete
-        switch_fanin_remap.clear();
-    }
-
+    switch_fanin_remap.clear();
     switch_fanin_remap.resize(arch_sw_inf.size());
+
     for (const int arch_sw_id : arch_sw_inf | std::views::keys) {
         for (auto fanin_rrswitch : arch_switch_fanins[arch_sw_id]) {
             // the fanin value is in it->first, and we'll need to set what index this i_arch_switch/fanin
@@ -56,8 +70,6 @@ static void load_rr_switch_inf(RRGraphBuilder& rr_graph_builder,
     }
 }
 
-/* Allocates space for the global device_ctx.rr_switch_inf variable and returns the
- * number of rr switches that were allocated */
 static void alloc_rr_switch_inf(RRGraphBuilder& rr_graph_builder,
                                 t_arch_switch_fanin& arch_switch_fanins,
                                 const std::map<int, t_arch_switch_inf>& arch_sw_map) {
@@ -71,7 +83,10 @@ static void alloc_rr_switch_inf(RRGraphBuilder& rr_graph_builder,
     rr_graph_builder.resize_switches(num_rr_switches);
 }
 
-// This function is same as create_rr_switch_from_arch_switch() in terms of functionality. It is tuned for clients functions in routing resource graph builder
+//
+// Non-static Function Definitions
+//
+
 void load_rr_switch_from_arch_switch(RRGraphBuilder& rr_graph_builder,
                                      const std::map<int, t_arch_switch_inf>& arch_sw_inf,
                                      int arch_switch_idx,
@@ -79,8 +94,7 @@ void load_rr_switch_from_arch_switch(RRGraphBuilder& rr_graph_builder,
                                      int fanin,
                                      const float R_minW_nmos,
                                      const float R_minW_pmos) {
-    // figure out, by looking at the arch switch's Tdel map, what the delay of the new
-    // rr switch should be
+    // figure out, by looking at the arch switch's Tdel map, what the delay of the new rr switch should be
     double rr_switch_Tdel = arch_sw_inf.at(arch_switch_idx).Tdel(fanin);
 
     // copy over the arch switch to rr_switch_inf[rr_switch_idx], but with the changed Tdel value
@@ -92,11 +106,11 @@ void load_rr_switch_from_arch_switch(RRGraphBuilder& rr_graph_builder,
     rr_graph_builder.rr_switch()[RRSwitchId(rr_switch_idx)].Tdel = rr_switch_Tdel;
     rr_graph_builder.rr_switch()[RRSwitchId(rr_switch_idx)].mux_trans_size = arch_sw_inf.at(arch_switch_idx).mux_trans_size;
     if (arch_sw_inf.at(arch_switch_idx).buf_size_type == BufferSize::AUTO) {
-        //Size based on resistance
+        // Size based on resistance
         rr_graph_builder.rr_switch()[RRSwitchId(rr_switch_idx)].buf_size = trans_per_buf(arch_sw_inf.at(arch_switch_idx).R, R_minW_nmos, R_minW_pmos);
     } else {
         VTR_ASSERT(arch_sw_inf.at(arch_switch_idx).buf_size_type == BufferSize::ABSOLUTE);
-        //Use the specified size
+        // Use the specified size
         rr_graph_builder.rr_switch()[RRSwitchId(rr_switch_idx)].buf_size = arch_sw_inf.at(arch_switch_idx).buf_size;
     }
     rr_graph_builder.rr_switch()[RRSwitchId(rr_switch_idx)].name = arch_sw_inf.at(arch_switch_idx).name;
@@ -104,17 +118,12 @@ void load_rr_switch_from_arch_switch(RRGraphBuilder& rr_graph_builder,
     rr_graph_builder.rr_switch()[RRSwitchId(rr_switch_idx)].power_buffer_size = arch_sw_inf.at(arch_switch_idx).power_buffer_size;
 }
 
-/* This function creates a routing switch for the usage of routing resource graph, based on a routing switch defined in architecture file.
- *
- * Since users can specify a routing switch whose buffer size is automatically tuned for routing architecture, the function here sets a definite buffer size, as required by placers and routers.
- */
 t_rr_switch_inf create_rr_switch_from_arch_switch(const t_arch_switch_inf& arch_sw_inf,
                                                   const float R_minW_nmos,
                                                   const float R_minW_pmos) {
     t_rr_switch_inf rr_switch_inf;
 
-    // figure out, by looking at the arch switch's Tdel map, what the delay of the new
-    // rr switch should be
+    // figure out, by looking at the arch switch's Tdel map, what the delay of the new rr switch should be
     double rr_switch_Tdel = arch_sw_inf.Tdel(0);
 
     // copy over the arch switch to rr_switch_inf[rr_switch_idx], but with the changed Tdel value
@@ -126,11 +135,11 @@ t_rr_switch_inf create_rr_switch_from_arch_switch(const t_arch_switch_inf& arch_
     rr_switch_inf.Tdel = rr_switch_Tdel;
     rr_switch_inf.mux_trans_size = arch_sw_inf.mux_trans_size;
     if (arch_sw_inf.buf_size_type == BufferSize::AUTO) {
-        //Size based on resistance
+        // Size based on resistance
         rr_switch_inf.buf_size = trans_per_buf(arch_sw_inf.R, R_minW_nmos, R_minW_pmos);
     } else {
         VTR_ASSERT(arch_sw_inf.buf_size_type == BufferSize::ABSOLUTE);
-        //Use the specified size
+        // Use the specified size
         rr_switch_inf.buf_size = arch_sw_inf.buf_size;
     }
     rr_switch_inf.name = arch_sw_inf.name;
@@ -190,10 +199,6 @@ void alloc_and_load_rr_switch_inf(RRGraphBuilder& rr_graph_builder,
                        arch_switch_fanins);
 
     // next, walk through rr nodes again and remap their switch indices to rr_switch_inf
-
-    /* switch indices of each rr_node original point into the global device_ctx.arch_switch_inf array.
-     * now we want to remap these indices to point into the global device_ctx.rr_switch_inf array
-     * which contains switch info at different fan-in values */
     rr_graph_builder.remap_rr_node_switch_indices(arch_switch_fanins);
 
     // now we need to set the wire_to_rr_ipin_switch variable which points the detailed routing architecture
@@ -207,11 +212,10 @@ void alloc_and_load_rr_switch_inf(RRGraphBuilder& rr_graph_builder,
         VPR_FATAL_ERROR(VPR_ERROR_ARCH,
                         "Not currently allowing an ipin cblock switch to have multiple fan-ins");
     } else {
-        //This likely indicates that no connection block has been constructed, indicating significant issues with
-        //the generated RR graph.
-        //
-        //Instead of throwing an error we issue a warning. This means that check_rr_graph() etc. will run to give more information
-        //and allow graphics to be brought up for users to debug their architectures.
+        // This likely indicates that no connection block has been constructed, indicating significant issues with
+        // the generated RR graph.
+        // Instead of throwing an error we issue a warning. This means that check_rr_graph() etc. will run to give more information
+        // and allow graphics to be brought up for users to debug their architectures.
         (*wire_to_rr_ipin_switch) = UNDEFINED;
         VTR_LOG_WARN("No switch found for the ipin cblock in RR graph. Check if there is an error in arch file, or if no connection blocks are being built in RR graph\n");
     }
