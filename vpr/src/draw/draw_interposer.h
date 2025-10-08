6fefc2480061cfe7c8e3665f721ac3abad0e1908
Soheil Shahrouz <soheilqs@gmail.com>
2025-10-08 17:25:45 -0400

    doxygen comment for draw_interposer_cuts()

--- vpr/src/draw/draw_interposer.h
+++ vpr/src/draw/draw_interposer.h
@@ -5,4 +5,8 @@ namespace ezgl {
     class renderer;
 }
 
+/// @brief Draws interposer cut lines.
+/// During placement stage, the cut lines are drawn in the middle of routing channels.
+/// During routing, the cut lines are drawn closer to the side of routing channel to make it clear
+/// which side of the cut owns the channel.
 void draw_interposer_cuts(ezgl::renderer* g);
