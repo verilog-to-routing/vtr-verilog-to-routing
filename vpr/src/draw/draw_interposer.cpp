0ee28c89ee728b4ab4cdc62ea5d87c49d54761ef
Soheil Shahrouz <soheilqs@gmail.com>
2025-10-10 16:50:12 -0400

    draw interposer cut lines exactly in the middle of channel/between the last wire and next tile during placement/routing

--- vpr/src/draw/draw_interposer.cpp
+++ vpr/src/draw/draw_interposer.cpp
@@ -31,11 +31,6 @@ void draw_interposer_cuts(ezgl::renderer* g) {
     g->set_line_dash(ezgl::line_dash::asymmetric_5_3);
     g->set_line_width(2);
 
-    // Offset factor used to slightly shift the interposer cut lines away from tile boundaries.
-    // During placement, we draw the line in the middle of a routing channel.
-    // During routing, we draw the line closer to the tile that does not own the channel.
-    const float offset_factor = draw_state->pic_on_screen == e_pic_type::PLACEMENT ? -0.5f : -0.5f / device_ctx.chan_width.max;
-
     const std::vector<std::vector<int>>& horizontal_cuts = grid.get_horizontal_interposer_cuts();
     const std::vector<std::vector<int>>& vertical_cuts = grid.get_vertical_interposer_cuts();
     std::vector<std::pair<ezgl::point2d, ezgl::point2d>> lines_to_draw;
@@ -46,12 +41,28 @@ void draw_interposer_cuts(ezgl::renderer* g) {
         }
 
         for (int cut_y : horizontal_cuts[layer]) {
-            float y = draw_coords->tile_y[cut_y + 1] + offset_factor * draw_coords->get_tile_height();
+            float y;
+            if (draw_state->pic_on_screen == e_pic_type::PLACEMENT) {
+                y = (draw_coords->tile_y[cut_y + 1] + draw_coords->tile_y[cut_y]) / 2.0f;
+            } else if (draw_state->pic_on_screen == e_pic_type::ROUTING) {
+                y = draw_coords->tile_y[cut_y + 1] - 0.5f;
+            } else {
+                VTR_ASSERT(false);
+            }
+
             lines_to_draw.push_back({{world.left(), y}, {world.right(), y}});
         }
 
         for (int cut_x : vertical_cuts[layer]) {
-            float x = draw_coords->tile_x[cut_x + 1] + offset_factor * draw_coords->get_tile_width();
+            float x;
+            if (draw_state->pic_on_screen == e_pic_type::PLACEMENT) {
+                x = (draw_coords->tile_x[cut_x + 1] + draw_coords->tile_x[cut_x]) / 2.0f;
+            } else if (draw_state->pic_on_screen == e_pic_type::ROUTING) {
+                x = draw_coords->tile_x[cut_x + 1] - 0.5f;
+            } else {
+                VTR_ASSERT(false);
+            }
+
             lines_to_draw.push_back({{x, world.bottom()}, {x, world.top()}});
         }
     }
