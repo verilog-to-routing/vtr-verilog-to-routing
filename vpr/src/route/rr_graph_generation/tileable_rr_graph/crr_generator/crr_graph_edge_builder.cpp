852890afa80764e81e3a20126ecfa6514ebc4dcf
Amin Mohaghegh <amohaghegh@quicklogic.com>
2025-10-23 10:19:11 -0700

    [vpr][route][crr] add custom_rr_graph_builder file

--- vpr/src/route/rr_graph_generation/tileable_rr_graph/crr_generator/crr_graph_edge_builder.cpp
+++
@@ -1,7 +0,0 @@
-#include "crr_graph_edge_builder.h"
-
-void build_crr_graph_edges(const RRGraphView& rr_graph,
-                           RRGraphBuilder& rr_graph_builder,
-                           const t_crr_opts& crr_opts) {
-    // TODO: Implement
-}
