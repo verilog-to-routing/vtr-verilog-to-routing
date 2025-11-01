d2af59250f97c6e61b319e6fb1ec5fb390580260
Soheil Shahrouz <soheilqs@gmail.com>
2025-11-01 15:21:33 -0400

    move add_and_connect_non_3d_sg_links() to rr_graph_sg

--- vpr/src/route/rr_graph_generation/rr_graph_sg.h
+++ vpr/src/route/rr_graph_generation/rr_graph_sg.h
@@ -39,3 +39,27 @@ void add_edges_opin_chanz(const RRGraphView& rr_graph,
                           int num_seg_types,
                           t_rr_edge_info_set& rr_edges_to_create,
                           const std::vector<t_bottleneck_link>& interdie_3d_links);
+
+/**
+ * @brief Adds and connects non-3D scatter–gather (SG) links to the RR graph.
+ *
+ * For each bottleneck link, this function creates a corresponding RR node
+ * representing the non-3D SG link, and records edges between the node and
+ * gather and scatter wires. The edges are stored in `non_3d_sg_rr_edges_to_create`
+ * for deferred creation.
+ *
+ * @param rr_graph_builder     Reference to the RR graph builder.
+ * @param sg_links             List of scatter–gather bottleneck links.
+ * @param sg_node_indices      RR node IDs and track numbers for SG links.
+ * @param chan_details_x       Channel details for CHANX segments.
+ * @param chan_details_y       Channel details for CHANY segments.
+ * @param num_seg_types_x      Number of segment types in the X direction.
+ * @param non_3d_sg_rr_edges_to_create  Set collecting RR edges to create later.
+ */
+void add_and_connect_non_3d_sg_links(RRGraphBuilder& rr_graph_builder,
+                                     const std::vector<t_bottleneck_link>& sg_links,
+                                     const std::vector<std::pair<RRNodeId, int>>& sg_node_indices,
+                                     const t_chan_details& chan_details_x,
+                                     const t_chan_details& chan_details_y,
+                                     size_t num_seg_types_x,
+                                     t_rr_edge_info_set& non_3d_sg_rr_edges_to_create);
