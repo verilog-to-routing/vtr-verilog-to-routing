3952894faa724deecbfe2acb3f24e83cc2c6e882
Amin Mohaghegh <amohaghegh@quicklogic.com>
2025-11-03 07:45:14 -0800

    [vpr][route][crr] change connection's switch_id to delay_ps

--- vpr/src/route/rr_graph_generation/tileable_rr_graph/crr_generator/crr_common.h
+++ vpr/src/route/rr_graph_generation/tileable_rr_graph/crr_generator/crr_common.h
@@ -161,26 +161,27 @@ struct Sizing {
  */
 class Connection {
   public:
-    Connection(RRNodeId sink_node, RRNodeId src_node, SwitchId switch_id) noexcept
+    Connection(RRNodeId sink_node, RRNodeId src_node, int delay_ps) noexcept
         : sink_node_(sink_node)
         , src_node_(src_node)
-        , switch_id_(switch_id) {}
+        , delay_ps_(delay_ps) {}
+
     RRNodeId sink_node() const { return sink_node_; }
     RRNodeId src_node() const { return src_node_; }
-    SwitchId switch_id() const { return switch_id_; }
+    int delay_ps() const { return delay_ps_; }
 
     bool operator<(const Connection& other) const {
-        return std::tie(sink_node_, src_node_, switch_id_) < std::tie(other.sink_node_, other.src_node_, other.switch_id_);
+        return std::tie(sink_node_, src_node_, delay_ps_) < std::tie(other.sink_node_, other.src_node_, other.delay_ps_);
     }
 
     bool operator==(const Connection& other) const {
-        return sink_node_ == other.sink_node_ && src_node_ == other.src_node_ && switch_id_ == other.switch_id_;
+        return sink_node_ == other.sink_node_ && src_node_ == other.src_node_ && delay_ps_ == other.delay_ps_;
     }
 
   private:
     RRNodeId sink_node_;
     RRNodeId src_node_;
-    SwitchId switch_id_;
+    int delay_ps_;
 };
 
 // Node hash type for lookups
