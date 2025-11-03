f85d1c6ab3fb09c151183379191ef635a8679832
Amin Mohaghegh <amohaghegh@quicklogic.com>
2025-11-03 08:46:27 -0800

    [vpr][route][crr] add find_crr_switch_id to get switch id from delay

--- vpr/src/route/rr_graph_generation/tileable_rr_graph/crr_generator/crr_edge_builder.cpp
+++ vpr/src/route/rr_graph_generation/tileable_rr_graph/crr_generator/crr_edge_builder.cpp
@@ -1,6 +1,23 @@
 #include "crr_edge_builder.h"
+#include "globals.h"
 
 #include "crr_connection_builder.h"
+
+static RRSwitchId find_crr_switch_id(const int delay_ps) {
+    const auto& arch_switches = g_vpr_ctx.device().arch_switch_inf;
+    std::string switch_name;
+    if (delay_ps == 0) {
+        switch_name = "sw_zero";
+    } else {
+        switch_name = "sw_" + std::to_string(delay_ps);
+    }
+    for (int sw_id = 0; sw_id < (int)arch_switches.size(); sw_id++) {
+        if (arch_switches[sw_id].name == switch_name) {
+            return RRSwitchId(sw_id);
+        }
+    }
+    return RRSwitchId::INVALID();
+}
 
 void build_crr_gsb_track_to_track_edges(RRGraphBuilder& rr_graph_builder,
                                         const RRGSB& rr_gsb,
@@ -10,7 +27,8 @@ void build_crr_gsb_track_to_track_edges(RRGraphBuilder& rr_graph_builder,
 
     std::vector<crrgenerator::Connection> gsb_connections = connection_builder.get_tile_connections(gsb_x, gsb_y);
     for (const auto& connection : gsb_connections) {
-        RRSwitchId rr_switch_id = RRSwitchId(connection.switch_id());
+        RRSwitchId rr_switch_id = find_crr_switch_id(connection.delay_ps());
+        VTR_ASSERT(rr_switch_id != RRSwitchId::INVALID());
         rr_graph_builder.create_edge_in_cache(connection.src_node(), connection.sink_node(), rr_switch_id, false);
     }
 }
