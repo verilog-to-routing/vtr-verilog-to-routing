5f1c9c324c5ae7ccd9b50243016cce4861fb6d8f
Amir Poolad <amirhossein.poolad@gmail.com>
2025-11-12 14:29:15 -0500

    Fix some code duplication issues

--- vpr/src/route/rr_graph_generation/interposer_cut.cpp
+++ vpr/src/route/rr_graph_generation/interposer_cut.cpp
@@ -4,6 +4,7 @@
 #include <ranges>
 
 #include "device_grid.h"
+#include "interposer_types.h"
 #include "rr_graph_builder.h"
 #include "rr_graph_fwd.h"
 #include "rr_graph_view.h"
@@ -143,31 +144,11 @@ std::vector<RREdgeId> mark_interposer_cut_edges_for_removal(const RRGraphView& r
 }
 
 /**
- * @brief Update a CHANY node's bounding box in RRGraph and SpatialLookup entries if it crosses cut_loc_y
- * 
- * @param node CHANY RR graph node that might cross the interposer cut line
- * @param cut_loc_y Y location of horizontal interposer cut line
- * @param sg_node_indices List of scatter-gather node IDs. We do not want to cut these nodes as they're allowed to cross an interposer cut line.
- * @note This function is very similar to cut_chan_x_node. If you're modifying this you probably also want to modify that function too.
- */
-static void cut_chan_y_node(RRNodeId node, int cut_loc_y, const RRGraphView& rr_graph, RRGraphBuilder& rr_graph_builder, RRSpatialLookup& spatial_lookup) {
-    VTR_ASSERT_SAFE(rr_graph.node_type(node) == e_rr_type::CHANY);
-
-    int x_high = rr_graph.node_xhigh(node);
-    int x_low = rr_graph.node_xlow(node);
-
-    int y_high = rr_graph.node_yhigh(node);
-    int y_low = rr_graph.node_ylow(node);
-
-    int layer = rr_graph.node_layer_low(node);
-
-    int ptc_num = rr_graph.node_ptc_num(node);
-
-    if (!should_cut(y_low, y_high, cut_loc_y)) {
-        return;
-    }
-
-    if (rr_graph.node_direction(node) == Direction::INC) {
+  * @brief Update a CHANY node's bounding box in RRGraph and SpatialLookup entries. This function assumes that the channel node actually crosses the cut location and might not function correctly otherwise.
+  * This is a low level function, you should use cut_channel_node that wraps this up in a nicer API.
+  */
+static void cut_chan_y_node(RRNodeId node, int x_low, int y_low, int x_high, int y_high, int layer, int ptc_num, int cut_loc_y, Direction node_direction, RRGraphBuilder& rr_graph_builder, RRSpatialLookup& spatial_lookup) {
+    if (node_direction == Direction::INC) {
         // Anything above cut_loc_y shouldn't exist
         rr_graph_builder.set_node_coordinates(node, x_low, y_low, x_high, cut_loc_y);
 
@@ -175,7 +156,7 @@ static void cut_chan_y_node(RRNodeId node, int cut_loc_y, const RRGraphView& rr_
         for (int y_loc = cut_loc_y + 1; y_loc <= y_high; y_loc++) {
             spatial_lookup.remove_node(node, layer, x_low, y_loc, e_rr_type::CHANY, ptc_num);
         }
-    } else if (rr_graph.node_direction(node) == Direction::DEC) {
+    } else if (node_direction == Direction::DEC) {
         // Anything below cut_loc_y (inclusive) shouldn't exist
         rr_graph_builder.set_node_coordinates(node, x_low, cut_loc_y + 1, x_high, y_high);
 
@@ -189,31 +170,11 @@ static void cut_chan_y_node(RRNodeId node, int cut_loc_y, const RRGraphView& rr_
 }
 
 /**
- * @brief Update a CHANX node's bounding box in RRGraph and SpatialLookup entries if it crosses cut_loc_x
- * 
- * @param node CHANX RR graph node that might cross the interposer cut line
- * @param cut_loc_y X location of vertical interposer cut line
- * @param sg_node_indices List of scatter-gather node IDs. We do not want to cut these nodes as they're allowed to cross an interposer cut line.
- * @note This function is very similar to cut_chan_y_node. If you're modifying this you probably also want to modify that function too.
- */
-static void cut_chan_x_node(RRNodeId node, int cut_loc_x, const RRGraphView& rr_graph, RRGraphBuilder& rr_graph_builder, RRSpatialLookup& spatial_lookup) {
-    VTR_ASSERT_SAFE(rr_graph.node_type(node) == e_rr_type::CHANX);
-
-    int x_high = rr_graph.node_xhigh(node);
-    int x_low = rr_graph.node_xlow(node);
-
-    int y_high = rr_graph.node_yhigh(node);
-    int y_low = rr_graph.node_ylow(node);
-
-    int layer = rr_graph.node_layer_low(node);
-
-    int ptc_num = rr_graph.node_ptc_num(node);
-
-    if (!should_cut(x_low, x_high, cut_loc_x)) {
-        return;
-    }
-
-    if (rr_graph.node_direction(node) == Direction::INC) {
+  * @brief Update a CHANX node's bounding box in RRGraph and SpatialLookup entries. This function assumes that the channel node actually crosses the cut location and might not function correctly otherwise.
+  * This is a low level function, you should use cut_channel_node that wraps this up in a nicer API.
+  */
+static void cut_chan_x_node(RRNodeId node, int x_low, int y_low, int x_high, int y_high, int layer, int ptc_num, int cut_loc_x, Direction node_direction, RRGraphBuilder& rr_graph_builder, RRSpatialLookup& spatial_lookup) {
+    if (node_direction == Direction::INC) {
         // Anything to the right of cut_loc_x shouldn't exist
         rr_graph_builder.set_node_coordinates(node, x_low, y_low, cut_loc_x, y_high);
 
@@ -221,7 +182,7 @@ static void cut_chan_x_node(RRNodeId node, int cut_loc_x, const RRGraphView& rr_
         for (int x_loc = cut_loc_x + 1; x_loc <= x_high; x_loc++) {
             spatial_lookup.remove_node(node, layer, x_loc, y_low, e_rr_type::CHANX, ptc_num);
         }
-    } else if (rr_graph.node_direction(node) == Direction::DEC) {
+    } else if (node_direction == Direction::DEC) {
         // Anything to the left of cut_loc_x (inclusive) shouldn't exist
         rr_graph_builder.set_node_coordinates(node, cut_loc_x + 1, y_low, x_high, y_high);
 
@@ -234,9 +195,54 @@ static void cut_chan_x_node(RRNodeId node, int cut_loc_x, const RRGraphView& rr_
     }
 }
 
+/**
+ * @brief Update a CHANX or CHANY node's bounding box in RRGraph and SpatialLookup entries if it crosses cut_loc
+ * 
+ * @param node Channel segment RR graph node that might cross the interposer cut line
+ * @param cut_loc location of vertical interposer cut line
+ * @param interposer_cut_type Type of the interposer cut line (Horizontal or vertical)
+ * @param sg_node_indices Sorted list of scatter-gather node IDs. We do not want to cut these nodes as they're allowed to cross an interposer cut line.
+ * @note This function is very similar to cut_chan_y_node. If you're modifying this you probably also want to modify that function too.
+ */
+static void cut_channel_node(RRNodeId node, int cut_loc, e_interposer_cut_type interposer_cut_type, const RRGraphView& rr_graph, RRGraphBuilder& rr_graph_builder, RRSpatialLookup& spatial_lookup, const std::vector<std::pair<RRNodeId, int>>& sg_node_indices) {
+    constexpr auto node_indice_compare = [](RRNodeId l, RRNodeId r) noexcept { return size_t(l) < size_t(r); };
+    bool is_sg_node = std::ranges::binary_search(std::views::keys(sg_node_indices), node, node_indice_compare);
+    if (is_sg_node) {
+        return;
+    }
+
+    int x_high = rr_graph.node_xhigh(node);
+    int x_low = rr_graph.node_xlow(node);
+
+    int y_high = rr_graph.node_yhigh(node);
+    int y_low = rr_graph.node_ylow(node);
+
+    int layer = rr_graph.node_layer_low(node);
+    int ptc_num = rr_graph.node_ptc_num(node);
+    Direction node_direction = rr_graph.node_direction(node);
+    e_rr_type node_type = rr_graph.node_type(node);
+
+    if (interposer_cut_type == e_interposer_cut_type::HORZ) {
+        VTR_ASSERT(node_type == e_rr_type::CHANY);
+        if (!should_cut(y_low, y_high, cut_loc)) {
+            return;
+        }
+    } else if (interposer_cut_type == e_interposer_cut_type::VERT) {
+        VTR_ASSERT(node_type == e_rr_type::CHANX);
+        if (!should_cut(x_low, x_high, cut_loc)) {
+            return;
+        }
+    }
+
+    if (interposer_cut_type == e_interposer_cut_type::HORZ) {
+        cut_chan_y_node(node, x_low, y_low, x_high, y_high, layer, ptc_num, cut_loc, node_direction, rr_graph_builder, spatial_lookup);
+    } else if (interposer_cut_type == e_interposer_cut_type::VERT) {
+        cut_chan_x_node(node, x_low, y_low, x_high, y_high, layer, ptc_num, cut_loc, node_direction, rr_graph_builder, spatial_lookup);
+    }
+}
+
 void update_interposer_crossing_nodes_in_spatial_lookup_and_rr_graph_storage(const RRGraphView& rr_graph, const DeviceGrid& grid, RRGraphBuilder& rr_graph_builder, const std::vector<std::pair<RRNodeId, int>>& sg_node_indices) {
 
-    constexpr auto node_indice_compare = [](RRNodeId l, RRNodeId r) noexcept { return size_t(l) < size_t(r); };
     VTR_ASSERT(std::is_sorted(sg_node_indices.begin(), sg_node_indices.end()));
 
     RRSpatialLookup& spatial_lookup = rr_graph_builder.node_lookup();
@@ -245,12 +251,7 @@ void update_interposer_crossing_nodes_in_spatial_lookup_and_rr_graph_storage(con
             for (size_t x_loc = 0; x_loc < grid.width(); x_loc++) {
                 std::vector<RRNodeId> channel_nodes = spatial_lookup.find_channel_nodes(layer, x_loc, cut_loc_y, e_rr_type::CHANY);
                 for (RRNodeId node : channel_nodes) {
-                    bool is_sg_node = std::ranges::binary_search(std::views::keys(sg_node_indices), node, node_indice_compare);
-                    if (is_sg_node) {
-                        continue;
-                    }
-
-                    cut_chan_y_node(node, cut_loc_y, rr_graph, rr_graph_builder, spatial_lookup);
+                    cut_channel_node(node, cut_loc_y, e_interposer_cut_type::HORZ, rr_graph, rr_graph_builder, spatial_lookup, sg_node_indices);
                 }
             }
         }
@@ -259,12 +260,7 @@ void update_interposer_crossing_nodes_in_spatial_lookup_and_rr_graph_storage(con
             for (size_t y_loc = 0; y_loc < grid.height(); y_loc++) {
                 std::vector<RRNodeId> channel_nodes = spatial_lookup.find_channel_nodes(layer, cut_loc_x, y_loc, e_rr_type::CHANX);
                 for (RRNodeId node : channel_nodes) {
-                    bool is_sg_node = std::ranges::binary_search(std::views::keys(sg_node_indices), node, node_indice_compare);
-                    if (is_sg_node) {
-                        continue;
-                    }
-
-                    cut_chan_x_node(node, cut_loc_x, rr_graph, rr_graph_builder, spatial_lookup);
+                    cut_channel_node(node, cut_loc_x, e_interposer_cut_type::VERT, rr_graph, rr_graph_builder, spatial_lookup, sg_node_indices);
                 }
             }
         }
