0a9a061b39b9df00b2e49808e229f79204596050
Amir Poolad <amirhossein.poolad@gmail.com>
2025-11-12 13:54:30 -0500

    Add doxygen to interposer cut functions

--- vpr/src/route/rr_graph_generation/interposer_cut.h
+++ vpr/src/route/rr_graph_generation/interposer_cut.h
@@ -5,6 +5,19 @@
 #include "rr_graph_view.h"
 #include "device_grid.h"
 
+/**
+ * @brief Goes through all edges in the RR Graph and returns a list of the edges that cross an interposer cut line.
+ * 
+ * @return std::vector<RREdgeId> List of all edges that cross an interposer cut line.
+ */
 std::vector<RREdgeId> mark_interposer_cut_edges_for_removal(const RRGraphView& rr_graph, const DeviceGrid& grid);
 
+/**
+ * @brief Shortens the channel nodes that cross an interposer cut line
+ * 
+ * @param rr_graph RRGraphView, used to read the RR Graph.
+ * @param grid Device grid, used to access interposer cut locations.
+ * @param rr_graph_builder RRGraphBuilder, to modify the RRGraph.
+ * @param sg_node_indices list of scatter-gather node IDs. We do not want to cut these nodes as they're allowed to cross an interposer cut line.
+ */
 void update_interposer_crossing_nodes_in_spatial_lookup_and_rr_graph_storage(const RRGraphView& rr_graph, const DeviceGrid& grid, RRGraphBuilder& rr_graph_builder, const std::vector<std::pair<RRNodeId, int>>& sg_node_indices);
