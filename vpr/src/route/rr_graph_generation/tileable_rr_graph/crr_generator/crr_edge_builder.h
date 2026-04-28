bac23bfea631d3fab67fe397334cdee1abaf6bcc
Amin Mohaghegh <amohaghegh@quicklogic.com>
2026-04-28 11:29:53 -0700

    [vpr][route] create crr switches from min to max and retrieve delay from there

--- vpr/src/route/rr_graph_generation/tileable_rr_graph/crr_generator/crr_edge_builder.h
+++ vpr/src/route/rr_graph_generation/tileable_rr_graph/crr_generator/crr_edge_builder.h
@@ -7,11 +7,29 @@
  * This file contains the main function to be called from RR Graph Generator to build CRR edges.
  */
 
+#include <unordered_map>
+
 #include "vtr_vector.h"
 #include "rr_gsb.h"
 #include "rr_graph_builder.h"
 
 #include "crr_connection_builder.h"
+
+/**
+ * @brief Pre-create one CRR architecture switch per integer delay (ps) in the
+ *        inclusive range [min_delay_ps, max_delay_ps] and return a map from
+ *        delay to RRSwitchId.
+ *
+ * Switches are inserted into the device's all_sw_inf and indexed only by
+ * their delay - template ids are intentionally not used.
+ *
+ * @param min_delay_ps Minimum delay observed across all switch template files
+ * @param max_delay_ps Maximum delay observed across all switch template files
+ * @param verbosity Logging verbosity
+ */
+std::unordered_map<int, RRSwitchId> pre_create_crr_switches(const int min_delay_ps,
+                                                            const int max_delay_ps,
+                                                            const int verbosity);
 
 /**
  * @brief Builds edges for a CRR GSB
@@ -20,6 +38,9 @@
  * @param rr_node_driver_switches The driver switches for each RR node
  * @param rr_gsb The GSB for which edges are to be built
  * @param connection_builder The connection builder to use to get the connections at each tile
+ * @param delay_to_switch_id Map from delay (ps) to a pre-created RRSwitchId. Any
+ *                           connection delay other than -1 must be present in
+ *                           this map; otherwise an assertion is raised.
  * @param verbosity The verbosity level of the log
  */
 void build_crr_gsb_edges(RRGraphBuilder& rr_graph_builder,
@@ -27,5 +48,5 @@ void build_crr_gsb_edges(RRGraphBuilder& rr_graph_builder,
                          const vtr::vector<RRNodeId, RRSwitchId>& rr_node_driver_switches,
                          const RRGSB& rr_gsb,
                          const crrgenerator::CRRConnectionBuilder& connection_builder,
-                         std::unordered_map<std::string, int>& template_id_cache,
+                         const std::unordered_map<int, RRSwitchId>& delay_to_switch_id,
                          const int verbosity);
