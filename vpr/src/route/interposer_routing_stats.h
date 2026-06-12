e27c70788784a8fac1e840184e11b400ab72efe9
Soheil Shahrouz <soheilqs@gmail.com>
2026-06-12 12:21:58 -0400

    make format

--- vpr/src/route/interposer_routing_stats.h
+++ vpr/src/route/interposer_routing_stats.h
@@ -6,13 +6,13 @@
 
 /// @brief Per-interposer-cut routing statistics collected from the current routing.
 struct t_interposer_cut_routing_stats {
-    int num_nets_crossing = 0;        ///< Number of nets that cross this cut
-    int num_used_wires = 0;           ///< Number of used routing wires that cross this cut
-    int num_nets_crossing_inc = 0;    ///< Nets crossing in the incremental direction (relative to the net source)
-    int num_nets_crossing_dec = 0;    ///< Nets crossing in the decremental direction (relative to the net source)
-    int num_used_wires_inc = 0;       ///< Used incremental wires crossing
-    int num_used_wires_dec = 0;       ///< Used decremental wires crossing
-    int num_used_wires_bidir = 0;     ///< Used bidirectional wires crossing
+    int num_nets_crossing = 0;     ///< Number of nets that cross this cut
+    int num_used_wires = 0;        ///< Number of used routing wires that cross this cut
+    int num_nets_crossing_inc = 0; ///< Nets crossing in the incremental direction (relative to the net source)
+    int num_nets_crossing_dec = 0; ///< Nets crossing in the decremental direction (relative to the net source)
+    int num_used_wires_inc = 0;    ///< Used incremental wires crossing
+    int num_used_wires_dec = 0;    ///< Used decremental wires crossing
+    int num_used_wires_bidir = 0;  ///< Used bidirectional wires crossing
 };
 
 /// @brief Prints routing statistics for interposer-based devices.
