968bdb49606037154d4374637f3e63f5a1963ee4
ayan20985 <ayanali20985@gmail.com>
2026-07-30 02:34:38 -0400

    add frankenstein parse metrics

--- vtr_flow/scripts/qor_compare.py
+++ vtr_flow/scripts/qor_compare.py
@@ -36,6 +36,7 @@ DEFAULT_METRICS = [
     # Run-time Metrics
     "odin_synth_time",
     "parmys_synth_time",
+    "frankenstein_synth_time",
     "abc_synth_time",
     "pack_time",
     "place_time",
