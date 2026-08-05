1370261d9949abd20ed912b6c86f99ca31edaa53
Soheil Shahrouz <soheilqs@gmail.com>
2026-08-05 02:47:47 -0400

    Use at() to avoid spurious -Wnull-dereference warning

--- vpr/src/pack/cluster_placement.h
+++ vpr/src/pack/cluster_placement.h
@@ -113,9 +113,7 @@ class t_intra_cluster_placement_stats {
      */
     inline t_cluster_placement_primitive* get_pb_graph_node_placement_primitive(const t_pb_graph_node* pb_graph_node) {
         VTR_ASSERT_SAFE(pb_graph_node != nullptr);
-        auto it = pb_graph_node_placement_primitive.find(pb_graph_node);
-        VTR_ASSERT(it != pb_graph_node_placement_primitive.end());
-        return it->second;
+        return pb_graph_node_placement_primitive.at(pb_graph_node);
     }
 
     /**
